4bbbff530aaa07f3618576fd19963052da83fe00
Lisa Vogt <lisavogt.sf@gmail.com>
2020-04-04 11:29:48 -0700

    Remove sign up option
    - #35 turn off ability to create your own account
    - remove devise 'registerable' functionality from user model
    
    Co-authored-by: h-m-m@users.noreply.github.com

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_paper_trail
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   belongs_to :casa_org
