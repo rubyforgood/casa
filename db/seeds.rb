f714e42076ba922fb313b206e66ac681c1a37aa1
Polly Sileo <pollysileo@gmail.com>
2020-04-05 14:21:59 -0400

    Remove admin for super admin of all CASAs from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-all_casa_admin_1 = AllCasaAdmin.create(
-  email: 'all_casa_admin1@example.com',
-  password: '123456',
-  password_confirmation: '123456'
-)
-
 pg_casa = CasaOrg.create(name: 'Prince George CASA')
 other_casa = CasaOrg.create(name: 'Other CASA org')
 
