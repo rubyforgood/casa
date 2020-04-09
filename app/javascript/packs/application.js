2973bfa549c46f15e7814142a45ca2189a1b10ea
duaimei <adetwiler@optoro.com>
2020-04-09 14:41:44 -0400

    removed a hello world function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,3 @@ require("packs/new_casa_contact")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-function helloWorld() {
-  console.log('Hello world')
-}
