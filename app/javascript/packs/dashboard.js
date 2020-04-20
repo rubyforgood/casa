fbce196e9eda1c20ce74524202873b3b151cb718
colinsoleim <soleimc@gmail.com>
2020-04-19 23:00:49 -0400

    Adds transition aged youth filter to dashboard
    - Adds new decorator function to display if a volunteer is assigned
    - Adds new filter using that column to hide/show volunteers accordingly

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -3,6 +3,7 @@ $('document').ready(() => {
     function( settings, data, dataIndex ) {
       var supervisor_array = [""];
       var status_array = [];
+      var assigned_to_transition_youth_array = [];
 
       $('.supervisor-options').find('input[type="checkbox"]').each(function() {
         if($(this).is(':checked')) {
@@ -16,10 +17,19 @@ $('document').ready(() => {
         }
       });
 
+      $('.transition-youth-options').find('input[type="checkbox"]').each(function() {
+        if($(this).is(':checked')) {
+          assigned_to_transition_youth_array.push($(this).data('value'));
+        }
+      });
+
       var supervisor = data[1];
       var status = data[2];
+      var assigned_to_transition_youth = data[3];
 
-      if(supervisor_array.includes(supervisor) && status_array.includes(status)) {
+      if(supervisor_array.includes(supervisor) &&
+        status_array.includes(status) &&
+        assigned_to_transition_youth_array.includes(assigned_to_transition_youth)) {
         return true;
       }
 
