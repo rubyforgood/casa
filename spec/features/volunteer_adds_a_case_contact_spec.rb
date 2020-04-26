a1a9c878bd2b10e961918459f5e6174a20c2269c
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-04-25 22:13:45 -0400

    Fixed testing to not check for other_type in the second condition

--- spec/features/volunteer_adds_a_case_contact_spec.rb
+++ spec/features/volunteer_adds_a_case_contact_spec.rb
@@ -21,27 +21,4 @@ RSpec.describe 'volunteer adds a case contact', type: :feature do
     expect(CaseContact.first.contact_type).to eq 'school'
     expect(CaseContact.first.duration_minutes).to eq 105
   end
-
-  it 'chooses other' do
-    volunteer = create(:user, :volunteer, :with_casa_cases)
-    volunteer_casa_case_one = volunteer.casa_cases.first
-
-    sign_in volunteer
-
-    visit new_case_contact_path
-
-    find(:css, "input.casa-case-id-check[value='#{volunteer_casa_case_one.id}']").set(true)
-    select 'School', from: 'case_contact[contact_type]'
-    select '3 hours', from: 'case_contact[duration_hours]'
-    select '15 minutes', from: 'case_contact[duration_minutes]'
-    fill_in 'case_contact_occurred_at', with: '04/04/2020'
-    select 'Other', from: 'case_contact[contact_type]'
-
-    click_on 'Submit'
-
-    expect(CaseContact.first.casa_case_id).to eq volunteer_casa_case_one.id
-    expect(CaseContact.first.contact_type).to eq 'other'
-    expect(CaseContact.first.other_type_text).to eq ''
-    expect(CaseContact.first.duration_minutes).to eq 195
-  end
 end
