ae83dbd8f22698877671c9f5721922a443d2b69f
Zee Spencer <50284+zspencer@users.noreply.github.com>
2020-04-29 10:47:08 -0700

    Remove leftover JS from deletion of `other` case_contact contact_type

--- app/javascript/packs/new_casa_contact.js
+++ app/javascript/packs/new_casa_contact.js
@@ -1,16 +1,5 @@
 import $ from 'jquery'
 $('document').ready(() => {
-  // inside the app/views/casa_contact/_form, we are trying to only show a field if 'other' is chosen.
-  $("#contact_type").change((e) => {
-    if (e.target.value === 'other') {
-      $(".other-contact-type").removeAttr('hidden')
-      $(".other-contact-type input").attr('required', 'required')
-    } else {
-      $(".other-contact-type").attr('hidden', true)
-      $(".other-contact-type input").removeAttr('required')
-    }
-  })
-
   // Ensure at least one casa_case checkbox is checked by setting required to false
   $("input:checkbox.casa-case-id-check").on('click', function() {
     var case_id_checkbox_group = $("input:checkbox.casa-case-id-check");
