0865a7ed262bfc5abd636a70e7cd8a40a2af2581
colinsoleim <soleimc@gmail.com>
2020-04-29 11:44:24 -0400

    Adds full contacts table to casa_case#show

--- app/javascript/packs/show_casa_case.js
+++ app/javascript/packs/show_casa_case.js
@@ -1,7 +1,3 @@
 $('document').ready(() => {
-  // Enable all data tables on dashboard but only filter on volunteers table
-  $('.show-volunteer-case-contacts').on('click', function(e) {
-    var table_target = $(e.target).data('target');
-    $(table_target).toggleClass('collapse');
-  })
+
 });
