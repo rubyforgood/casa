c731edffe8c1960246b5ac4f81d3934071edd18a
colinsoleim <soleimc@gmail.com>
2020-05-16 14:50:58 -0400

    Hide new case contact button if volunteer has no assigned cases

--- app/policies/dashboard_policy.rb
+++ app/policies/dashboard_policy.rb
@@ -16,7 +16,7 @@ class DashboardPolicy
   end
 
   def create_case_contacts?
-    user.role == "volunteer"
+    user.role == "volunteer" && user.casa_cases.present?
   end
 
   def see_cases_section?
