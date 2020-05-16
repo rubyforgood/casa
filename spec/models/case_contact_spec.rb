afa492e4d08d0d93307df75be9dc4f216ff73719
Collin Jilbert <cjilbert504@gmail.com>
2020-05-16 15:02:36 -0500

    Add test for not allowing occurred at in future because of auditing concerns

--- spec/models/case_contact_spec.rb
+++ spec/models/case_contact_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe CaseContact, type: :model do
   it { is_expected.to(belong_to(:creator).class_name("User")) }
   it { is_expected.to(belong_to(:casa_case)) }
   it { is_expected.to(validate_presence_of(:contact_types)) }
+  it "verifies occured at is not in the future" do
+    case_contact = create(:case_contact, occurred_at: Time.now + 1.week)
+    expect(case_contact).to_not be_valid
+    expect(case_contact.errors[:occurred_at]).to eq("occured_at cannot be in the future")
+  end
 end
+
