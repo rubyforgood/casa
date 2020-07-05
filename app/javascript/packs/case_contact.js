a134a8fdd8200d08a3a6e3e7bb99f02cf3625716
Linda <linda@semprehealth.com>
2020-07-05 15:25:50 -0700

    More advanced checking for #381

--- app/javascript/packs/case_contact.js
+++ app/javascript/packs/case_contact.js
@@ -1,8 +1,11 @@
-window.onload = function() {
-  var milesInput = document.getElementById("case_contact_miles_driven");
-  milesInput.onchange = function(){
-    if(milesInput.value > 0){
-      alert('Are you sure?');
+window.onload = function () {
+  const milesDriven = document.getElementById("case_contact_miles_driven");
+
+  milesDriven.onchange = function () {
+    const contactMedium = document.getElementById("case_contact_medium_type").value || "(contact medium not set)";
+    const contactMediumInPerson = `${contactMedium}` === 'in-person';
+    if (milesDriven.value > 0 && !contactMediumInPerson) {
+      alert(`Just checking: you drove ${milesDriven.value} miles for a ${contactMedium} contact?`);
     }
   };
 };
