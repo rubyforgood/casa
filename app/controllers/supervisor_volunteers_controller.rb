d1ce4f09bba0962e5ba71e558449ff2ffc17317b
Clifton McIntosh <cliff.mcintosh@gmail.com>
2020-07-12 15:17:35 -0500

    Limit volunteer unassignment to admins and supervisors

--- app/controllers/supervisor_volunteers_controller.rb
+++ app/controllers/supervisor_volunteers_controller.rb
@@ -1,5 +1,6 @@
 class SupervisorVolunteersController < ApplicationController
   before_action :authenticate_user!
+  before_action :must_be_admin_or_supervisor, only: :destroy
 
   def create
     supervisor_volunteer = supervisor_volunteer_parent.supervisor_volunteers.new(supervisor_volunteer_params)
