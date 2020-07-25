06baf4c0b91a07b573fa99f511a65b5b39c98e9b
guillermo saavedra <gs8rj@virginia.edu>
2020-07-25 19:31:45 -0400

    Sets the occurred at form field as the occurred time when editing a case contact

---
+++ spec/views/case_contacts/new.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "case_contacts/new" do
+  it  "displays current time in the occurred at form field" do 
+    case_contact = create(:case_contact)
+    assign :case_contact, case_contact
+    assign :casa_cases, [case_contact.casa_case]
+
+    user = build_stubbed(:user, :volunteer)
+    allow(view).to receive(:current_user).and_return(user)
+
+    render template: "case_contacts/new"
+    expect(rendered).to include(Time.zone.now.strftime('%Y-%m-%d'))
+  end
+end
