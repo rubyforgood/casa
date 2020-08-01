e884dae63386a78f9605a12ca21eab5b02d97ef4
Ben Klang <bklang@wirehack.net>
2020-08-01 14:25:18 -0400

    Activate volunteers from Casa Case edit screen
    Closes #443

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -38,7 +38,11 @@ class VolunteersController < ApplicationController
   def activate
     @volunteer = authorize User.find(params[:id])
     if @volunteer.update(role: "volunteer")
-      redirect_to edit_volunteer_path(@volunteer), notice: "Volunteer was activated."
+      if params[:redirect_to_path] == "casa_case" && casa_case = CasaCase.find(params[:casa_case_id])
+        redirect_to edit_casa_case_path(casa_case), notice: "Volunteer was activated."
+      else
+        redirect_to edit_volunteer_path(@volunteer), notice: "Volunteer was activated."
+      end
     else
       render :edit
     end
