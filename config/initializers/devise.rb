1863663194226d5a63703d668e8ade66aa48038e
Sean Marcia <smarcia@gmail.com>
2020-08-05 23:33:29 -0400

    Put in the sendgrid stuff.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "no-reply@casa-production.herokuapp.com"
+  config.mailer_sender = "no-reply@#{ENV["DEFAULT_URL_HOST"]}"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
