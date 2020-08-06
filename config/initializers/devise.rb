cc72cee8efb72d6561181d5660179663d82c5740
Linda <linda@semprehealth.com>
2020-08-05 18:22:17 -0700

    Update devise default mailer_sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "no-reply@casa-r4g-staging.herokuapp.com"
+  config.mailer_sender = "no-reply@casa-production.herokuapp.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
