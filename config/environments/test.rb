d1614a10c33020cffb49db532e3e612a9d8daea5
Oksana Garbar <lorelsblog@gmail.com>
2020-08-14 09:47:14 +0300

    Edit config for Bullet gem

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,4 +47,9 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.console = true
+    Bullet.bullet_logger = true
+  end
 end
