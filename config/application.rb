80d497678998b94273a7d13bb300126471664f2b
Adam Bachman <adam.bachman@gmail.com>
2020-08-22 18:27:44 -0400

    replace all sprockets related asset code with webpacker
    - move /stylesheets and /images from app/assets to app/javascript/src
    - replace asset path references to webpack compatible forms
    - import stylesheets from appropriate libraries
    - some light autoformatting

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module Casa
   class Application < Rails::Application
     config.load_defaults 6.0
-    config.assets.compile = true
+    config.assets.compile = false
     config.serve_static_assets = true
     config.skylight.environments << "staging"
     config.action_mailer.preview_path = "#{Rails.root}/lib/mailers/previews"
