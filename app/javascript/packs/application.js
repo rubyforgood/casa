1a494c43428513dd435ad0ba585b46830e4f86cb
Jennifer Konikowski <jmkoni@icloud.com>
2020-08-23 13:41:36 -0400

    584 fix linting issues with standard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,28 +2,29 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+/* global $ */
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-require("bootstrap-datepicker")
-require('datatables.net-dt')
-require('bootstrap-select')
-require("src/case_contact")
-require("src/dashboard")
-require("src/index_reports")
-require("src/new_casa_contact")
-require("src/sessions")
-
-import "bootstrap"
+import 'bootstrap'
 import 'bootstrap-select'
 
-window.setTimeout(function() {
-  $(".alert").not(".error").fadeTo(1000, 0).slideUp(1000, function() {
-    $(this).remove();
-  });
-}, 2500);
+require('@rails/ujs').start()
+require('@rails/activestorage').start()
+require('channels')
+require('jquery')
+require('bootstrap-datepicker')
+require('datatables.net-dt')
+require('bootstrap-select')
+require('src/case_contact')
+require('src/dashboard')
+require('src/index_reports')
+require('src/new_casa_contact')
+require('src/sessions')
+
+window.setTimeout(function () {
+  $('.alert').not('.error').fadeTo(1000, 0).slideUp(1000, function () {
+    $(this).remove()
+  })
+}, 2500)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/src/case_contact.js
+++ app/javascript/src/case_contact.js
@@ -1,46 +1,44 @@
+/* global alert */
 window.onload = function () {
-  const milesDriven = document.getElementById("case_contact_miles_driven");
-  const durationHours = document.getElementById("case-contact-duration-hours");
-  const durationHourDisplay = document.getElementById("casa-contact-duration-hours-display")
-  const durationMinutes = document.getElementById("case-contact-duration-minutes");
-  const durationMinuteDisplay = document.getElementById("casa-contact-duration-minutes-display")
-  const caseContactSubmit = document.getElementById("case-contact-submit")
-  const contactTypeForm = document.getElementById("contact-type-form")
+  const milesDriven = document.getElementById('case_contact_miles_driven')
+  const durationHours = document.getElementById('case-contact-duration-hours')
+  const durationHourDisplay = document.getElementById('casa-contact-duration-hours-display')
+  const durationMinutes = document.getElementById('case-contact-duration-minutes')
+  const durationMinuteDisplay = document.getElementById('casa-contact-duration-minutes-display')
+  const caseContactSubmit = document.getElementById('case-contact-submit')
+  const contactTypeForm = document.getElementById('contact-type-form')
 
   milesDriven.onchange = function () {
-    const contactMedium = document.getElementById("case_contact_medium_type").value || "(contact medium not set)";
-    const contactMediumInPerson = `${contactMedium}` === 'in-person';
+    const contactMedium = document.getElementById('case_contact_medium_type').value || '(contact medium not set)'
+    const contactMediumInPerson = `${contactMedium}` === 'in-person'
     if (milesDriven.value > 0 && !contactMediumInPerson) {
-      alert(`Just checking: you drove ${milesDriven.value} miles for a ${contactMedium} contact?`);
+      alert(`Just checking: you drove ${milesDriven.value} miles for a ${contactMedium} contact?`)
     }
-  };
-  durationHours.onchange = function () {
-    if (durationHourDisplay.value !== durationHours.value) {
-      durationHourDisplay.value = durationHours.value
-    }
+  }
+  durationHours.onchange = updateHours()
+  durationHourDisplay.onchange = updateHours()
+  durationHourDisplay.onkeyup = updateHours()
 
-  }
-  durationHourDisplay.onchange, durationHourDisplay.onkeyup = function ()  {
-    if (durationHourDisplay.value !== durationHours.value) {
-      durationHours.value = durationHourDisplay.value
-    }
-  }
-  durationMinutes.onchange = function () {
-    if (durationMinuteDisplay.value !== durationMinutes.value) {
-      durationMinuteDisplay.value = durationMinutes.value
-    }
+  durationMinutes.onchange = updateMinutes()
+  durationMinuteDisplay.onchange = updateMinutes()
+  durationMinuteDisplay.onkeyup = updateMinutes()
 
-  }
-  durationMinuteDisplay.onchange, durationMinuteDisplay.onkeyup = function ()  {
+  function updateMinutes () {
     if (durationMinuteDisplay.value !== durationMinutes.value) {
       durationMinutes.value = durationMinuteDisplay.value
     }
   }
 
-  function validateContactType() {
+  function updateHours () {
+    if (durationHourDisplay.value !== durationHours.value) {
+      durationHourDisplay.value = durationHours.value
+    }
+  }
+
+  function validateContactType () {
     const childElements = Array.from(contactTypeForm.children)
     const isAtLeastOneChecked = childElements.filter(x => {
-      return x.querySelector("input") && x.querySelector("input").checked
+      return x.querySelector('input') && x.querySelector('input').checked
     }).length
     if (!isAtLeastOneChecked) {
       childElements[2].querySelector('input').setAttribute('required', true)
@@ -49,16 +47,15 @@ window.onload = function () {
     }
   }
 
-  function validateDuration() {
+  function validateDuration () {
     const msg = 'Please enter a minimum duration of 15 minutes (even if you spent less time than this).'
-    const fifteen_minutes = 15
-    let total_minutes = durationMinutes.value + durationHours.value * 60
+    const fifteenMinutes = 15
+    const totalMinutes = durationMinutes.value + durationHours.value * 60
 
-    if (total_minutes < fifteen_minutes) {
+    if (totalMinutes < fifteenMinutes) {
       durationMinutes.setCustomValidity(msg)
-    }
-    else {
-      durationMinutes.setCustomValidity("")
+    } else {
+      durationMinutes.setCustomValidity('')
     }
   }
 
@@ -66,4 +63,4 @@ window.onload = function () {
     validateContactType()
     validateDuration()
   }
-};
+}
