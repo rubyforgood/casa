38c5bfc83cd6aacb964e2efb86fe2daa5d36e459
Adam Bachman <adam.bachman@gmail.com>
2020-08-24 20:59:12 -0400

    590 really actually get rid of Sprockets to fix deployment (#591)
    fixes #590

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,25 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie" # but not sprockets!
+require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 
 module Casa
   class Application < Rails::Application
     config.load_defaults 6.0
-    config.assets.compile = false
     config.serve_static_assets = true
     config.skylight.environments << "staging"
     config.action_mailer.preview_path = "#{Rails.root}/lib/mailers/previews"
