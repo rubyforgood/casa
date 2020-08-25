903291e4a7b0f3fcd0a63efe94611aea70dda859
Adam Bachman <adam.bachman@gmail.com>
2020-08-24 20:23:50 -0400

    really actually get rid of Sprockets
    fixes #590

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,7 +6,6 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.assets.compile = false
   config.active_storage.service = :local
   config.log_level = :debug
   config.log_tags = [:request_id]
