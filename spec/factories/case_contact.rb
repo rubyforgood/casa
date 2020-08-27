b901a5eea84aeac8572d59ac1c31e6828ed95bb6
duaimei <adetwiler@optoro.com>
2020-08-26 21:09:25 -0400

    fixed the case contact spec

--- spec/factories/case_contact.rb
+++ spec/factories/case_contact.rb
@@ -7,7 +7,6 @@ FactoryBot.define do
     duration_minutes { 60 }
     occurred_at { Time.zone.now }
     contact_made { false }
-    contact_medium { "text/email"}
     miles_driven { nil }
     medium_type { CaseContact::CONTACT_MEDIUMS.first }
     want_driving_reimbursement { false }
