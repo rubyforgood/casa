0d9541075ca91e7e1a2e7227803795fc2f180053
Keith Bennett <keithrbennett@gmail.com>
2020-08-29 00:18:30 -0400

    Remove inadvertent change to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,8 +21,7 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system) do
     if ENV["DOCKER"]
-      # driven_by :selenium_chrome_headless_in_container
-      driven_by :selenium_chrome_in_container
+      driven_by :selenium_chrome_headless_in_container
       Capybara.server_host = "0.0.0.0"
       Capybara.server_port = 4000
       Capybara.app_host = "http://web:4000"
