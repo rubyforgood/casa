373edc8c1ffa744c5dc013045145cf93488d1261
Dominique Cuevas <cuevasdominique@gmail.com>
2020-09-03 23:00:55 -0700

    validates if volunteer_id is unique if active in SupervisorVolunteer table

--- app/models/supervisor_volunteer.rb
+++ app/models/supervisor_volunteer.rb
@@ -1,18 +1,15 @@
-class VolunteerSingleSupervisorValidator < ActiveModel:: Validator 
-  def validate(record)
-    if SupervisorVolunteer.where(volunteer_id: record.volunteer_id, is_active: true).size > 1
-      record.errors[:base] << "A volunteer cannot have more than 1 supervisor"
-    end
-  end
-end
-
 # relationship between a supervisor and volunteer
 class SupervisorVolunteer < ApplicationRecord
   has_paper_trail
   belongs_to :volunteer, class_name: "User"
   belongs_to :supervisor, class_name: "User"
   validates :supervisor_id, uniqueness: {scope: :volunteer_id} # only 1 row allowed per supervisor-volunteer pair
-  validates_with VolunteerSingleSupervisorValidator
+  validates :volunteer_id, uniqueness: {scope: :is_active}, if: :is_active?
+
+  def is_active?
+    is_active == true
+  end
+
 end
 
 # == Schema Information
--- spec/models/supervisor_volunteer_spec.rb
+++ spec/models/supervisor_volunteer_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe SupervisorVolunteer do
 
   it "should only allow 1 supervisor per volunteer" do
     supervisor_1.volunteers << volunteer_1
-        
+    supervisor_1.save
     expect{ supervisor_2.volunteers << volunteer_1 }.to raise_error(StandardError)
   end
 end
