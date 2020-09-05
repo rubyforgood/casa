96554d492a7da796b4d24fa18552bc9ed3871024
duaimei <adetwiler@optoro.com>
2020-09-05 12:06:37 -0400

    Made some changes to staging seeds

--- db/seeds/staging.rb
+++ db/seeds/staging.rb
@@ -17,70 +17,34 @@ SUPERVISOR_COUNT = 5
 
 SEED_PASSWORD = "123456"
 
-AllCasaAdmin.create(
+AllCasaAdmin.first_or_create(
   email: "allcasaadmin@example.com",
   password: SEED_PASSWORD,
   password_confirmation: SEED_PASSWORD
 )
-# seed users for all types [volunteer supervisor casa_admin]
-# volunteer users
-Volunteer.create(
-  casa_org: pg_casa,
-  # display_name intentionally left blank
-  email: "volunteer1@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
-volunteer_user_2 = Volunteer.create(
-  casa_org: pg_casa,
-  display_name: Faker::Name.name,
-  email: "volunteer2@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
-volunteer_user_3 = Volunteer.create(
-  casa_org: pg_casa,
-  display_name: "Myra Shanjar",
-  email: "volunteer3@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
-# intentionally leaving volunteer_user_1 out so it will remain unassigned
-volunteer_users = [volunteer_user_2, volunteer_user_3]
+
+volunteer_users = []
 
 # generate volunteer users via Faker gem
-VOLUNTEER_USER_COUNT.times do
-  volunteer_name = Faker::Name.name
-  volunteer_email_name = volunteer_name.downcase.sub(" ", "")
-  volunteer_user = Volunteer.create(
+VOLUNTEER_USER_COUNT.times do |index|
+  volunteer_email = "volunteer#{index+1}@example.com"
+  volunteer_user = Volunteer.where(email: volunteer_email).first_or_create!(
     casa_org: pg_casa,
-    display_name: volunteer_name,
-    # Generates an RFC 2606 compliant fake email, which means it will never deliver successfully
-    email: Faker::Internet.safe_email(name: volunteer_email_name),
+    display_name: Faker::Name.unique.name,
     password: SEED_PASSWORD,
-    password_confirmation: SEED_PASSWORD
+    password_confirmation: SEED_PASSWORD,
+    active: index % 30 != 0 #creates an inactive user every 30 times this is run
   )
   volunteer_users.push(volunteer_user)
 end
 
-# supervisor users
-supervisor_user_1 = Supervisor.create(
-  casa_org_id: pg_casa.id,
-  display_name: "Gloria O'Malley",
-  email: "supervisor1@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
-
 # generate more supervisor users via Faker gem
-supervisor_users = [supervisor_user_1]
+supervisor_users = []
 SUPERVISOR_COUNT.times do |index|
-  supervisor_name = Faker::Name.unique.name
-  supervisor_email_name = supervisor_name.downcase.sub(" ", "")
-  new_supervisor_user = Supervisor.create(
+  supervisor_email ="supervisor#{index+1}@example.com"
+  new_supervisor_user = Supervisor.where(email: supervisor_email).first_or_create!(
     casa_org_id: pg_casa.id,
-    display_name: supervisor_name,
-    email: Faker::Internet.safe_email(name: supervisor_email_name),
+    display_name: Faker::Name.unique.name,
     password: SEED_PASSWORD,
     password_confirmation: SEED_PASSWORD
   )
@@ -88,45 +52,16 @@ SUPERVISOR_COUNT.times do |index|
 end
 
 # casa_admin users
-CasaAdmin.create(
-  casa_org_id: pg_casa.id,
-  display_name: "1;DROP TABLE users",
-  email: "casa_admin1@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
-CasaAdmin.create(
-  casa_org_id: pg_casa.id,
-  display_name: "Uche O'Donnel",
-  email: "casa_admin2@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
-CasaAdmin.create(
-  casa_org_id: pg_casa.id,
-  display_name: "Zenne Zown",
-  email: "casa_admin3@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
-
-# inactive users
-Volunteer.create(
-  active: false,
-  casa_org_id: pg_casa.id,
-  display_name: "undefined Kent II",
-  email: "inactive1@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
-Volunteer.create(
-  active: false,
-  casa_org_id: pg_casa.id,
-  display_name: "בְּרֵאשִׁית, בָּרָא אֱלֹהִים, אֵת הַשָּׁמַיִם, וְאֵת הָאָרֶץ",
-  email: "inactive2@example.com",
-  password: SEED_PASSWORD,
-  password_confirmation: SEED_PASSWORD
-)
+CASA_ADMIN_COUNT = 3
+CASA_ADMIN_COUNT.times do |index|
+  casa_admin_email = "casa_admin#{index+1}@example.com"
+  CasaAdmin.where(email: casa_admin_email).first_or_create!(
+    casa_org_id: pg_casa.id,
+    display_name: Faker::Name.unique.name,
+    password: SEED_PASSWORD,
+    password_confirmation: SEED_PASSWORD
+  )
+end
 
 def case_number_generator
   # CINA-YY-XXXX
@@ -136,16 +71,14 @@ def case_number_generator
   "CINA-#{yy}-#{sequence_num}"
 end
 
-def chance_of_transition_aged
-  rand(1..21) > 13
-end
 
 # generate more CasaCases, add data, assign case to volunteer
 casa_cases = []
+years = ((DateTime.now.year - 20)..DateTime.now.year).to_a
+yy = years.sample.to_s[2..3]
 CASA_CASE_COUNT.times do |index|
-  new_casa_case = CasaCase.create(
+  new_casa_case = CasaCase.where(case_number: "CINA-#{yy}-#{1001+index}").first_or_create!(
     casa_org_id: pg_casa.id,
-    case_number: case_number_generator,
     transition_aged_youth: chance_of_transition_aged
   )
   volunteer_assigned = volunteer_users[index % volunteer_users.length]
@@ -159,11 +92,7 @@ end
 # associate volunteers with supervisors
 volunteer_users.each_with_index do |volunteer_user, index|
   supervisor_assigned = supervisor_users[index % supervisor_users.length]
-  SupervisorVolunteer.create(
-    [
-      {supervisor_id: supervisor_assigned.id, volunteer_id: volunteer_user.id}
-    ]
-  )
+  SupervisorVolunteer.create(supervisor: supervisor_assigned, volunteer: volunteer_user)
 end
 
 def even_odds
