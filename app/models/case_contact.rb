680a4314639f551f90be2e4aafdd8758828fffd7
Brian Borge <brianborge@gmail.com>
2020-09-06 11:13:06 -0400

    Check validation on update only - This is because the validaiton was running on create and update but we only want to check when case contacts are updated.

--- app/models/case_contact.rb
+++ app/models/case_contact.rb
@@ -10,7 +10,7 @@ class CaseContact < ApplicationRecord
   validates :occurred_at, presence: true
   validate :occurred_at_not_in_future
   validate :reimbursement_only_when_miles_driven
-  validate :check_if_allow_edit
+  validate :check_if_allow_edit, on: :update
 
   belongs_to :creator, class_name: "User"
   belongs_to :casa_case
