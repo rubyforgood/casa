37e10ee4e3720d6f9bdd3c4cff9650cc2e78837e
Brian Borge <brianborge@gmail.com>
2020-09-06 11:41:59 -0400

    fixup: spelling error caused failing test

--- spec/views/case_contacts/case_contact.html.erb_spec.rb
+++ spec/views/case_contacts/case_contact.html.erb_spec.rb
@@ -14,7 +14,7 @@ describe "case_contacts/case_contact" do
   end
 
   it "does not show edit button when occured_at is after the last day of the month in the quarter that the case contact was created" do
-    case_contact = create(:case_contact, occured_at: Time.zone.now - 1.year)
+    case_contact = create(:case_contact, occurred_at: Time.zone.now - 1.year)
     assign :case_contact, case_contact
     assign :casa_cases, [case_contact.casa_case]
 
