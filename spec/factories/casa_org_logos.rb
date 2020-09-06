f3b07e6602e5f5f06f9b352db43af7892606d8a7
Gina Sekhar <ginasekhar@yahoo.com>
2020-09-06 16:05:29 -0400

    add system spec to test logo and banner display

--- spec/factories/casa_org_logos.rb
+++ spec/factories/casa_org_logos.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :casa_org_logo do
     casa_org
-    url { "www.example.com" }
+    url { "media/src/images/favicon-16x16.png" }
     alt_text { "alt text" }
     size { "10x10" }
   end
