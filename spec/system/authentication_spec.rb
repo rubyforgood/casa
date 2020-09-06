cc131d032afea398b728bdfe154ee843c2c26c23
Adam Bachman <adam.bachman@gmail.com>
2020-09-06 09:18:23 -0400

    update all casa admin sessions/new to match main login, add auth page system tests for both user types
    fixes #683

--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -7,6 +7,42 @@ RSpec.describe "Authentication", type: :system do
       expect(page).to have_text "Log in"
       expect(page).to_not have_text "sign in before continuing"
     end
+
+    %w[volunteer supervisor casa_admin].each do |user_type|
+      it "allows #{user_type} to sign in" do
+        user = create(user_type.to_sym)
+
+        visit "/"
+        expect(page).to have_text "Log in"
+        expect(page).to_not have_text "sign in before continuing"
+
+        fill_in "Email", with: user.email
+        fill_in "Password", with: "123456"
+        within ".actions" do
+          click_on "Log in"
+        end
+
+        within ".navbar-nav" do
+          expect(page).to have_text user.email
+        end
+      end
+    end
+
+    it "does not allow AllCasaAdmin to sign in" do
+      user = create(:all_casa_admin)
+
+      visit "/"
+      expect(page).to have_text "Log in"
+      expect(page).to_not have_text "sign in before continuing"
+
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "123456"
+      within ".actions" do
+        click_on "Log in"
+      end
+
+      expect(page).to have_text "Invalid Email or password"
+    end
   end
 
   context "when authenticated user" do
