91caea3db884397ecffde191b6f9ef71a7930048
Nicole Calasich <nicolecalasich@gmail.com>
2020-09-12 09:01:41 -0700

    systems spec for all_casa_admin edit

---
+++ spec/system/all_casa_admins/edit_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "all_casa_admin_edit_spec", type: :system do
+let(:admin) {create(:all_casa_admin)}
+before do
+    sign_in admin
+end
+describe"with valid parameters" do
+    it "updates email" do
+    end
+end
+
+end
