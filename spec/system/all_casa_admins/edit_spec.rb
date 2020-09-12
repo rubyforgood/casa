b52e4cda4c4dc1a218cdaed1e590bbf786f78e8e
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-09-12 10:17:55 -0700

    Flesh out all casa admin system spec for edit

--- spec/system/all_casa_admins/edit_spec.rb
+++ spec/system/all_casa_admins/edit_spec.rb
@@ -1,14 +1,29 @@
 require "rails_helper"
 
 RSpec.describe "all_casa_admin_edit_spec", type: :system do
-  let(:admin) {create(:all_casa_admin)}
+  let(:admin) { create(:all_casa_admin) }
 
   before do
     sign_in admin
+    visit edit_all_casa_admins_path
   end
 
-  describe"with valid parameters" do
+  describe "with valid parameters" do
     it "updates email" do
+      fill_in "all_casa_admin_email", with: "newemail@example.com"
+      click_on "Update Profile"
+      expect(page).to have_text "successfully updated"
+    end
+  end
+
+  describe "with invalid parameters" do
+    let!(:other_admin) { create(:all_casa_admin) }
+
+    it "updates email" do
+      visit edit_all_casa_admins_path
+      fill_in "all_casa_admin_email", with: other_admin.email
+      click_on "Update Profile"
+      expect(page).to have_text "already been taken"
     end
   end
 end
