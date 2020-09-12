8005393a5dd010c49ca5d79d31ca754bfb490dcc
Dominique Cuevas <cuevasdominique@gmail.com>
2020-09-12 12:49:47 -0700

    Implemented Organization edit for admin

--- spec/requests/casa_orgs_request_spec.rb
+++ spec/requests/casa_orgs_request_spec.rb
@@ -1,19 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "CasaOrgs", type: :request do
-  let(:organization) { create(:casa_org) }
-  let(:valid_attributes) { {case_number: "1234", transition_aged_youth: true, casa_org_id: organization.id} }
-  let(:invalid_attributes) { {case_number: nil} }
-  let(:casa_case) { create(:casa_case, casa_org: organization) }
+  let(:casa_org) { create(:casa_org) }
+  let(:valid_attributes) { {name: "name", display_name: "display_name", address: "address"} }
+  let(:invalid_attributes) { {name: nil} }
+  let(:casa_case) { create(:casa_case, casa_org: casa_org) }
 
   describe "as an admin" do
-    before { sign_in create(:casa_admin, casa_org: organization) }
+    before { sign_in create(:casa_admin, casa_org: casa_org) }
     describe "GET /edit" do
       it "render a successful response" do
-        get edit_casa_org_url(organization)
+        get edit_casa_org_url(casa_org)
         expect(response).to be_successful
       end
     end
+    describe "PATCH /update" do
+      context "with valid parameters" do
 
+        it "updates the requested casa_org" do
+          patch casa_org_url(casa_org), params: {casa_org: valid_attributes}
+          casa_org.reload
+          expect(casa_org.name).to eq "name"
+          expect(casa_org.display_name).to eq "display_name"
+          expect(casa_org.address).to eq "address"
+        end
+
+        it "redirects to the casa_org" do
+          patch casa_org_url(casa_org), params: {casa_org: valid_attributes}
+          casa_org.reload
+          expect(response).to redirect_to(edit_casa_org_path)
+        end
+      end
+
+      context "with invalid parameters" do
+        it "renders a successful response displaying the edit template" do
+          patch casa_org_url(casa_org), params: {casa_org: invalid_attributes}
+          expect(response).to be_successful
+          expect(response.body).to match(/error_explanation/)
+        end
+      end
+    end
   end
 end
