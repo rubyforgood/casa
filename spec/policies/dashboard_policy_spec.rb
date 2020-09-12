429faa5b5f1841d97a8a84572b9c69d7180d39c8
Aaron Hill <armahillo@gmail.com>
2020-09-12 14:35:03 -0400

    Admin section now lists other admins for the organization

--- spec/policies/dashboard_policy_spec.rb
+++ spec/policies/dashboard_policy_spec.rb
@@ -65,4 +65,18 @@ RSpec.describe DashboardPolicy do
       expect(subject).not_to permit(create(:volunteer))
     end
   end
+
+  permissions :see_admins_section? do
+    it "allows casa_admins" do
+      expect(subject).to permit(create(:casa_admin))
+    end
+
+    it "does not allow supervisors" do
+      expect(subject).not_to permit(create(:supervisor))
+    end
+
+    it "does not allow volunteers" do
+      expect(subject).not_to permit(create(:volunteer))
+    end
+  end
 end
--- spec/system/admin_views_dashboard_spec.rb
+++ spec/system/admin_views_dashboard_spec.rb
@@ -200,14 +200,18 @@ RSpec.describe "admin views dashboard", type: :system do
     admin2 = create(:casa_admin, email: "Jon@org.com", casa_org: organization)
     admin3 = create(:casa_admin, email: "Bon@org.com", casa_org: organization)
     different_org_admin = create(:casa_admin, email: "Jovi@something.else", casa_org: create(:casa_org))
+    supervisor = create(:supervisor, email: "super@visor.com", casa_org: organization)
+    volunteer = create(:volunteer, email: "volun@tear.com", casa_org: organization)
 
     sign_in admin
     visit root_path
 
     within "#admins" do
-      expect(page).to have_content(admin2.name)
-      expect(page).to have_content(admin3.name)
-      expect(page).to have_no_content(different_org_admin.name)
+      expect(page).to have_content(admin2.email)
+      expect(page).to have_content(admin3.email)
+      expect(page).to have_no_content(different_org_admin.email)
+      expect(page).to have_no_content(supervisor.email)
+      expect(page).to have_no_content(volunteer.email)
     end
   end
 end
