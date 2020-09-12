9271bacdeb96779ccb2b999bfedad54684ca7697
Adam Bachman <adam.bachman@gmail.com>
2020-09-12 17:00:00 -0400

    AllCasaAdmin can create a new CASA organization

--- spec/system/all_casa_admins/all_casa_admins_casa_orgs_spec.rb
+++ spec/system/all_casa_admins/all_casa_admins_casa_orgs_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "all casa admins with casa orgs", type: :system do
+describe "all casa admins with casa orgs", type: :system do
   let(:all_casa_admin) { create(:all_casa_admin) }
   let!(:casa_org) { create(:casa_org) }
 
@@ -25,5 +25,26 @@ RSpec.describe "all casa admins with casa orgs", type: :system do
     expect(page).to_not have_text sup.email
     expect(page).to have_text ca1.email
     expect(page).to have_text ca2.email
+  end
+
+  it "lets admin create a casa org" do
+    visit "/"
+    expect(page).to have_text "All CASA Admin"
+
+    click_on "New CASA Organization"
+    expect(page).to have_text "Create a new CASA Organization"
+
+    fill_in "Name", with: "A new org"
+    fill_in "Display name", with: "A new org"
+    fill_in "Address", with: "123 Whole St"
+
+    click_on "Create CASA Organization"
+
+    expect(page).to have_text "CASA Organization was successfully created."
+
+    new_org = CasaOrg.last
+    expect(new_org.name).to eq "A new org"
+    expect(new_org.display_name).to eq "A new org"
+    expect(new_org.address).to eq "123 Whole St"
   end
 end
