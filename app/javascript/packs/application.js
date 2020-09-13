d235b6f1509ba531a2b5172a9a82f86e46b0b7b9
Adam Bachman <adam.bachman@gmail.com>
2020-09-13 09:13:01 -0400

    [A

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import 'src/stylesheets/application.scss'
 
 import 'bootstrap'
 import 'bootstrap-select'
-import Select2 from "select2"
+import Select2 from 'select2'
 
 require('@rails/ujs').start()
 require('@rails/activestorage').start()
@@ -17,7 +17,7 @@ require('jquery')
 require('bootstrap-datepicker')
 require('datatables.net-dt')
 require('datatables.net-dt/css/jquery.dataTables.css')
-require("select2/dist/css/select2")
+require('select2/dist/css/select2')
 
 require('src/case_contact')
 require('src/select')
