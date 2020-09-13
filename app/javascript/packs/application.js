7c8ecfd280568b84979c7548425cf210656aad9d
Oksana Garbar <lorelsblog@gmail.com>
2020-09-13 13:22:47 +0300

    Add Select2 for form selects

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import 'src/stylesheets/application.scss'
 
 import 'bootstrap'
 import 'bootstrap-select'
+import Select2 from "select2"
 
 require('@rails/ujs').start()
 require('@rails/activestorage').start()
@@ -16,8 +17,10 @@ require('jquery')
 require('bootstrap-datepicker')
 require('datatables.net-dt')
 require('datatables.net-dt/css/jquery.dataTables.css')
+require("select2/dist/css/select2")
 
 require('src/case_contact')
+require('src/select')
 require('src/dashboard')
 require('src/index_reports')
 require('src/sessions')
