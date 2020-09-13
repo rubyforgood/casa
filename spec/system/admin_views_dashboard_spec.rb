8a5f27583001e9397e073f7d23be7b040dedb249
Roger Robertson Jr <rjrjr@pm.me>
2020-09-13 08:50:10 -0700

    wrapping up new user testing

--- spec/system/admin_views_dashboard_spec.rb
+++ spec/system/admin_views_dashboard_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "admin views dashboard", type: :system do
 
   context "when no logo_url" do
     it "can see volunteers and navigate to their cases" do
-      volunteer = create(:volunteer, email: "casa@example.com", casa_org: organization)
+      volunteer = create(:volunteer, display_name: "User 1" email: "casa@example.com", casa_org: organization)
       volunteer.casa_cases << create(:casa_case, casa_org: organization)
       volunteer.casa_cases << create(:casa_case, casa_org: organization)
       casa_case = volunteer.casa_cases[0]
@@ -18,7 +18,7 @@ RSpec.describe "admin views dashboard", type: :system do
 
       visit root_path
 
-      expect(page).to have_text("casa@example.com")
+      expect(page).to have_text("User 1")
       expect(page).to have_text(casa_case.case_number)
 
       within "#volunteers" do
