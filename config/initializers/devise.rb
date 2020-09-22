098f9391fcf8b9c70a26f1d84e31e7371144481f
Ryan Rodriguez <ryanrodriguez@protonmail.com>
2020-09-22 10:04:37 -0500

    Removing rememberable functionality

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -204,7 +204,7 @@ Devise.setup do |config|
   # config.remember_for = 2.weeks
 
   # Invalidates all the remember me tokens when the user signs out.
-  config.expire_all_remember_me_on_sign_out = true
+  # config.expire_all_remember_me_on_sign_out = true
 
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
