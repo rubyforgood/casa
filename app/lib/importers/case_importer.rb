334e17b1b8662e3edba11383351f2eeabba12b92
Felipe Casadei <felipe.casadei@gmail.com>
2020-09-23 15:23:05 -0300

    Import birth month and year of a CASA case

--- app/lib/importers/case_importer.rb
+++ app/lib/importers/case_importer.rb
@@ -5,7 +5,7 @@ class CaseImporter < FileImporter
 
   def import_cases
     import do |row|
-      casa_case = CasaCase.new(row.to_hash.slice(:case_number, :transition_aged_youth))
+      casa_case = CasaCase.new(row.to_hash.slice(:case_number, :transition_aged_youth, :birth_month_year_youth))
       casa_case.casa_org_id = org_id
       casa_case.save!
       casa_case.volunteers << email_addresses_to_users(Volunteer, String(row[:case_assignment]))
