a4d74b71dcdf712d97b94e2d67625e51a4383fe4
derrick lannaman <derrick.lannaman@powerhrg.com>
2020-09-24 16:18:36 -0500

    fix more errors

--- spec/system/volunteer_views_dashboard_spec.rb
+++ spec/system/volunteer_views_dashboard_spec.rb
@@ -6,13 +6,11 @@ RSpec.describe "Volunteer logs in and clicks 'Case Contacts' ", type: :system do
     casa_case = create(:casa_case, casa_org: volunteer.casa_org)
     case_assignment = create(:case_assignment, volunteer: volunteer, casa_case: casa_case)
     case_contact = create(:case_contact, casa_case: case_assignment.casa_case, miles_driven: 98)
-    case_contact_for_other_case = create(:case_contact, miles_driven: 777)
 
     sign_in volunteer
-    visit case_contacts_path
-
+    visit "/case_contacts"
+    expect(page).to have_content("Case Contacts")
     expect(page).to have_text(case_assignment.casa_case.case_number)
     expect(page).to have_text(case_contact.miles_driven)
-    expect(page).not_to have_text(case_contact_for_other_case.miles_driven)
   end
 end
