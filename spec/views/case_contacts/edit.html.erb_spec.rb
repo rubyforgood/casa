51ded9151018ab4617e8153a2e3810abefc439cc
Gabriel Pereira <gmfvpereira@gmail.com>
2020-09-24 10:12:00 -0300

    Fix some tests [2]

--- spec/views/case_contacts/edit.html.erb_spec.rb
+++ spec/views/case_contacts/edit.html.erb_spec.rb
@@ -13,6 +13,7 @@ describe "case_contacts/edit" do
     assign :casa_cases, [case_contact.casa_case]
     assign :selected_cases, [case_contact.casa_case]
     assign :contact_types, [contact_type]
+    assign :selected_contact_types, []
 
     render template: "case_contacts/edit"
     expect(rendered).to include(contact_type.name)
@@ -26,6 +27,7 @@ describe "case_contacts/edit" do
     assign :casa_cases, [case_contact.casa_case]
     assign :selected_cases, [case_contact.casa_case]
     assign :contact_types, [contact_type]
+    assign :selected_contact_types, []
 
     render template: "case_contacts/edit"
     expect(rendered).to include(case_contact.occurred_at.strftime("%Y-%m-%d"))
