89d9228d42f0c12cdf715f12f3159b4be327ad85
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-09-24 16:59:16 -0400

    Latest

--- app/policies/volunteer_policy.rb
+++ app/policies/volunteer_policy.rb
@@ -1,6 +1,10 @@
 class VolunteerPolicy < UserPolicy
   attr_reader :user, :record
 
+  def index?
+    user&.volunteer?
+  end
+
   def initialize(user, record)
     @user = user
     @record = record
