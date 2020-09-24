2b74fd402bf02e7c8b2139654708ddf977c086c8
Cole Erikson <cole.erikson@powerhrg.com>
2020-09-24 15:27:11 -0300

    Code cleanup, formatting

--- app/javascript/src/sidebar.js
+++ app/javascript/src/sidebar.js
@@ -3,18 +3,18 @@ $('document').ready(() => {
   $('#toggle-sidebar-js').on("click", function() {
     const isOpen = $( "#sidebar-js" ).hasClass( "sidebar-open" );
     if (isOpen) {
-      $('#sidebar-js').removeClass('sidebar-open')
+      $('#sidebar-js').removeClass('sidebar-open');
     } else {
-      $('#sidebar-js').addClass('sidebar-open')
+      $('#sidebar-js').addClass('sidebar-open');
     }
   });
 
   $('#sidebar-js').on("click", function() {
     const isOpen = $( "#sidebar-js" ).hasClass( "sidebar-open" );
     if (isOpen) {
-      $('#sidebar-js').removeClass('sidebar-open')
+      $('#sidebar-js').removeClass('sidebar-open');
     } else {
-      $('#sidebar-js').addClass('sidebar-open')
+      $('#sidebar-js').addClass('sidebar-open');
     }
   });
 })
