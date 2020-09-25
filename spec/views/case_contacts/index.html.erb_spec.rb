afb5ce0fd153371b3b70d06175417991c2be534c
Cole Erikson <cole.erikson@powerhrg.com>
2020-09-25 17:57:11 -0300

    merge cleanup

--- spec/views/case_contacts/index.html.erb_spec.rb
+++ spec/views/case_contacts/index.html.erb_spec.rb
@@ -1,25 +1,27 @@
 require "rails_helper"
 
-describe "case_contacts/index" do
-  before do
-    user = build_stubbed(:volunteer)
-    allow(view).to receive(:current_user).and_return(user)
+describe "case_contacts/case_contact" do
+  it "shows edit button when occured_at is before the last day of the month in the quarter that the case contact was created" do
     case_contact = create(:case_contact)
     assign :case_contact, case_contact
     assign :casa_cases, [case_contact.casa_case]
-    render template: "case_contacts/index"
+
+    user = build_stubbed(:casa_admin)
+    allow(view).to receive(:current_user).and_return(user)
+
+    render(partial: "case_contacts/case_contact", locals: {contact: case_contact})
+    expect(rendered).to have_link(nil, href: "/case_contacts/#{case_contact.id}/edit")
   end
 
-  it "Displays the Case Contacts title" do
-    expect(rendered).to have_text("Case Contacts")
-  end
+  it "does not show edit button when occured_at is after the last day of the month in the quarter that the case contact was created" do
+    case_contact = create(:case_contact, occurred_at: Time.zone.now - 1.year)
+    assign :case_contact, case_contact
+    assign :casa_cases, [case_contact.casa_case]
 
-  it "Has a New Case Contact button" do
-    expect(rendered).to have_link("Create New Case Contact", href: new_case_contact_path)
-  end
+    user = build_stubbed(:casa_admin)
+    allow(view).to receive(:current_user).and_return(user)
 
-  it "Displays case contact table titles" do
-    expect(rendered).to have_selector("th", text: "Contact Made")
-    expect(rendered).to have_selector("th", text: "Miles Driven")
+    render(partial: "case_contacts/case_contact", locals: {contact: case_contact})
+    expect(rendered).to have_no_link(nil, href: "/case_contacts/#{case_contact.id}/edit")
   end
 end
