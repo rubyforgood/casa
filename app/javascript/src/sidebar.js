2fe3905e6eecd76a2c52a30ee850c1210793b417
Cole Erikson <cole.erikson@powerhrg.com>
2020-09-25 11:43:57 -0300

    DRY JS, remove partial

--- app/javascript/src/sidebar.js
+++ app/javascript/src/sidebar.js
@@ -1,20 +1,13 @@
 /* global $ */
-$('document').ready(() => {
-  $('#toggle-sidebar-js').on("click", function() {
-    const isOpen = $( "#sidebar-js" ).hasClass( "sidebar-open" );
-    if (isOpen) {
-      $('#sidebar-js').removeClass('sidebar-open');
-    } else {
-      $('#sidebar-js').addClass('sidebar-open');
-    }
-  });
+function toggleSidebar() {
+  const isOpen = $( "#sidebar-js" ).hasClass( "sidebar-open" );
+  if (isOpen) {
+    $('#sidebar-js').removeClass('sidebar-open');
+  } else {
+    $('#sidebar-js').addClass('sidebar-open');
+  }
+}
 
-  $('#sidebar-js').on("click", function() {
-    const isOpen = $( "#sidebar-js" ).hasClass( "sidebar-open" );
-    if (isOpen) {
-      $('#sidebar-js').removeClass('sidebar-open');
-    } else {
-      $('#sidebar-js').addClass('sidebar-open');
-    }
-  });
+$('document').ready(() => {
+  $('#toggle-sidebar-js, #sidebar-js').on("click", toggleSidebar);
 })
