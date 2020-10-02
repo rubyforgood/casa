6188688d54b873dfe74b075ce7bb5a5098984d19
PuZZleDucK <puzzleduck@gmail.com>
2020-10-03 06:01:45 +1000

    add court date to forms and allow in controller

--- app/policies/casa_case_policy.rb
+++ app/policies/casa_case_policy.rb
@@ -40,6 +40,7 @@ class CasaCasePolicy
 
   def permitted_attributes
     common_attrs = [
+      :court_date,
       :court_report_submitted,
       casa_case_contact_types_attributes: [:contact_type_id],
     ]
