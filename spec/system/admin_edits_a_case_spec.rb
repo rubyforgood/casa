c9d717b77c9beca44568939d6416c183033cef49
PuZZleDucK <puzzleduck@gmail.com>
2020-10-04 19:09:46 +1100

    create 'admin edit' test case

--- spec/system/admin_edits_a_case_spec.rb
+++ spec/system/admin_edits_a_case_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "admin edits case", type: :system do
+  let(:organization) { create(:casa_org) }
+  let(:admin) { create(:casa_admin, casa_org: organization) }
+  let(:casa_case) { create(:casa_case, casa_org: organization) }
+  let(:contact_type_group) { create(:contact_type_group, casa_org: organization) }
+  let!(:school) { create(:contact_type, name: "School", contact_type_group: contact_type_group) }
+  let!(:therapist) { create(:contact_type, name: "Therapist", contact_type_group: contact_type_group) }
+
   # Add back when Travis CI correctly handles large screen size
   xit "clicks back button after editing case" do
     volunteer = create(:volunteer)
@@ -23,9 +30,21 @@ RSpec.describe "admin edits case", type: :system do
     expect(page).to have_text("Volunteer")
     expect(page).to have_text("Case")
     expect(page).to have_text("Supervisor")
-    expect(page).to have_text("Court Date")
-    expect(page).to have_text("Day")
-    expect(page).to have_text("Month")
-    expect(page).to have_text("Year")
   end
+
+  it 'edits case' do
+    sign_in admin
+    visit casa_case_path(casa_case.id)
+    click_on 'Edit Case Details'
+    has_no_checked_field? :court_report_submitted
+    check 'Court report submitted'
+    click_on 'Update CASA Case'
+
+    has_checked_field? :court_report_submitted
+    expect(page).to have_text('Court Date')
+    expect(page).to have_text('Day')
+    expect(page).to have_text('Month')
+    expect(page).to have_text('Year')
+  end
+
 end
