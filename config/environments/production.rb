fd30701d120fadc5f7e8900f668d86b31669a469
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2020-10-04 16:24:35 -0700

    Use DOMAIN env var instead of DEFAULT_URL_HOST since they are the same

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = {host: ENV["DEFAULT_URL_HOST"]} # for devise authentication
+  config.action_mailer.default_url_options = { host: ENV["DOMAIN"] }
   config.action_mailer.raise_delivery_errors = true
   # Do not send emails in staging or qa
   config.action_mailer.perform_deliveries = ENV["APP_ENVIRONMENT"] == "production"
