b547c88fd3c1fdc4e618df7d2ffca1b008f19146
Oksana Garbar <lorelsblog@gmail.com>
2020-10-05 12:10:42 +0300

    Fix voluneers dashboard spec after redesign

--- spec/system/volunteer_views_dashboard_spec.rb
+++ spec/system/volunteer_views_dashboard_spec.rb
@@ -1,17 +1,20 @@
 require "rails_helper"
 
 RSpec.describe "volunteer views dashboard", type: :system do
-  xit "sees all case contacts for their case" do
+  it "sees all their casa cases" do
     volunteer = create(:volunteer)
-    casa_case = create(:casa_case, casa_org: volunteer.casa_org)
-    case_assignment = create(:case_assignment, volunteer: volunteer, casa_case: casa_case)
-    case_contact = create(:case_contact, casa_case: case_assignment.casa_case, miles_driven: 98)
-    case_contact_for_other_case = create(:case_contact, miles_driven: 777)
+    casa_case_1 = create(:casa_case, casa_org: volunteer.casa_org)
+    casa_case_2 = create(:casa_case, casa_org: volunteer.casa_org)
+    casa_case_3 = create(:casa_case, casa_org: volunteer.casa_org)
+    create(:case_assignment, volunteer: volunteer, casa_case: casa_case_1)
+    create(:case_assignment, volunteer: volunteer, casa_case: casa_case_2)
+
 
     sign_in volunteer
-    visit root_path
-    expect(page).to have_text(case_assignment.casa_case.case_number)
-    expect(page).to have_text(case_contact.miles_driven)
-    expect(page).not_to have_text(case_contact_for_other_case.miles_driven)
+    visit root_path 
+    expect(page).to have_text("My Cases")
+    expect(page).to have_text(casa_case_1.case_number)
+    expect(page).to have_text(casa_case_2.case_number)
+    expect(page).not_to have_text(casa_case_3.case_number)
   end
 end
