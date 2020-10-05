d394eef9806957369708ff2cad7d6c4a4d7d6527
Oksana Garbar <lorelsblog@gmail.com>
2020-10-05 14:04:58 +0300

    Fixed Casa Case numbers to avoid accidental coincidences

--- spec/system/volunteer_views_dashboard_spec.rb
+++ spec/system/volunteer_views_dashboard_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe "volunteer views dashboard", type: :system do
   it "sees all their casa cases" do
     volunteer = create(:volunteer)
-    casa_case_1 = create(:casa_case, casa_org: volunteer.casa_org)
-    casa_case_2 = create(:casa_case, casa_org: volunteer.casa_org)
-    casa_case_3 = create(:casa_case, casa_org: volunteer.casa_org)
+    casa_case_1 = create(:casa_case, casa_org: volunteer.casa_org, case_number: "SLAVA-1")
+    casa_case_2 = create(:casa_case, casa_org: volunteer.casa_org, case_number: "SLAVA-2")
+    casa_case_3 = create(:casa_case, casa_org: volunteer.casa_org, case_number: "SLAVA-3")
     create(:case_assignment, volunteer: volunteer, casa_case: casa_case_1)
     create(:case_assignment, volunteer: volunteer, casa_case: casa_case_2)
 
