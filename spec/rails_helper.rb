5bce4e2d7e9c7b19a67a6afd640f87446a2e74d0
CW <compiledwrong+github@gmail.com>
2020-10-05 18:07:34 -0700

    Help for ppl debugging env

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../config/environment", __dir__) # Prevent database truncation if the environment is production
+ENV["RAILS_ENV"] ||= "TEST"
+require File.expand_path("../config/environment", __dir__) # Prevent database truncation in production. Local? Try RAILS_ENV=TEST
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
