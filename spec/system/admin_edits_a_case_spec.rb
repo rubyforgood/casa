4bd1e30a655ac401262d389064dc3846a63d36b8
Maycow Meira <maycowmeira@gmail.com>
2020-10-05 22:58:37 -0300

    Fixed test case, caused by using same record.

--- spec/system/admin_edits_a_case_spec.rb
+++ spec/system/admin_edits_a_case_spec.rb
@@ -3,25 +3,26 @@ require "rails_helper"
 RSpec.describe "admin edits case", type: :system do
   let(:organization) { create(:casa_org) }
   let(:admin) { create(:casa_admin, casa_org: organization) }
-  let(:casa_case) { create(:casa_case, casa_org: organization) }
+  let(:back_button_casa_case) { create(:casa_case, casa_org: organization) }
+  let(:edit_casa_case) { create(:casa_case, casa_org: organization) }
   let(:contact_type_group) { create(:contact_type_group, casa_org: organization) }
   let!(:school) { create(:contact_type, name: "School", contact_type_group: contact_type_group) }
   let!(:therapist) { create(:contact_type, name: "Therapist", contact_type_group: contact_type_group) }
 
   it "clicks back button after editing case" do
     sign_in admin
-    visit edit_casa_case_path(casa_case)
+    visit edit_casa_case_path(back_button_casa_case)
     check "Court report submitted"
     has_checked_field? :court_report_submitted
     click_on "Back"
-    visit edit_casa_case_path(casa_case)
+    visit edit_casa_case_path(back_button_casa_case)
 
     has_no_checked_field? :court_report_submitted
   end
 
   it "edits case" do
     sign_in admin
-    visit casa_case_path(casa_case.id)
+    visit casa_case_path(edit_casa_case.id)
     click_on "Edit Case Details"
     has_no_checked_field? :court_report_submitted
     check "Court report submitted"
