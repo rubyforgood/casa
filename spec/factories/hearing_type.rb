22394da5a35615b5af0af8614140d4d18f7c8ebc
Hayden Rouille <hayden@rouille.dev>
2020-10-07 06:45:32 -0400

    add specs for hearing types

---
+++ spec/factories/hearing_type.rb
@@ -0,0 +1,36 @@
+FactoryBot.define do
+  factory :user do
+    casa_org { create(:casa_org) }
+    sequence(:email) { |n| "email#{n}@example.com" }
+    sequence(:display_name) { |n| "User #{n}" }
+    password { "123456" }
+    password_confirmation { "123456" }
+    case_assignments { [] }
+
+    trait :inactive do
+      volunteer
+      active { false }
+      role { :inactive }
+    end
+
+    trait :with_casa_cases do
+      after(:create) do |user, _|
+        create_list(:case_assignment, 2, volunteer: user)
+      end
+    end
+
+    trait :with_case_contact do
+      after(:create) do |user, _|
+        create(:case_assignment, volunteer: user)
+        create(:case_contact, creator: user, casa_case: user.casa_cases.first, contact_made: true)
+      end
+    end
+
+    trait :with_case_contact_wants_driving_reimbursement do
+      after(:create) do |user, _|
+        create(:case_assignment, volunteer: user)
+        create(:case_contact, :wants_reimbursement, creator: user, casa_case: user.casa_cases.first, contact_made: true)
+      end
+    end
+  end
+end
---
+++ spec/models/hearing_type_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe HearingType, type: :model do
+  it { is_expected.to belong_to(:casa_org) }
+  it { is_expected.to validate_presence_of(:active) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+end
