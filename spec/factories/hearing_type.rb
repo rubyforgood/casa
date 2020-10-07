b02bc02139eb1d757b2822cf3b943053bfe9df40
Hayden Rouille <hayden@rouille.dev>
2020-10-07 06:49:29 -0400

    amend name of hearing type factory

--- spec/factories/hearing_type.rb
+++ spec/factories/hearing_type.rb
@@ -1,36 +1,7 @@
 FactoryBot.define do
-  factory :user do
+  factory :hearing_type do
     casa_org { create(:casa_org) }
-    sequence(:email) { |n| "email#{n}@example.com" }
-    sequence(:display_name) { |n| "User #{n}" }
-    password { "123456" }
-    password_confirmation { "123456" }
-    case_assignments { [] }
-
-    trait :inactive do
-      volunteer
-      active { false }
-      role { :inactive }
-    end
-
-    trait :with_casa_cases do
-      after(:create) do |user, _|
-        create_list(:case_assignment, 2, volunteer: user)
-      end
-    end
-
-    trait :with_case_contact do
-      after(:create) do |user, _|
-        create(:case_assignment, volunteer: user)
-        create(:case_contact, creator: user, casa_case: user.casa_cases.first, contact_made: true)
-      end
-    end
-
-    trait :with_case_contact_wants_driving_reimbursement do
-      after(:create) do |user, _|
-        create(:case_assignment, volunteer: user)
-        create(:case_contact, :wants_reimbursement, creator: user, casa_case: user.casa_cases.first, contact_made: true)
-      end
-    end
+    name { "Emergency Hearing" }
+    active { true }
   end
 end
