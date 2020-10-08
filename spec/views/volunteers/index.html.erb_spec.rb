c592643bc65099cf1a3a5b17ab9ee8cb04326230
Alexei <alexeim763@gmail.com>
2020-10-07 22:28:01 -0500

    Activate pundit_view in this view

--- spec/views/volunteers/index.html.erb_spec.rb
+++ spec/views/volunteers/index.html.erb_spec.rb
@@ -4,10 +4,17 @@ describe "volunteers" do
   subject { render template: "volunteers/index" }
 
   context "while signed in as other user diferent to admin" do
-    before do
-      sign_in_as_volunteer
-    end
+    let(:user) { build_stubbed :volunteer}
 
-    it { is_expected.not_to have_selector("a", text: "New Volunteer") }
+    before do
+      enable_pundit(view, user)
+      allow(view).to receive(:current_user).and_return(user)
+      assign :volunteers, []
+      sign_in user
+    end
+    
+    it do 
+      is_expected.not_to have_selector("a", text: "New Volunteer")
+    end
   end
 end
