428e1177b01fb711ceac2040e849168c37bb1a83
RachelWyatt <rwyatt@taphere.com>
2020-10-13 15:57:59 -0400

    update case_contact factory

--- spec/factories/case_contact.rb
+++ spec/factories/case_contact.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     association :creator, factory: :user
     association :casa_case
 
-    contact_types { ["therapist"] }
     duration_minutes { 60 }
     occurred_at { Time.zone.now }
     contact_made { false }
