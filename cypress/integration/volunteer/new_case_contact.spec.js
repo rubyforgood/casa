f44856dcd34bc4718786ddac82c45bf1e49246fd
duaimei <adetwiler@optoro.com>
2020-10-12 22:32:25 -0400

    a travis-ty

--- cypress/integration/volunteer/new_case_contact.spec.js
+++ cypress/integration/volunteer/new_case_contact.spec.js
@@ -1,6 +1,6 @@
 context("Logging into cypress as a volunteer", () => {
   before(() => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
     cy.loginAsVolunteer();
   });
   it("should go to the new case contact page", () => {
