b81ff5c64c48ccc069d002ba081814d46c8004f7
Erik Tran <erik.tran.tt@gmail.com>
2020-10-15 00:57:54 +1100

    added routes for #index, #show, #generate and CaseCourtReport and instance method #generate! to turn Word template into a report

--- app/controllers/case_court_reports_controller.rb
+++ app/controllers/case_court_reports_controller.rb
@@ -2,8 +2,83 @@
 
 class CaseCourtReportsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_casa_case, only: %i[show check_access]
 
+  # GET /case_court_reports
   def index
     @assigned_cases = CasaCase.actively_assigned_to(current_user)
+                              .select(:id, :case_number, :transition_aged_youth)
+  end
+
+  # GET /case_court_reports/:id
+  def show
+    report_path = path_to_report(params[:id])
+
+    unless File.exist?(report_path)
+      flash[:error] = 'File is not found.'
+      redirect_to case_court_reports_path and return
+    end
+
+    respond_to do |format|
+      format.docx { send_file report_path, type: :docx, disposition: 'attachment', status: :ok }
+    end
+  end
+
+  # POST /case_court_reports
+  def generate
+    casa_case   = CasaCase.find_by(case_params)
+
+    report      = generate_report(casa_case)
+    client_link = generate_case_court_reports_path.gsub('generate', report_file_name(casa_case.case_number))
+    status      = File.exist?(report.report_path) ? :ok : :not_found
+
+    respond_to do |format|
+      format.json do
+        render json: { link: client_link, status: status }
+      end
+    end
+  end
+
+  private
+
+  def case_params
+    params.require(:case_court_report).permit(:case_number)
+  end
+
+  def set_casa_case
+    @casa_case = CasaCase.find_by(case_number: params[:id])
+  end
+
+  def generate_report(casa_case)
+    return unless casa_case
+
+    court_report = CaseCourtReport.new(
+      volunteer_id: current_user.id,
+      case_id: casa_case.id,
+      path_to_template: path_to_template,
+      path_to_report: path_to_report(casa_case.case_number)
+    )
+    court_report.generate!
+    court_report
+  end
+
+  def directory_to_template
+    directory = "tmp/reports"
+
+    FileUtils.mkdir_p directory unless File.directory?(directory)
+
+    directory
+  end
+
+  def report_file_name(case_number)
+    "#{case_number}.docx"
+  end
+
+  def path_to_report(case_number)
+    "#{directory_to_template}/#{report_file_name(case_number)}"
+  end
+
+  def path_to_template
+    'app/documents/templates/report_template_transition.docx'
   end
 end
---
+++ app/models/case_court_report.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "date"
+require "pathname"
+require "sablon"
+require "ostruct"
+
+class CaseCourtReport
+  attr_reader :report_path
+
+  def initialize(args = {})
+    @casa_case      = CasaCase.find(args[:case_id])
+    @volunteer      = Volunteer.find(args[:volunteer_id])
+    @template_path  = args[:path_to_template]
+    @report_path    = args[:path_to_report]
+  end
+
+  def generate!
+    context = {
+      created_date: format_long_date(Date.today),
+      casa_case: prepare_case_details,
+      case_contacts: prepare_case_contacts,
+      volunteer: {
+        name: @volunteer.display_name,
+        supervisor_name: @volunteer.supervisor.display_name
+      }
+    }
+
+    Sablon.template(@template_path).render_to_file(@report_path, context)
+  end
+
+  private
+
+  def format_long_date(date)
+    date.strftime("%B %d, %Y")
+  end
+
+  def format_short_date(date)
+    date.strftime("%-m/%d")
+  end
+
+  def format_date_contact_attempt(case_contact)
+    format_short_date(case_contact.occurred_at)
+      .concat(case_contact.contact_made ? "" : "*")
+  end
+
+  def prepare_case_contacts
+    interviewees = CaseContactContactType.includes(:case_contact, :contact_type).where("case_contacts.casa_case_id": @casa_case.id)
+    interviewees = interviewees.where("occurred_at > ?", @casa_case.court_date) if @casa_case.court_date
+
+    contact_dates = aggregate_contact_dates(interviewees)
+
+    contact_dates.map do |type, dates|
+      {
+        name: "Firstname Lastname",
+        type: type,
+        dates: dates.join(", ")
+      }
+    end
+  end
+
+  def aggregate_contact_dates(people)
+    contact_dates = Hash.new([])
+
+    people.each do |person|
+      contact_type = person.contact_type.name
+      date_with_format = format_date_contact_attempt(person.case_contact)
+
+      contact_dates[contact_type] << date_with_format and next if contact_dates.key?(contact_type)
+
+      contact_dates[contact_type] = [date_with_format]
+    end
+
+    contact_dates
+  end
+
+  def prepare_case_details
+    {
+      court_date: @casa_case.court_date,
+      case_number: @casa_case.case_number,
+      dob: @casa_case.birth_month_year_youth,
+      assignment_date: format_long_date(@volunteer.case_assignments.find_by(casa_case: @casa_case).created_at)
+    }
+  end
+end
