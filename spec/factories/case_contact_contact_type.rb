98e285d062f7b4446c95a538bb25b369292a8e06
RachelWyatt <rwyatt@taphere.com>
2020-10-14 16:51:14 -0400

    fix formatting

--- spec/factories/case_contact_contact_type.rb
+++ spec/factories/case_contact_contact_type.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-    factory :case_contact_contact_type do
-        contact_type
-        case_contact
-    end
+  factory :case_contact_contact_type do
+    contact_type
+    case_contact
   end
+end
