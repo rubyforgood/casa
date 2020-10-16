6c416a1f5ffa210ec9f209f2157ae37d3f7b60e8
duaimei <adetwiler@optoro.com>
2020-10-15 21:48:28 -0400

    Added seed

--- db/seeds/test.rb
+++ db/seeds/test.rb
@@ -1,6 +1,15 @@
 User.destroy_all
 seed_password = "123456"
-
+pg_casa = CasaOrg.where(name: "Prince George CASA").first_or_create!(
+  casa_org_logo: logo,
+  display_name: "CASA / Prince George's County, MD",
+  address: "6811 Kenilworth Avenue, Suite 402 Riverdale, MD 20737",
+  footer_links: [
+    ["https://pgcasa.org/contact/", "Contact Us"],
+    ["https://pgcasa.org/subscribe-to-newsletter/", "Subscribe to newsletter"],
+    ["https://www.givedirect.org/give/givefrm.asp?CID=4450", "Donate"]
+  ]
+)
 volunteer = Volunteer.first_or_create!(
   casa_org: pg_casa,
   display_name: Faker::Name.name,
