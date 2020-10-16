50d0c803a277dcf0af6d227ecd8d266c92274981
Amy Detwiler <detwiler.amy@gmail.com>
2020-10-16 11:53:53 -0400

    Add Cypress tests to CI (#1010)

--- cypress/integration/volunteer/new_case_contact.spec.js
+++ cypress/integration/volunteer/new_case_contact.spec.js
@@ -1,12 +1,12 @@
-context('Logging into cypress as a volunteer', () => {
+context("Logging into cypress as a volunteer", () => {
   before(() => {
-    cy.visit('http://127.0.0.1:3000')
-    cy.loginAsVolunteer()
-  })
-  it('should go to the new case contact page', () => {
-    cy.get('#toggle-sidebar-js').click()
-    cy.get('[href="/case_contacts"]').click()
-    cy.get('.col-sm-12 > .btn').click()
-    cy.contains('New Case Contact').should('exist')
-  })
-})
+    cy.visit("0.0.0.0:4040");
+    cy.loginAsVolunteer();
+  });
+  it("should go to the new case contact page", () => {
+    cy.get("#toggle-sidebar-js").click();
+    cy.get('[href="/case_contacts"]').click();
+    cy.get(".col-sm-12 > .btn").click();
+    cy.contains("New Case Contact").should("exist");
+  });
+});
---
+++ db/seeds/test.rb
@@ -0,0 +1,26 @@
+User.destroy_all
+CasaOrgLogo.destroy_all
+
+seed_password = "123456"
+logo = CasaOrgLogo.new(
+  url: "media/src/images/logo.png",
+  alt_text: "CASA Logo",
+  size: "70x38"
+)
+pg_casa = CasaOrg.where(name: "Prince George CASA").first_or_create!(
+  casa_org_logo: logo,
+  display_name: "CASA / Prince George's County, MD",
+  address: "6811 Kenilworth Avenue, Suite 402 Riverdale, MD 20737",
+  footer_links: [
+    ["https://pgcasa.org/contact/", "Contact Us"],
+    ["https://pgcasa.org/subscribe-to-newsletter/", "Subscribe to newsletter"],
+    ["https://www.givedirect.org/give/givefrm.asp?CID=4450", "Donate"]
+  ]
+)
+volunteer = Volunteer.first_or_create!(
+  casa_org: pg_casa,
+  display_name: Faker::Name.name,
+  email: "volunteer1@example.com",
+  password: seed_password,
+  password_confirmation: seed_password
+)
