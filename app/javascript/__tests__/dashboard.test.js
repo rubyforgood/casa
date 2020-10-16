44765a7a69026ccdf916bb9917f18434d8b99c92
Gabby Skifstad <gskifstad@gmail.com>
2020-10-16 18:06:41 -0500

    [1079] move from rspec to jest tests

--- app/javascript/__tests__/dashboard.test.js
+++ app/javascript/__tests__/dashboard.test.js
@@ -3,15 +3,19 @@
 
 require('jest')
 var $ = require('jquery')
+var dt = require( 'datatables.net' )()
 var _ = require('lodash')
 global.$ = $
-global.jQuery = $
+//global.jQuery = $
+import { defineCaseContactsTable } from '../src/dashboard'
 
 describe('dashboard.js', () => {
   describe('DataTable tests', () => {
-    function initializeDocumentWithTable (id, numberOfRows = 1) {
-      var mockTable = `<table id=${id}>` +
-                      '<tr> <th>Column1</th> <th>Column2</th> <th>Column3</th> </tr>'
+    function initializeDocumentWithTable (id, numberOfRows = 1, classes = '') {
+      var mockTable = `<table id=${id} class=${classes}>` +
+                      '<thead>' +
+                      '<tr> <th>Column1</th> <th>Column2</th> <th>Column3</th> </tr>' +
+                      '</thead> <tbody>'
       _.range(numberOfRows).forEach((rowNum) => (
         mockTable += '<tr> ' +
                       `  <td>some data#${rowNum}</td>` +
@@ -19,10 +23,8 @@ describe('dashboard.js', () => {
                       `  <td>more data#${rowNum}</td> ` +
                       '</tr>'
       ))
-      mockTable += '</table>'
+      mockTable += '</tbody> </table>'
       document.body.innerHTML = mockTable
-      // trying to get the sideffects to trigger
-      require('../src/dashboard')
     }
 
     describe('casa_cases table', () => {
@@ -37,7 +39,8 @@ describe('dashboard.js', () => {
 
     describe('tables with scroll enabled', () => {
       beforeEach(() => {
-        initializeDocumentWithTable('case_contacts')
+        initializeDocumentWithTable('case_contacts', 1, 'case-contacts-table')
+        defineCaseContactsTable()
       })
 
       test("case contacts table renders with 'width=100%'", () => {
@@ -46,9 +49,11 @@ describe('dashboard.js', () => {
         *  column/header misalignment" bug
         *  (see https://datatables.net/manual/tech-notes/6)
         */
-        $('table').each(function () {
+        $('table#case_contacts').each(function () {
           expect($(this).css('width')).toEqual('100%')
         })
+        // formatting the DataTables scrollHeadInner makes the styling a
+        // bit more responsive, so we should validate its styling too
         expect($('.dataTables_scrollHeadInner').css('width')).toEqual('100%')
       })
     })
