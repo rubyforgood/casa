c9195aac4e9a54f5b7cf697bf72e7be3496ac479
sammy <exgin3538@yahoo.com>
2020-10-18 17:11:17 -0400

    fix duplication of contact_type_group model names

---
+++ spec/models/contact_type_group_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+require "contact_type_group"
+
+RSpec.describe ContactTypeGroup, type: :model do
+  it "should not have duplicate names" do
+    name = create(:contact_type_group, {name: "Test1"})
+
+    expect{create(:contact_type_group, {name: "Test1"})}.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Name has already been taken')
+  end
+end
