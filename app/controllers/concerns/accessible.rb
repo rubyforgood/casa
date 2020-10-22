e74e13e492cb1df9b4bd0010fa417b346e173be4
Jennifer Konikowski <jmkoni@icloud.com>
2020-10-22 12:56:52 -0400

    and return not && return

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -11,11 +11,11 @@ module Accessible
     if current_all_casa_admin
       flash.clear
       # if you have rails_admin. You can redirect anywhere really
-      redirect_to(authenticated_all_casa_admin_root_path) && return
+      redirect_to(authenticated_all_casa_admin_root_path) and return # rubocop:disable Style/AndOr
     elsif current_user
       flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(authenticated_user_root_path) && return
+      redirect_to(authenticated_user_root_path) and return # rubocop:disable Style/AndOr
     end
   end
 end
