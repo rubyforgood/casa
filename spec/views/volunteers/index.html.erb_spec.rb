cdbb83fddf03dfa919b6a10340ecfb991bb2653c
Nick Schimek <cfme00@gmail.com>
2020-10-24 11:12:06 +0900

    Create test for volunteer links
    Created a test to check if the volunteer name is a link. Moved the test
    setup code to the top level scope.
    
    In the index file I prepended some messages with the decorate message.
    For some reason in the view test it would error with message not found.
    I'm not sure what the architecture of the decorator is; however it seems
    strange that when live testing it is not needed. Does anybody have any
    insight on this?

--- spec/views/volunteers/index.html.erb_spec.rb
+++ spec/views/volunteers/index.html.erb_spec.rb
@@ -2,19 +2,27 @@ require "rails_helper"
 
 describe "volunteers" do
   subject { render template: "volunteers/index" }
+  let(:user) { build_stubbed :volunteer }
+  let(:volunteer) { create :volunteer }
 
-  context "while signed in as other user diferent to admin" do
-    let(:user) { build_stubbed :volunteer }
+  before do
+    enable_pundit(view, user)
+    allow(view).to receive(:current_user).and_return(user)
+    assign :volunteers, [volunteer]
+    sign_in user
+  end
 
-    before do
-      enable_pundit(view, user)
-      allow(view).to receive(:current_user).and_return(user)
-      assign :volunteers, []
-      sign_in user
-    end
+  context "when NOT signed in as an admin" do
+    it { is_expected.not_to have_selector("a", text: "New Volunteer") }
+  end
 
-    it do
-      is_expected.not_to have_selector("a", text: "New Volunteer")
-    end
+  context "when signed in as an admin" do
+    let(:user) { build_stubbed :casa_admin }
+
+    it { is_expected.to have_selector("a", text: "New Volunteer") }
+  end
+
+  context "links" do
+    it { is_expected.to have_link(volunteer.decorate.name) }
   end
 end
