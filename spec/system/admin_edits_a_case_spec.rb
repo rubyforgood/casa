d182752949849638eccbbc6e7116d9b70ddac4f7
RachelWyatt <rwyatt@taphere.com>
2020-10-25 10:25:41 -0400

    Update Admin Edits A Case Spec - Minor refactor: Move sign_in admin to a before block instead of repeating in each test - Test that admin cannot see data for an inactivated case - Tests that admin cannot reactivate or update an inactivated case

--- spec/system/admin_edits_a_case_spec.rb
+++ spec/system/admin_edits_a_case_spec.rb
@@ -8,8 +8,11 @@ RSpec.describe "admin edits case", type: :system do
   let!(:school) { create(:contact_type, name: "School", contact_type_group: contact_type_group) }
   let!(:therapist) { create(:contact_type, name: "Therapist", contact_type_group: contact_type_group) }
 
-  it "clicks back button after editing case" do
+  before do
     sign_in admin
+  end
+
+  it "clicks back button after editing case" do
     visit edit_casa_case_path(casa_case)
     check "Court report submitted"
     has_checked_field? :court_report_submitted
@@ -20,7 +23,6 @@ RSpec.describe "admin edits case", type: :system do
   end
 
   it "edits case" do
-    sign_in admin
     visit casa_case_path(casa_case.id)
     click_on "Edit Case Details"
     has_no_checked_field? :court_report_submitted
@@ -33,5 +35,20 @@ RSpec.describe "admin edits case", type: :system do
     expect(page).to have_text("Day")
     expect(page).to have_text("Month")
     expect(page).to have_text("Year")
+    expect(page).not_to have_text("Deactivate Case")
+  end
+
+  it "views deactivated case" do
+    casa_case.deactivate
+    visit edit_casa_case_path(casa_case)
+
+    expect(page).to have_text("This case has been inactivated by a supervisor.")
+    expect(page).not_to have_text("Court Date")
+    expect(page).not_to have_text("Court Report Due Date")
+    expect(page).not_to have_text("Day")
+    expect(page).not_to have_text("Month")
+    expect(page).not_to have_text("Year")
+    expect(page).not_to have_text("Reactivate Case")
+    expect(page).not_to have_text("Update Casa Case")
   end
 end
