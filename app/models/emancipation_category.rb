8c3573762171dcaea3d3f625e27f62852a00f9e8
firelemons <shenyang64@gmail.com>
2020-10-27 23:43:55 -0500

    fixed model not being able to add rows tih mutually excluive = false

--- app/models/emancipation_category.rb
+++ app/models/emancipation_category.rb
@@ -2,7 +2,7 @@ class EmancipationCategory < ApplicationRecord
   has_many :emancipation_options, -> { where mutually_exclusive: true }
   has_one  :emancipation_options, -> { where mutually_exclusive: false }
   validates :name, presence: true
-  validates :mutually_exclusive, presence: true 
+  validates :mutually_exclusive, inclusion: { in: [ true, false ] }
 
   def addOption(optionName)
     EmancipationOption.create(emancipation_category_id: self[:id], name: optionName)
