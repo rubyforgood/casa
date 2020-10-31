c966e996db183227fcf54be7c044ff4db46a8dea
firelemons <shenyang64@gmail.com>
2020-10-31 11:49:53 -0500

    added case/emancipation association action

--- app/controllers/emancipations_controller.rb
+++ app/controllers/emancipations_controller.rb
@@ -3,11 +3,19 @@ class EmancipationsController < ApplicationController
 
   # GET /casa_cases/:casa_case_id/emancipation
   def show
-   @case_options = params[:casa_case_id]
-   @emancipation_form_data = EmancipationCategory.all.map { |category| {:category => category, :options => EmancipationOption.categoryOptions(category.id)} }
+    @case_options = CasaCase.find(params[:casa_case_id]).emancipation_options
+    @emancipation_form_data = EmancipationCategory.all.map { |category| {:category => category, :options => EmancipationOption.categoryOptions(category.id)} }
   end
 
   # POST /casa_cases/:casa_case_id/emancipation/save
   def save
+    current_case = CasaCase.find(params[:casa_case_id])
+    option_action = params[:option_action]
+    if option_action == "add"
+      current_case.addEmancipationOption(params[:option_id])
+    elsif option_action == "delete"
+      current_case.removeEmancipationOption(params[:option_id])
+    else
+    end
   end
 end
