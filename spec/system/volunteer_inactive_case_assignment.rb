879e8e97eadfd8ee0933c1569d280a167cc568d8
Chase Southard <csouthard@gitlab.com>
2020-10-31 09:59:05 -0400

    fix standardrb violations

--- spec/system/volunteer_inactive_case_assignment.rb
+++ spec/system/volunteer_inactive_case_assignment.rb
@@ -4,12 +4,12 @@ RSpec.describe "volunteer assigned to multiple cases", type: :system do
   let(:organization) { create(:casa_org) }
   let(:supervisor) { create(:casa_admin, casa_org: organization) }
 
-  describe 'innactive case visibility' do
+  describe "innactive case visibility" do
     let!(:active_casa_case) { create(:casa_case, casa_org: organization, case_number: "ACTIVE") }
     let!(:inactive_casa_case) { create(:casa_case, casa_org: organization, active: false, case_number: "INACTIVE") }
     let!(:volunteer) { create(:volunteer, display_name: "Awesome Volunteer", casa_org: organization) }
 
-    it 'supervisor does not have inactive cases as an option to assign to a volunteer' do
+    it "supervisor does not have inactive cases as an option to assign to a volunteer" do
       sign_in supervisor
       visit edit_volunteer_path(volunteer)
 
