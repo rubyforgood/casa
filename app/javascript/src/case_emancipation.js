9b8cf179800687c333f40956b805299f92ffe867
firelemons <shenyang64@gmail.com>
2020-11-07 21:41:36 -0600

    script handles json responses now

--- app/javascript/src/case_emancipation.js
+++ app/javascript/src/case_emancipation.js
@@ -4,54 +4,48 @@ const emancipationPage = {
   waitingSaveOperationCount: 0
 }
 
-// Adds or deletes an option from the current casa case
-//  @param    {boolean}           isAdding true if adding the option to the case, false if deleting the option from the case
-//  @param    {integeri | string} optionId The id of the emancipation option to add or delete
-//  @returns  {array} a jQuery jqXHR object. See https://api.jquery.com/jQuery.ajax/#jqXHR
+// Shows an error notification
+//  @param    {string | Error}  error The error to be displayed
 //  @throws   {TypeError}  for a parameter of the incorrect type
-//  @throws   {RangeError} if optionId is negative
-function addOrDeleteOption(isAdding, optionId){
-  // Input check
-  if (typeof optionId === 'string') {
-    let optionIdAsNum = parseInt(optionId)
-
-    if (!optionIdAsNum) {
-      throw new TypeError('Param optionId is not an integer')
-    } else if (optionIdAsNum < 0) {
-      throw new RangeError('Param optionId cannot be negative')
-    }
-  } else {
-    if(!Number.isInteger(optionId)) {
-      throw new TypeError('Param optionId is not an integer')
-    } else if (optionId < 0) {
-      throw new RangeError('Param optionId cannot be negative')
-    }
+function notifyError(error) {
+  if (error instanceof Error) {
+    error = error.message
   }
 
-  // Post request
-  return $.post(emancipationPage.savePath, {
-    option_action: isAdding ? 'add' : 'delete',
-    option_id: optionId
+  if (typeof error !== 'string') {
+    throw new TypeError('Param error is neither a string or Error object')
+  }
+
+  emancipationPage.notifications.append(`
+  <div class="async-failure-indicator">
+    Error: ${error}
+    <button class="btn btn-danger btn-sm">×</button>
+  </div>`).find('.async-failure-indicator button').click( function () {
+    $(this).parent().remove()
   })
 }
 
 // Called when an async operation completes. May show notifications describing how the operation completed
-//  @param    {string=}  errorMsg The error message to be displayed if there was an error
+//  @param    {string | Error=}  error The error to be displayed if there was an error
 //  @throws   {TypeError}  for a parameter of the incorrect type
 //  @throws   {Error}      for trying to resolve more async operations than the amount currently awaiting
-function resolveAsyncOperation (errorMsg) {
+function resolveAsyncOperation (error) {
   if (emancipationPage.waitingSaveOperationCount < 1) {
     throw new Error('Attempted to resolve an async operation when awaiting none')
   }
 
-  if (errorMsg) {
-    if (typeof errorMsg !== 'string') {
-      throw new TypeError('Param errorMsg is not a string')
+  if (error instanceof Error) {
+    error = error.message
+  }
+
+  if (error) {
+    if (typeof error !== 'string') {
+      throw new TypeError('Param error is not a string')
     }
 
     emancipationPage.notifications.append(`
     <div class="async-failure-indicator">
-      Error: ${errorMsg}
+      Error: ${error}
       <button class="btn btn-danger btn-sm">×</button>
     </div>`).find('.async-failure-indicator button').click( function () {
       $(this).parent().remove()
@@ -81,6 +75,47 @@ function resolveAsyncOperation (errorMsg) {
 function waitForAsyncOperation () {
   emancipationPage.waitingSaveOperationCount++
   emancipationPage.asyncWaitIndicator.show()
+}
+
+// Adds or deletes an option from the current casa case
+//  @param    {boolean}           isAdding true if adding the option to the case, false if deleting the option from the case
+//  @param    {integer | string} optionId The id of the emancipation option to add or delete
+//  @returns  {array} a jQuery jqXHR object. See https://api.jquery.com/jQuery.ajax/#jqXHR
+//  @throws   {TypeError}  for a parameter of the incorrect type
+//  @throws   {RangeError} if optionId is negative
+function addOrDeleteOption(isAdding, optionId){
+  // Input check
+  if (typeof optionId === 'string') {
+    let optionIdAsNum = parseInt(optionId)
+
+    if (!optionIdAsNum) {
+      throw new TypeError('Param optionId is not an integer')
+    } else if (optionIdAsNum < 0) {
+      throw new RangeError('Param optionId cannot be negative')
+    }
+  } else {
+    if(!Number.isInteger(optionId)) {
+      throw new TypeError('Param optionId is not an integer')
+    } else if (optionId < 0) {
+      throw new RangeError('Param optionId cannot be negative')
+    }
+  }
+
+  // Post request
+  return $.post(emancipationPage.savePath, {
+    option_action: isAdding ? 'add' : 'delete',
+    option_id: optionId
+  }).done(function (response, textStatus) {
+    if (response.error) {
+      resolveAsyncOperation(response.error)
+    } else if (response === 'success'){
+      resolveAsyncOperation()
+    } else {
+      resolveAsyncOperation('Unknown response')
+    }
+  }).fail(function (jqXHR, textStatus, error) {
+    resolveAsyncOperation(error)
+  })
 }
 
 $('document').ready(() => {
@@ -114,7 +149,6 @@ $('document').ready(() => {
         if (thisSelect.val()) {
           addOrDeleteOption(true, thisSelect.val())
           .done(function( response ) {
-            console.log(response)
             resolveAsyncOperation()
           });
         }
@@ -122,7 +156,6 @@ $('document').ready(() => {
     } else if (thisSelect.val()) {
       addOrDeleteOption(true, thisSelect.val())
       .done(function( response ) {
-        console.log(response)
         resolveAsyncOperation()
       });
     }
@@ -138,13 +171,11 @@ $('document').ready(() => {
     if (thisCheckBox.prop('checked')) {
       addOrDeleteOption(true, thisCheckBox.val())
       .done(function( response ) {
-        console.log(response)
         resolveAsyncOperation()
       });
     } else {
       addOrDeleteOption(false, thisCheckBox.val())
       .done(function( response ) {
-        console.log(response)
         resolveAsyncOperation()
       });
     }
