6abeeb608d6c92432d7c328ce451c3805cbd62fb
CW <compiledwrong+github@gmail.com>
2020-11-17 19:17:23 -0800

    test failing on date format matching

--- scripts/import_casa_case_date_of_birth.rb
+++ scripts/import_casa_case_date_of_birth.rb
@@ -1,7 +1,7 @@
 # This will be run by hand with real prod data in prod console,
 # because we don't want to check in the data
 # and we don't want to make Sarah from PG CASA do hundreds of these by hand in the UI
-# because import didn't take DOC when PGCASA onboarded.
+# because import didn't have DOB when PG CASA onboarded.
 
 # data = """
 # 1/21/2000,,,,CINA 11-1234,
@@ -9,27 +9,42 @@
 # 3/23/2000,,,,CINA 13-1234,
 # """
 
-def update_casa_case_dates_of_birth(data)
-  case_not_found = []
-  already_has_nonmatching_date = []
+case_not_found = []
+already_has_nonmatching_date = []
+no_edit_made = []
+
+def update_casa_case_birth_month_year_youth(casa_case, new_date)
+  casa_case.update!(birth_month_year_youth: Date.new(new_date.year, new_date.month, 1))
+end
+
+def dates_match(casa_case, new_date)
+  casa_case.birth_month_year_youth.year == new_date.year && casa_case.birth_month_year_youth.month == new_date.month
+end
+
+def update_casa_case_dates_of_birth(data, case_not_found, already_has_nonmatching_date, no_edit_made)
+  casa_org = CasaOrg.find_by(name: "Prince George CASA")
+  return "Prince George CASA not found" unless casa_org
   data.split("\n").map(&:strip).reject(&:empty?).each do |row|
     chunks = row.split(",").compact
     d1 = chunks[0]
     p d1
     d2 = Date.strptime(d1, "%m/%d/%Y") # https://ruby-doc.org/stdlib-2.4.1/libdoc/date/rdoc/Date.html#method-i-strftime
     p d2
-
     case_number = chunks.last
-    cc = CasaCase.find_by(case_number: case_number)
+    cc = CasaCase.find_by(case_number: case_number, casa_org_id: casa_org.id)
     if cc
-      if cc.birth_month_year_youth && (cc.birth_month_year_youth.year != d2.year || cc.birth_month_year_youth.month != d2.month)
-        already_has_nonmatching_date << {cc_id: cc.id, prev_date: cc.birth_month_year_youth, import_date: d2}
+      if cc.birth_month_year_youth
+        if !dates_match(cc, d2)
+          already_has_nonmatching_date << {case_number: case_number, prev_date: cc.birth_month_year_youth, import_date: d2}
+        else
+          no_edit_made << case_number
+        end
+      else
+        update_casa_case_birth_month_year_youth(cc, d2)
       end
-      p [d2.year, d2.month, 1]
-      cc.update!(birth_month_year_youth: Date.new(d2.year, d2.month, 1))
     else
       case_not_found << case_number
     end
   end
-  {not_found: case_not_found, nonmatching: already_has_nonmatching_date}
+  {not_found: case_not_found, nonmatching: already_has_nonmatching_date, no_edit_made: no_edit_made}
 end
--- spec/scripts/import_casa_case_date_of_birth_spec.rb
+++ spec/scripts/import_casa_case_date_of_birth_spec.rb
@@ -2,35 +2,53 @@ require "rails_helper"
 require_relative "../../scripts/import_casa_case_date_of_birth"
 
 RSpec.describe "ImportCasaCaseDateOfBirth" do
-  data = "" "
+  data = "
     1/21/2000,,,,CINA 11-1234,
     2/22/2000,,,,TPR 12-1234,
     3/3/2000,,,,CINA 13-1234,
-    " ""
+    "
+
   it "returns not found" do
-    result = update_casa_case_dates_of_birth(data)
-    expect(result).to eq({nonmatching: [], not_found: ["CINA 11-1234", "TPR 12-1234", "CINA 13-1234"]})
+    #def update_casa_case_dates_of_birth(data, case_not_found, already_has_nonmatching_date, no_edit_made)
+    case_not_found = []
+    already_has_nonmatching_date = []
+    no_edit_made = []
+    create(:casa_org, name: "Prince George CASA")
+    result = update_casa_case_dates_of_birth(data, case_not_found, already_has_nonmatching_date, no_edit_made)
+    expect(result).to eq({nonmatching: [], not_found: ["CINA 11-1234", "TPR 12-1234", "CINA 13-1234"], no_edit_made: []})
+    expect(case_not_found).to eq(["CINA 11-1234", "TPR 12-1234", "CINA 13-1234"])
+    expect(already_has_nonmatching_date).to eq([])
+    expect(no_edit_made).to eq([])
   end
 
   it "returns not found" do
-    create(:casa_case, case_number: "CINA 11-1234", birth_month_year_youth: nil)
-    cc2 = create(:casa_case, case_number: "TPR 12-1234", birth_month_year_youth: Date.new(2000, 2, 22)) # matching date
-    cc3 = create(:casa_case, case_number: "CINA 13-1234", birth_month_year_youth: Date.new(2000, 9, 1)) # NON-matching date
-    create(:casa_case, case_number: "CINA 13-9999")
-    result = update_casa_case_dates_of_birth(data)
+    casa_org = create(:casa_org, name: "Prince George CASA")
+    create(:casa_case, casa_org: casa_org, case_number: "CINA 11-1234", birth_month_year_youth: nil)
+    create(:casa_case, casa_org: casa_org, case_number: "TPR 12-1234", birth_month_year_youth: Date.new(2000, 2, 22)) # matching date
+    create(:casa_case, casa_org: casa_org, case_number: "CINA 13-1234", birth_month_year_youth: Date.new(2000, 9, 1)) # NON-matching date
+    create(:casa_case, casa_org: casa_org, case_number: "CINA 14-8888")
+    create(:casa_case, case_number: "CINA 15-9999") # different casa org
+    case_not_found = []
+    already_has_nonmatching_date = []
+    no_edit_made = []
+    result = update_casa_case_dates_of_birth(data, case_not_found, already_has_nonmatching_date, no_edit_made)
     expect(result).to eq(
       nonmatching: [
         {
-          cc_id: cc2.id,
-          import_date: Date.new(2000, 2, 22),
-          prev_date: Date.new(2000, 2, 22)
-        }, {
-          cc_id: cc3.id,
-          import_date: Date.new(2000, 3, 3),
-          prev_date: Date.new(2000, 9, 1)
+          case_number: "CINA 13-1234",
+          import_date: DateTime.new(2000, 3, 3),
+          prev_date: DateTime.new(2000, 9, 1)
         }
       ],
-      not_found: []
+      not_found: [],
+      no_edit_made: ["TPR 12-1234"]
     )
+    expect(case_not_found).to eq([])
+    expect(already_has_nonmatching_date).to eq([{
+                                                    case_number: "CINA 13-1234",
+                                                    import_date: Date.new(2000, 3, 3),
+                                                    prev_date: Date.new(2000, 9, 1)
+                                                }])
+    expect(no_edit_made).to eq(["TPR 12-1234"])
   end
 end
