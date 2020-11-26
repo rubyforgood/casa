910717660584e1fd736bea1dba319b920e336412
firelemons <shenyang64@gmail.com>
2020-11-26 17:06:06 -0600

    emancipation checklists when volunteer has 1 transitioning case

--- app/controllers/emancipation_checklists_controller.rb
+++ app/controllers/emancipation_checklists_controller.rb
@@ -5,12 +5,12 @@ class EmancipationChecklistsController < ApplicationController
   def index
     authorize :application, :see_emancipation_checklist?
     org_cases = current_user.casa_org.casa_cases.includes(:assigned_volunteers)
-    @casa_cases = policy_scope(org_cases).includes([:hearing_type, :judge])
+    @casa_transitioning_cases = policy_scope(org_cases).where(transition_aged_youth: true).includes([:hearing_type, :judge])
 
-    if @casa_cases.count == 1
-      puts '################################'
-      puts @casa_cases.count
-      redirect_to casa_case_emancipation_path(@casa_cases[0])
+    puts @casa_transitioning_cases
+
+    if @casa_transitioning_cases.count == 1
+      redirect_to casa_case_emancipation_path(@casa_transitioning_cases[0])
     end
   end
 end
