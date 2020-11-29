fe97723e4c5738bd30f68a1cbaa239dee8696975
Patrick Arnett <patrick.a.arnett@gmail.com>
2020-11-29 14:49:09 -0500

    Only list current org's supervisors.

--- spec/views/volunteers/index.html.erb_spec.rb
+++ spec/views/volunteers/index.html.erb_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe "volunteers" do
   before do
     enable_pundit(view, user)
     allow(view).to receive(:current_user).and_return(user)
+    allow(view).to receive(:current_organization).and_return user.casa_org
     assign :volunteers, [volunteer]
     sign_in user
   end
