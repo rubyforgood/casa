c727693d4547a5400ededb8a0942cca1352ec581
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-12-01 16:31:58 -0700

    Added validations for submission date and status

--- app/models/casa_case.rb
+++ app/models/casa_case.rb
@@ -66,6 +66,22 @@ class CasaCase < ApplicationRecord
   delegate :name, to: :hearing_type, prefix: true, allow_nil: true
   delegate :name, to: :judge, prefix: true, allow_nil: true
 
+  # Validation class for :court_report_status & :court_report_submitted_at. Adds specific errors based on record data.
+  class CourtReportValidator < ActiveModel::Validator
+    def validate(record)
+      # Check for no timestamp and a submission value other than 'not_submitted'.
+      if record.court_report_submitted_at.nil? && record.court_report_status != "not_submitted"
+        record.errors[:court_report_status] << "Court report submission date can't be nil if status is anything but not_submitted."
+      # Check for timestamp with a 'not_submitted' status.
+      elsif !record.court_report_submitted_at.nil? && record.court_report_status == "not_submitted"
+        record.errors[:court_report_submitted_at] << "Submission date must be nil if court report status is not submitted."
+      end
+    end
+  end
+
+  # Validation to check timestamp and submission status of a case
+  validates_with CourtReportValidator, fields: [:court_report_status, :court_report_submitted_at]
+
   def court_report_status=(value)
     super
     if court_report_not_submitted?
