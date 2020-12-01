895ae212adda30985cbff20f4b28630be3d864b8
Patrick Arnett <patrick.a.arnett@gmail.com>
2020-12-01 14:35:53 -0500

    Remove unused file and method.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,16 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  def self.current_table_name
-    current_table = current_scope.arel.source.left
-
-    case current_table
-    when Arel::Table
-      current_table.name
-    when Arel::Nodes::TableAlias
-      current_table.right
-    else
-      fail
-    end
-  end
 end
