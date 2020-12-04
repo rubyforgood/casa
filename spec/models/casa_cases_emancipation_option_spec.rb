eb4b28afda9f8239504a01e5e22a79002b27b5ef
firelemons <shenyang64@gmail.com>
2020-12-04 00:38:37 -0600

    created factory for case/emancipation option join entry model

--- spec/models/casa_cases_emancipation_option_spec.rb
+++ spec/models/casa_cases_emancipation_option_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe CasaCasesEmancipationOption, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    case_option_association = build(:casa_cases_emancipation_option)
+    expect(case_option_association.valid?).to be true
+  end
 end
