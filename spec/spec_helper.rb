9411d933b6b0ae91e10210261a5915df1a4e5d03
Jason Hsu <jhsu@midwestmortgageanalytics.com>
2020-12-13 00:41:41 -0600

    Seeking the output of cat /etc/os-release

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@ SimpleCov.start do
   add_group "Database", "/db"
 end
 
+system('cat /etc/os-release')
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
