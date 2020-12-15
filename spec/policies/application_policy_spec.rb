20c54bce3a6c9a16a18e3b8fe06b16fb3b4d7fe4
Scott Olsen <solsen300@gmail.com>
2020-12-15 14:49:51 -0600

    Punditize CaseContactReportsController

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe ApplicationPolicy do
   subject { described_class }
 
-  let(:casa_admin) { create(:casa_admin) }
-  let(:supervisor) { create(:supervisor) }
-  let(:volunteer) { create(:volunteer) }
+  let(:casa_admin) { build_stubbed(:casa_admin) }
+  let(:supervisor) { build_stubbed(:supervisor) }
+  let(:volunteer) { build_stubbed(:volunteer) }
 
   permissions :see_reports_page? do
     it "allows casa_admins" do
