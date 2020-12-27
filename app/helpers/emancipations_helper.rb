b654fd6c04a0ef0face2c478aa0ef7e53206e3ae
firelemons <shenyang64@gmail.com>
2020-12-27 17:31:06 -0600

    rename helper methods

--- app/helpers/emancipations_helper.rb
+++ app/helpers/emancipations_helper.rb
@@ -1,9 +1,13 @@
 module EmancipationsHelper
-  def emancipation_select_option_selected(casa_case, emancipation_option)
+  def emancipation_option_select_selected?(casa_case, emancipation_option)
     casa_case.emancipation_options.include?(emancipation_option) ? "selected" : nil
   end
 
-  def emancipation_checkbox_option_checked(casa_case, emancipation_option)
+  def emancipation_category_checkbox_checked?(casa_case, emancipation_category)
+    casa_case.emancipation_categories.include?(emancipation_category) ? "checked" : nil
+  end
+
+  def emancipation_option_checkbox_checked?(casa_case, emancipation_option)
     casa_case.emancipation_options.include?(emancipation_option) ? "checked" : nil
   end
 end
