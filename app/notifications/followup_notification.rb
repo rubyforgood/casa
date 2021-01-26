684f881e86e310dc3b318ee44f0765eed5c5f397
Scott Olsen <solsen300@gmail.com>
2021-01-26 17:52:55 -0600

    Fixes notification title when it's a new followup

--- app/notifications/followup_notification.rb
+++ app/notifications/followup_notification.rb
@@ -17,6 +17,10 @@ class FollowupNotification < Noticed::Base
 
   # Define helper methods to make rendering easier.
   #
+  def title
+    t(".title")
+  end
+
   def message
     t(".message", created_by_name: params[:created_by_name])
   end
