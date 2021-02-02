26a2f0c562ed3f14cc514c6e1f5103c10180d306
Ryan Polley <rypolley@gmail.com>
2021-02-02 14:36:52 -0600

    lint autofix

--- spec/views/all_casa_admins/casa_orgs/show.html.erb_spec.rb
+++ spec/views/all_casa_admins/casa_orgs/show.html.erb_spec.rb
@@ -2,46 +2,46 @@ require "rails_helper"
 
 RSpec.describe "all_casa_admins/casa_orgs/show", type: :view do
   context "All casa admin organization dashboard" do
-    let(:organization) {create :casa_org }
-  
+    let(:organization) { create :casa_org }
+
     let(:org_info) {
       [
         {
           type: :casa_admin,
           number: 3,
           active: true,
-          description: "admin",
+          description: "admin"
         },
         {
           type: :supervisor,
           number: 7,
           active: true,
-          description: "supervisor",
+          description: "supervisor"
         },
         {
           type: :volunteer,
           number: 20,
           active: true,
-          description: "active volunteer",
+          description: "active volunteer"
         },
         {
           type: :volunteer,
           number: 7,
           active: false,
-          description: "inactive volunteer",
+          description: "inactive volunteer"
         },
         {
           type: :casa_case,
           number: 50,
           active: true,
-          description: "active case",
+          description: "active case"
         },
         {
           type: :casa_case,
           number: 11,
           active: false,
-          description: "inactive case",
-        },
+          description: "inactive case"
+        }
       ]
     }
 
@@ -60,7 +60,7 @@ RSpec.describe "all_casa_admins/casa_orgs/show", type: :view do
       org_info.each do |group|
         expect(rendered).to have_text(
           "Number of #{group[:description]}s: #{group[:number]}",
-          normalize_ws: true,
+          normalize_ws: true
         )
       end
     end
