67ac0ad25aad6b11d6a3d3c06361c5acca618543
Erin Claudio <eclaudio@gmail.com>
2021-02-02 11:45:41 -0500

    add three new failing tests

--- spec/controllers/emancipations_controller_spec.rb
+++ spec/controllers/emancipations_controller_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 RSpec.describe EmancipationsController, type: :controller do
 let(:organization) { create(:casa_org) }
-let(:volunteer){create(:volunteer, :with_casa_cases, casa_org: organization)}
-
+let(:volunteer) {create(:volunteer, :with_casa_cases, casa_org: organization)}
+#let(test_case_category = create(:casa_case_emancipation_category))
   before do
     allow(controller).to receive(:authenticate_user!).and_return(true)
     allow(controller).to receive(:current_user).and_return(volunteer)
   end
 
-  it "raises add_category error message" do
+  it "raises Missing param casa_case_id error message" do
     test_case_category = create(:casa_case_emancipation_category)
     post :save, params:{casa_case_id: '1'}
-    expect(response.body).to eq({"error": "Missing param check_item_action"}.to_json)
+    expect(response.body).to eq({"error": "Missing param casa_case_id"}.to_json)
   end
 
   it"raises add_option error message" do
@@ -21,19 +21,21 @@ let(:volunteer){create(:volunteer, :with_casa_cases, casa_org: organization)}
     expect(response.body).to eq({"error": "Param casa_case_id must be a positive integer"}.to_json)
   end
 
-  it"raises delete_category error message" do
+  it "raises add_category error message" do
+    test_case_category = create(:casa_case_emancipation_category)
+    post :save, params:{casa_case_id: '1'}
+    expect(response.body).to eq({"error": "Missing param check_item_action"}.to_json)
   end
 
-  it"raises delete_option error message" do
+  it "raises param check_item_id error message" do
+    test_case_category = create(:casa_case_emancipation_category)
+    post :save, params:{casa_item_id: '1'}
+    expect(response.body).to eq({"error": "Missing param check_item_id"}.to_json)
   end
 
-  it"raises set_option error message" do
+  it "raises must be positive integer error message" do
+    test_case_category = create(:casa_case_emancipation_category)
+    post :save, params:{casa_item_id: '-1'}
+    expect(response.body).to eq({"error": "Param check_item_id must be a positive integer"}.to_json)
   end
 end
-
-# rescue ActiveRecord::RecordNotFound
-#   render json: {error: "Could not find option from id given by param check_item_id"}
-# rescue ActiveRecord::RecordNotUnique
-#   render json: {error: "Option already added to case"}
-# rescue => error
-#   render json: {error: error.message}
