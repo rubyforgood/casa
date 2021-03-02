2c50c03aef66d77b98fc4b2a7cc80bb60f21eed6
Rhian Moraes <rhian.garcia@codeminer42.com>
2021-03-02 10:52:46 -0300

    Added a way to delete court mandates

--- app/javascript/src/casa_case.js
+++ app/javascript/src/casa_case.js
@@ -10,6 +10,61 @@ id="casa_case_case_court_mandates_attributes_1_mandate_text">\
   $(list).children(':last').trigger('focus')
 }
 
+function remove_mandate_with_confirmation() {
+  Swal.fire({
+    icon: 'warning',
+    title: 'Delete mandate?',
+    text: 'Are you sure you want to remove this court mandate? Doing so will delete all records of it unless it was included in a previous court report.',
+
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfirm: false,
+
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#39c',
+
+    confirmButtonText: 'Delete',
+    cancelButtonText: 'Go back'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      remove_mandate_action($(this))
+    }
+  })
+}
+
+function remove_mandate_action(ctx) {
+  id = get_mandate_id(ctx)
+
+  $.ajax({
+    url: `/case_court_mandates/${id}`,
+    method: "delete",
+    success: () => {
+      remove_mandate_entry(ctx)
+      Swal.fire({
+        icon: 'success',
+        text: 'Court mandate has been removed.',
+        showCloseButton: true,
+      })
+    },
+    error: () => {
+      Swal.fire({
+        icon: 'error',
+        text: 'Something went wrong when attempting to delete this court mandate.',
+        showCloseButton: true,
+      })
+    }
+  })
+}
+
+function get_mandate_id(ctx) {
+  return ctx.parent().next('input[type="hidden"]').val()
+}
+
+function remove_mandate_entry(ctx) {
+  ctx.parent().remove()
+}
+
 $('document').ready(() => {
   $('button#add-mandate-button').on('click', add_court_mandate_input)
+  $('button.remove-mandate-button').on('click', remove_mandate_with_confirmation)
 })
