56328c4a75c4c42b74a83218ce7aca1340523d8c
CW <compiledwrong+github@gmail.com>
2021-03-11 00:03:18 -0800

    Fix rake factory_bot:lint

--- spec/factories/case_court_mandates.rb
+++ spec/factories/case_court_mandates.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :case_court_mandate do
+    association :casa_case
     mandate_text { Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 20) }
   end
 end
