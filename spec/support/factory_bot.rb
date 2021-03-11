e2171a0fb5f4e20f9969d4f9a6799c68f748c547
CW <compiledwrong+github@gmail.com>
2021-03-10 23:31:42 -0800

    Enable factorybot printing as a reminder to cleanit uppppp - this will be  messy, we can take it out when we're sick of it

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -19,29 +19,29 @@ RSpec.configure do |config|
 
   # This will output records as they are created. Handy for debugging but very
   # noisy.
-  # config.before(:each) do
-  #   ActiveSupport::Notifications.subscribe("factory_bot.run_factory") do |name, start, finish, id, payload|
-  #     $stderr.puts "FactoryBot: #{payload[:strategy]}(:#{payload[:name]})"
-  #   end
-  # end
+  config.before(:each) do
+    ActiveSupport::Notifications.subscribe("factory_bot.run_factory") do |name, start, finish, id, payload|
+      $stderr.puts "FactoryBot: #{payload[:strategy]}(:#{payload[:name]})"
+    end
+  end
 
   # This will output total database records being created. Commented out to
   # keep the spec output clean.
-  #
-  # factory_bot_results = {}
-  # config.before(:suite) do
-  #   ActiveSupport::Notifications.subscribe("factory_bot.run_factory") do |name, start, finish, id, payload|
-  #     factory_name = payload[:name]
-  #     strategy_name = payload[:strategy]
-  #     factory_bot_results[factory_name] ||= {}
-  #     factory_bot_results[factory_name][:total] ||= 0
-  #     factory_bot_results[factory_name][:total] += 1
-  #     factory_bot_results[factory_name][strategy_name] ||= 0
-  #     factory_bot_results[factory_name][strategy_name] += 1
-  #   end
-  # end
-  #
-  # config.after(:suite) do
-  #   p factory_bot_results
-  # end
+
+  factory_bot_results = {}
+  config.before(:suite) do
+    ActiveSupport::Notifications.subscribe("factory_bot.run_factory") do |name, start, finish, id, payload|
+      factory_name = payload[:name]
+      strategy_name = payload[:strategy]
+      factory_bot_results[factory_name] ||= {}
+      factory_bot_results[factory_name][:total] ||= 0
+      factory_bot_results[factory_name][:total] += 1
+      factory_bot_results[factory_name][strategy_name] ||= 0
+      factory_bot_results[factory_name][strategy_name] += 1
+    end
+  end
+
+  config.after(:suite) do
+    p factory_bot_results
+  end
 end
--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -7,11 +7,11 @@ module SessionHelper
     sign_in(Volunteer.first || create(:volunteer))
   end
 
-  def sign_in_as_supervisor
+  def sign_in_as_supervisor # TODO use this more
     sign_in(Supervisor.first || create(:supervisor))
   end
 
-  def sign_in_as_all_casa_admin
+  def sign_in_as_all_casa_admin # TODO use this more
     sign_in(AllCasaAdmin.first || create(:all_casa_admin))
   end
 end
