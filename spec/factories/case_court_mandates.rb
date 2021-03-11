14e6e673a6133fc8ed05e0d17466c2b693443bb7
CW <compiledwrong+github@gmail.com>
2021-03-11 00:34:08 -0800

    factory_bot association : is only needed when the factory name doesnt match - see https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md\#explicit-definition

--- spec/factories/case_court_mandates.rb
+++ spec/factories/case_court_mandates.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :case_court_mandate do
-    association :casa_case
+    casa_case
     mandate_text { Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 20) }
   end
 end
--- spec/factories/contact_type.rb
+++ spec/factories/contact_type.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :contact_type do
-    association :contact_type_group
+    contact_type_group
     sequence(:name) { |n| "Type #{n}" }
   end
 end
