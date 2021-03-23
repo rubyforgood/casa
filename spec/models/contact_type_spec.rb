5afaf1b4514591fe3bc066ad1c79763d9b4e6314
efgalvao <solfieri@hotmail.com>
2021-03-23 16:35:08 -0300

    Create tests

---
+++ spec/models/contact_type_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+RSpec.describe ContactType, type: :model do
+  # let(:casa_org) { create(:casa_org) }
+  # let(:contact_type_group) { create(:contact_type_group, {name: "Test1", casa_org_id: casa_org.id}) }
+  let(:contact_type) { create(:contact_type, name: "Name") }
+  
+  describe "#create"  do
+    it "does have a name" do
+        # contact_type = create(:contact_type, name: nil)
+        # create_contact_type = -> { create(:contact_type, name: nil) }
+        # create_contact_type.call
+        # expect(create_contact_type.call).to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
+        # expect(contact_type.errors.message[:name]).to eq["Name can't be blank"]
+        is_expected.to validate_presence_of(:name)
+      # expect(contact_type).to_not be_valid
+    end
+  end
+
+  describe "#update" do
+    it "can update to a valid name" do
+      contact_type.name = "New name"
+      contact_type.save
+      expect(contact_type.name).to eq("New name")
+    end
+
+    it "can't update to an invalid name" do
+      contact_type.name = nil
+      # expect(contact_type.errors.messages[:name]).to eq(["Name can't be blank"])
+      expect {contact_type.save!}.to  raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
+    end
+
+    it "can update contact type group" do
+      new_group = create(:contact_type_group, name: "New contact group")
+      contact_type.contact_type_group_id = new_group.id
+      expect(contact_type.contact_type_group.name).to eq("New contact group")
+      # expect {contact_type.save!}.to  raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
+    end
+
+    it "can deactivate contact type " do
+      contact_type.active = false
+      expect(contact_type.active?).to be_falsey
+    end
+
+  end
+end
