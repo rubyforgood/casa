81244b2fc90dc2550855eea61aa32e3e04525e37
Harefx <harefx.dev@gmail.com>
2021-04-03 08:38:18 +0700

    add more condition to display the delete and undelete button

--- app/policies/case_contact_policy.rb
+++ app/policies/case_contact_policy.rb
@@ -11,7 +11,7 @@ class CaseContactPolicy < ApplicationPolicy
   alias_method :new?, :admin_or_supervisor_or_volunteer?
   alias_method :show?, :is_creator_or_casa_admin?
   alias_method :create?, :is_creator_or_casa_admin?
-  alias_method :destroy?, :is_creator_or_casa_admin?
+  alias_method :destroy?, :is_admin?
   alias_method :update?, :is_creator_or_supervisor_or_casa_admin?
   alias_method :edit?, :is_creator_or_supervisor_or_casa_admin?
   alias_method :restore?, :is_admin?
