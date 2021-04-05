ca8299243c80c79f42ffa9e3524ca366b9ee4208
CW <compiledwrong+github@gmail.com>
2021-04-04 20:49:38 -0700

    Use invite and login partial for supervisor and admin edit pages

--- spec/views/casa_admins/edit.html.erb_spec.rb
+++ spec/views/casa_admins/edit.html.erb_spec.rb
@@ -1,18 +1,20 @@
 require "rails_helper"
 
 RSpec.describe "casa_admins/edit", type: :view do
-  describe "does not allow" do
-    let(:admin) { build_stubbed :casa_admin }
+  let(:admin) { build_stubbed :casa_admin }
 
-    it "an admin to edit an admin last sign in" do
-      enable_pundit(view, admin)
-      allow(view).to receive(:current_user).and_return(admin)
+  it "shows invite and login info" do
+    enable_pundit(view, admin)
+    allow(view).to receive(:current_user).and_return(admin)
 
-      assign :casa_admin, admin
+    assign :casa_admin, admin
 
-      render template: "casa_admins/edit"
+    render template: "casa_admins/edit"
 
-      expect(rendered).to have_field("casa_admin_last_sign_in_at", disabled: true)
-    end
+    expect(rendered).to have_text("Added to system ")
+    expect(rendered).to have_text("Invitation email sent \n  never")
+    expect(rendered).to have_text("Last logged in")
+    expect(rendered).to have_text("Invitation accepted \n  never")
+    expect(rendered).to have_text("Password reset last sent \n  never")
   end
 end
