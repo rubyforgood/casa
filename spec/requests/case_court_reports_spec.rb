8dcf8bd103f447eb4be70086efe079668da2d3c4
firelemons <shenyang64@gmail.com>
2021-04-19 15:43:00 -0500

    added test for custom template

--- spec/requests/case_court_reports_spec.rb
+++ spec/requests/case_court_reports_spec.rb
@@ -64,41 +64,41 @@ RSpec.describe "/case_court_reports", type: :request do
 
   # case_court_reports#generate
   describe "POST /case_court_reports" do
-    before do
-      post generate_case_court_reports_path,
-        params: {
-          "case_court_report": {"case_number": casa_case.case_number.to_s}
-        },
-        headers: {"ACCEPT": "application/json"}
-    end
-
-    context "when a valid / existing case is sent" do
-      let(:casa_case) { volunteer.casa_cases.first }
-
-      it "sends response as a JSON string" do
-        expect(response.content_type).to eq("application/json; charset=utf-8")
-        expect(JSON.parse(response.body)).to be_instance_of Hash
+    context "when no custom template is set" do
+      before do
+        post generate_case_court_reports_path,
+          params: {
+            "case_court_report": {"case_number": casa_case.case_number.to_s}
+          },
+          headers: {"ACCEPT": "application/json"}
       end
 
-      it "has keys ['link', 'status'] in JSON string" do
-        body_hash = JSON.parse(response.body)
+      context "when a valid / existing case is sent" do
+        let(:casa_case) { volunteer.casa_cases.first }
 
-        expect(body_hash).to have_key "link"
-        expect(body_hash).to have_key "status"
-      end
+        it "sends response as a JSON string" do
+          expect(response.content_type).to eq("application/json; charset=utf-8")
+          expect(JSON.parse(response.body)).to be_instance_of Hash
+        end
 
-      it "sends response with status :ok" do
-        expect(response).to have_http_status(:ok)
-      end
+        it "has keys ['link', 'status'] in JSON string" do
+          body_hash = JSON.parse(response.body)
 
-      it "contains a link ending with .DOCX extension" do
-        body_hash = JSON.parse(response.body)
+          expect(body_hash).to have_key "link"
+          expect(body_hash).to have_key "status"
+        end
 
-        expect(body_hash["link"].length).to be > 0
-        expect(body_hash["link"]).to end_with("#{casa_case.case_number}.docx")
-      end
+        it "sends response with status :ok" do
+          expect(response).to have_http_status(:ok)
+        end
 
-      context "when a custom template is not set" do
+        it "contains a link ending with .DOCX extension" do
+          body_hash = JSON.parse(response.body)
+
+          expect(body_hash["link"].length).to be > 0
+          expect(body_hash["link"]).to end_with("#{casa_case.case_number}.docx")
+        end
+
         it "uses the default template" do
           get JSON.parse(response.body)["link"]
 
@@ -112,43 +112,63 @@ RSpec.describe "/case_court_reports", type: :request do
         end
       end
 
-      context "when a custom template is set" do
-        it "uses the custom template" do
+      context "when an INVALID / non-existing case is sent" do
+        let(:casa_case) { build_stubbed(:casa_case) }
+
+        it "sends response as a JSON string" do
+          expect(response.content_type).to eq("application/json; charset=utf-8")
+          expect(JSON.parse(response.body)).to be_instance_of Hash
+        end
+
+        it "has keys ['link','status','error_messages'] in JSON string" do
+          body_hash = JSON.parse(response.body)
+
+          expect(body_hash).to have_key "link"
+          expect(body_hash).to have_key "status"
+          expect(body_hash).to have_key "error_messages"
+        end
+
+        it "sends response with status :not_found" do
+          expect(response).to have_http_status(:not_found)
+        end
+
+        it "contains a empty link" do
+          body_hash = JSON.parse(response.body)
+
+          expect(body_hash["link"].length).to be 0
+        end
+
+        it "contains error messages with words 'not found'" do
+          body_hash = JSON.parse(response.body)
+
+          expect(body_hash["error_messages"].length).to be > 0
+          expect(body_hash["error_messages"]).to match(/not found/)
         end
       end
     end
 
-    context "when an INVALID / non-existing case is sent" do
-      let(:casa_case) { build_stubbed(:casa_case) }
+    context "when a custom template is set" do
+      let(:casa_case) { volunteer.casa_cases.first }
 
-      it "sends response as a JSON string" do
-        expect(response.content_type).to eq("application/json; charset=utf-8")
-        expect(JSON.parse(response.body)).to be_instance_of Hash
+      before do
+        volunteer.casa_org.court_report_template.attach(io: File.new(Rails.root.join("app", "documents", "templates", "montgomery_report_template.docx")), filename: "montgomery_report_template.docx")
+
+        post generate_case_court_reports_path,
+          params: {
+            "case_court_report": {"case_number": casa_case.case_number.to_s}
+          },
+          headers: {"ACCEPT": "application/json"}
       end
+      it "uses the custom template" do
+        get JSON.parse(response.body)["link"]
 
-      it "has keys ['link','status','error_messages'] in JSON string" do
-        body_hash = JSON.parse(response.body)
+        Tempfile.create("court_report.zip", "tmp") do |file|
+          file << response.body
 
-        expect(body_hash).to have_key "link"
-        expect(body_hash).to have_key "status"
-        expect(body_hash).to have_key "error_messages"
-      end
-
-      it "sends response with status :not_found" do
-        expect(response).to have_http_status(:not_found)
-      end
-
-      it "contains a empty link" do
-        body_hash = JSON.parse(response.body)
-
-        expect(body_hash["link"].length).to be 0
-      end
-
-      it "contains error messages with words 'not found'" do
-        body_hash = JSON.parse(response.body)
-
-        expect(body_hash["error_messages"].length).to be > 0
-        expect(body_hash["error_messages"]).to match(/not found/)
+          Zip::File.open(file.path) do |docx_extracted|
+            expect(docx_extracted.find_entry("word/header2.xml").get_input_stream.read.force_encoding("UTF-8")).to include("Voices for Children Montgomery")
+          end
+        end
       end
     end
   end
