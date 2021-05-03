93a22b9b64df3a8b3bc34507172afff555a38d31
CW <compiledwrong+github@gmail.com>
2021-05-02 22:06:59 -0700

    Fix spec temporarily

--- spec/views/supervisor_mailer/weekly_digest.html.erb_spec.rb
+++ spec/views/supervisor_mailer/weekly_digest.html.erb_spec.rb
@@ -57,7 +57,6 @@ RSpec.describe "supervisor_mailer/weekly_digest", type: :view do
     it { expect(rendered).to have_text("No contact attempts were logged for this week.") }
   end
 
-
   context "when a volunteer has been reassigned to a new supervisor" do
     before(:each) do
       supervisor.volunteers << volunteer
@@ -76,6 +75,7 @@ RSpec.describe "supervisor_mailer/weekly_digest", type: :view do
     let(:other_supervisor) { create(:supervisor) }
 
     it { expect(rendered).not_to have_text(volunteer.display_name) }
+  end
 
   context "when a volunteer has been unassigned" do
     before(:each) do
@@ -108,8 +108,5 @@ RSpec.describe "supervisor_mailer/weekly_digest", type: :view do
     it { expect(rendered).to have_text("The following volunteers have been unassigned from you") }
     it { expect(rendered).to have_text("- #{volunteer.display_name}") }
     it { expect(rendered).to have_text("(not assigned to a new supervisor)") }
-
   end
-
-  # TODO: Add more cases here
 end
