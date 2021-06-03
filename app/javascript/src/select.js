20e0630de1ab105bb055f372911294975900858a
Libby Rodriguez <librod89@gmail.com>
2021-06-03 12:27:53 -0400

    lint javascript

--- app/javascript/src/select.js
+++ app/javascript/src/select.js
@@ -4,30 +4,29 @@ $('document').ready(() => {
   $('.select2').select2({ theme: 'classic', matcher: matchCustom })
 })
 
-function matchCustom(params, data) {
+function matchCustom (params, data) {
   // If there are no search terms, return all of the data
   if ($.trim(params.term) === '') {
-    return data;
+    return data
   }
 
   // Do not display the item if there is no 'text' property
   if (typeof data.text === 'undefined') {
-    return null;
+    return null
   }
 
   // `params.term` should be the term that is used for searching
   // `data.text` is the text that is displayed for the data object
   if (data.text.toUpperCase().indexOf(params.term.toUpperCase()) > -1) {
-    return data;
+    return data
   }
 
   // custom search using lookup data
-  var lookup = $(data.element).data('lookup');
+  const lookup = $(data.element).data('lookup')
   if (lookup && lookup.toUpperCase().indexOf(params.term.toUpperCase()) > -1) {
-    return data;
+    return data
   }
 
   // Return `null` if the term should not be displayed
-  return null;
+  return null
 }
-
