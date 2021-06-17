35ea47cc09b40233d9e2e0d04a24a827beca51fa
firelemons <shenyang64@gmail.com>
2021-06-16 21:56:13 -0500

    moved assetlinks path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  match "/.well-known/assetlinks.json", to: redirect("/assetlinks.json"), via: :get
-
   devise_for :all_casa_admins, path: "all_casa_admins", controllers: {sessions: "all_casa_admins/sessions"}
   devise_for :users, controllers: {sessions: "users/sessions"}
 
