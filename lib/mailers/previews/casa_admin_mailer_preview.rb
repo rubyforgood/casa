a2fa3edb958abafcaa97ad8cd37fe0054cf927c4
efgalvao <solfieri@hotmail.com>
2021-06-24 14:18:42 -0300

    Adds deactivation mailer preview

--- lib/mailers/previews/casa_admin_mailer_preview.rb
+++ lib/mailers/previews/casa_admin_mailer_preview.rb
@@ -2,4 +2,10 @@ class CasaAdminMailerPreview < ActionMailer::Preview
   def account_setup
     CasaAdminMailer.account_setup(CasaAdmin.last)
   end
+
+  def deactivation
+    CasaAdminMailer.deactivation(CasaAdmin.last)
+  end
 end
+
+
