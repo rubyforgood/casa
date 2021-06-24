deeef5ac9fbee07461486ab6726c96e4379999ad
Rhian Moraes <rhian.garcia@codeminer42.com>
2021-06-24 12:32:59 -0300

    Add option to add notes to followups

--- app/javascript/src/case_contact.js
+++ app/javascript/src/case_contact.js
@@ -120,6 +120,44 @@ window.onload = function () {
     validateDuration()
   }
 }
+
+async function displayFollowupAlert() {
+  const { value: text, isConfirmed } = await fireSwalFollowupAlert();
+
+  if(!isConfirmed) return;
+
+  const params = text ? { note: text } : {}
+  const caseContactId  = this.id.replace('followup-button-', '');
+
+  $.post(
+    `/case_contacts/${caseContactId}/followups`,
+    params,
+    () => location.reload()
+  );
+}
+
+async function fireSwalFollowupAlert() {
+  const inputLabel = 'Optional: Add a note about what followup is needed.'
+
+  return await Swal.fire({
+    input: 'textarea',
+    inputLabel: inputLabel,
+    inputPlaceholder: 'Type your note here...',
+    inputAttributes: { 'aria-label': 'Type your note here' },
+
+    showCancelButton: true,
+    showCloseButton: true,
+
+    confirmButtonText: 'Confirm',
+    confirmButtonColor: '#dc3545',
+
+    customClass: {
+      inputLabel: 'mx-5'
+    }
+  })
+}
+
 $('document').ready(() => {
   $('[data-toggle="tooltip"]').tooltip()
+  $('.followup-button').on('click', displayFollowupAlert)
 })
