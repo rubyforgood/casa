a3d78e0da0e8a6af8053d78d1cf1dab6d19dd726
aedwardg <aedwardg1@gmail.com>
2021-07-05 01:34:32 -0700

    Add Sent Email view tests

--- spec/factories/sent_emails.rb
+++ spec/factories/sent_emails.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :sent_email do
     association :user, factory: :user
     casa_org { CasaOrg.first || create(:casa_org) }
-    mailer_type { "MyString" }
-    category { "MyString" }
-    sent_address { "MyString" }
+    mailer_type { "Spec Test Mailer Type" }
+    category { "Spec Test Mail Action Category" }
+    sent_address { user.email }
   end
 end
--- spec/views/layouts/sidebar.html.erb_spec.rb
+++ spec/views/layouts/sidebar.html.erb_spec.rb
@@ -162,6 +162,8 @@ RSpec.describe "layout/sidebar", :disable_bullet, type: :view do
       expect(rendered).to_not have_link("Case Contacts", href: "/case_contacts")
       expect(rendered).to have_link("Supervisors", href: "/supervisors")
       expect(rendered).to have_link("Admins", href: "/casa_admins")
+      expect(rendered).to have_link("System Imports", href: "/imports")
+      expect(rendered).to have_link("Edit Organization", href: "/casa_orgs/#{user.casa_org.id}/edit")
       expect(rendered).to have_link("Report a site issue", href: "https://rubyforgood.typeform.com/to/iXY4BubB")
       expect(rendered).to have_link("Generate Court Reports", href: "/case_court_reports")
       expect(rendered).to have_link("Export Data", href: "/reports")
