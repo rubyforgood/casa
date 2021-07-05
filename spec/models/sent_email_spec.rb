b379b53f1ba584c3fb192af059d517a15b36e21e
aedwardg <aedwardg1@gmail.com>
2021-07-04 18:59:55 -0700

    Add initial Model tests

--- spec/models/sent_email_spec.rb
+++ spec/models/sent_email_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe SentEmail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:casa_org) }
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to validate_presence_of(:mailer_type) }
+  it { is_expected.to validate_presence_of(:category) }
+  it { is_expected.to validate_presence_of(:sent_address) }
 end
