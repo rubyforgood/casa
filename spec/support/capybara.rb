7ea9042aa5301b26f0749dc2fb58da874f4bee1a
CW <compiledwrong+github@gmail.com>
2021-07-07 18:06:32 -0700

    capybara tuning

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,16 +7,16 @@ Capybara.register_driver :selenium_chrome_in_container do |app|
   Capybara::Selenium::Driver.new app,
     browser: :remote,
     url: "http://selenium_chrome:4444/wd/hub",
-    desired_capabilities: :chrome
+    capabilities: [:chrome]
 end
 
 Capybara.register_driver :selenium_chrome_headless_in_container do |app|
   Capybara::Selenium::Driver.new app,
     browser: :remote,
     url: "http://selenium_chrome:4444/wd/hub",
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: {args: %w[headless disable-gpu --window-size=1280,900]}
-    )
+    capabilities: [Selenium::WebDriver::Remote::Capabilities.chrome(
+      "goog:chromeOptions" => {"args" => %w[headless disable-gpu window-size=1280,900]}
+    )]
 end
 
 Capybara.register_driver :selenium_chrome_headless do |app|
