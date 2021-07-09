1903b97af3632afb6570ea3a8ce49080b4e29154
Rhian Moraes <rhian.garcia@codeminer42.com>
2021-07-09 13:53:30 -0300

    Refactor spec/system/supervisors/index_spec.rb

--- spec/system/supervisors/index_spec.rb
+++ spec/system/supervisors/index_spec.rb
@@ -1,51 +1,52 @@
 require "rails_helper"
 
 RSpec.describe "supervisors/index", :disable_bullet, type: :system do
+  shared_examples_for "functioning sort buttons" do
+    it "sorts table columns" do
+      expect(page).to have_selector("tr:nth-child(1)", text: expected_first_ordered_value)
+
+      find("th", text: column_to_sort).click
+
+      expect(page).to have_selector("th.sorting_asc", text: column_to_sort)
+      expect(page).to have_selector("tr:nth-child(1)", text: expected_last_ordered_value)
+    end
+  end
+
   let(:organization) { create(:casa_org) }
-  let(:user) { create(:supervisor, casa_org: organization) }
+  let(:supervisor_user) { create(:supervisor, casa_org: organization) }
 
-  it "can edit supervisor by clicking on the edit link from the supervisors list page" do
-    supervisor_name = "Leslie Knope"
-    create(:supervisor, display_name: supervisor_name, casa_org: organization)
-    sign_in user
+  before { sign_in supervisor_user }
 
-    visit supervisors_path
+  context "when editing supervisor" do
+    let(:supervisor_name) { "Leslie Knope" }
+    let!(:supervisor) { create(:supervisor, display_name: supervisor_name, casa_org: organization) }
 
-    expect(page).to have_text(supervisor_name)
+    before { visit supervisors_path }
 
-    within "#supervisors" do
-      click_on "Edit", match: :first
+    it "can edit supervisor by clicking on the edit link from the supervisors list page" do
+      expect(page).to have_text(supervisor_name)
+
+      within "#supervisors" do
+        click_on "Edit", match: :first
+      end
+
+      expect(page).to have_text("Editing Supervisor")
     end
 
-    expect(page).to have_text("Editing Supervisor")
+    it "can edit supervisor by clicking on the supervisor's name from the supervisors list page" do
+      expect(page).to have_text(supervisor_name)
+
+      within "#supervisors" do
+        click_on supervisor_name
+      end
+
+      expect(page).to have_text("Editing Supervisor")
+    end
   end
 
-  it "can edit supervisor by clicking on the supervisor's name from the supervisors list page" do
-    supervisor_name = "Leslie Knope"
-    create(:supervisor, display_name: supervisor_name, casa_org: organization)
-    sign_in user
-
-    visit supervisors_path
-
-    within "#supervisors" do
-      click_on supervisor_name, match: :first
-    end
-
-    expect(page).to have_text("Editing Supervisor")
-  end
-
-  describe "can sort table columns" do
-    def verify_numeric_sort(column)
-      expect(page).to have_selector("tr:nth-child(1)", text: "11")
-
-      find("th", text: column).click
-
-      expect(page).to have_selector("th.sorting_asc", text: column)
-      expect(page).to have_selector("tr:nth-child(1)", text: "9")
-    end
-
+  describe "supervisor table" do
     let!(:first_supervisor) { create(:supervisor, display_name: "First Supervisor", casa_org: organization) }
-    let!(:last_supervisor) { create(:supervisor, display_name: "Last Supervisor", casa_org: organization) }
+    let!(:last_supervisor)  { create(:supervisor, display_name: "Last Supervisor",  casa_org: organization) }
 
     before(:each) do
       # Stub our `@supervisors` collection so we've got control over column values for sorting.
@@ -61,61 +62,75 @@ RSpec.describe "supervisors/index", :disable_bullet, type: :system do
       allow(last_supervisor).to receive(:volunteers_serving_transition_aged_youth).and_return(11)
       allow(last_supervisor).to receive(:no_contact_for_two_weeks).and_return(11)
 
-      sign_in user
+      sign_in supervisor_user
       visit supervisors_path
     end
 
-    it "by supervisor name", js: true do
-      expect(page).to have_selector("th.sorting_desc", text: "Supervisor Name")
-      expect(page).to have_selector("tr:nth-child(1)", text: "Last Supervisor")
+    context "when sorting supervisors" do
+      let(:expected_first_ordered_value) { "11" }
+      let(:expected_last_ordered_value)  { "9" }
 
-      find("th", text: "Supervisor Name").click
+      it "by supervisor name", js: true do
+        expect(page).to have_selector("th.sorting_desc", text: "Supervisor Name")
+        expect(page).to have_selector("tr:nth-child(1)", text: "Last Supervisor")
 
-      expect(page).to have_selector("th.sorting_asc", text: "Supervisor Name")
-      expect(page).to have_selector("tr:nth-child(1)", text: "First Supervisor")
-    end
+        find("th", text: "Supervisor Name").click
 
-    it "by volunteer count", js: true do
-      verify_numeric_sort("Volunteer Assignments")
-    end
+        expect(page).to have_selector("th.sorting_asc", text: "Supervisor Name")
+        expect(page).to have_selector("tr:nth-child(1)", text: "First Supervisor")
+      end
 
-    it "by transition-aged youth", js: true do
-      verify_numeric_sort("Serving Transition Aged Youth")
-    end
+      describe "by volunteer count", js: true do
+        let(:column_to_sort) { "Volunteer Assignments" }
 
-    it "by no-contact count", js: true do
-      verify_numeric_sort("No Contact (14 days)")
+        it_behaves_like "functioning sort buttons"
+      end
+
+      describe "by transition-aged youth", js: true do
+        let(:column_to_sort) { "Serving Transition Aged Youth" }
+
+        it_behaves_like "functioning sort buttons"
+      end
+
+      describe "by no-contact count", js: true do
+        let(:column_to_sort) { "No Contact (14 days)" }
+
+        it_behaves_like "functioning sort buttons"
+      end
     end
 
     context "with unassigned volunteers" do
-      let!(:unassigned_volunteer) { create(:volunteer, casa_org: organization, display_name: "Tony Ruiz") }
+      let(:unassigned_volunteer_name) { "Tony Ruiz" }
+      let!(:unassigned_volunteer) { create(:volunteer, casa_org: organization, display_name: unassigned_volunteer_name) }
 
       before do
-        sign_in user
+        sign_in supervisor_user
         visit supervisors_path
       end
 
       it "will show a list of unassigned volunteers" do
         expect(page).to have_text("Active volunteers not assigned to supervisors")
         expect(page).to have_text("Assigned to Case(s)")
-        expect(page).to have_text("Tony Ruiz")
+        expect(page).to have_text(unassigned_volunteer_name)
+
         expect(page).not_to have_text("There are no unassigned volunteers")
       end
 
       it "links to edit page of volunteer" do
-        click_on("Tony Ruiz")
-        expect(page).to have_current_path("/volunteers/#{Volunteer.find_by(display_name: "Tony Ruiz").id}/edit")
+        click_on unassigned_volunteer_name
+        expect(page).to have_current_path("/volunteers/#{unassigned_volunteer.id}/edit")
       end
     end
 
     context "without unassigned volunteers" do
       before do
-        sign_in user
+        sign_in supervisor_user
         visit supervisors_path
       end
 
       it "will not show a list of volunteers not assigned to supervisors" do
         expect(page).to have_text("There are no active volunteers without supervisors to display here")
+
         expect(page).not_to have_text("Active volunteers not assigned to supervisors")
         expect(page).not_to have_text("Assigned to Case(s)")
       end
@@ -123,19 +138,27 @@ RSpec.describe "supervisors/index", :disable_bullet, type: :system do
   end
 
   context "with inactive volunteers assigned" do
-    before do
+    let!(:active_volunteer) do
       volunteer = create(:volunteer, :with_casa_cases, casa_org: organization)
-      FactoryBot.create(:supervisor_volunteer, supervisor: user, volunteer: volunteer)
+      FactoryBot.create(:supervisor_volunteer, supervisor: supervisor_user, volunteer: volunteer)
 
-      inactive_volunteer = create(:volunteer, :inactive, casa_org: organization)
-      FactoryBot.create(:supervisor_volunteer, supervisor: user, volunteer: inactive_volunteer)
+      volunteer
+    end
 
-      sign_in user
+    let!(:inactive_volunteer) do
+      volunteer = create(:volunteer, :inactive, casa_org: organization)
+      FactoryBot.create(:supervisor_volunteer, supervisor: supervisor_user, volunteer: volunteer)
+
+      volunteer
+    end
+
+    before do
+      sign_in supervisor_user
       visit supervisors_path
     end
 
     it "count only active volunteers" do
-      within "td#volunteer-assignments-#{user.id}" do
+      within "td#volunteer-assignments-#{supervisor_user.id}" do
         expect(page).to have_content("1")
       end
     end
