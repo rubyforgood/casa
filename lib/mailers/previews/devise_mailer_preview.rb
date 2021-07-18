681ecc44fa803a9509ecca6eaa8ea377a6a61f7b
firelemons <shenyang64@gmail.com>
2021-07-17 23:17:51 -0500

    guarantee preview works without altering test data

--- lib/mailers/previews/devise_mailer_preview.rb
+++ lib/mailers/previews/devise_mailer_preview.rb
@@ -5,8 +5,15 @@ class DeviseMailerPreview < ActionMailer::Preview
     Devise::Mailer.reset_password_instructions(User.first, "faketoken")
   end
 
-  def invitation_instructions
-    Devise::Mailer.invitation_instructions(AllCasaAdmin.first, "faketoken")
+  def all_casa_admin_invitation_instructions
+    all_casa_admin_invitation_sent_at = AllCasaAdmin.first.invitation_sent_at
+
+    # Temporarily set :invitation_sent_at to guarantee the preview works
+    AllCasaAdmin.first.update_attribute(:invitation_sent_at, Date.today)
+    preview = Devise::Mailer.invitation_instructions(AllCasaAdmin.first, "faketoken")
+    AllCasaAdmin.first.update_attribute(:invitation_sent_at, all_casa_admin_invitation_sent_at)
+
+    preview
   end
 end
 # :nocov:
