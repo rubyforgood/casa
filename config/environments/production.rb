2f1688c6de559ab5a7c600bbeb37848ded91ee32
firelemons <shenyang64@gmail.com>
2021-07-22 11:39:27 -0500

    attempt 4 at non prod previews

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: ENV["DOMAIN"]}
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.show_previews = ENV["APP_ENVIRONMENT"] != "production"
   # Do not send emails in staging or qa
   config.action_mailer.perform_deliveries = ENV["APP_ENVIRONMENT"] == "production"
   config.action_mailer.delivery_method = :smtp
