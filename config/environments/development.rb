4aa01af84df9533f8a4f2308726b1d9d3df4ed06
firelemons <shenyang64@gmail.com>
2021-07-21 22:33:37 -0500

    attempt 2 previews in non prod envs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,12 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.preview_path ||= defined?(Rails.root) ? Rails.root.join("lib", "mailers", "previews") : nil
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.show_previews = true
 
