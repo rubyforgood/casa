bbb6871cad4f90d63cf53cc26f1c04ec352b15ba
efgalvao <solfieri@hotmail.com>
2021-07-25 12:45:28 -0300

    Changes resend_invitation method

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -74,7 +74,7 @@ class VolunteersController < ApplicationController
   def resend_invitation
     authorize @volunteer
     @volunteer = Volunteer.find(params[:id])
-    if @volunteer.invitation_accepted_at == nil
+    if @volunteer.invitation_accepted_at.nil?
       @volunteer.invite!(current_user)
 
       redirect_to edit_volunteer_path(@volunteer), notice: "Invitation sent"
