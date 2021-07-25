aabc62cbee7ec5f9f2f5e08df633828253e1519e
firelemons <shenyang64@gmail.com>
2021-07-25 01:23:08 -0500

    linter

--- lib/mailers/previews/devise_mailer_preview.rb
+++ lib/mailers/previews/devise_mailer_preview.rb
@@ -31,8 +31,9 @@ class DeviseMailerPreview < ActionMailer::Preview
   end
 
   private
+
   # Unused email types
-  
+
   def update_invitation_sent_at(model)
     # Set :invitation_sent_at to guarantee the preview works
     model.update_attribute(:invitation_sent_at, Date.today)
