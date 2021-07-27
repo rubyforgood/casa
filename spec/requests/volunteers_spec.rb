2020d7cd30ca73306e8ff20b0625a3afcbc4ef42
Libby Rodriguez <librod89@gmail.com>
2021-07-27 11:39:36 -0600

    fix unit test

--- spec/requests/volunteers_spec.rb
+++ spec/requests/volunteers_spec.rb
@@ -197,12 +197,12 @@ RSpec.describe "/volunteers", :disable_bullet, type: :request do
     end
   end
 
-  describe "PATCH /resend_invitation" do
+  describe "GET /resend_invitation" do
     before { sign_in admin }
     it "resends an invitation email" do
       expect(volunteer.invitation_created_at.present?).to eq(false)
 
-      patch resend_invitation_volunteer_path(volunteer)
+      get resend_invitation_volunteer_path(volunteer)
       volunteer.reload
 
       expect(volunteer.invitation_created_at.present?).to eq(true)
