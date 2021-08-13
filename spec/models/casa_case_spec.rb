8596fc7cf3a238535362687ab97954e0b97e1394
firelemons <shenyang64@gmail.com>
2021-08-13 10:52:33 -0500

    alphabetize tests

--- spec/models/casa_case_spec.rb
+++ spec/models/casa_case_spec.rb
@@ -38,27 +38,6 @@ RSpec.describe CasaCase, type: :model do
     end
   end
 
-  describe "#should_transition" do
-    it "returns only youth who should have transitioned but have not" do
-      not_transitioned_13_yo = create(:casa_case,
-        birth_month_year_youth: Date.current - 13.years,
-        transition_aged_youth: false)
-      transitioned_14_yo = create(:casa_case,
-        birth_month_year_youth: Date.current - 14.years,
-        transition_aged_youth: true)
-      not_transitioned_14_yo = create(:casa_case,
-        birth_month_year_youth: Date.current - 14.years,
-        transition_aged_youth: false)
-      cases = CasaCase.should_transition
-      aggregate_failures do
-        expect(cases.length).to eq 1
-        expect(cases.include?(not_transitioned_14_yo)).to eq true
-        expect(cases.include?(not_transitioned_13_yo)).to eq false
-        expect(cases.include?(transitioned_14_yo)).to eq false
-      end
-    end
-  end
-
   describe ".actively_assigned_to" do
     it "only returns cases actively assigned to a volunteer" do
       current_user = create(:volunteer)
@@ -108,7 +87,146 @@ RSpec.describe CasaCase, type: :model do
     end
   end
 
-  describe "#court_report_status=" do
+  describe ".available_for_volunteer" do
+    let(:casa_org) { create(:casa_org) }
+    let!(:casa_case1) { create(:casa_case, :with_case_assignments, case_number: "foo", casa_org: casa_org) }
+    let!(:casa_case2) { create(:casa_case, :with_case_assignments, case_number: "bar", casa_org: casa_org) }
+    let!(:casa_case3) { create(:casa_case, case_number: "baz", casa_org: casa_org) }
+    let!(:casa_case4) { create(:casa_case, casa_org: create(:casa_org)) }
+    let(:volunteer) { create(:volunteer, casa_org: casa_org) }
+
+    context "when volunteer has no case assignments" do
+      it "returns all cases in volunteer's organization" do
+        expect(described_class.available_for_volunteer(volunteer)).to eq [casa_case2, casa_case3, casa_case1]
+      end
+    end
+
+    context "when volunteer has case assignments" do
+      let(:volunteer2) { create(:volunteer, casa_org: casa_org) }
+      let(:casa_case) { create(:casa_case, casa_org: casa_org) }
+
+      it "returns cases to which volunteer is not assigned in same org" do
+        casa_case.volunteers << volunteer
+        casa_case.volunteers << volunteer2
+        expect(described_class.available_for_volunteer(volunteer)).to eq [casa_case2, casa_case3, casa_case1]
+      end
+    end
+  end
+
+  describe ".should_transition" do
+    it "returns only youth who should have transitioned but have not" do
+      not_transitioned_13_yo = create(:casa_case,
+        birth_month_year_youth: Date.current - 13.years,
+        transition_aged_youth: false)
+      transitioned_14_yo = create(:casa_case,
+        birth_month_year_youth: Date.current - 14.years,
+        transition_aged_youth: true)
+      not_transitioned_14_yo = create(:casa_case,
+        birth_month_year_youth: Date.current - 14.years,
+        transition_aged_youth: false)
+      cases = CasaCase.should_transition
+      aggregate_failures do
+        expect(cases.length).to eq 1
+        expect(cases.include?(not_transitioned_14_yo)).to eq true
+        expect(cases.include?(not_transitioned_13_yo)).to eq false
+        expect(cases.include?(transitioned_14_yo)).to eq false
+      end
+    end
+  end
+
+  describe "#active_case_assignments" do
+    it "only includes active assignments" do
+      casa_org = create(:casa_org)
+      casa_case = create(:casa_case, casa_org: casa_org)
+      case_assignments = 2.times.map { create(:case_assignment, casa_case: casa_case, volunteer: create(:volunteer, casa_org: casa_org)) }
+
+      expect(casa_case.active_case_assignments).to eq case_assignments
+
+      case_assignments.first.update(active: false)
+      expect(casa_case.reload.active_case_assignments).to eq [case_assignments.last]
+    end
+  end
+
+  context "#add_emancipation_category" do
+    let(:casa_case) { create(:casa_case) }
+    let(:emancipation_category) { create(:emancipation_category) }
+
+    it "associates an emacipation category with the case when passed the id of the category" do
+      expect {
+        casa_case.add_emancipation_category(emancipation_category.id)
+      }.to change { casa_case.emancipation_categories.count }.from(0).to(1)
+    end
+  end
+
+  context "#add_emancipation_option" do
+    let(:casa_case) { create(:casa_case) }
+    let(:emancipation_category) { create(:emancipation_category, mutually_exclusive: true) }
+    let(:emancipation_option_a) { create(:emancipation_option, emancipation_category: emancipation_category) }
+    let(:emancipation_option_b) { create(:emancipation_option, emancipation_category: emancipation_category, name: "Not the same name as option A to satisfy unique contraints") }
+
+    it "associates an emacipation option with the case when passed the id of the option" do
+      expect {
+        casa_case.add_emancipation_option(emancipation_option_a.id)
+      }.to change { casa_case.emancipation_options.count }.from(0).to(1)
+    end
+
+    it "raises an error when attempting to add multiple options belonging to a mutually exclusive category" do
+      expect {
+        casa_case.add_emancipation_option(emancipation_option_a.id)
+        casa_case.add_emancipation_option(emancipation_option_b.id)
+      }.to raise_error("Attempted adding multiple options belonging to a mutually exclusive category")
+    end
+  end
+
+  describe "#assigned_volunteers" do
+    let(:casa_org) { create(:casa_org) }
+    let(:casa_case) { create(:casa_case, casa_org: casa_org) }
+    let(:volunteer1) { create(:volunteer, casa_org: casa_org) }
+    let(:volunteer2) { create(:volunteer, casa_org: casa_org) }
+    let!(:case_assignment1) { create(:case_assignment, casa_case: casa_case, volunteer: volunteer1) }
+    let!(:case_assignment2) { create(:case_assignment, casa_case: casa_case, volunteer: volunteer2) }
+
+    it "only includes volunteers through active assignments" do
+      expect(casa_case.assigned_volunteers.order(:id)).to eq [volunteer1, volunteer2].sort_by(&:id)
+
+      case_assignment1.update(active: false)
+      expect(casa_case.reload.assigned_volunteers).to eq [volunteer2]
+    end
+
+    it "only includes active volunteers" do
+      expect(casa_case.assigned_volunteers.order(:id)).to eq [volunteer1, volunteer2].sort_by(&:id)
+
+      volunteer1.update(active: false)
+      expect(casa_case.reload.assigned_volunteers).to eq [volunteer2]
+    end
+  end
+
+  describe "#clear_court_dates" do
+    context "when court date has passed" do
+      it "clears court date" do
+        casa_case = create(:casa_case, court_date: "2020-09-13 02:11:58")
+        casa_case.clear_court_dates
+
+        expect(casa_case.court_date).to be nil
+      end
+
+      it "clears report due date" do
+        casa_case = create(:casa_case, court_date: "2020-09-13 02:11:58", court_report_due_date: "2020-09-13 02:11:58")
+        casa_case.clear_court_dates
+
+        expect(casa_case.court_report_due_date).to be nil
+      end
+
+      it "sets court report as unsubmitted" do
+        casa_case = create(:casa_case, court_date: "2020-09-13 02:11:58", court_report_status: :submitted)
+        casa_case.clear_court_dates
+
+        expect(casa_case.court_report_status).to eq "not_submitted"
+      end
+    end
+  end
+
+  describe "#court_report_status" do
     let(:casa_case) { build(:casa_case) }
     subject { casa_case.court_report_status = court_report_status }
 
@@ -166,63 +284,6 @@ RSpec.describe CasaCase, type: :model do
     end
   end
 
-  describe ".available_for_volunteer" do
-    let(:casa_org) { create(:casa_org) }
-    let!(:casa_case1) { create(:casa_case, :with_case_assignments, case_number: "foo", casa_org: casa_org) }
-    let!(:casa_case2) { create(:casa_case, :with_case_assignments, case_number: "bar", casa_org: casa_org) }
-    let!(:casa_case3) { create(:casa_case, case_number: "baz", casa_org: casa_org) }
-    let!(:casa_case4) { create(:casa_case, casa_org: create(:casa_org)) }
-    let(:volunteer) { create(:volunteer, casa_org: casa_org) }
-
-    context "when volunteer has no case assignments" do
-      it "returns all cases in volunteer's organization" do
-        expect(described_class.available_for_volunteer(volunteer)).to eq [casa_case2, casa_case3, casa_case1]
-      end
-    end
-
-    context "when volunteer has case assignments" do
-      let(:volunteer2) { create(:volunteer, casa_org: casa_org) }
-      let(:casa_case) { create(:casa_case, casa_org: casa_org) }
-
-      it "returns cases to which volunteer is not assigned in same org" do
-        casa_case.volunteers << volunteer
-        casa_case.volunteers << volunteer2
-        expect(described_class.available_for_volunteer(volunteer)).to eq [casa_case2, casa_case3, casa_case1]
-      end
-    end
-  end
-
-  context "#add_emancipation_category" do
-    let(:casa_case) { create(:casa_case) }
-    let(:emancipation_category) { create(:emancipation_category) }
-
-    it "associates an emacipation category with the case when passed the id of the category" do
-      expect {
-        casa_case.add_emancipation_category(emancipation_category.id)
-      }.to change { casa_case.emancipation_categories.count }.from(0).to(1)
-    end
-  end
-
-  context "#add_emancipation_option" do
-    let(:casa_case) { create(:casa_case) }
-    let(:emancipation_category) { create(:emancipation_category, mutually_exclusive: true) }
-    let(:emancipation_option_a) { create(:emancipation_option, emancipation_category: emancipation_category) }
-    let(:emancipation_option_b) { create(:emancipation_option, emancipation_category: emancipation_category, name: "Not the same name as option A to satisfy unique contraints") }
-
-    it "associates an emacipation option with the case when passed the id of the option" do
-      expect {
-        casa_case.add_emancipation_option(emancipation_option_a.id)
-      }.to change { casa_case.emancipation_options.count }.from(0).to(1)
-    end
-
-    it "raises an error when attempting to add multiple options belonging to a mutually exclusive category" do
-      expect {
-        casa_case.add_emancipation_option(emancipation_option_a.id)
-        casa_case.add_emancipation_option(emancipation_option_b.id)
-      }.to raise_error("Attempted adding multiple options belonging to a mutually exclusive category")
-    end
-  end
-
   context "#remove_emancipation_category" do
     let(:casa_case) { create(:casa_case) }
     let(:emancipation_category) { create(:emancipation_category) }
@@ -264,67 +325,6 @@ RSpec.describe CasaCase, type: :model do
 
       expect(casa_case.casa_case_contact_types.count).to be 1
       expect(casa_case.contact_types.reload).to match_array([type2])
-    end
-  end
-
-  describe "#clear_court_dates" do
-    context "when court date has passed" do
-      it "clears court date" do
-        casa_case = create(:casa_case, court_date: "2020-09-13 02:11:58")
-        casa_case.clear_court_dates
-
-        expect(casa_case.court_date).to be nil
-      end
-
-      it "clears report due date" do
-        casa_case = create(:casa_case, court_date: "2020-09-13 02:11:58", court_report_due_date: "2020-09-13 02:11:58")
-        casa_case.clear_court_dates
-
-        expect(casa_case.court_report_due_date).to be nil
-      end
-
-      it "sets court report as unsubmitted" do
-        casa_case = create(:casa_case, court_date: "2020-09-13 02:11:58", court_report_status: :submitted)
-        casa_case.clear_court_dates
-
-        expect(casa_case.court_report_status).to eq "not_submitted"
-      end
-    end
-  end
-
-  describe "#active_case_assignments" do
-    it "only includes active assignments" do
-      casa_org = create(:casa_org)
-      casa_case = create(:casa_case, casa_org: casa_org)
-      case_assignments = 2.times.map { create(:case_assignment, casa_case: casa_case, volunteer: create(:volunteer, casa_org: casa_org)) }
-
-      expect(casa_case.active_case_assignments).to eq case_assignments
-
-      case_assignments.first.update(active: false)
-      expect(casa_case.reload.active_case_assignments).to eq [case_assignments.last]
-    end
-  end
-
-  describe "#assigned_volunteers" do
-    let(:casa_org) { create(:casa_org) }
-    let(:casa_case) { create(:casa_case, casa_org: casa_org) }
-    let(:volunteer1) { create(:volunteer, casa_org: casa_org) }
-    let(:volunteer2) { create(:volunteer, casa_org: casa_org) }
-    let!(:case_assignment1) { create(:case_assignment, casa_case: casa_case, volunteer: volunteer1) }
-    let!(:case_assignment2) { create(:case_assignment, casa_case: casa_case, volunteer: volunteer2) }
-
-    it "only includes volunteers through active assignments" do
-      expect(casa_case.assigned_volunteers.order(:id)).to eq [volunteer1, volunteer2].sort_by(&:id)
-
-      case_assignment1.update(active: false)
-      expect(casa_case.reload.assigned_volunteers).to eq [volunteer2]
-    end
-
-    it "only includes active volunteers" do
-      expect(casa_case.assigned_volunteers.order(:id)).to eq [volunteer1, volunteer2].sort_by(&:id)
-
-      volunteer1.update(active: false)
-      expect(casa_case.reload.assigned_volunteers).to eq [volunteer2]
     end
   end
 
