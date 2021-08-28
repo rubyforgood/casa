ce94d3ecc5ce68100fbd610c38ea1622cba2aeff
firelemons <shenyang64@gmail.com>
2021-08-28 14:54:55 -0500

    created test stubs and prepared document body

---
+++ app/javascript/__tests__/notifier.test.js
@@ -0,0 +1,30 @@
+/* eslint-env jest */
+
+require('jest')
+
+test('notify should display a green notification when passed a message and level=\'info\'', () => {
+  document.body.innerHTML =
+    `<div id="async-notifications">
+      <div id="async-waiting-indicator" style="display: none">
+        Saving <div class="load-spinner"></div>
+      </div>
+      <div id="async-success-indicator" class="async-success-indicator" style="display: none">
+        Saved
+      </div>
+    </div>`
+
+  require('../src/case_contact')
+
+  expect(() => {
+    window.onload()
+  }).not.toThrow()
+})
+
+test('notify should display a red notification when passed a message and level=\'error\'', () => {
+})
+
+test('notify should throw a RangeError when passed an unsupported message level', () => {
+})
+
+test('notify should throw a TypeError when param message is not a string', () => {
+})
