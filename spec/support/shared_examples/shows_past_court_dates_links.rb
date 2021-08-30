47977fa0245d56eb51922e0b6267249fb7248bd2
efgalvao <solfieri@hotmail.com>
2021-08-30 09:39:02 -0300

    Change test

--- spec/support/shared_examples/shows_past_court_dates_links.rb
+++ spec/support/shared_examples/shows_past_court_dates_links.rb
@@ -1,4 +1,12 @@
 shared_examples_for "shows past court dates links" do
+  let!(:newest_pcd) do
+    create(:past_court_date, date: DateTime.current - 5.days, casa_case: casa_case)
+  end
+
+  let!(:oldest_pcd) do
+    create(:past_court_date, date: DateTime.current - 10.days, casa_case: casa_case)
+  end
+
   let(:past_court_date_with_details) do
     create(:past_court_date, :with_court_details, casa_case: casa_case)
   end
@@ -21,7 +29,9 @@ shared_examples_for "shows past court dates links" do
   end
 
   it "past court dates are ordered" do
-    expect(page).to have_content((DateTime.current - 10.days).strftime("%B %-d, %Y").to_s)
+    visit casa_case_path(casa_case)
+
+    expect(page).to have_text((DateTime.current - 10.days).strftime("%B %-d, %Y").to_s)
     expect(page.body).to match /#{oldest_pcd.date.strftime('%B %-d, %Y')}.*#{newest_pcd.date.strftime('%B %-d, %Y')}/m
   end
 end
