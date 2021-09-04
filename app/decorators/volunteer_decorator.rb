c8d0e54445dc58e72acf813b4d0df8fff68198a9
aedwardg <aedwardg1@gmail.com>
2021-09-04 14:00:46 -0700

    Move cc checkbox text to i18n

--- app/decorators/volunteer_decorator.rb
+++ app/decorators/volunteer_decorator.rb
@@ -3,11 +3,9 @@ class VolunteerDecorator < UserDecorator
 
   def cc_reminder_text
     if h.current_user.supervisor?
-      checkbox_text = "Send CC to Supervisor"
+      h.t("volunteers.send_reminder_button.supervisor_checkbox_text")
     elsif h.current_user.casa_admin?
-      checkbox_text = "Send CC to Supervisor and Admin"
+      h.t("volunteers.send_reminder_button.admin_checkbox_text")
     end
-
-    checkbox_text
   end
 end
