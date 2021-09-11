4d2e6bfacb01a6d93c4a0d14c973b5bcb54f5e9d
Shen Yang <shenyang64@gmail.com>
2021-09-11 12:25:04 -0500

    grouped notify tests, added test for startAsyncOPeration

--- app/javascript/__tests__/notifier.test.js
+++ app/javascript/__tests__/notifier.test.js
@@ -22,92 +22,111 @@ beforeEach(() => {
   })
 })
 
-test("notify should display a green notification when passed a message and level='info'", (done) => {
-  const notificationMessage = "'Y$deH[|%ROii]jy"
+describe('notify', () => {
+  test("notify should display a green notification when passed a message and level='info'", (done) => {
+    const notificationMessage = "'Y$deH[|%ROii]jy"
 
-  $(document).ready(() => {
-    notifier.notify(notificationMessage, 'info')
+    $(document).ready(() => {
+      notifier.notify(notificationMessage, 'info')
 
-    try {
-      const successMessages = asyncNotificationsElement.find('.async-success-indicator')
+      try {
+        const successMessages = asyncNotificationsElement.find('.async-success-indicator')
 
-      // Notifications contain the "Saved" message and the new message
-      expect(successMessages.length).toBe(2)
-      expect(successMessages[1].innerHTML).toContain(notificationMessage)
-      done()
-    } catch (error) {
-      done(error)
-    }
+        // Notifications contain the "Saved" message and the new message
+        expect(successMessages.length).toBe(2)
+        expect(successMessages[1].innerHTML).toContain(notificationMessage)
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
+  })
+
+  test("notify should display a red notification when passed a message and level='error'", (done) => {
+    const notificationMessage = '\\+!h0bbH"yN7dx9.'
+
+    $(document).ready(() => {
+      notifier.notify(notificationMessage, 'error')
+
+      try {
+        const failureMessages = asyncNotificationsElement.find('.async-failure-indicator')
+
+        expect(failureMessages.length).toBe(1)
+        expect(failureMessages[0].innerHTML).toContain(notificationMessage)
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
+  })
+
+  test('notify should append a dismissable message to the async-notifications widget', (done) => {
+    $(document).ready(() => {
+      notifier.notify('', 'error')
+      notifier.notify('', 'info')
+
+      try {
+        let failureMessages = asyncNotificationsElement.find('.async-failure-indicator')
+        let successMessages = asyncNotificationsElement.find('.async-success-indicator')
+
+        expect(failureMessages.length).toBe(1)
+        expect(successMessages.length).toBe(2)
+
+        failureMessages.children('button').click()
+        failureMessages = asyncNotificationsElement.find('.async-failure-indicator')
+
+        expect(failureMessages.length).toBe(0)
+
+        $(successMessages[1]).children('button').click()
+        successMessages = asyncNotificationsElement.find('.async-success-indicator')
+
+        expect(successMessages.length).toBe(1)
+
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
+  })
+
+  test('notify should throw a RangeError when passed an unsupported message level', (done) => {
+    $(document).ready(() => {
+      try {
+        expect(() => {
+          notifier.notify('message', 'unsupported level')
+        }).toThrow(RangeError)
+
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
+  })
+
+  test('notify should throw a TypeError when param message is not a string', (done) => {
+    $(document).ready(() => {
+      try {
+        expect(() => {
+          notifier.notify(6, 'info')
+        }).toThrow(TypeError)
+
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
   })
 })
 
-test("notify should display a red notification when passed a message and level='error'", (done) => {
-  const notificationMessage = '\\+!h0bbH"yN7dx9.'
-
+test('startAsyncOperation should display the loading toast', (done) => {
   $(document).ready(() => {
-    notifier.notify(notificationMessage, 'error')
+    const loadingToast = $('#async-waiting-indicator')
 
     try {
-      const failureMessages = asyncNotificationsElement.find('.async-failure-indicator')
+      expect(loadingToast.css('display')).toBe('none')
 
-      expect(failureMessages.length).toBe(1)
-      expect(failureMessages[0].innerHTML).toContain(notificationMessage)
-      done()
-    } catch (error) {
-      done(error)
-    }
-  })
-})
-
-test('notify should append a dismissable message to the async-notifications widget', (done) => {
-  $(document).ready(() => {
-    notifier.notify('', 'error')
-    notifier.notify('', 'info')
-
-    try {
-      let failureMessages = asyncNotificationsElement.find('.async-failure-indicator')
-      let successMessages = asyncNotificationsElement.find('.async-success-indicator')
-
-      expect(failureMessages.length).toBe(1)
-      expect(successMessages.length).toBe(2)
-
-      failureMessages.children('button').click()
-      failureMessages = asyncNotificationsElement.find('.async-failure-indicator')
-
-      expect(failureMessages.length).toBe(0)
-
-      $(successMessages[1]).children('button').click()
-      successMessages = asyncNotificationsElement.find('.async-success-indicator')
-
-      expect(successMessages.length).toBe(1)
-
-      done()
-    } catch (error) {
-      done(error)
-    }
-  })
-})
-
-test('notify should throw a RangeError when passed an unsupported message level', (done) => {
-  $(document).ready(() => {
-    try {
-      expect(() => {
-        notifier.notify('message', 'unsupported level')
-      }).toThrow(RangeError)
-
-      done()
-    } catch (error) {
-      done(error)
-    }
-  })
-})
-
-test('notify should throw a TypeError when param message is not a string', (done) => {
-  $(document).ready(() => {
-    try {
-      expect(() => {
-        notifier.notify(6, 'info')
-      }).toThrow(TypeError)
+      notifier.startAsyncOperation()
+      expect(loadingToast.attr('style')).toEqual(expect.not.stringContaining('display: none'))
 
       done()
     } catch (error) {
