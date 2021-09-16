c72d257bea79a2e81096101d3dba5ecd105f9946
firelemons <shenyang64@gmail.com>
2021-09-16 15:58:39 -0500

    created court order module

---
+++ app/javascript/src/court_order_list.js
@@ -0,0 +1,17 @@
+const $ = require('jquery')
+
+module.exports = class CourtOrderList {
+  // @param {object} courtOrdersWidget The div containing the list of court orders
+  constructor (courtOrdersWidget) {
+    this.courtOrdersWidget = courtOrdersWidget
+  }
+
+  // Adds a row containing a text field to write the court order and a dropdown to specify the order status
+  addCourtOrder () {
+  }
+  
+  // Removes a row of elements representing a single court order
+  removeCourtOrder () {
+  }
+}
+
