c64e7830091b79fedec4b7b51e76f1ac624a20f8
CW <compiledwrong+github@gmail.com>
2021-09-20 20:48:58 -0700

    refactor CaseContactsContactDates

--- app/services/case_contacts_contact_dates.rb
+++ app/services/case_contacts_contact_dates.rb
@@ -1,49 +1,22 @@
 class CaseContactsContactDates
-  attr_reader :case_contacts, :filtered_columns
-
-  def initialize(interviewees)
-    @interviewees = interviewees
+  def initialize(case_contact_contact_type)
+    @case_contact_contact_type = case_contact_contact_type
   end
 
   def contact_dates_details
-    contact_dates_as_hash = aggregate_contact_dates(@interviewees)
-    contact_dates_as_hash.map do |type, dates|
+    contact_type_names = @case_contact_contact_type.map(&:contact_type).map(&:name).uniq#.sort # TODO sort after refactor
+    contact_type_names.map do |contact_type_name|
       {
         name: "Names of persons involved, starting with the child's name",
-        type: type,
-        dates: dates.join(", ")
+        type: contact_type_name,
+        dates: @case_contact_contact_type.select { |ccct|
+          ccct.contact_type.name == contact_type_name
+        }.map(&:case_contact).sort_by { |case_contact|
+          case_contact.occurred_at
+        }.map { |case_contact|
+          CourtReportFormatContactDate.new(case_contact).format
+        }.join(", ")
       }
     end
-  end
-
-  private
-
-  def aggregate_contact_dates(people)
-    contact_dates = Hash.new([])
-
-    people.each do |person|
-      contact_type = person.contact_type.name
-      date_with_format = format_date_contact_attempt(person.case_contact)
-
-      contact_dates[contact_type] << (date_with_format) && next if contact_dates.key?(contact_type)
-
-      contact_dates[contact_type] = [date_with_format]
-    end
-
-    sort_dates(contact_dates)
-  end
-
-  def sort_dates(contact_dates)
-    contact_dates.each_value do |dates|
-      next if dates.size <= 1
-
-      # TODO fix - this will break on 1/01 vs 12/01
-      dates.sort! { |first_date, second_date| first_date.delete("*") <=> second_date.delete("*") }
-    end
-  end
-
-  def format_date_contact_attempt(case_contact)
-    I18n.l(case_contact.occurred_at, format: :short_date, default: nil)
-      .concat(case_contact.contact_made ? "" : "*")
   end
 end
