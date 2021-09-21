98965514e5428aaf0616f95fe506d0238ebf7524
CW <compiledwrong+github@gmail.com>
2021-09-20 21:03:03 -0700

    minor name fixes

--- app/services/case_contacts_contact_dates.rb
+++ app/services/case_contacts_contact_dates.rb
@@ -1,15 +1,15 @@
 class CaseContactsContactDates
-  def initialize(case_contact_contact_type)
-    @case_contact_contact_type = case_contact_contact_type
+  def initialize(case_contact_contact_types)
+    @case_contact_contact_types = case_contact_contact_types
   end
 
   def contact_dates_details
-    contact_type_names = @case_contact_contact_type.map(&:contact_type).map(&:name).uniq # .sort # TODO sort after refactor
+    contact_type_names = @case_contact_contact_types.map(&:contact_type).map(&:name).uniq # .sort # TODO sort after refactor
     contact_type_names.map do |contact_type_name|
       {
         name: "Names of persons involved, starting with the child's name",
         type: contact_type_name,
-        dates: @case_contact_contact_type.select { |ccct|
+        dates: @case_contact_contact_types.select { |ccct|
           ccct.contact_type.name == contact_type_name
         }.map(&:case_contact).sort_by { |case_contact|
           case_contact.occurred_at
