53591f9668477f82d87a3c2b26bfae16c59e9819
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2021-09-24 13:53:39 -0400

    Add service to render emancipation view to string [#2628]

---
+++ app/services/emancipation_checklist_download_html.rb
@@ -0,0 +1,17 @@
+class EmancipationChecklistDownloadHtml
+  def initialize(current_case, emancipation_form_data)
+    @current_case = current_case
+    @emancipation_form_data = emancipation_form_data
+  end
+
+  def call
+    html_body = ApplicationController.render(
+      template: "emancipations/download",
+      layout: false,
+      assigns: {
+        current_case: @current_case,
+        emancipation_form_data: @emancipation_form_data
+      },
+    )
+  end
+end
