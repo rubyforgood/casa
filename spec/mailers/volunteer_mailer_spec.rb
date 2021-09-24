eb3b8a2ba6f9403ba64bbdf79147bb7c6909beb9
Keith Walters <keith.walters@cattywamp.us>
2021-09-24 15:37:49 -0400

    Fix the remaining volunteer mailer tests
    Ensures that the VolunteerMailer tests work with the new changes to
    handling CC recipients and expands on the testing for sending reminders
    to volunteers in the VolunteerController Edit Spec.
    
    Co-authored-by: Amy Gurski <amygurski@gmail.com>

--- spec/mailers/volunteer_mailer_spec.rb
+++ spec/mailers/volunteer_mailer_spec.rb
@@ -26,24 +26,17 @@ RSpec.describe VolunteerMailer, type: :mailer do
   end
 
   describe ".case_contacts_reminder" do
-    let(:mail) { VolunteerMailer.case_contacts_reminder(volunteer, false) }
-    let(:mail_with_cc) { VolunteerMailer.case_contacts_reminder(volunteer_with_supervisor, true) }
-    let(:mail_with_cc_no_supervisor) { VolunteerMailer.case_contacts_reminder(volunteer, true) }
-
     it "sends an email reminding volunteer" do
+      mail = VolunteerMailer.case_contacts_reminder(volunteer, [])
       expect(mail.body.encoded).to match("Hello #{volunteer.display_name}")
       expect(mail.body.encoded).to match("as a reminder")
-      expect(mail.cc).to be_nil
+      expect(mail.cc).to be_empty
     end
 
-    it "sends a cc to supervisor" do
-      expect(mail_with_cc.cc).to include(volunteer_with_supervisor.supervisor.email.to_s)
-    end
-
-    it "does not fail with cc selected and no supervisor" do
-      expect(mail_with_cc_no_supervisor.body.encoded).to match("Hello #{volunteer.display_name}")
-      expect(mail_with_cc_no_supervisor.body.encoded).to match("as a reminder")
-      expect(mail_with_cc_no_supervisor.cc).to be_nil
+    it "sends and cc's recipients" do
+      cc_recipients = %w[supervisor@example.com admin@example.com]
+      mail = VolunteerMailer.case_contacts_reminder(volunteer, cc_recipients)
+      expect(mail.cc).to match_array(cc_recipients)
     end
   end
 
