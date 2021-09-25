637fe9a2c1dd8ec8afb7c51cb92eeb28a4fa72b0
Mauricio Lima <mauricio.lima@codeminer42.com>
2021-09-25 12:22:29 -0300

    feat: add SupervisorDatatable service

--- spec/factories/supervisor.rb
+++ spec/factories/supervisor.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :supervisor, class: "Supervisor", parent: :user do
+    display_name { Faker::Name.unique.name }
+
     trait :with_casa_cases do
       after(:create) do |user, _|
         create_list(:case_assignment, 2, volunteer: user)
