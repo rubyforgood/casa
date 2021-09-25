7326758b72436e823005ccd32ef04f6bc03bcb01
Amy Gurski <amygurski@gmail.com>
2021-09-25 12:02:49 -0400

    Autosave case contact form details
    Removes the save button on the case contact form. The case contact form
    notes are autosaving every 5 seconds instead. Also removes the alerts
    about the form data saving because they were distracting with the autosave.
    
    Co-authored-by: Jeanine Soterwood <jeanine@littleforestconsulting.com>

--- app/javascript/src/case_contact_autosave.js
+++ app/javascript/src/case_contact_autosave.js
@@ -1,42 +1,30 @@
 
 $('document').ready(() => {
   if ($('.case_contacts-new').length > 0) {
-    const formId = 'casa-contact-form' // ID of the form
-    const form = document.querySelector('#casa-contact-form')
-    const formIdentifier = `${formId}` // Identifier used to identify the form
-    const saveButton = document.querySelector('#case-contact-save') // select save button
-    const alertBox = document.querySelector('#case-contact-draft-alerts') // select alert display div
+    const formId = 'casa-contact-form'
+    const form = document.querySelector(`#${formId}`)
     const caseNotes = document.querySelector('#case_contact_notes')
 
-    saveButton.onclick = event => {
-      event.preventDefault()
-      const data = {}
-      data[formIdentifier] = caseNotes.value
-      window.localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]))
-      const message = 'Form draft has been saved!'
-      displayAlert(message)
+    const populateForm = () => {
+      if (window.localStorage.key(formId)) {
+        const savedData = JSON.parse(window.localStorage.getItem(formId)) // get and parse the saved data from localStorage
+        caseNotes.value = savedData
+      }
     }
 
-    const displayAlert = message => {
-      alertBox.innerText = message // add the message into the alert box
-      alertBox.style.display = 'block' // make the alert box visible
-      setTimeout(function () {
-        alertBox.style.display = 'none' // hide the alert box after 1 second
+    const autoSave = () => {
+      setInterval(function () {
+        const data = {}
+        data[formId] = caseNotes.value
+        window.localStorage.setItem(formId, JSON.stringify(data[formId]))
       }, 5000)
     }
 
-    const populateForm = () => {
-      if (window.localStorage.key(formIdentifier)) {
-        const savedData = JSON.parse(window.localStorage.getItem(formIdentifier)) // get and parse the saved data from localStorage
-        caseNotes.value = savedData
-        const message = 'Form has been refilled with saved data!'
-        displayAlert(message)
-      }
-    }
+    document.onload = autoSave()
     document.onload = populateForm() // populate the form when the document is loaded
 
     form.onsubmit = event => {
-      window.localStorage.removeItem(formIdentifier)
+      window.localStorage.removeItem(formId)
     }
   }
 })
