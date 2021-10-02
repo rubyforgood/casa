ff9e65a5c4f282d90cd12bd512733bbd57ff67ac
Libby Rodriguez <librod89@gmail.com>
2021-10-01 19:53:34 -0600

    Default sort judges alphabetically

--- app/models/judge.rb
+++ app/models/judge.rb
@@ -4,7 +4,7 @@ class Judge < ApplicationRecord
   belongs_to :casa_org
 
   validates :name, presence: true, uniqueness: {scope: %i[casa_org]}
-
+  default_scope { order(name: :asc) }
   scope :for_organization, ->(org) { where(casa_org: org) }
   scope :active, -> { where(active: true) }
 end
