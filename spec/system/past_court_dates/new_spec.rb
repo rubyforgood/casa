bcaf978b62a295afc154f2507d76a6635c1c67f1
Shen Yang <shenyang64@gmail.com>
2021-10-06 13:54:34 -0500

    update element id for tests

--- spec/system/past_court_dates/new_spec.rb
+++ spec/system/past_court_dates/new_spec.rb
@@ -31,8 +31,8 @@ RSpec.describe "past_court_dates/new", type: :system do
 
       find("#add-mandate-button").click
 
-      fill_in "casa_case_case_court_mandates_attributes_0_mandate_text", with: mandate_text
-      select "Partially implemented", from: "casa_case_case_court_mandates_attributes_0_implementation_status"
+      fill_in "past_court_date_case_court_mandates_attributes_0_mandate_text", with: mandate_text
+      select "Partially implemented", from: "past_court_date_case_court_mandates_attributes_0_implementation_status"
 
       within ".top-page-actions" do
         click_on "Create"
