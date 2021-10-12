5e91fa7500ea1593e918c0f81610cc3d83b59f0e
Andrew Kloecker <lt.kvothe@gmail.com>
2021-10-12 09:33:08 -0400

    Adds the case button state function tests
    I don't believe there is a way to run this in jest without exporting the
    functions from the casa_case.js file. If there is a way todo this
    without mutating said file Id love to learn!

--- app/javascript/__tests__/case_buttons.test.js
+++ app/javascript/__tests__/case_buttons.test.js
@@ -1,8 +1,47 @@
 require('jest')
 
-const caseButtons = require('../src/casa_case.js')
+import {
+  showBtn,
+  hideBtn,
+  disableBtn,
+  enableBtn
+} from "../src/casa_case"
 
-test("casa_case buttons work as intended", () => {
-  expect(1 + 1).toEqual(2)
+let button
+
+beforeEach(() => {
+  document.body.innerHTML =
+    `<button id="test-button">Disable Reports</button>`
+  button = document.getElementById("test-button")
+})
+
+describe("casa_case generate report button applies correct classes and attributes", () => {
+  test("show button", () => {
+    button.classList.add('d-none')
+    showBtn(button)
+    expect(button.classList.contains('d-none')).toBe(false)
+  })
+
+  test("hide button", () => {
+    hideBtn(button)
+    expect(button.classList.contains('d-none')).toBe(true)
+  })
+
+  test("disable button", () => {
+    disableBtn(button)
+    expect(button.classList.contains('disabled')).toBe(true)
+    expect(button.hasAttribute('aria-disabled')).toBe(true)
+    expect(button.disabled).toBe(true)
+  })
+
+  test("enable button", () => {
+    button.disabled = true
+    button.classList.add('disabled')
+    button.setAttribute('aria-disabled', true)
+    enableBtn(button)
+    expect(button.classList.contains('disabled')).toBe(false)
+    expect(button.hasAttribute('aria-disabled')).toBe(false)
+    expect(button.disabled).toBe(false)
+  })
 })
 
