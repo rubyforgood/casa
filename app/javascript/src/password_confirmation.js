ad554d292130161ba9e022b683bd94376db625a3
Andrew Kloecker <lt.kvothe@gmail.com>
2021-10-12 16:04:40 -0400

    Disables submit when passwords don't match

--- app/javascript/src/password_confirmation.js
+++ app/javascript/src/password_confirmation.js
@@ -1,11 +1,27 @@
+const button = document.getElementsByClassName("submit-password")[0]
+
+function disableBtn (el) {
+  el.disabled = true
+  el.classList.add('disabled')
+  el.setAttribute('aria-disabled', true)
+}
+
+function enableBtn (el) {
+  el.disabled = false
+  el.classList.remove('disabled')
+  el.removeAttribute('aria-disabled')
+}
+
 function addConfirmationEventListeners() {
   const password = document.getElementsByClassName("password-new")
   const confirmation = document.getElementsByClassName("password-confirmation")
 
+  disableBtn(button)
+
   if(confirmation.length === 0 || password.length === 0){
     console.error("Unable to find password confirmation field", { confirmation })
   }else{
-    document.addEventListener("keyUp", function(){
+    confirmation[0].addEventListener("keyup", function(){
       checkConfirmationEqual(password, confirmation)
     })
   }
@@ -15,8 +31,8 @@ function checkConfirmationEqual(password, confirmation){
   const passwordText = password[0].value
   const confirmationText = confirmation[0].value
   if (passwordText === confirmationText){
-    console.log("EQUAL")
+    enableBtn(button)
   }else{
-    console.log("UNEQUAL")
+    disableBtn(button)
   }
 }
