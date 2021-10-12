13f14d5dedf43bb025ec9a4493a486afe3ab1eea
Libby Rodriguez <librod89@gmail.com>
2021-10-12 13:35:57 -0600

    add migration to change casa case court date column to court date reference

---
+++ db/migrate/20211012180102_change_casa_cases_court_date_to_reference.rb
@@ -0,0 +1,12 @@
+class ChangeCasaCasesCourtDateToReference < ActiveRecord::Migration[6.1]
+  def up
+    CasaCase.find_each do |casa_case|
+      CourtDate.create(
+        date: casa_case.court_date,
+        casa_case: casa_case,
+        hearing_type: casa_case.hearing_type,
+        judge: casa_case.judge,
+      )
+    end
+  end
+end
