22e2fd5fc636e4cf8794b3773a174f2ac4b80bde
Andrew Kloecker <lt.kvothe@gmail.com>
2021-10-13 09:36:48 -0400

    Cleans up code
    This will make it easier to test the functionality

--- app/javascript/src/password_confirmation.js
+++ app/javascript/src/password_confirmation.js
@@ -1,24 +1,54 @@
+/* eslint-env jquery */
+
 import Swal from 'sweetalert2'
 
 import { disableBtn, enableBtn } from './casa_case'
 
-$('document').ready(() => {
-  $('.password-confirmation').on('blur', () => {
-    const button = document.getElementsByClassName('submit-password')[0]
-    const password = document.getElementsByClassName('password-new')
-    const confirmation = document.getElementsByClassName('password-confirmation')
-    const passwordText = password[0].value
-    const confirmationText = confirmation[0].value
+const SUBMIT_BUTTON_CLASS = 'submit-password'
+const PASSWORD_FIELD_CLASS = 'password-new'
+const CONFIRMATION_FIELD_CLASS = 'password-confirmation'
 
-    if (passwordText === confirmationText) {
-      enableBtn(button)
-    } else {
+// Checks if the password is equivalent to confirmation and has at least 1 character. If not,
+// it will disable the button
+//  @param    {HTMLElement}  button - submit button for the form field
+//  @param    {HTMLElement}  password - text input form field
+//  @param    {HTMLElement}  confirmation - text input form field
+//  @param    {boolean}  enablePopup - display popup when field is not in focus
+function checkPasswordsAndDisplayPopup (button, password, confirmation, enablePopup = false) {
+  const passwordText = password[0].value
+  const confirmationText = confirmation[0].value
+  if (passwordText === confirmationText && passwordText > 0) {
+    enableBtn(button)
+  } else {
+    if (enablePopup) {
       Swal.fire({
         icon: 'error',
         title: 'Password Error',
         text: 'The password and the confirmation password do not match'
       })
-      disableBtn(button)
     }
-  })
+    disableBtn(button)
+  }
+}
+
+$('document').ready(() => {
+  if ($(`.${SUBMIT_BUTTON_CLASS}`)) {
+    const button = $(`.${SUBMIT_BUTTON_CLASS}`)[0]
+    const password = $(`.${PASSWORD_FIELD_CLASS}`)
+    const confirmation = $(`.${CONFIRMATION_FIELD_CLASS}`)
+
+    disableBtn(button)
+
+    $(`.${PASSWORD_FIELD_CLASS}`).on('blur', () => {
+      checkPasswordsAndDisplayPopup(button, password, confirmation)
+    })
+
+    $(`.${CONFIRMATION_FIELD_CLASS}`).on('blur', () => {
+      checkPasswordsAndDisplayPopup(button, password, confirmation, true)
+    })
+  }
 })
+
+export {
+  checkPasswordsAndDisplayPopup
+}
