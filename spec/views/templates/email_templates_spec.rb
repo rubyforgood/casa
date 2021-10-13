4e94b08ad56aafc5416d04c078db10455d485f53
Dang Huynh <dhhuynh2@gmail.com>
2021-10-13 10:29:35 -0400

    2373 - Minor refactoring to remove reuse

--- spec/views/templates/email_templates_spec.rb
+++ spec/views/templates/email_templates_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.shared_examples 'compares opening and closing tags' do
     it "validates html tags" do
-        file_content = File.read(file_path)
+        file_content = File.read(Rails.root.join file_path)
         tags_are_equal = validate_closing_tags_exist(file_content)
         
         expect(tags_are_equal).to be true
@@ -11,13 +11,13 @@ RSpec.shared_examples 'compares opening and closing tags' do
 
 RSpec.describe "casa_admin_mailer", type: :view do
     describe "should validate that account_setup email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/casa_admin_mailer/account_setup.html.erb" }
+        let(:file_path) { "app/views/casa_admin_mailer/account_setup.html.erb" }
 
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that deactivation email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/casa_admin_mailer/deactivation.html.erb" }
+        let(:file_path) { "app/views/casa_admin_mailer/deactivation.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
@@ -25,37 +25,37 @@ end
 
 RSpec.describe "devise", type: :view do
     describe "should validate that confirmation_instructions email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/devise/mailer/confirmation_instructions.html.erb" }
+        let(:file_path) { "app/views/devise/mailer/confirmation_instructions.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that email_changed email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/devise/mailer/email_changed.html.erb" }
+        let(:file_path) { "app/views/devise/mailer/email_changed.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that invitation_instruction email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/devise/mailer/invitation_instructions.html.erb" }
+        let(:file_path) { "app/views/devise/mailer/invitation_instructions.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that password_change email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/devise/mailer/password_change.html.erb" }
+        let(:file_path) { "app/views/devise/mailer/password_change.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that reset_password_instructions email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/devise/mailer/reset_password_instructions.html.erb" }
+        let(:file_path) { "app/views/devise/mailer/reset_password_instructions.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that unlock_instructions email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/devise/mailer/unlock_instructions.html.erb" }
+        let(:file_path) { "app/views/devise/mailer/unlock_instructions.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
@@ -63,13 +63,13 @@ end
 
 RSpec.describe "supervisor_mailer", type: :view do
     describe "should validate that account_setup email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/supervisor_mailer/account_setup.html.erb" }
+        let(:file_path) { "app/views/supervisor_mailer/account_setup.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that weekly_digest email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/supervisor_mailer/weekly_digest.html.erb" }
+        let(:file_path) { "app/views/supervisor_mailer/weekly_digest.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
@@ -77,7 +77,7 @@ end
 
 RSpec.describe "user_mailer", type: :view do
     describe "should validate that password_changed_reminder email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/user_mailer/password_changed_reminder.html.erb" }
+        let(:file_path) { "app/views/user_mailer/password_changed_reminder.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
@@ -85,19 +85,19 @@ end
 
 RSpec.describe "volunteer_mailer", type: :view do
     describe "should validate that account_setup email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/volunteer_mailer/account_setup.html.erb" }
+        let(:file_path) { "app/views/volunteer_mailer/account_setup.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that case_contacts_reminder email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/volunteer_mailer/case_contacts_reminder.html.erb" }
+        let(:file_path) { "app/views/volunteer_mailer/case_contacts_reminder.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
 
     describe "should validate that court_report_reminder email template is valid" do
-        let(:file_path) { Rails.root.join "app/views/volunteer_mailer/court_report_reminder.html.erb" }
+        let(:file_path) { "app/views/volunteer_mailer/court_report_reminder.html.erb" }
         
         it_behaves_like "compares opening and closing tags"
     end
