8445dd728721de7f53008e08b7dc47a6cab678f7
Dang Huynh <dhhuynh2@gmail.com>
2021-10-13 10:39:48 -0400

    2373 - ruby linting fixes

--- app/helpers/template_helper.rb
+++ app/helpers/template_helper.rb
@@ -1,38 +1,38 @@
-module TemplateHelper 
-    def sanitize_opening_tags(opening_tags)
-        self_closing_tags = ["<%>", "<%=>", "<area>", "<base>", "<br>", "<col>", "<command>", "<!DOCTYPE>", "<embed>", "<hr>", "<img>", "<input>", "<keygen>", "<link>", "<meta>", "<param>", "<source>", "<track>", "<wbr>"]
+module TemplateHelper
+  def sanitize_opening_tags(opening_tags)
+    self_closing_tags = ["<%>", "<%=>", "<area>", "<base>", "<br>", "<col>", "<command>", "<!DOCTYPE>", "<embed>", "<hr>", "<img>", "<input>", "<keygen>", "<link>", "<meta>", "<param>", "<source>", "<track>", "<wbr>"]
 
-        opening_tags = opening_tags.map { |tag| 
-            if (tag.include? " ")
-                tag.squish.partition(" ").first << ">" 
-            else
-                tag
-            end
-        }
+    opening_tags = opening_tags.map { |tag|
+      if tag.include? " "
+        tag.squish.partition(" ").first << ">"
+      else
+        tag
+      end
+    }
 
-        opening_tags - self_closing_tags
-    end
+    opening_tags - self_closing_tags
+  end
 
-    def sanitize_closing_tags(closing_tags)
-        closing_tags.map { |tag|
-            if (tag.include? "\/")
-                tag.squish.tr('\/', '')
-            else
-                tag
-            end
-        }
-    end
+  def sanitize_closing_tags(closing_tags)
+    closing_tags.map { |tag|
+      if tag.include? "\/"
+        tag.squish.tr('\/', "")
+      else
+        tag
+      end
+    }
+  end
 
-    def validate_closing_tags_exist(file_content)
-        opening_regex = /<[^\/](?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])*>/
-        closing_regex = /<[\/](?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])*>/
+  def validate_closing_tags_exist(file_content)
+    opening_regex = /<[^\/](?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])*>/
+    closing_regex = /<\/(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])*>/
 
-        opening_tags = file_content.scan(opening_regex)
-        closing_tags = file_content.scan(closing_regex)
-        
-        opening_tags = sanitize_opening_tags(opening_tags)
-        closing_tags = sanitize_closing_tags(closing_tags)
-        
-        opening_tags.sort == closing_tags.sort
-    end
+    opening_tags = file_content.scan(opening_regex)
+    closing_tags = file_content.scan(closing_regex)
+
+    opening_tags = sanitize_opening_tags(opening_tags)
+    closing_tags = sanitize_closing_tags(closing_tags)
+
+    opening_tags.sort == closing_tags.sort
+  end
 end
