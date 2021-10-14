f9c1acfe882e2b9fb0a4c3359f1305ef749d39b4
Rob <robert.gentile@powerhrg.com>
2021-10-14 14:45:35 -0400

    mark cypress test as pending

--- cypress/integration/volunteer/edit_profile.spec.js
+++ cypress/integration/volunteer/edit_profile.spec.js
@@ -1,10 +1,10 @@
-context("Logging into cypress as a volunteer", () => {
+context.skip("Logging into cypress as a volunteer", () => {
   before(() => {
     cy.visit("0.0.0.0:4040");
     cy.loginAsVolunteer();
   });
   // TODO - fix flakey spec ref: https://github.com/rubyforgood/casa/issues/2550#issuecomment-943474980
-  it.skip("should edit the profile", () => {
+  it("should edit the profile", () => {
     cy.get("#toggle-sidebar-js").click();
     cy.contains("Edit Profile").click();
     cy.contains("Change Password").click();
