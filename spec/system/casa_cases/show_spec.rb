1a077d398979040d6445fcbc62055ebb65d4e3cc
Neha Abraham <neha.abraham@powerhrg.com>
2021-10-15 09:45:06 -0400

    add js for spec

--- spec/system/casa_cases/show_spec.rb
+++ spec/system/casa_cases/show_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe "casa_cases/show", type: :system do
       expect(page).to have_content(casa_case.case_court_orders[0].implementation_status_symbol)
     end
 
-    it "can see next court date" do
+    it "can see next court date", js: true do
       expect(page).to have_content("Next Court Date: #{I18n.l(future_court_date.date, format: :day_and_date)}")
       expect(page).to have_content("Add Court Report Due Date to Calendar")
       expect(page).to have_content("Add Next Court Date to Calendar")
