cdbc778eac08d410989ecb068c2124cda1bdaeeb
Neha Abraham <neha.abraham@powerhrg.com>
2021-10-15 11:18:17 -0400

    adjust specs

--- spec/controllers/reimbursements_controller_spec.rb
+++ spec/controllers/reimbursements_controller_spec.rb
@@ -1,8 +1,18 @@
 require "rails_helper"
 
 RSpec.describe ReimbursementsController, type: :controller do
-  describe "GET index" do
-    it "renders the index template" do
+let(:admin) { create(:casa_admin) }
+
+  before do
+    allow(controller).to receive(:authenticate_user!).and_return(:admin)
+  end
+
+  describe "as admin" do
+    before do
+      allow(controller).to receive(:current_user).and_return(admin)
+    end
+
+    it "can see reimbursements page" do
       get :index
       expect(response).to render_template("index")
     end
