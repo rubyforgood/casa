359de54f5f6a7f2eaa5e055bfb864e24c60390b2
firelemons <shenyang64@gmail.com>
2021-10-17 20:29:51 -0500

    remove login requirement for timestamp of latest deploy

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,4 +1,6 @@
 class HealthController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
     render json: {last_deploy_timestamp: Rails.application.config.latest_deploy_time}
   end
