45b17141a86f63c9e068f2b3f79e46345bc0137e
firelemons <shenyang64@gmail.com>
2021-10-19 07:28:41 -0500

    linter

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "Health", type: :request do
     end
 
     it "has key latest_deploy_time" do
+      hash_body = nil # This is here for the linter
       expect { hash_body = JSON.parse(response.body).with_indifferent_access }.not_to raise_exception
       expect(hash_body.keys).to match_array([:latest_deploy_time])
     end
