17bbe36769befdc7c9276e093b8170f5b702f89a
firelemons <shenyang64@gmail.com>
2021-10-23 18:38:38 -0500

    linter

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -4,7 +4,7 @@ class HealthController < ApplicationController
   def index
     unless Rails.application.config.respond_to?(:latest_deploy_time)
 
-      Rails.application.config.latest_deploy_time = JSON.parse(File.read(Rails.root.join("tmp", "deploy_time.json").to_s))['latest_deploy_time']
+      Rails.application.config.latest_deploy_time = JSON.parse(File.read(Rails.root.join("tmp", "deploy_time.json").to_s))["latest_deploy_time"]
     end
 
     render json: {latest_deploy_time: Rails.application.config.latest_deploy_time}
