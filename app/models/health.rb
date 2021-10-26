3f0a5954ffeada40621bd6ee4e3f9fc2726752ee
firelemons <shenyang64@gmail.com>
2021-10-25 22:06:48 -0500

    created method to access instance

--- app/models/health.rb
+++ app/models/health.rb
@@ -2,6 +2,10 @@ class Health < ApplicationRecord
   # The "singleton_guard" column is a unique column which must always be set to '0'
   # This ensures that only one Health row is created
   validates_inclusion_of :singleton_guard, :in => [0]
+
+  def instance
+    first_or_create!(singleton_guard: 0)
+  end
 end
 
 # == Schema Information
