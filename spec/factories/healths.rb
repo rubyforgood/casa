83001e996a5a47990cade6fa15e9600d1de8cf44
firelemons <shenyang64@gmail.com>
2021-10-27 21:11:20 -0500

    fixed invalid factory

--- spec/factories/healths.rb
+++ spec/factories/healths.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :health do
-    latest_deploy_time { "2021-10-25 09:37:09" }
-    singleton_guard { 1 }
+    latest_deploy_time { Time.now }
+    singleton_guard { 0 }
   end
 end
