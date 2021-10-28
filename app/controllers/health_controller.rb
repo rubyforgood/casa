e3aae35535284afe3fe740cb74143e7673b2956b
firelemons <shenyang64@gmail.com>
2021-10-27 20:57:01 -0500

    /health uses latest deploy time from the database now

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,11 +2,6 @@ class HealthController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    unless Rails.application.config.respond_to?(:latest_deploy_time)
-
-      Rails.application.config.latest_deploy_time = JSON.parse(File.read(Rails.root.join("tmp", "deploy_time.json").to_s))["latest_deploy_time"]
-    end
-
-    render json: {latest_deploy_time: Rails.application.config.latest_deploy_time}
+    render json: {latest_deploy_time: Health.instance.latest_deploy_time}
   end
 end
--- app/models/health.rb
+++ app/models/health.rb
@@ -3,7 +3,7 @@ class Health < ApplicationRecord
   # This ensures that only one Health row is created
   validates_inclusion_of :singleton_guard, in: [0]
 
-  def instance
+  def self.instance
     first_or_create!(singleton_guard: 0)
   end
 end
