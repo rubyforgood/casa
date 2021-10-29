34c8fdecca7fa08e7843f3ded13239ea82d492a1
CW <compiledwrong+github@gmail.com>
2021-10-28 20:19:09 -0700

    rake db migrate and add TODOs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_011923) do
+ActiveRecord::Schema.define(version: 2021_10_26_014614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -201,6 +201,21 @@ ActiveRecord::Schema.define(version: 2021_10_24_011923) do
     t.index ["judge_id"], name: "index_court_dates_on_judge_id"
   end
 
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
   create_table "emancipation_categories", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "mutually_exclusive", null: false
@@ -343,7 +358,8 @@ ActiveRecord::Schema.define(version: 2021_10_24_011923) do
   end
 
   create_table "versions", force: :cascade do |t|
-    t.string "item_type", null: false
+    t.string "item_type"
+    t.string "{:null=>false}"
     t.bigint "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
