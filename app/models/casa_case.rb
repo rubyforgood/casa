d4737e285fdb9c0eff2b05f6e773a97500aff184
Valerie Woolard <vwoolard@salesforce.com>
2021-10-31 11:20:05 -0700

    Make field not required

--- app/models/casa_case.rb
+++ app/models/casa_case.rb
@@ -37,7 +37,6 @@ class CasaCase < ApplicationRecord
   belongs_to :hearing_type, optional: true
   belongs_to :judge, optional: true
   belongs_to :casa_org
-  validates :birth_month_year_youth, presence: true
 
   has_many :casa_case_contact_types
   has_many :contact_types, through: :casa_case_contact_types, source: :contact_type
