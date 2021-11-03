1356ed294a42220302c604cb315cbf61dbb769f2
Jesse LaSalle <jesselasalle@gmail.com>
2021-11-03 03:32:58 -0400

    Linting issue

--- spec/views/reimbursements/index.html.erb_spec.rb
+++ spec/views/reimbursements/index.html.erb_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe "reimbursements/index", type: :view do
   before do
-      admin = build_stubbed :casa_admin
-      enable_pundit(view, admin)
-      allow(view).to receive(:current_user).and_return(admin)
+    admin = build_stubbed :casa_admin
+    enable_pundit(view, admin)
+    allow(view).to receive(:current_user).and_return(admin)
   end
 
   it "should not have any translation missing classes" do
