55df8f6c283e04a32219de0c9faa1d847e556d87
Edigleysson Silva <edigleyssonsilva@gmail.com>
2021-11-06 03:02:56 -0300

    fix casa_admin mailer preview

--- lib/mailers/previews/casa_admin_mailer_preview.rb
+++ lib/mailers/previews/casa_admin_mailer_preview.rb
@@ -1,11 +1,11 @@
 class CasaAdminMailerPreview < ActionMailer::Preview
   def account_setup
-    casa_admin = CasaAdmin.find_by(id: params[:id]) || CasaAdmin.last
+    casa_admin = if params.has_key?(:id) then CasaAdmin.find_by(id: params[:id]) else CasaAdmin.last end
     CasaAdminMailer.account_setup(casa_admin)
   end
 
   def deactivation
-    casa_admin = CasaAdmin.find_by(id: params[:id]) || CasaAdmin.last
+    casa_admin = if params.has_key?(:id) then CasaAdmin.find_by(id: params[:id]) else CasaAdmin.last end
     CasaAdminMailer.deactivation(casa_admin)
   end
 end
