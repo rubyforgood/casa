f34264e2df0fff4f7bc28f6f849a692d4d147d9d
Edigleysson Silva <edigleyssonsilva@gmail.com>
2021-11-06 03:03:15 -0300

    fix volunteer mailer preview

--- lib/mailers/previews/volunteer_mailer_preview.rb
+++ lib/mailers/previews/volunteer_mailer_preview.rb
@@ -2,18 +2,18 @@
 # :nocov:
 class VolunteerMailerPreview < ActionMailer::Preview
   def account_setup
-    user = User.find_by(id: params[:id]) || User.last
+    user = if params.has_key?(:id) then User.find_by(id: params[:id]) else User.last end
     VolunteerMailer.account_setup(user)
   end
 
   def court_report_reminder
-    user = User.find_by(id: params[:id]) || User.last
+    user = if params.has_key?(:id) then User.find_by(id: params[:id]) else User.last end
     VolunteerMailer.court_report_reminder(user, Date.today)
   end
 
   def case_contacts_reminder
-    user = User.find_by(id: params[:id]) || User.last
-    user.supervisor = User.find_by(id: params[:id]) || User.first
+    user = if params.has_key?(:id) then User.find_by(id: params[:id]) else User.last end
+    user.supervisor = if params.has_key?(:id) then User.find_by(id: params[:id]) else User.first end
     VolunteerMailer.case_contacts_reminder(user, true)
   end
 end
