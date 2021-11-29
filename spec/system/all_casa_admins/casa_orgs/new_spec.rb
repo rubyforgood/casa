5269efbfa6e243a5e17ddf73afae76341344fce7
Linda Goldstein <linda.goldstein@parachutehealth.com>
2021-11-28 16:04:24 -0800

    lint

--- spec/system/all_casa_admins/casa_orgs/new_spec.rb
+++ spec/system/all_casa_admins/casa_orgs/new_spec.rb
@@ -24,9 +24,9 @@ RSpec.describe "all_casa_admins/casa_orgs/new", type: :system do
         expect(page).to have_text "1 error prohibited this Casa org from being saved:"
         expect(page).to have_text "Name has already been taken"
       }.to change(
-               CasaOrg,
-               :count
-           ).by(0)
+        CasaOrg,
+        :count
+      ).by(0)
 
       fill_in "Name", with: "A new org"
       fill_in "Display name", with: "A new org"
@@ -34,9 +34,9 @@ RSpec.describe "all_casa_admins/casa_orgs/new", type: :system do
         click_on "Create CASA Organization"
         expect(page).to have_text "CASA Organization was successfully created."
       }.to change(
-               CasaOrg,
-               :count
-           ).by(1)
+        CasaOrg,
+        :count
+      ).by(1)
       new_org = CasaOrg.last
       expect(new_org.name).to eq "A new org"
       expect(new_org.display_name).to eq "A new org"
@@ -48,9 +48,9 @@ RSpec.describe "all_casa_admins/casa_orgs/new", type: :system do
         click_on "Create CASA Organization"
         expect(page).to have_text "Name can't be blank"
       }.to change(
-               CasaOrg,
-               :count
-           ).by(0)
+        CasaOrg,
+        :count
+      ).by(0)
     end
   end
 
