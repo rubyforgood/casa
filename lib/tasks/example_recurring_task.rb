20c677c978bad2842dbee9111a29cbb318265fa6
CW <compiledwrong+github@gmail.com>
2021-12-29 19:46:17 -0800

    lint

--- lib/tasks/example_recurring_task.rb
+++ lib/tasks/example_recurring_task.rb
@@ -1,8 +1,8 @@
 class ExampleRecurringTask
   include Delayed::RecurringJob
   run_every 1.day
-  run_at '11:00am'
-  timezone 'US/Pacific'
+  run_at "11:00am"
+  timezone "US/Pacific"
 
   def perform
     Bugsnag.notify("This is just ExampleRecurringTask saying hi in #{Rails.env}")
