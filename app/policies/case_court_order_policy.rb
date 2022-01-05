60a8ac0e8b8b46ea254f9ee04640326e2be901a3
CW <compiledwrong+github@gmail.com>
2022-01-04 20:41:39 -0800

    use an existing method

--- app/policies/case_court_order_policy.rb
+++ app/policies/case_court_order_policy.rb
@@ -1,7 +1,3 @@
 class CaseCourtOrderPolicy < ApplicationPolicy
-  alias_method :destroy?, :admin_or_supervisor?
-
-  def is_volunteer_assigned_to_case_for_court_order
-    @record.casa_case.case_assignments.exists?(volunteer_id: @user.id, active: true)
-  end
+  alias_method :destroy?, :admin_or_supervisor_or_volunteer?
 end
