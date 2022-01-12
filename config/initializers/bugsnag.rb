55b12a41fbe12e17e9424eb145623d3d22ae1f2e
CW <compiledwrong+github@gmail.com>
2022-01-11 18:44:25 -0800

    Add current user info to bugsnag events when it is available because it is more useful than IP

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,4 +2,10 @@ Bugsnag.configure do |config|
   config.api_key = ENV["BUGSNAG_API_KEY"]
   config.ignore_classes << ActiveRecord::RecordNotFound
   config.release_stage = ENV["HEROKU_APP_NAME"] || ENV["APP_ENVIRONMENT"]
+
+  callback = proc do |event|
+    event.set_user(current_user.id, current_user.email)
+  end
+
+  config.add_on_error(callback)
 end
