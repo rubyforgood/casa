26b39877096b8a1dbc7d5c2283131dfbd0edabf8
Drew Peterson <drew.a.peterson@hotmail.com>
2022-01-17 13:50:21 -0800

    correctly failing test for additional expense test

---
+++ spec/system/case_contacts/additional_expenses_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+require "action_view"
+
+RSpec.describe "addtional_expenses", type: :system do
+
+  it "additional expenses", js: true do
+    organization = build(:casa_org)
+    volunteer = create(:volunteer, casa_org: organization)
+    casa_case = create(:casa_case, casa_org: organization)
+    create(:case_assignment, casa_case: casa_case, volunteer: volunteer)
+    contact_type_group = build(:contact_type_group, casa_org: organization)
+    contact_type = create(:contact_type)
+    school = create(:contact_type, name: "School", contact_type_group: contact_type_group)
+
+    sign_in volunteer
+    
+    visit casa_case_path(casa_case.id)
+
+    click_on "New Case Contact"
+
+    check "School"
+    choose "Yes"
+    select "Video", from: "case_contact[medium_type]"
+    fill_in "case_contact_occurred_at", with: "04/04/2020"
+
+    fill_in "case-contact-duration-hours", with: "1"
+    fill_in "case-contact-duration-minutes", with: "45"
+    # should not be needed anymore
+    fill_in "case_contact_miles_driven", with: "0"
+
+    expect {
+      click_on "Submit"
+    }.to change(CaseContact, :count).by(1)
+
+    expect(CaseContact.first.additional_expenses.length).to eq 1
+  end
+end
