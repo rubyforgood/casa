256259cb474996b23ebb7831f62b84064d7b8488
Drew Peterson <drew.a.peterson@hotmail.com>
2022-01-26 10:34:04 -0800

    refactor addition expense params method for codeclimate

--- app/controllers/case_contacts_controller.rb
+++ app/controllers/case_contacts_controller.rb
@@ -192,11 +192,9 @@ class CaseContactsController < ApplicationController
     additional_expenses = params.dig("case_contact", "additional_expenses_attributes")
     additional_expenses && 0.upto(10).map do |i|
       possible_key = i.to_s
-      if additional_expenses&.key?(possible_key)
-        if !additional_expenses[i.to_s]["other_expense_amount"].blank?
+      if additional_expenses&.key?(possible_key) && !additional_expenses[i.to_s]["other_expense_amount"].blank?
           additional_expenses[i.to_s]&.permit(:other_expense_amount, :other_expenses_describe, :id)
         end
-      end
     end.compact
   end
 end
