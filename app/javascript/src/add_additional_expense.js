1b3f3460b7f4974a598250dd39d5cd38f00e2bd5
Drew Peterson <drew.a.peterson@hotmail.com>
2022-01-27 12:39:35 -0800

    Added js functionality to unhide a new set of additional expense fields with each successive click of the add another expense link, up to 10 total sets of additional expense fields

--- app/javascript/src/add_additional_expense.js
+++ app/javascript/src/add_additional_expense.js
@@ -1,16 +1,10 @@
-// $(document).on("click", "#add-another-expense", function() {
-//   $(".hide-field").show();
-// });
-
 $(document).on("click", "#add-another-expense", function() {
-  if ($("#case_contact_additional_expenses_attributes_0_other_expense_amount").length > 0) {
-    console.log("0 detected")
-    const elementAmount = document.getElementById("case_contact_additional_expenses_attributes_1_other_expense_amount")
-    elementAmount.classList.remove('hide-field')
-    const elementDescribe = document.getElementById("case_contact_additional_expenses_attributes_1_other_expenses_describe")
-    elementDescribe.classList.remove('hide-field')
-    $("case_contact_additional_expenses_attributes_1_other_expenses_describe").classList.remove('hide-field')
+  for (let i=1; i < 10; i++) {
+    if ($(`#case_contact_additional_expenses_attributes_${i}_other_expense_amount`).is(":hidden")) {
+      $(`#case_contact_additional_expenses_attributes_${i}_other_expense_amount`).show();
+      $(`#case_contact_additional_expenses_attributes_${i}_other_expenses_describe`).show();
+      break;
+    }
   }
 });
 
-// if (document.body.contains("#case_contact_additional_expenses_attributes_0_other_expense_amount")) {
