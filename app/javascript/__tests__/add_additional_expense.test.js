d37ca98a17c87a93245d0a0d20924d91a150d421
Drew Peterson <drew.a.peterson@hotmail.com>
2022-02-01 19:55:38 -0800

    fix linter issue on javascript jest test to remove require in document ready section

--- app/javascript/__tests__/add_additional_expense.test.js
+++ app/javascript/__tests__/add_additional_expense.test.js
@@ -2,28 +2,25 @@
 
 require('jest')
 
-
 beforeEach(() => {
   const $ = require('jquery')
   document.body.innerHTML =
   '<style>' +
   '  #case_contact_additional_expenses_attributes_1_other_expense_amount { display: none; }' +
-  '</style>' + 
-  '<div>' +   
-  ' <a href="#" class="add-another-expense" id="add-another-expense">Add another expense</a>' + 
-  ' <input placeholder="Describe the expense" class="form-control other-expenses-describe hide-field" type="text" name="case_contact[additional_expenses_attributes][1][other_expenses_describe]" id="case_contact_additional_expenses_attributes_1_other_expense_amount" style="display: inline-block;">' + 
-  ' <input placeholder="Describe the expense" class="form-control other-expenses-describe hide-field" type="text" name="case_contact[additional_expenses_attributes][1][other_expenses_describe]" id="case_contact_additional_expenses_attributes_1_other_expenses_describe" style="display: inline-block;" >' + 
+  '</style>' +
+  '<div>' +
+  ' <a href="#" class="add-another-expense" id="add-another-expense">Add another expense</a>' +
+  ' <input placeholder="Describe the expense" class="form-control other-expenses-describe hide-field" type="text" name="case_contact[additional_expenses_attributes][1][other_expenses_describe]" id="case_contact_additional_expenses_attributes_1_other_expense_amount" style="display: inline-block;">' +
+  ' <input placeholder="Describe the expense" class="form-control other-expenses-describe hide-field" type="text" name="case_contact[additional_expenses_attributes][1][other_expenses_describe]" id="case_contact_additional_expenses_attributes_1_other_expenses_describe" style="display: inline-block;" >' +
   '</div>'
-  
-  
+
   $(document).ready(() => {
-    require('/src/add_additional_expense.js')
   })
 })
 
 describe('Add Additional Expense tests', () => {
   test('Test link to add expenses is present', () => {
-    expect($('#add-another-expense').text()).toEqual("Add another expense")
+    expect($('#add-another-expense').text()).toEqual('Add another expense')
   })
   test('Test that initial expense amount field is hidden', () => {
     expect($('#case_contact_additional_expenses_attributes_1_other_expense_amount').is(':hidden')).toBe(true)
@@ -32,11 +29,10 @@ describe('Add Additional Expense tests', () => {
     expect($('#case_contact_additional_expenses_attributes_1_other_expenses_describe').is(':hidden')).toBe(true)
   })
   test('Displays a new set of fields after a click', () => {
-    
     require('../src/add_additional_expense')
 
-    $('#add-additional-expense').trigger("click")
+    $('#add-additional-expense').trigger('click')
 
-    expect($('#case_contact_additional_expenses_attributes_1_other_expense_amount').css("display: inline-block"))
+    expect($('#case_contact_additional_expenses_attributes_1_other_expense_amount').css('display: inline-block'))
   })
 })
