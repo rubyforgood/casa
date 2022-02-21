111d39b7ca037a4e1a84ece44965d0e7fc10f527
Drew Peterson <drew.a.peterson@hotmail.com>
2022-02-21 10:41:14 -0800

    fix linting issues.  Extra space on form and and add addtional expense js as well as double quotes instead of single

--- app/javascript/src/add_additional_expense.js
+++ app/javascript/src/add_additional_expense.js
@@ -1,7 +1,7 @@
 function showAdditionalExpense () {
   for (let i = 1; i < 10; i++) {
     if ($(`#case_contact_additional_expenses_attributes_${i}_other_expense_amount`).is(':hidden')) {
-      $(`#expense${i + 1}`).wrap( "<li></li>" )
+      $(`#expense${i + 1}`).wrap('<li></li>')
       $(`#case_contact_additional_expenses_attributes_${i}_other_expense_amount`).show()
       $(`#case_contact_additional_expenses_attributes_${i}_other_expenses_describe`).show()
       break
