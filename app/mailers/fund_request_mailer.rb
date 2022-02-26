9ff89969540570cbf8e261870fa8cf8b1b53503c
compwron <compwron@users.noreply.github.com>
2022-02-26 01:17:56 -0800

    email subject and body tuning

--- app/mailers/fund_request_mailer.rb
+++ app/mailers/fund_request_mailer.rb
@@ -5,6 +5,6 @@ class FundRequestMailer < ApplicationMailer
     to_recipient_email = ENV["FUND_REQUEST_RECIPIENT_EMAIL"]
     Bugsnag.notify("No user for FUND_REQUEST_RECIPIENT_EMAIL for fund request from: #{fund_request.submitter_email}") unless to_recipient_email
     @fund_request = fund_request
-    mail(layout: nil, to: [to_recipient_email, fund_request.submitter_email], subject: "Fund request from #{fund_request.requested_by_and_relationship}")
+    mail(layout: nil, to: [to_recipient_email, fund_request.submitter_email], subject: "Fund request from #{fund_request.submitter_email}")
   end
 end
