c17a6d7b226c68314f354af3692bb5ffbe56dc16
Linda Goldstein <linda.goldstein@parachutehealth.com>
2022-02-26 23:40:50 -0800

    Revert "Not working yet- real PDF filling"
    This reverts commit 065c8cfc0f2fc006e794628557e81ef48b63425f.

--- app/mailers/fund_request_mailer.rb
+++ app/mailers/fund_request_mailer.rb
@@ -5,13 +5,6 @@ class FundRequestMailer < ApplicationMailer
     to_recipient_email = ENV["FUND_REQUEST_RECIPIENT_EMAIL"]
     Bugsnag.notify("No user for FUND_REQUEST_RECIPIENT_EMAIL for fund request from: #{fund_request.submitter_email}") unless to_recipient_email
     @fund_request = fund_request
-    pdf_attachment = PdfService.new(
-      inputs: @fund_request,
-      pdf_template_path: File.join(["data", "fund_request.pdf"]),
-      basename: ["fund_request", "pdf"]
-    ).write_to_file
-    submitter_name = fund_request.submitter_email.split("@").first
-    attachments.inline["fund-request-#{Date.today}-#{submitter_name}.pdf"] = pdf_attachment
     mail(layout: nil, to: [to_recipient_email, fund_request.submitter_email], subject: "Fund request from #{fund_request.submitter_email}")
   end
 end
