846cb01189c86d63b61888a274506dc69c7b91a2
compwron <compwron@users.noreply.github.com>
2022-02-28 00:07:43 -0800

    still send fund email even if pdf fails

--- app/mailers/fund_request_mailer.rb
+++ app/mailers/fund_request_mailer.rb
@@ -3,16 +3,21 @@ class FundRequestMailer < ApplicationMailer
 
   def send_request(_user, fund_request)
     to_recipient_email = ENV["FUND_REQUEST_RECIPIENT_EMAIL"]
+    Bugsnag.notify("No user for FUND_REQUEST_RECIPIENT_EMAIL for fund request from: #{submitter_email}") unless to_recipient_email
+
     submitter_email = fund_request.submitter_email
     @inputs = fund_request.as_json
-    Bugsnag.notify("No user for FUND_REQUEST_RECIPIENT_EMAIL for fund request from: #{submitter_email}") unless to_recipient_email
-    pdf_attachment = FdfInputsService.new(
-      inputs: @inputs,
-      pdf_template_path: File.join(["data", "fund_request.pdf"]),
-      basename: ["fund_request", "pdf"]
-    ).write_to_file.read
     submitter_name = fund_request.submitter_email.split("@").first
-    attachments.inline["fund-request-#{Date.today}-#{submitter_name}.pdf"] = pdf_attachment
+    begin
+      pdf_attachment = FdfInputsService.new(
+          inputs: @inputs,
+          pdf_template_path: File.join(["data", "fund_request.pdf"]),
+          basename: ["fund_request", "pdf"]
+      ).write_to_file.read
+      attachments.inline["fund-request-#{Date.today}-#{submitter_name}.pdf"] = pdf_attachment
+    rescue => e
+      Bugsnag.notify(e)
+    end
     mail(layout: nil, to: [to_recipient_email, submitter_email], subject: "Fund request from #{submitter_email}")
   end
 end
