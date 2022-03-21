5ecd58cdb63fb3e31299fb070af21b01151d5c25
compwron <compwron@users.noreply.github.com>
2022-03-20 19:29:04 -0700

    More test for rembursements

--- spec/system/reimbursements/reimbursements_spec.rb
+++ spec/system/reimbursements/reimbursements_spec.rb
@@ -10,9 +10,17 @@ RSpec.describe "reimbursements", type: :system do
     contact2 = create(:case_contact, :wants_reimbursement)
 
     visit reimbursements_path
+    expect(page).to have_content("Reimbursement Queue")
     expect(page).to have_content("Needs Review")
     expect(page).to have_content("Reimbursement Complete")
+    expect(page).to have_content("Volunteer")
+    expect(page).to have_content("Case Number")
+    expect(page).to have_content("Contact Types")
     expect(page).to have_content("Occurred At")
+    expect(page).to have_content("Expense Type")
+    expect(page).to have_content("Description")
+    expect(page).to have_content("Miles Driven")
+    expect(page).to have_content("Reimbursement Complete")
     expect(page).to have_content(contact1.casa_case.case_number)
     expect(page).to have_content(contact2.miles_driven)
   end
