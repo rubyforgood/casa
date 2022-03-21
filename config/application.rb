ec0648965f611b517eae94316a0b54d04969f100
Amy Gurski <amygurski@gmail.com>
2022-03-21 15:16:19 -0400

    Load rails 6.1 configuration defaults
    Load configuration defaults for rails 6.1 as listed here:
    https://guides.rubyonrails.org/configuring.html#default-values-for-target-version-6-1
    
    The one exception is config.active_record.has_many_inversing because
    it caused breaking changes due to ActiveRecord::NotNullViolations. Set
    config to false to opt out for now. rails PR for this has_many_inversing:
    https://github.com/rails/rails/pull/37429

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module Casa
   class Application < Rails::Application
     config.action_mailer.preview_path ||= defined?(Rails.root) ? Rails.root.join("lib", "mailers", "previews") : nil
     config.eager_load_paths << Rails.root.join("app", "lib", "importers")
-    config.load_defaults 6.0
+    config.load_defaults 6.1
+    config.active_record.has_many_inversing = false
     config.serve_static_assets = true
   end
 end
