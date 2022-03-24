6ff3de0b68b4d873a4eb686834e8a7043451ff36
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-03-24 15:35:24 -0400

    cherry pick 2

--- app/controllers/learning_hours_controller.rb
+++ app/controllers/learning_hours_controller.rb
@@ -3,5 +3,27 @@ class LearningHoursController < ApplicationController
   end
 
   def new
+    @learning_hour = LearningHour.new
+  end
+
+  def create
+    @learning_hour = LearningHour.new(learning_hours_params)
+    respond_to do |format|
+      if @learning_hour.save
+        format.html { redirect_to volunteer_learning_hours_path, notice: "New entry was successfully created." }
+      else
+        format.html { render (:new), status: 404 }
+        
+      end
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def learning_hours_params
+    params.require(:learning_hour).permit(:occurred_at, :duration_minutes, :duration_hours, :name, :user_id, :learning_type)
   end
 end
