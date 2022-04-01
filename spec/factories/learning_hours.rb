3bd04a56398cea1f76e58104f0187c9ccb7c7da2
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-04-01 16:42:22 -0400

    more testing

--- spec/factories/learning_hours.rb
+++ spec/factories/learning_hours.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
-  factory :learning_hour do
+  factory :learning_hour, class: LearningHour, parent: :volunteer do
+    name {"New Learning Hour"}
     duration_minutes { 1 }
     occurred_at { "2022-03-24 10:17:58" }
   end
--- spec/models/learning_hour_spec.rb
+++ spec/models/learning_hour_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe LearningHour, type: :model do
     expect(learning_hour).to_not be_valid
     expect(learning_hour.errors[:name]).to eq(["/ Title cannot be blank"])
   end
-  
+
   it "has a learning_type" do
     learning_hour = build_stubbed(:learning_hour, learning_type: nil)
     expect(learning_hour).to_not be_valid
@@ -26,9 +26,7 @@ RSpec.describe LearningHour, type: :model do
   end
 
   it "has date that is not in the future" do
-    learning_hour = build_stubbed(:learning_hour, occurred_at: 1.day.from_now.strftime('%d %b %Y'))
+    learning_hour = build_stubbed(:learning_hour, occurred_at: 1.day.from_now.strftime("%d %b %Y"))
     expect(learning_hour).to_not be_valid
   end
-
-  
 end
