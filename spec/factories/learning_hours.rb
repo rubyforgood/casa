ae46f5f1a56fb53b8d33a8e04281def342fb67cd
Caitlin Henry <caitlinhenry.ss@gmail.com>
2022-04-01 17:32:19 -0400

    even more testing

--- spec/factories/learning_hours.rb
+++ spec/factories/learning_hours.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :learning_hour, class: LearningHour, parent: :volunteer do
+  factory :learning_hour do
+    user = User.first
+    user_id { user.id }
     name {"New Learning Hour"}
     duration_minutes { 1 }
     occurred_at { "2022-03-24 10:17:58" }
