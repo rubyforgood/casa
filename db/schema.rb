f263f01e870ba8aff3a0353977eccc0d3de7f63d
Isaac M <isaacm@users.noreply.github.com>
2022-04-11 16:56:55 -0400

    Add uniqueness constraint to feature flag name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_02_201247) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_11_180242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -247,6 +247,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_02_201247) do
     t.boolean "enabled", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_feature_flags_on_name", unique: true
   end
 
   create_table "followups", force: :cascade do |t|
