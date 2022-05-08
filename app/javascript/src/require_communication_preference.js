b5743daebe7c493212af46feadaf64bd1dcabf53
7riumph <triumpharamunde@gmail.com>
2022-05-08 09:05:30 -0600

    Added 'displayPopUpIfPreferencesIsInvalid' function

--- app/javascript/src/require_communication_preference.js
+++ app/javascript/src/require_communication_preference.js
@@ -3,3 +3,22 @@
 import Swal from 'sweetalert2'
 
 import { disableBtn, enableBtn } from './casa_case'
+
+const EMAIL_TOGGLE_CLASS = 'toggle-email-notifications'
+const SMS_TOGGLE_CLASS = 'toggle-sms-notifications'
+const SAVE_BUTTON_CLASS = 'save-preference'
+
+function displayPopUpIfPreferencesIsInvalid (receiveSms = false, receiveEmail = true) {
+    let emailNotificationState = $("#user_receive_email_notifications").prop('checked')
+    let smsNotificationState = $("#user_receive_sms_notifications").prop('checked')
+
+    if (smsNotificationState == false && emailNotificationState == false) {
+        
+        Swal.fire({
+            icon: 'error',
+            title: 'Preference Error',
+            text: 'At least one communication preference required'
+          })
+    } 
+
+}
