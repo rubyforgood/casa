714a313a195249bd4b58052f5d94f74bb3462270
7riumph <triumpharamunde@gmail.com>
2022-05-08 12:19:40 -0600

    Branch clean up

--- app/javascript/src/require_communication_preference.js
+++ app/javascript/src/require_communication_preference.js
@@ -9,35 +9,34 @@ const SMS_TOGGLE_CLASS = 'toggle-sms-notifications'
 const SAVE_BUTTON_CLASS = 'save-preference'
 
 function displayPopUpIfPreferencesIsInvalid () {
-    let emailNotificationState = $("#user_receive_email_notifications").prop('checked')
-    let smsNotificationState = $("#user_receive_sms_notifications").prop('checked')
+  const emailNotificationState = $('#user_receive_email_notifications').prop('checked')
+  const smsNotificationState = $('#user_receive_sms_notifications').prop('checked')
 
-    if (smsNotificationState == false && emailNotificationState == false) {
-        disableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
-        Swal.fire({
-            icon: 'error',
-            title: 'Preference Error',
-            text: 'At least one communication preference required'
-          })
-    } else {
-        enableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
-    }
+  if (smsNotificationState === false && emailNotificationState === false) {
+    disableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
+    Swal.fire({
+      icon: 'error',
+      title: 'Preference Error',
+      text: 'At least one communication preference required'
+    })
+  } else {
+    enableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
+  }
 }
 
 $('document').ready(() => {
-    if ($(`.${SAVE_BUTTON_CLASS}`).length > 0) {
-        enableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
-        $(`.${SMS_TOGGLE_CLASS}`).on('blur', () => {
-            displayPopUpIfPreferencesIsInvalid() 
-        })
-    
-        $(`.${EMAIL_TOGGLE_CLASS}`).on('blur', () => {
-            displayPopUpIfPreferencesIsInvalid() 
-        })
-      }
+  if ($(`.${SAVE_BUTTON_CLASS}`).length > 0) {
+    enableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
+    $(`.${SMS_TOGGLE_CLASS}`).on('blur', () => {
+      displayPopUpIfPreferencesIsInvalid()
     })
 
+    $(`.${EMAIL_TOGGLE_CLASS}`).on('blur', () => {
+      displayPopUpIfPreferencesIsInvalid()
+    })
+  }
+})
+
 export {
-    displayPopUpIfPreferencesIsInvalid
+  displayPopUpIfPreferencesIsInvalid
 }
-     
