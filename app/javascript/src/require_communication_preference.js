828ac3b2c94ef28bd6dd3ee14ee9a522bf7cf7cc
7riumph <triumpharamunde@gmail.com>
2022-05-11 23:46:59 -0600

    Ensured users can always submit valid preferences (tests)

--- app/javascript/src/require_communication_preference.js
+++ app/javascript/src/require_communication_preference.js
@@ -8,17 +8,21 @@ const EMAIL_TOGGLE_CLASS = 'toggle-email-notifications'
 const SMS_TOGGLE_CLASS = 'toggle-sms-notifications'
 const SAVE_BUTTON_CLASS = 'save-preference'
 
-function displayPopUpIfPreferencesIsInvalid () {
+function displayPopUpIfPreferencesIsInvalid (receiveEmail, receiveSMS, triggerPopup = false) {
   const emailNotificationState = $('#user_receive_email_notifications').prop('checked')
   const smsNotificationState = $('#user_receive_sms_notifications').prop('checked')
+  receiveSMS = smsNotificationState
+  receiveEmail = emailNotificationState
 
-  if (smsNotificationState === false && emailNotificationState === false) {
+  if (receiveSMS === false && receiveEmail === false) {
     disableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
-    Swal.fire({
-      icon: 'error',
-      title: 'Preference Error',
-      text: 'At least one communication preference required'
-    })
+    if (triggerPopup) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Preference Error',
+        text: 'At least one communication preference required'
+      })
+    }
   } else {
     enableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
   }
@@ -26,13 +30,15 @@ function displayPopUpIfPreferencesIsInvalid () {
 
 $('document').ready(() => {
   if ($(`.${SAVE_BUTTON_CLASS}`).length > 0) {
+    const receiveSMS = $(`.${SMS_TOGGLE_CLASS}`)[0]
+    const receiveEmail = $(`.${EMAIL_TOGGLE_CLASS}`)[0]
     enableBtn($(`.${SAVE_BUTTON_CLASS}`)[0])
     $(`.${SMS_TOGGLE_CLASS}`).on('blur', () => {
-      displayPopUpIfPreferencesIsInvalid()
+      displayPopUpIfPreferencesIsInvalid(receiveEmail, receiveSMS, true)
     })
 
     $(`.${EMAIL_TOGGLE_CLASS}`).on('blur', () => {
-      displayPopUpIfPreferencesIsInvalid()
+      displayPopUpIfPreferencesIsInvalid(receiveEmail, receiveSMS, true)
     })
   }
 })
