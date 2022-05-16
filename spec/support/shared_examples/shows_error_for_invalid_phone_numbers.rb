4929278539b310be4c4934b08a9f8c6b279da33c
Harsohail Brar <harsohail.brar1@gmail.com>
2022-05-15 22:43:40 -0600

    change tests to have the same error message as phone number helper

--- spec/support/shared_examples/shows_error_for_invalid_phone_numbers.rb
+++ spec/support/shared_examples/shows_error_for_invalid_phone_numbers.rb
@@ -14,12 +14,12 @@ shared_examples_for "shows error for invalid phone numbers" do
   it "shows error message for bad phone number" do
     role == "admin" || role == "user" ? fill_in("Phone number", with: "+141632u809o") : fill_in("#{role}_phone_number", with: "+141632u809o")
     role == "user" ? click_on("Update Profile") : click_on("Submit")
-    expect(page).to have_text "Phone number must have correct format"
+    expect(page).to have_text "Phone number must be 12 digits including country code (+1)"
   end
 
   it "shows error message for phone number without country code" do
     role == "admin" || role == "user" ? fill_in("Phone number", with: "+24163218092") : fill_in("#{role}_phone_number", with: "+24163218092")
     role == "user" ? click_on("Update Profile") : click_on("Submit")
-    expect(page).to have_text "Phone number must have a valid country code (+1)"
+    expect(page).to have_text "Phone number must be 12 digits including country code (+1)"
   end
 end
