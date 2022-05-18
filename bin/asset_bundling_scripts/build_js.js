1e55656a8425af662e53e12dc0396aa3acd1f3b5
firelemons <shenyang64@gmail.com>
2022-05-17 21:17:39 -0500

    fix sometimes erroneous statement

--- bin/asset_bundling_scripts/build_js.js
+++ bin/asset_bundling_scripts/build_js.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const CLIArgs = process.argv.slice(2)
+const isWatching = CLIArgs.includes('--watch')
 
 const esbuild = require('esbuild')
 const logger = require('./logger.js')
@@ -9,10 +10,13 @@ esbuild.build({
   entryPoints: ['app/javascript/application.js'],
   outdir: 'app/assets/builds',
   bundle: true,
-  watch: CLIArgs.includes('--watch')
+  watch: isWatching
 }).then(result => {
   logger.info('application.js built successfully')
-  logger.info('watching for changes')
+
+  if (isWatching) {
+    logger.info('watching for changes')
+  }
 }).catch((err) => {
   logger.error('failed to build application.js')
   logger.error(err)
