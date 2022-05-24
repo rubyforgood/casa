92e0605ff369d8807d06a77ef938774154d10025
Iraline <iraline@hotmail.com>
2022-05-24 10:56:31 -0300

    Create a new and update views for the other_duty table

---
+++ spec/views/other_duties/edit.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe "other_duties/edit", type: :view do
+  let(:other_duty) { create(:other_duty) }
+
+  before do
+    assign :other_duty, other_duty
+  end
+
+  it "display all form fields" do
+    render template: "other_duties/edit"
+
+    expect(rendered).to have_text("Editing Duty")
+    expect(rendered).to have_text("Occurred On")
+    expect(rendered).to have_text("Duration Meeting")
+    expect(rendered).to have_text("Enter Notes")
+  end
+
+  it "displays occurred time in the occurred at form field" do
+    render template: "other_duties/edit"
+
+    expect(rendered).to include(other_duty.occurred_at.strftime("%Y-%m-%d"))
+  end
+
+  it "displays notes in the notes form field" do
+    render template: "other_duties/edit"
+
+    expect(rendered).to include(other_duty.notes)
+  end
+end
