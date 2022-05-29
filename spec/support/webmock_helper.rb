7beb5dd26c410ec43d3fea97e7843b08d0b796b4
Harsohail Brar <harsohail.brar1@gmail.com>
2022-05-29 16:49:34 -0700

    remove excess header attributes

--- spec/support/webmock_helper.rb
+++ spec/support/webmock_helper.rb
@@ -28,10 +28,8 @@ def stubbed_requests
       headers: {
         "Accept" => "application/json",
         "Accept-Charset" => "utf-8",
-        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
         "Authorization" => "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
         "Content-Type" => "application/x-www-form-urlencoded",
-        "User-Agent" => "twilio-ruby/5.67.1 (darwin21 x86_64) Ruby/3.1.0"
       }
     )
     .to_return(body: "{\"error_code\":null, \"status\":\"sent\", \"body\":\"It's been 60 days or more since you've reached out to these members of your youth's network:\"}")
@@ -41,10 +39,8 @@ def stubbed_requests
       headers: {
         "Accept" => "application/json",
         "Accept-Charset" => "utf-8",
-        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
         "Authorization" => "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
         "Content-Type" => "application/x-www-form-urlencoded",
-        "User-Agent" => "twilio-ruby/5.67.1 (darwin21 x86_64) Ruby/3.1.0"
       }
     )
     .to_return(body: "{\"error_code\":null, \"status\":\"sent\", \"body\":\"test\"}")
@@ -54,49 +50,8 @@ def stubbed_requests
       headers: {
         "Accept" => "application/json",
         "Accept-Charset" => "utf-8",
-        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
         "Authorization" => "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
         "Content-Type" => "application/x-www-form-urlencoded",
-        "User-Agent" => "twilio-ruby/5.67.1 (darwin21 x86_64) Ruby/3.1.0"
-      }
-    )
-    .to_return(body: "{\"error_code\":null, \"status\":\"sent\", \"body\":\"If you have made contact with them in the past 60 days, remember to log it: [link to create new case contact for assigned case]\"}")
-  stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/articuno34/Messages.json")
-    .with(
-      body: {"Body" => "It's been 60 days or more since you've reached out to these members of your youth's network:", "From" => "+15555555555", "To" => "+12222222222"},
-      headers: {
-        "Accept" => "application/json",
-        "Accept-Charset" => "utf-8",
-        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-        "Authorization" => "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
-        "Content-Type" => "application/x-www-form-urlencoded",
-        "User-Agent" => "twilio-ruby/5.67.1 (linux x86_64) Ruby/3.1.0"
-      }
-    )
-    .to_return(body: "{\"error_code\":null, \"status\":\"sent\", \"body\":\"It's been 60 days or more since you've reached out to these members of your youth's network:\"}")
-  stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/articuno34/Messages.json")
-    .with(
-      body: {"Body" => "test", "From" => "+15555555555", "To" => "+12222222222"},
-      headers: {
-        "Accept" => "application/json",
-        "Accept-Charset" => "utf-8",
-        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-        "Authorization" => "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
-        "Content-Type" => "application/x-www-form-urlencoded",
-        "User-Agent" => "twilio-ruby/5.67.1 (linux x86_64) Ruby/3.1.0"
-      }
-    )
-    .to_return(body: "{\"error_code\":null, \"status\":\"sent\", \"body\":\"test\"}")
-  stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/articuno34/Messages.json")
-    .with(
-      body: {"Body" => "If you have made contact with them in the past 60 days, remember to log it: [link to create new case contact for assigned case]", "From" => "+15555555555", "To" => "+12222222222"},
-      headers: {
-        "Accept" => "application/json",
-        "Accept-Charset" => "utf-8",
-        "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-        "Authorization" => "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
-        "Content-Type" => "application/x-www-form-urlencoded",
-        "User-Agent" => "twilio-ruby/5.67.1 (linux x86_64) Ruby/3.1.0"
       }
     )
     .to_return(body: "{\"error_code\":null, \"status\":\"sent\", \"body\":\"If you have made contact with them in the past 60 days, remember to log it: [link to create new case contact for assigned case]\"}")
