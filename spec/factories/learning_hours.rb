47b279d6a5033b485fef31d8ed281bf440b76e81
compwron <compwron@users.noreply.github.com>
2022-06-02 18:59:46 -0700

    fix factory

--- spec/factories/learning_hours.rb
+++ spec/factories/learning_hours.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :learning_hour do
-    user = User.first
-    user_id { user.id }
+    user
     name { "New Learning Hour" }
-    duration_minutes { 1 }
+    duration_hours { 1 }
+    duration_minutes { 2 }
     occurred_at { "2022-03-24 10:17:58" }
   end
 end
