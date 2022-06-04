0ff2a80c7a47deb7aad1cb1c0aa7aa8a344bf9e2
Jesse LaSalle <jesselasalle@gmail.com>
2022-06-04 12:25:23 -0400

    Add other duties view and controller action

--- app/controllers/other_duties_controller.rb
+++ app/controllers/other_duties_controller.rb
@@ -1,5 +1,5 @@
 class OtherDutiesController < ApplicationController
-  before_action :set_other_duty, except: [:new, :create]
+  before_action :set_other_duty, except: [:new, :create, :index]
   before_action :convert_duration_minutes, only: [:update, :create]
 
   def new
@@ -19,6 +19,16 @@ class OtherDutiesController < ApplicationController
   def edit
   end
 
+  def index
+    @volunteer_duties = if current_user.casa_admin?
+      generate_other_duty_list(Volunteer.all)
+    elsif current_user.supervisor?
+      generate_other_duty_list(current_user.volunteers)
+    else
+      render file: 'public/403.html', status: :unauthorized
+    end
+  end
+
   def update
     if @other_duty.update(other_duty_params)
       redirect_to casa_cases_path, notice: "Duty was successfully updated."
@@ -34,6 +44,15 @@ class OtherDutiesController < ApplicationController
     converted_duration_hours = duration_hours * 60
     duration_minutes = params[:other_duty][:duration_minutes].to_i
     params[:other_duty][:duration_minutes] = (converted_duration_hours + duration_minutes).to_s
+  end
+
+  def generate_other_duty_list(volunteers)
+    other_duties = volunteers.map do |volunteer|
+      {
+        volunteer: volunteer,
+        other_duties: volunteer.other_duties
+      }
+    end
   end
 
   def other_duty_params
