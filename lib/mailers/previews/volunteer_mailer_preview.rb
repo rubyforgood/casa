738dcb8a9cc490ff8b988446b39895d9347cc651
Jesse LaSalle <jesselasalle@gmail.com>
2022-06-05 21:31:30 -0400

    Limit previews to volunteers only and add a test

--- lib/mailers/previews/volunteer_mailer_preview.rb
+++ lib/mailers/previews/volunteer_mailer_preview.rb
@@ -2,19 +2,26 @@
 # :nocov:
 class VolunteerMailerPreview < ActionMailer::Preview
   def account_setup
-    user = params.has_key?(:id) ? User.find_by(id: params[:id]) : User.last
+    user = params.has_key?(:id) ? get_user(params[:id]) : Volunteer.last
     VolunteerMailer.account_setup(user)
   end
 
   def court_report_reminder
-    user = params.has_key?(:id) ? User.find_by(id: params[:id]) : User.last
+    user = params.has_key?(:id) ? get_user(params[:id]) : Volunteer.last
     VolunteerMailer.court_report_reminder(user, Date.today)
   end
 
   def case_contacts_reminder
-    user = params.has_key?(:id) ? User.find_by(id: params[:id]) : User.last
+    user = params.has_key?(:id) ? get_user(params[:id]) : Volunteer.last
     user.supervisor = params.has_key?(:id) ? User.find_by(id: params[:id]) : User.first
     VolunteerMailer.case_contacts_reminder(user, true)
+  end
+
+private
+
+  def get_user(user_id)
+    user = User.find_by(id: user_id)
+    user && user.volunteer? ? user : Volunteer.last
   end
 end
 # :nocov:
--- spec/mailers/previews/volunteer_mailer_preview_spec.rb
+++ spec/mailers/previews/volunteer_mailer_preview_spec.rb
@@ -3,23 +3,59 @@ require File.join(Rails.root, "lib", "mailers", "previews", "volunteer_mailer_pr
 
 RSpec.describe VolunteerMailerPreview do
   let(:subject) { described_class.new }
-  let!(:user) { create(:user) }
+  let!(:volunteer)  { create(:volunteer) }
+  let!(:supervisor) { create(:supervisor) }
+  let!(:admin)      { create(:casa_admin) }
 
   describe "#account_setup" do
     let(:message) { subject.account_setup }
 
-    it { expect(message.to).to eq [user.email] }
+    it { expect(message.to).to eq [volunteer.email] }
   end
 
   describe "#court_report_reminder" do
     let(:message) { subject.court_report_reminder }
 
-    it { expect(message.to).to eq [user.email] }
+    it { expect(message.to).to eq [volunteer.email] }
   end
 
   describe "#case_contacts_reminder" do
     let(:message) { subject.case_contacts_reminder }
 
-    it { expect(message.to).to eq [user.email] }
+    it { expect(message.to).to eq [volunteer.email] }
+  end
+
+  describe "#get_user" do
+    context "uses a volunteer record" do
+      context "when no id is provided" do
+        let(:message) { subject.account_setup }
+
+        it { expect(message.to).to eq [volunteer.email] }
+      end
+
+      context "when a volunteer id is provided" do
+        let(:params) { { id: volunteer.id } }
+        let(:subject) { described_class.new params }
+        let(:message) { subject.account_setup }
+
+        it { expect(message.to).to eq [volunteer.email] }
+      end
+
+      context "when a supervisor id is provided" do
+        let(:params) { { id: supervisor.id } }
+        let(:subject) { described_class.new params }
+        let(:message) { subject.account_setup }
+
+        it { expect(message.to).to eq [volunteer.email] }
+      end
+
+      context "when an admin id is provided" do
+        let(:params) { { id: admin.id } }
+        let(:subject) { described_class.new params }
+        let(:message) { subject.account_setup }
+
+        it { expect(message.to).to eq [volunteer.email] }
+      end
+    end
   end
 end
