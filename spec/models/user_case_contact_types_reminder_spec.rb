6abded83b2b51d15a4a3234e8eedf2e9efc5f793
Harsohail Brar <harsohail.brar1@gmail.com>
2022-06-06 21:30:53 -0700

    add test to UserCaseContactTypesReminder spec

--- spec/models/user_case_contact_types_reminder_spec.rb
+++ spec/models/user_case_contact_types_reminder_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
 RSpec.describe UserCaseContactTypesReminder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:user) }
 end
