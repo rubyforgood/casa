a059308309c2aac3a783dfe67f2316500e2cee8a
Andy Strube <andystrube64@gmail.com>
2022-06-13 15:00:35 -0700

    Added association tests

--- spec/models/checklist_item_spec.rb
+++ spec/models/checklist_item_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ChecklistItem, type: :model do
+  describe "associations" do
+    it { should belong_to(:hearing_type) }
+  end
+
   describe "validations" do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:category) }
