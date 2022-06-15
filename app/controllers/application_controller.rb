379b2404bfd8b2fc2763fea3e701992044921ce7
Xihai Luo <xihai.luo01@gmail.com>
2022-06-15 16:44:36 -0400

    refactor exception handling so twilio responses with error codes not nil are also handled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,8 +51,8 @@ class ApplicationController < ActionController::Base
     }
 
     begin
-      twilio.send_sms(req_params)
-      "sent"
+      twilio_res = twilio.send_sms(req_params)
+      return twilio_res.error_code.nil? ? "sent" : "error"
     rescue Twilio::REST::RestError
       "error"
     end
@@ -66,7 +66,7 @@ class ApplicationController < ActionController::Base
       return "New #{resource_name} created successfully. SMS not sent due to error."
     end
     if sms_status === "sent"
-      "New #{resource_name} created successfully. SMS has been sent!"
+      return "New #{resource_name} created successfully. SMS has been sent!"
     end
   end
 
