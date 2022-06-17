cadced8a55c6b345ff6e6ad83287b991e67b663a
firelemons <shenyang64@gmail.com>
2022-06-17 01:23:54 -0500

    minor patch note type change

--- db/seeds/patch_note_type_data.rb
+++ db/seeds/patch_note_type_data.rb
@@ -2,4 +2,4 @@
 
 PatchNoteType.where(name: "Coming Up").first_or_create
 PatchNoteType.where(name: "Fixes").first_or_create
-PatchNoteType.where(name: "What's New").first_or_create
+PatchNoteType.where(name: "What's New?").first_or_create
