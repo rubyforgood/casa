01cbb608cd8358f5062097b1a62dfcb9d6d7a4d0
Moussa Diop <mussa.joop@gmail.com>
2022-06-20 22:16:32 +0000

    Add nested attribute parameters

--- app/values/case_contact_parameters.rb
+++ app/values/case_contact_parameters.rb
@@ -14,7 +14,8 @@ class CaseContactParameters < SimpleDelegator
         :want_driving_reimbursement,
         :notes,
         case_contact_contact_type_attributes: [:contact_type_id],
-        additional_expense_attributes: [:id, :other_expense_amount, :other_expenses_describe]
+        additional_expense_attributes: [:id, :other_expense_amount, :other_expenses_describe],
+        casa_case_attributes: [:id, volunteers_attributes: [:id, address_attributes: [:id, :content]]]
       )
     new_params[:duration_minutes] = duration_minutes
     new_params[:miles_driven] = miles_driven
