73d59e385c266dc39a223b9fc703003d34c02504
firelemons <shenyang64@gmail.com>
2022-06-21 21:42:33 -0500

    create test record to fix tests

--- spec/models/patch_note_type_spec.rb
+++ spec/models/patch_note_type_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe PatchNoteType, type: :model do
+  let!(:patch_note_type) { create(:patch_note_type) }
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
 end
