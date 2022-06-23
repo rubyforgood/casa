657e7413f9e79b14cb4210a85731915e1857d4c2
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2022-06-23 14:15:44 -0300

    Fix flaky string order mismatching

--- spec/services/missing_data_export_csv_service_spec.rb
+++ spec/services/missing_data_export_csv_service_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe MissingDataExportCsvService do
 
   describe "#perform" do
     it "returns a string formatted as csv" do
-      expect(result).to eq("Casa Case Number,Youth Birth Month And Year,Upcoming Hearing Date,Court Orders\n"\
-        "#{casa_cases[0].case_number},OK,MISSING,MISSING\n"\
-        "#{casa_cases[1].case_number},OK,MISSING,MISSING\n"\
-        "#{casa_cases[2].case_number},OK,MISSING,MISSING\n")
+      expect(result).to match("Casa Case Number,Youth Birth Month And Year,Upcoming Hearing Date,Court Orders\n")
+      expect(result).to match("#{casa_cases[0].case_number},OK,MISSING,MISSING\n")
+      expect(result).to match("#{casa_cases[1].case_number},OK,MISSING,MISSING\n")
+      expect(result).to match("#{casa_cases[2].case_number},OK,MISSING,MISSING\n")
     end
   end
 end
