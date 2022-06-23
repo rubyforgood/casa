d94676134e805aef5517ec02383f43ffe8f19934
Andy Strube <andystrube64@gmail.com>
2022-06-23 12:20:04 -0700

    Refactored checklist item policy

--- app/policies/checklist_item_policy.rb
+++ app/policies/checklist_item_policy.rb
@@ -1,2 +1,21 @@
 class ChecklistItemPolicy < ApplicationPolicy
+  def new?
+    is_admin?
+  end
+
+  def create?
+    is_admin?
+  end
+
+  def edit?
+    is_admin?
+  end
+
+  def update?
+    is_admin?
+  end
+
+  def destroy?
+    is_admin?
+  end
 end
