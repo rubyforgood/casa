ccddceff46d5873d42fa0e56b6097a1888180617
Xihai Luo <xihai.luo01@gmail.com>
2022-06-24 15:06:11 -0400

    refactor sms body helper function to dynamically create body message with short links provided

--- app/helpers/sms_body_helper.rb
+++ app/helpers/sms_body_helper.rb
@@ -1,6 +1,17 @@
 module SmsBodyHelper
-  def account_activation_msg(resource = "primorgens", base_url = "hello kitty")
-    "A CASA #{resource} account was created for you.
-      Visit #{base_url + "/users/edit"} to change text messaging settings."
+  def account_activation_msg(resource = "primorgens", base_url = "hello kitty", hash_of_links = {})
+    first_msg = "A CASA #{resource} account was created for you."
+    second_msg = " First, set your password here #{hash_of_links[0]}."
+    third_msg = " Then visit #{hash_of_links[1]}" + "/users/edit" + " to change your text message settings."
+
+    if hash_of_links[0] && hash_of_links[1]
+      return first_msg + second_msg + third_msg
+    elsif hash_of_links[0] == nil && hash_of_links[1]
+      return first_msg + " Please check your email to set up your password." + third_msg
+    elsif hash_of_links[0] && hash_of_links[1] == nil
+      return first_msg + second_msg + " Go to profile edit page to change SMS settings."
+    else
+      return first_msg + " Please check your email to set up your password. Go to profile edit page to change SMS settings."
+    end
   end
 end
