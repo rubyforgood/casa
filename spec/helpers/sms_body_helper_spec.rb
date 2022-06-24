45f38ff0c3aa542567697f23d6843d290b6e4b58
Xihai Luo <xihai.luo01@gmail.com>
2022-06-24 15:05:18 -0400

    add helper spec for sms body text

---
+++ spec/helpers/sms_body_helper_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe SmsBodyHelper do
+  describe "#account_activation_msg" do
+    it "correct short links provided" do
+      expected_response = account_activation_msg("primogems", "hello doggy", {0 => "www.pasta.com", 1 => "www.yogurt.com"})
+      expect(expected_response).to include("First, set your password here www.pasta.com. Then visit www.yogurt.com/users/edit to change your text message settings.")
+    end
+
+    it "incorrect short links provided" do
+      expected_response = account_activation_msg("primogems", "hello kitty", {0 => nil, 1 => nil})
+      expect(expected_response).to include("Please check your email to set up your password. Go to profile edit page to change SMS settings.")
+    end
+
+    it "set up password link invalid" do
+      expected_response = account_activation_msg("primogems", "hello kitty", {0 => nil, 1 => "www.carfax.com"})
+      expect(expected_response).to include("Please check your email to set up your password. Then visit www.carfax.com/users/edit to change your text message settings.")
+    end
+
+    it "link to users/edit invalid" do
+      expected_response = account_activation_msg("primogems", "hello kitty", {0 => "www.yummy.com", 1 => nil})
+      expect(expected_response).to include("First, set your password here www.yummy.com. Go to profile edit page to change SMS settings.")
+    end
+  end
+end
