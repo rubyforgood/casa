abe80bf683d6efee42ca1618aec9bad7e11fbb5c
andreLumor <andre.moreira@codeminer42.com>
2022-06-29 18:03:04 -0300

    require hearing type name

---
+++ spec/views/hearing_types/edit.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "hearing_types/edit", type: :view do
+  let(:admin) { build_stubbed(:casa_admin) }
+
+  before do
+    assign :hearing_type, HearingType.new
+    sign_in admin
+
+    render template: "hearing_types/edit"
+  end
+
+  it "shows edit hearing type form" do
+    expect(rendered).to have_text("Edit")
+    expect(rendered).to have_selector("input", id: "hearing_type_name")
+    expect(rendered).to have_selector("input", id: "hearing_type_active")
+    expect(rendered).to have_selector("input[type=submit]")
+  end
+
+  it "requires name text_field" do
+    expect(rendered).to have_selector("input[required=required]", id: "hearing_type_name")
+  end
+end
---
+++ spec/views/hearing_types/new.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "hearing_types/new", type: :view do
+  let(:admin) { build_stubbed(:casa_admin) }
+
+  before do
+    assign :hearing_type, HearingType.new
+    sign_in admin
+
+    render template: "hearing_types/new"
+  end
+
+  it "shows new hearing type form" do
+    expect(rendered).to have_text("New Hearing Type")
+    expect(rendered).to have_selector("input", id: "hearing_type_name")
+    expect(rendered).to have_selector("input", id: "hearing_type_active")
+    expect(rendered).to have_selector("input[type=submit]")
+  end
+
+  it "requires name text_field" do
+    expect(rendered).to have_selector("input[required=required]", id: "hearing_type_name")
+  end
+end
