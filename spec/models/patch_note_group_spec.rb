4a3ac159c60fb4dd661dfc94d46150e28c5066ef
firelemons <shenyang64@gmail.com>
2022-06-28 21:24:36 -0500

    fix case sensitivity test

--- spec/models/patch_note_group_spec.rb
+++ spec/models/patch_note_group_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe PatchNoteGroup, type: :model do
-  let!(:patch_note_group) { create(:patch_note_group) }
+  let!(:patch_note_group) { create(:patch_note_group, value: "test") }
 
   it { is_expected.to validate_uniqueness_of(:value) }
   it { is_expected.to validate_presence_of(:value) }
