63fe3d586e6278ce048f3b9c4fe531a315d6805c
7riumph <triumpharamunde@gmail.com>
2022-07-03 23:48:10 -0600

    Fetch 'display_name' and format followup edit page url

--- app/notifications/delivery_methods/sms.rb
+++ app/notifications/delivery_methods/sms.rb
@@ -1,8 +1,22 @@
 class DeliveryMethods::Sms < Noticed::DeliveryMethods::Base
   def deliver
     twilio = TwilioService.new(recipient.casa_org.twilio_api_key_sid, recipient.casa_org.twilio_api_key_secret, recipient.casa_org.twilio_account_sid)
-    req_params = {From: recipient.casa_org.twilio_phone_number, Body: "-\n \n[supervisor/admin name] has flagged a Case Contact that needs follow up. Click to see more: [link]", To: recipient.phone_number}
+    req_params = {From: recipient.casa_org.twilio_phone_number, Body: "-\n \n#{supervisor_or_admin_display_name} has flagged a Case Contact that needs follow up. Click to see more: #{url}", To: recipient.phone_number}
     twilio_res = twilio.send_sms(req_params)
+  end
+
+  def url
+    Rails.application.credentials[:BASE_URL] + "/case_contacts/" + case_contact_id.to_s + "/edit"
+  end
+
+  private
+
+  def supervisor_or_admin_display_name
+    params[:created_by][:display_name]
+  end
+
+  def case_contact_id
+    params[:followup][:case_contact_id]
   end
 
   # You may override this method to validate options for the delivery method
