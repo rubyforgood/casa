bc92a82af7847817764bd0d440b3ea581669c104
7riumph <triumpharamunde@gmail.com>
2022-07-03 23:26:13 -0600

    Added '.send_sms' method and its parameters

--- app/notifications/delivery_methods/sms.rb
+++ app/notifications/delivery_methods/sms.rb
@@ -1,6 +1,8 @@
 class DeliveryMethods::Sms < Noticed::DeliveryMethods::Base
   def deliver
     twilio = TwilioService.new(recipient.casa_org.twilio_api_key_sid, recipient.casa_org.twilio_api_key_secret, recipient.casa_org.twilio_account_sid)
+    req_params = {From: recipient.casa_org.twilio_phone_number, Body: "-\n \n[supervisor/admin name] has flagged a Case Contact that needs follow up. Click to see more: [link]", To: recipient.phone_number}
+    twilio_res = twilio.send_sms(req_params)
   end
 
   # You may override this method to validate options for the delivery method
