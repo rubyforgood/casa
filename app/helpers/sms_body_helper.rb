eb666ba034a894a399ddafa21f1d8498f8e9b2ed
Harsohail Brar <harsohail.brar1@gmail.com>
2022-07-03 17:14:16 -0700

    lint

--- app/helpers/sms_body_helper.rb
+++ app/helpers/sms_body_helper.rb
@@ -19,7 +19,7 @@ module SmsBodyHelper
   end
 
   def court_report_due_msg(report_due_date, short_link)
-     "Your court report is due on #{report_due_date}. Generate a court report to complete & submit here: #{short_link}"
+    "Your court report is due on #{report_due_date}. Generate a court report to complete & submit here: #{short_link}"
   end
 
   def no_contact_made_msg(contact_type, short_link)
