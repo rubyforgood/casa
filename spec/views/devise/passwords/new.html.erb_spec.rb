f3ce7c86133b4e75dace86021fb0a7935c2f6b32
Xihai Luo <xihai.luo01@gmail.com>
2022-07-10 10:38:14 -0400

    create view test to verify email and phone number fields show up

---
+++ spec/views/devise/passwords/new.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "users/password/new", type: :view do
+  it "displays title" do
+    render template: "devise/passwords/new"
+    expect(rendered).to have_text("Forgot your password?")
+  end
+
+  it "displays contact fields for user to reset password" do
+    render template: "devise/passwords/new"
+    expect(rendered).to have_text("Email")
+    expect(rendered).to have_field("user_email")
+    expect(rendered).to have_text("Phone number")
+    expect(rendered).to have_field("user_phone_number")
+  end
+end
