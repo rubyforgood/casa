a2d80741c94711dd24d7b3e3bfd495b702593a32
Xihai Luo <xihai.luo01@gmail.com>
2022-07-10 11:32:27 -0400

    create system tests to verify fields can handle correct inputs

---
+++ spec/system/devise/passwords/new_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "users/passwords/new", type: :system do
+  before do
+    visit root_path
+    click_on "Forgot your password?"
+  end
+
+  it "displays error messages for non-existent user" do
+    fill_in "Email", with: "tangerine@forward.com"
+    fill_in "Phone number", with: "+16578900012"
+
+    click_on "Send me reset password instructions"
+    expect(page).to have_content "2 errors prohibited this User from being saved:"
+    expect(page).to have_text("Email not found")
+    expect(page).to have_text("Phone number not found")
+  end
+
+  it "displays phone number error messages for incorrect formatting" do
+    create(:user, email: "glados@aperture.labs")
+    fill_in "Email", with: "glados@aperture.labs"
+    fill_in "Phone number", with: "2134567eee"
+
+    click_on "Send me reset password instructions"
+    expect(page).to have_content "1 error prohibited this User from being saved:"
+    expect(page).to have_text("Phone number must be 12 digits including country code (+1)")
+  end
+end
