c2b8c8a3fa9526351ebba19ef9057c2543b3a9e7
compwron <compwron@users.noreply.github.com>
2022-07-23 17:00:04 -0700

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -451,6 +451,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_022147) do
     t.datetime "reminder_sent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "case_contact_types", precision: nil
     t.index ["user_id"], name: "index_user_reminder_times_on_user_id"
   end
 
