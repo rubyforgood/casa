7c36ce44b822d1b4eb7a6728c1a74d6044419a5f
firelemons <shenyang64@gmail.com>
2022-08-06 10:15:32 -0500

    click event attached to right element, can get patch note text

--- app/javascript/src/all_casa_admin/patch_notes.js
+++ app/javascript/src/all_casa_admin/patch_notes.js
@@ -6,5 +6,13 @@ $('document').ready(() => {
   const asyncNotificationsElement = $('#async-notifications')
   patchNotePage.notifier = new Notifier(asyncNotificationsElement)
 
+  const newPatchNoteElement = $('#new-patch-note')
+
+  newPatchNoteElement.children('button').click(() => {
+    console.log(`Patch Note: ${ newPatchNoteElement.children('textarea').val() }`)
+  })
+
+  console.log(newPatchNoteElement)
+
   patchNotePage.notifier.notify('test', 'info')
 })
