bdefb8918c323592996a7374cdfc73b080a82318
Firelemons <shenyang64@gmail.com>
2022-08-06 14:29:50 -0500

    disable dropdowns after form submit

--- app/javascript/src/all_casa_admin/patch_notes.js
+++ app/javascript/src/all_casa_admin/patch_notes.js
@@ -7,11 +7,20 @@ $('document').ready(() => {
   patchNotePage.notifier = new Notifier(asyncNotificationsElement)
 
   const newPatchNoteElement = $('#new-patch-note')
+  const newPatchNoteGroupDropdown = $('#new-patch-note-group')
+  const newPatchNoteTypeDropdown = $('#new-patch-note-type')
+
+  const disableNewPatchNoteForm = () => {
+    newPatchNoteGroupDropdown.prop('disabled', true)
+    newPatchNoteTypeDropdown.prop('disabled', true)
+  }
 
   newPatchNoteElement.children('button').click(() => {
     console.log(`Patch Note: ${newPatchNoteElement.children('textarea').val()}`)
-    console.log(`Patch Note Group ID: ${$('#new-patch-note-group').val()}`)
-    console.log(`Patch Note Type ID: ${$('#new-patch-note-type').val()}`)
+    console.log(`Patch Note Group ID: ${newPatchNoteGroupDropdown.val()}`)
+    console.log(`Patch Note Type ID: ${newPatchNoteTypeDropdown.val()}`)
+
+    disableNewPatchNoteForm()
   })
 
   patchNotePage.notifier.notify('test', 'info')
