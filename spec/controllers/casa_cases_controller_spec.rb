0a722cd6c137161b07bcfb37a814c451cd4ec864
Zeeshan Haider <haider_csharp@hotmail.com>
2022-08-08 18:59:41 -0400

    used double quotes instead of single

--- spec/controllers/casa_cases_controller_spec.rb
+++ spec/controllers/casa_cases_controller_spec.rb
@@ -1,18 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe CasaCasesController, type: :controller do
   let(:organization) { create(:casa_org) }
   let(:admin) { create(:casa_admin) }
   let!(:casa_case) { create(:casa_case, casa_org: organization) }
 
-  context 'when logged in as an admin user' do
+  context "when logged in as an admin user" do
     before do
       sign_in admin
     end
 
-    describe '#deactivate' do
-      context 'when request formate is json' do
-        it 'should deactivate the casa_case when provided valid case_id' do
+    describe "#deactivate" do
+      context "when request formate is json" do
+        it "should deactivate the casa_case when provided valid case_id" do
           patch :deactivate, format: :json, params: {
             id: casa_case.id
           }
@@ -21,7 +21,7 @@ RSpec.describe CasaCasesController, type: :controller do
           expect(response.body).to eq "Case #{casa_case.case_number} has been deactivated."
         end
 
-        it 'should return 404 error when provided invalid case_id' do
+        it "should return 404 error when provided invalid case_id" do
           patch :deactivate, format: :json, params: {
             id: 123
           }
@@ -29,8 +29,8 @@ RSpec.describe CasaCasesController, type: :controller do
         end
       end
 
-      context 'when request formate is html' do
-        it 'should deactivate the casa_case when provided valid case_id' do
+      context "when request formate is html" do
+        it "should deactivate the casa_case when provided valid case_id" do
           patch :deactivate, params: {
             id: casa_case.id
           }
@@ -42,9 +42,9 @@ RSpec.describe CasaCasesController, type: :controller do
       end
     end
 
-    describe '#reactivate' do
-      context 'when request formate is json' do
-        it 'should reactivate the casa_case when provided valid case_id' do
+    describe "#reactivate" do
+      context "when request formate is json" do
+        it "should reactivate the casa_case when provided valid case_id" do
           patch :reactivate, format: :json, params: {
             id: casa_case.id
           }
@@ -53,7 +53,7 @@ RSpec.describe CasaCasesController, type: :controller do
           expect(response.body).to eq "Case #{casa_case.case_number} has been reactivated."
         end
 
-        it 'should return 404 error when provided invalid case_id' do
+        it "should return 404 error when provided invalid case_id" do
           patch :reactivate, format: :json, params: {
             id: 123
           }
@@ -61,8 +61,8 @@ RSpec.describe CasaCasesController, type: :controller do
         end
       end
 
-      context 'when request formate is html' do
-        it 'should reactivate the casa_case when provided valid case_id' do
+      context "when request formate is html" do
+        it "should reactivate the casa_case when provided valid case_id" do
           patch :reactivate, params: {
             id: casa_case.id
           }
