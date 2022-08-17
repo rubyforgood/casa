8f4b9694c2a06b3ed29c624645a2b489b8d5319f
firelemons <shenyang64@gmail.com>
2022-08-16 21:20:23 -0500

    created tests to assert new patch note form elements exist

--- spec/views/all_casa_admins/patch_notes/index.html.erb_spec.rb
+++ spec/views/all_casa_admins/patch_notes/index.html.erb_spec.rb
@@ -27,5 +27,37 @@ RSpec.describe "patch_notes/index", type: :view do
 
       expect(parsed_html.css("#new-patch-note").length).to eq(1)
     end
+
+    it "contains a button to submit the form" do
+      render template: "all_casa_admins/patch_notes/index"
+
+      parsed_html = Nokogiri.HTML5(rendered)
+
+      expect(parsed_html.css("#new-patch-note button").length).to eq(1)
+    end
+
+    it "contains a textarea to enter the patch note" do
+      render template: "all_casa_admins/patch_notes/index"
+
+      parsed_html = Nokogiri.HTML5(rendered)
+
+      expect(parsed_html.css("#new-patch-note textarea").length).to eq(1)
+    end
+
+    it "contains a dropdown for the patch note type" do
+      render template: "all_casa_admins/patch_notes/index"
+
+      parsed_html = Nokogiri.HTML5(rendered)
+
+      expect(parsed_html.css("#new-patch-note #new-patch-note-type").length).to eq(1)
+    end
+
+    it "contains a dropdown for the patch note group" do
+      render template: "all_casa_admins/patch_notes/index"
+
+      parsed_html = Nokogiri.HTML5(rendered)
+
+      expect(parsed_html.css("#new-patch-note #new-patch-note-group").length).to eq(1)
+    end
   end
 end
