64433a33caa3beb5c34fe012ef06abb3a9a966c7
Andy Strube <andystrube64@gmail.com>
2022-08-26 18:18:37 -0700

    Added system tests for checklist item create functionality

---
+++ spec/system/checklist_items/new_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "checklist_items/new", type: :system do
+  let(:casa_admin) { create(:casa_admin) }
+  let(:hearing_type) { create(:hearing_type) }
+
+  before do
+    sign_in casa_admin
+    visit new_hearing_type_checklist_item_path(hearing_type)
+  end
+
+  it "creates with valid data", :aggregate_failures do
+    fill_in "Category", with: "checklist item category"
+    fill_in "Description", with: "checklist item description"
+    click_on "Submit"
+
+    expect(page).to have_text("Checklist item was successfully created.")
+    expect(page).to have_text("checklist item category")
+    expect(page).to have_text("checklist item description")
+  end
+
+  it "rejects with invalid data" do
+    fill_in "Category", with: ""
+    fill_in "Description", with: ""
+    click_on "Submit"
+
+    expect(page).to have_text("Add a new checklist item")
+  end
+end
