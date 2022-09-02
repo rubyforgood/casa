afbd6f95e4d765c2e7cf39fee5f9a3e10a4ef827
Andy Strube <andystrube64@gmail.com>
2022-09-01 17:11:55 -0700

    Small refactor

--- spec/system/checklist_items/destroy_spec.rb
+++ spec/system/checklist_items/destroy_spec.rb
@@ -2,11 +2,10 @@ require "rails_helper"
 
 RSpec.describe "checklist_items/destroy", type: :system do
   let(:casa_admin) { create(:casa_admin) }
-  let(:hearing_type) { create(:hearing_type) }
   let(:checklist_item) { create(:checklist_item) }
+  let(:hearing_type) { create(:hearing_type, checklist_items: [checklist_item]) }
 
   before do
-    hearing_type.checklist_items.push(checklist_item)
     sign_in casa_admin
     visit edit_hearing_type_path(hearing_type)
   end
