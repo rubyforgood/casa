e7837e6902ad617f16698526f911ea672c2baa35
firelemons <shenyang64@gmail.com>
2022-09-10 00:19:02 -0500

    new patch notes can be deleted

--- app/javascript/src/all_casa_admin/patch_notes.js
+++ app/javascript/src/all_casa_admin/patch_notes.js
@@ -9,47 +9,6 @@ jQuery.ajaxSetup({
   }
 })
 
-// Inserts a patch note display after the create patch note form in the patch note list and styles it as new
-//  @param    {number} patchNoteGroupId  The id of the group allowed to view the patch note
-//  @param    {jQuery} patchNoteList     A jQuery object representing the patch note list
-//  @param    {string} patchNoteText     The text of the patch note
-//  @param    {number} patchNoteTypeId   The id of the patch note type
-//  @throws   {TypeError}  for a parameter of the incorrect type
-//  @throws   {RangeError} if an id parameter is negative
-function addPatchNoteUI (patchNoteGroupId, patchNoteId, patchNoteList, patchNoteText, patchNoteTypeId) {
-  TypeChecker.checkPositiveInteger(patchNoteGroupId, 'patchNoteGroupId')
-  TypeChecker.checkPositiveInteger(patchNoteId, 'patchNoteId')
-  TypeChecker.checkPositiveInteger(patchNoteTypeId, 'patchNoteTypeId')
-  TypeChecker.checkNonEmptyJQueryObject(patchNoteList, 'patchNoteList')
-  TypeChecker.checkString(patchNoteText, 'patchNoteText')
-
-  const newPatchNoteForm = patchNoteList.children().eq(1)
-
-  if (!(newPatchNoteForm.length)) {
-    throw new ReferenceError('Could not find new patch note form')
-  }
-
-  const newPatchNoteUI = newPatchNoteForm.clone()
-  const newPatchNoteUIFormInputs = getPatchNoteFormInputs(newPatchNoteUI.children())
-
-  newPatchNoteUI.addClass('new')
-  newPatchNoteUIFormInputs.noteTextArea.val(patchNoteText)
-  newPatchNoteUIFormInputs.dropdownGroup.children().removeAttr('selected')
-  newPatchNoteUIFormInputs.dropdownGroup.children(`option[value="${patchNoteGroupId}"]`).attr('selected', true)
-  newPatchNoteUIFormInputs.dropdownType.children().removeAttr('selected')
-  newPatchNoteUIFormInputs.dropdownType.children(`option[value="${patchNoteTypeId}"]`).attr('selected', true)
-  newPatchNoteUIFormInputs.buttonControls.parent().html(`
-    <button type="button" class="btn btn-primary button-edit">
-      <i class="fa-solid fa-pen-to-square"></i>Edit
-    </button>
-    <button type="button" class="btn btn-danger button-delete">
-      <i class="fa-solid fa-trash-can"></i>Delete
-    </button>
-  `)
-
-  newPatchNoteForm.after(newPatchNoteUI)
-}
-
 // Creates a patch note
 //  @param    {number} patchNoteGroupId  The id of the group allowed to view the patch note
 //  @param    {string} patchNoteText     The text of the patch note
@@ -178,6 +137,73 @@ function resolveAsyncOperation (error) {
   pageNotifier.stopAsyncOperation(error)
 }
 
+function onDeletePatchNote () {
+  const patchNoteFormContainer = $(this).parent().parent()
+  const formInputs = getPatchNoteFormInputs(patchNoteFormContainer)
+
+  disablePatchNoteForm(formInputs)
+
+  deletePatchNote(
+    Number.parseInt(patchNoteFormContainer.attr('id').match(/patch-note-(\d+)/)[1])
+  ).then(function () {
+    patchNoteFormContainer.parent().remove()
+  }).fail(function () {
+    enablePatchNoteForm(formInputs)
+  })
+}
+
+// Add event listeners to a patch note form
+//  @param {object} patchNoteForm An jQuery object representing the patch note form
+//  @throws   {TypeError}  for a parameter of the incorrect type
+function initPatchNoteForm (patchNoteForm) {
+  TypeChecker.checkNonEmptyJQueryObject(patchNoteForm)
+
+  patchNoteForm.find('.button-delete').click(onDeletePatchNote)
+}
+
+// Inserts a patch note display after the create patch note form in the patch note list and styles it as new
+//  @param    {number} patchNoteGroupId  The id of the group allowed to view the patch note
+//  @param    {jQuery} patchNoteList     A jQuery object representing the patch note list
+//  @param    {string} patchNoteText     The text of the patch note
+//  @param    {number} patchNoteTypeId   The id of the patch note type
+//  @throws   {TypeError}  for a parameter of the incorrect type
+//  @throws   {RangeError} if an id parameter is negative
+function addPatchNoteUI (patchNoteGroupId, patchNoteId, patchNoteList, patchNoteText, patchNoteTypeId) {
+  TypeChecker.checkPositiveInteger(patchNoteGroupId, 'patchNoteGroupId')
+  TypeChecker.checkPositiveInteger(patchNoteId, 'patchNoteId')
+  TypeChecker.checkPositiveInteger(patchNoteTypeId, 'patchNoteTypeId')
+  TypeChecker.checkNonEmptyJQueryObject(patchNoteList, 'patchNoteList')
+  TypeChecker.checkString(patchNoteText, 'patchNoteText')
+
+  const newPatchNoteForm = patchNoteList.children().eq(1)
+
+  if (!(newPatchNoteForm.length)) {
+    throw new ReferenceError('Could not find new patch note form')
+  }
+
+  const newPatchNoteUI = newPatchNoteForm.clone()
+  const newPatchNoteUIFormInputs = getPatchNoteFormInputs(newPatchNoteUI.children())
+
+  newPatchNoteUI.addClass('new')
+  newPatchNoteUI.children().attr('id', `patch-note-${patchNoteId}`)
+  newPatchNoteUIFormInputs.noteTextArea.val(patchNoteText)
+  newPatchNoteUIFormInputs.dropdownGroup.children().removeAttr('selected')
+  newPatchNoteUIFormInputs.dropdownGroup.children(`option[value="${patchNoteGroupId}"]`).attr('selected', true)
+  newPatchNoteUIFormInputs.dropdownType.children().removeAttr('selected')
+  newPatchNoteUIFormInputs.dropdownType.children(`option[value="${patchNoteTypeId}"]`).attr('selected', true)
+  newPatchNoteUIFormInputs.buttonControls.parent().html(`
+    <button type="button" class="btn btn-primary button-edit">
+      <i class="fa-solid fa-pen-to-square"></i>Edit
+    </button>
+    <button type="button" class="btn btn-danger button-delete">
+      <i class="fa-solid fa-trash-can"></i>Delete
+    </button>
+  `)
+
+  newPatchNoteForm.after(newPatchNoteUI)
+  initPatchNoteForm(newPatchNoteUI)
+}
+
 $('document').ready(() => {
   if (!(window.location.pathname.includes('patch_notes'))) {
     return
@@ -218,20 +244,7 @@ $('document').ready(() => {
       })
     })
 
-    $('#patch-note-list .button-delete').click(function () {
-      const patchNoteFormContainer = $(this).parent().parent()
-      const formInputs = getPatchNoteFormInputs(patchNoteFormContainer)
-
-      disablePatchNoteForm(formInputs)
-
-      deletePatchNote(
-        Number.parseInt(patchNoteFormContainer.attr('id').match(/patch-note-(\d+)/)[1])
-      ).then(function () {
-        patchNoteFormContainer.parent().remove()
-      }).fail(function () {
-        enablePatchNoteForm(formInputs)
-      })
-    })
+    $('#patch-note-list .button-delete').click(onDeletePatchNote)
   } catch (err) {
     pageNotifier.notify('Could not intialize app', 'error')
     pageNotifier.notify(err.message, 'error')
