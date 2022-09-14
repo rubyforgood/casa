8453e39f5eb99cd490bae7ec71f6164a51754557
firelemons <shenyang64@gmail.com>
2022-09-13 22:35:42 -0500

    js build outputs information on watched rebuilds

--- bin/asset_bundling_scripts/build_js.js
+++ bin/asset_bundling_scripts/build_js.js
@@ -6,11 +6,23 @@ const isWatching = CLIArgs.includes('--watch')
 const esbuild = require('esbuild')
 const logger = require('./logger.js')
 
+const watchValue = isWatching ? {
+  onRebuild(error, result) {
+    if (error) {
+      logger.error('watch build failed:')
+      logger.error(error.stack)
+    } else {
+      logger.info('watch build succeeded:')
+      logger.info(JSON.stringify(result))
+    }
+  }
+} : false
+
 esbuild.build({
   entryPoints: ['app/javascript/application.js', 'app/javascript/all_casa_admin.js'],
   outdir: 'app/assets/builds',
   bundle: true,
-  watch: isWatching
+  watch: watchValue
 }).then(result => {
   logger.info('application.js, all_casa_admin.js built successfully')
 
