96d2c043fa36c62c9e5f1cee595533addf51ab12
elhalvers <ericleifhalverson@gmail.com>
2022-09-13 19:30:57 -0700

    Refactor system spec

--- spec/system/all_casa_admins/casa_orgs/show_spec.rb
+++ spec/system/all_casa_admins/casa_orgs/show_spec.rb
@@ -1,17 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "all_casa_admins/casa_orgs/show", type: :system do
-  let(:all_casa_admin) { build_stubbed(:all_casa_admin, email: "theexample@example.com") }
-  let(:organization) { create(:casa_org) }
-
-  before do
+  it "has link for New CASA Admin" do
+    all_casa_admin = build_stubbed(:all_casa_admin, email: "theexample@example.com")
+    organization = create(:casa_org)
     sign_in all_casa_admin
     visit all_casa_admins_casa_org_path(organization)
-  end
-
-  it "has link for New CASA Admin" do
     click_on "New CASA Admin"
-
     expect(page).to have_field("Email")
   end
 end
