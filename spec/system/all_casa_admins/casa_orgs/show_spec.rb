5357391462a3e080c41e069acdee4eaa2cf3b245
elhalvers <ericleifhalverson@gmail.com>
2022-09-13 19:59:46 -0700

    Add link show organization page back to all orgs

--- spec/system/all_casa_admins/casa_orgs/show_spec.rb
+++ spec/system/all_casa_admins/casa_orgs/show_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe "all_casa_admins/casa_orgs/show", type: :system do
     click_on "New CASA Admin"
     expect(page).to have_field("Email")
   end
+
+  it "has link to return to all organizations", js: true do
+    all_casa_admin = create(:all_casa_admin, email: "theexample@example.com")
+    organization = create(:casa_org)
+    sign_in all_casa_admin
+    visit all_casa_admins_casa_org_path(organization)
+    click_on "Return to Organizations"
+    expect(page).to have_text("CASA Organizations")
+  end
 end
