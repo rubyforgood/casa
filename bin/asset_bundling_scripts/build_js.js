1c7ab9d83fc5088c1dd0358ca2a12054c0de3086
firelemons <shenyang64@gmail.com>
2022-09-20 07:42:52 -0500

    linter

--- bin/asset_bundling_scripts/build_js.js
+++ bin/asset_bundling_scripts/build_js.js
@@ -6,17 +6,19 @@ const isWatching = CLIArgs.includes('--watch')
 const esbuild = require('esbuild')
 const logger = require('./logger.js')
 
-const watchValue = isWatching ? {
-  onRebuild(error, result) {
-    if (error) {
-      logger.error('watch build failed:')
-      logger.error(error.stack)
-    } else {
-      logger.info('watch build succeeded:')
-      logger.info(JSON.stringify(result, null, 2))
+const watchValue = isWatching
+  ? {
+      onRebuild (error, result) {
+        if (error) {
+          logger.error('watch build failed:')
+          logger.error(error.stack)
+        } else {
+          logger.info('watch build succeeded:')
+          logger.info(JSON.stringify(result, null, 2))
+        }
+      }
     }
-  }
-} : false
+  : false
 
 esbuild.build({
   entryPoints: ['app/javascript/application.js', 'app/javascript/all_casa_admin.js'],
