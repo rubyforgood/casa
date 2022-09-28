dfcc0abd8621a945583693fe03ad230edcf2e40f
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-28 14:05:40 -0300

    bug: court_report_due_date isn't being added or updated
    because it is a column inside casa_case, and now it is
    in a form that updates/creates court_dates. I'm trying
    to figure out how to create nested forms, that might work?

--- app/models/court_date.rb
+++ app/models/court_date.rb
@@ -11,6 +11,7 @@ class CourtDate < ApplicationRecord
   belongs_to :judge, optional: true
 
   accepts_nested_attributes_for :case_court_orders, reject_if: :all_blank
+  accepts_nested_attributes_for :casa_case
 
   scope :ordered_ascending, -> { order("date asc") }
 
