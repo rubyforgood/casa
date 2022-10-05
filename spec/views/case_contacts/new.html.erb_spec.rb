135e366439142d9f971565e6e725857f406fb995
Libby Rodriguez <librod89@gmail.com>
2022-10-05 10:38:35 -0600

    Load case_contacts/new when the case has no volunteers

--- spec/views/case_contacts/new.html.erb_spec.rb
+++ spec/views/case_contacts/new.html.erb_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.describe "case_contacts/new", type: :view do
   subject { render template: "case_contacts/new" }
+  let(:casa_org) { CasaOrg.first }
+  let(:current_time) { Time.zone.now.strftime("%Y-%m-%d") }
 
   before do
     case_contact = build_stubbed(:case_contact)
@@ -10,20 +12,32 @@ RSpec.describe "case_contacts/new", type: :view do
     assign :selected_cases, [case_contact.casa_case]
     assign :contact_types, []
     assign :current_organization_groups, []
+
+    allow(view).to receive(:current_organization).and_return(casa_org)
+    allow(view).to receive(:current_role).and_return(role)
   end
 
   context "while signed-in as a volunteer" do
-    let(:casa_org) { CasaOrg.first }
     let(:role) { "Volunteer" }
+
     before do
       sign_in_as_volunteer
-      allow(view).to receive(:current_organization).and_return(casa_org)
-      allow(view).to receive(:current_role).and_return(role)
     end
-
-    let(:current_time) { Time.zone.now.strftime("%Y-%m-%d") }
 
     it { is_expected.to have_field("c. Occurred On", with: current_time) }
     it { is_expected.to have_selector("textarea", id: "case_contact_notes") }
+  end
+
+  context "while signed-in as an admin" do
+    let(:role) { "Casa Admin" }
+
+    before do
+      sign_in_as_admin
+    end
+
+    context "when the case has no volunteers" do
+      it { is_expected.to have_field("c. Occurred On", with: current_time) }
+      it { is_expected.to have_selector("textarea", id: "case_contact_notes") }
+    end
   end
 end
