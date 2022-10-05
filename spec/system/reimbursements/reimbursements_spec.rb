8b7b68d20c71c658abefa895ac56b72dc570ee3c
Libby Rodriguez <librod89@gmail.com>
2022-10-05 14:12:43 -0600

    convert volunteers dropdown to searchable select

--- spec/system/reimbursements/reimbursements_spec.rb
+++ spec/system/reimbursements/reimbursements_spec.rb
@@ -4,14 +4,15 @@ require "rails_helper"
 
 RSpec.describe "reimbursements", type: :system do
   let(:admin) { create(:casa_admin) }
+  let!(:contact1) { create(:case_contact, :wants_reimbursement) }
+  let!(:contact2) { create(:case_contact, :wants_reimbursement) }
+
+  before do
+    sign_in admin
+    visit reimbursements_path
+  end
 
   it "shows reimbursements", js: true do
-    sign_in admin
-
-    contact1 = create(:case_contact, :wants_reimbursement)
-    contact2 = create(:case_contact, :wants_reimbursement)
-
-    visit reimbursements_path
     expect(page).to have_content("Needs Review")
     expect(page).to have_content("Reimbursement Complete")
     expect(page).to have_content("Occurred At")
@@ -20,10 +21,22 @@ RSpec.describe "reimbursements", type: :system do
   end
 
   it "shows pagination", js: true do
-    sign_in admin
-
-    visit reimbursements_path
     expect(page).to have_content("Previous")
     expect(page).to have_content("Next")
+  end
+
+  it "filters by volunteers", js: true do
+    expect(page).to have_selector("#reimbursements-datatable tbody tr", count: 2)
+
+    page.find(".select2-search__field").click
+    send_keys(contact1.creator.display_name)
+    send_keys(:enter)
+
+    expect(page).to have_selector("#reimbursements-datatable tbody tr", count: 1)
+    expect(page).to have_content contact1.creator.display_name
+
+    page.find(".select2-selection__choice__remove").click
+
+    expect(page).to have_selector("#reimbursements-datatable tbody tr", count: 2)
   end
 end
