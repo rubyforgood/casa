1e39e6f10b515d4e609fc3b41f86f739a83d1ed4
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-10-04 23:03:23 -0300

    added a test for our migrate_court_report_due_date_service

--- spec/lib/tasks/migrate_court_report_due_date_service_spec.rb
+++ spec/lib/tasks/migrate_court_report_due_date_service_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe MigrateCourtReportDueDateService do
   it "should update dates" do
     casa_case_without_court_dates = create :casa_case
     casa_case_with_court_date = create :casa_case, :with_upcoming_court_date
+    casa_case_with_court_date.court_report_due_date = Date.today
+    casa_case_with_court_date.save
+    expect(casa_case_with_court_date.court_dates.last.court_report_due_date).to be_nil
     described_class.new.run!
-    expect(casa_case_without_court_dates.court_dates).to eq([])
+    expect(casa_case_with_court_date.reload.court_dates.last.court_report_due_date).not_to be_nil
     expect(casa_case_with_court_date.court_dates.last).to be_present # ??
   end
 end
