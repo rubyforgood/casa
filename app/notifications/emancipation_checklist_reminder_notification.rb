52e5aa223877516ff85c8cada3c64ae6bc6f0217
Libby Rodriguez <librod89@gmail.com>
2022-10-07 13:26:58 -0600

    Rake task to send emancipation checklist reminder notification to volunteers

---
+++ app/notifications/emancipation_checklist_reminder_notification.rb
@@ -0,0 +1,31 @@
+# To deliver this notification:
+#
+# EmancipationChecklistNotification.with(post: @post).deliver_later(current_user)
+# EmancipationChecklistNotification.with(post: @post).deliver(current_user)
+
+class EmancipationChecklistReminderNotification < Noticed::Base
+  # Add your delivery methods
+  #
+  deliver_by :database
+  # deliver_by :email, mailer: "UserMailer"
+  # deliver_by :slack
+  # deliver_by :custom, class: "MyDeliveryMethod"
+
+  # Add required params
+  #
+  param :casa_case
+
+  # Define helper methods to make rendering easier.
+
+  def message
+    t(".message", case_number: params[:casa_case].case_number)
+  end
+
+  def title
+    t(".title")
+  end
+
+  def url
+    casa_case_emancipation_path(params[:casa_case].id)
+  end
+end
---
+++ spec/lib/tasks/emancipation_checklist_reminder_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+require "rake"
+Rake.application.rake_require "tasks/emancipation_checklist_reminder"
+
+RSpec.describe "lib/tasks/emancipation_checklist_reminder.rake" do
+  let(:rake_task) { Rake::Task["emancipation_checklist_reminder"].invoke }
+
+  before do
+    Rake::Task.clear
+    Casa::Application.load_tasks
+  end
+
+  context "volunteer with transition age youth case" do
+    let!(:casa_case) { create(:casa_case, :with_one_case_assignment) }
+
+    it "should send notification" do
+      expect { rake_task }.to change { casa_case.case_assignments.first.volunteer.notifications.count }.by(1)
+    end
+  end
+
+  context "volunteer with multiple transition age youth cases" do
+    let!(:volunteer) { create(:volunteer, :with_casa_cases) }
+
+    it "sends notification for each case" do
+      expect { rake_task }.to change { volunteer.notifications.count }.by(CasaCase.count)
+    end
+  end
+
+  context "volunteer without transition age youth case" do
+    let!(:casa_case) { create(:casa_case, :with_one_case_assignment, birth_month_year_youth: 13.years.ago) }
+
+    it "should not send notification" do
+      expect { rake_task }.not_to change { casa_case.case_assignments.first.volunteer.notifications.count }
+    end
+  end
+
+  context "when there are no case assignments" do
+    it "does not raise error" do
+      expect { rake_task }.not_to raise_error
+    end
+  end
+end
