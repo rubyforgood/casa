da527d61b1d19f2d66bafe4d93857fa8541d7331
aedwardg <aedwardg1@gmail.com>
2022-10-09 01:08:38 -0700

    add medium_type data in CaseContactsContactDates

--- spec/services/case_contacts_contact_dates_spec.rb
+++ spec/services/case_contacts_contact_dates_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe CaseContactsContactDates do
     context "without interviewees" do
       let(:interviewees) { [] }
 
-      it "returns formatted data" do
+      it "returns an empty array" do
         expect(subject).to eq([])
       end
     end
@@ -23,7 +23,17 @@ RSpec.describe CaseContactsContactDates do
       let(:ccct_1) { create(:case_contact_contact_type, contact_type: contact_type_1) }
       let(:ccct_2) { create(:case_contact_contact_type, contact_type: contact_type_2) }
       let(:ccct_3) { create(:case_contact_contact_type, contact_type: contact_type_2, case_contact: create(:case_contact, occurred_at: 1.month.ago)) }
-      let(:ccct_4) { create(:case_contact_contact_type, contact_type: contact_type_2, case_contact: create(:case_contact, occurred_at: 2.months.ago)) }
+      let(:ccct_4) do
+        create(
+          :case_contact_contact_type,
+          contact_type: contact_type_2,
+          case_contact: create(
+            :case_contact,
+            occurred_at: 2.months.ago,
+            medium_type: CaseContact::TEXT_EMAIL
+          )
+        )
+      end
       let(:ccct_5) { create(:case_contact_contact_type, contact_type: contact_type_3, case_contact: create(:case_contact, occurred_at: 2.months.ago)) }
 
       let(:interviewees) { [ccct_1, ccct_2, ccct_3, ccct_4, ccct_5] }
@@ -31,12 +41,15 @@ RSpec.describe CaseContactsContactDates do
       it "returns formatted data" do
         expect(subject).to eq([
           {dates: "6/01*",
+           dates_by_medium_type: {"in-person" => "6/01*"},
            name: "Names of persons involved, starting with the child's name",
            type: "Mental therapist"},
           {dates: "4/01*, 5/01*, 6/01*",
+           dates_by_medium_type: {"in-person" => "5/01*, 6/01*", "text/email" => "4/01*"},
            name: "Names of persons involved, starting with the child's name",
            type: "Physical therapist"},
           {dates: "4/01*",
+           dates_by_medium_type: {"in-person" => "4/01*"},
            name: "Names of persons involved, starting with the child's name",
            type: "Aunt"}
         ])
