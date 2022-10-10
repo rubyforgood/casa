5671dd0aba1a747c36624a1e1c81d4be1a746534
compwron <compwron@users.noreply.github.com>
2022-10-10 07:32:26 -0700

    Revert "Add Ougai for logging"

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,9 +1,3 @@
 Rails.application.configure do
   config.lograge.enabled = true
-
-  config.lograge.formatter = Class.new do |fmt|
-    def fmt.call(data)
-      {msg: "Request", request: data}
-    end
-  end
 end
