81c3e8d3ab60336479434988ec8d248d5778a665
firelemons <shenyang64@gmail.com>
2022-10-24 23:00:02 -0500

    try create create patch note test

--- spec/system/all_casa_admins/patch_notes/index_spec.rb
+++ spec/system/all_casa_admins/patch_notes/index_spec.rb
@@ -16,5 +16,27 @@ RSpec.describe "all_casa_admins/patch_notes/index", type: :system do
         expect(page).to have_selector(".async-warn-indicator", text: "Cannot save an empty patch note")
       end
     end
+
+    context "when the patch note form is filled out" do
+      let(:patch_note_text) {"2y]@WX\\lBI:c,j,"}
+      let!(:patch_note_group) { create(:patch_note_type, :all_users) }
+      let!(:patch_note_type) { create(:patch_note_type, name: "5[1ht=d\\%*^qRON") }
+
+      it "displays a the new patch note on the page", js: true do
+        sign_in all_casa_admin
+        visit all_casa_admins_patch_notes_path
+
+        within "#new-patch-note" do
+          text_area = first(:css, "#new-patch-note textarea").native
+          text_area.send_keys(patch_note_text)
+
+          click_on "Create"
+
+          wait_for_ajax
+
+          expect(page).to have_css(".patch-note-list-item.card.new textarea", text: patch_note_text)
+        end
+      end
+    end
   end
 end
