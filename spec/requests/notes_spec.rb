1c28ba1ce55058efc29c8cf49e626cafb5d9d98f
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-10-25 19:07:09 -0700

    Refactor notes request spec

--- spec/requests/notes_spec.rb
+++ spec/requests/notes_spec.rb
@@ -1,24 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "/volunteers/notes", type: :request do
-  let(:organization) { create(:casa_org) }
-  let(:admin) { create(:casa_admin, casa_org_id: organization.id) }
-  let(:volunteer) { create(:volunteer, :with_assigned_supervisor, casa_org_id: organization.id) }
-  let(:note) { volunteer.notes.create(creator: admin, content: "Good job.") }
-
   describe "PATCH /update" do
-    subject(:request) { patch volunteer_note_path(volunteer, note), params: params }
-
     context "when logged in as an admin" do
-      before do
-        sign_in admin
-        request
-      end
-
       context "with valid params" do
-        let(:params) { {note: {content: "Very nice!"}} }
+        it "redirects to edit volunteer page and updates note" do
+          organization = create(:casa_org)
+          admin = create(:casa_admin, casa_org: organization)
+          volunteer = create(:volunteer, :with_assigned_supervisor, casa_org: organization)
+          note = create(:note, notable: volunteer, creator: admin, content: "Good job.")
 
-        it "returns success response" do
+          sign_in admin
+          patch volunteer_note_path(volunteer, note), params: {note: {content: "Very nice!"}}
+
           expect(response).to redirect_to(edit_volunteer_path(volunteer))
           expect(note.reload.content).to eq "Very nice!"
         end
@@ -27,26 +21,32 @@ RSpec.describe "/volunteers/notes", type: :request do
   end
 
   describe "DELETE /destroy" do
-    let!(:note_1) { volunteer.notes.create(creator: admin, content: "Note_1") }
-
-    before do
-      sign_in admin
-      request
-    end
-
     context "when logged in as an admin" do
       it "can delete notes about a volunteer" do
-        expect do
-          delete volunteer_note_path(volunteer, note_1)
-        end.to change(Note, :count).by(-1)
+        organization = create(:casa_org)
+        admin = create(:casa_admin, casa_org: organization)
+        volunteer = create(:volunteer, :with_assigned_supervisor, casa_org: organization)
+        note = create(:note, creator: admin, notable: volunteer)
+
+        sign_in admin
+        expect {
+          delete volunteer_note_path(volunteer, note)
+        }.to change(Note, :count).by(-1)
       end
     end
 
     context "when logged in as a supervisor" do
       it "can delete notes about a volunteer" do
-        expect do
-          delete volunteer_note_path(volunteer, note_1)
-        end.to change(Note, :count).by(-1)
+        organization = create(:casa_org)
+        supervisor = create(:supervisor, casa_org: organization)
+        other_supervisor = create(:supervisor, casa_org: organization)
+        volunteer = create(:volunteer, :with_assigned_supervisor, casa_org: organization)
+        note = create(:note, creator: other_supervisor, notable: volunteer)
+
+        sign_in supervisor
+        expect {
+          delete volunteer_note_path(volunteer, note)
+        }.to change(Note, :count).by(-1)
       end
     end
   end
