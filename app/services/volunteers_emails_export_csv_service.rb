af3f0d25aefe41ea3a408ec0e8fe888869392a71
Snow, Chad D <Chad.D.Snow@uscis.dhs.gov>
2022-10-26 14:50:46 -0400

    4122, add name, transition aged youth CSV columns

--- app/services/volunteers_emails_export_csv_service.rb
+++ app/services/volunteers_emails_export_csv_service.rb
@@ -21,7 +21,9 @@ class VolunteersEmailsExportCsvService
   def full_data(volunteer = nil)
     {
       email: volunteer&.email,
-      case_number: volunteer&.casa_cases&.active&.pluck(:case_number).to_a.join(", ")
+      case_number: volunteer&.casa_cases&.active&.pluck(:case_number).to_a.join(", "),
+      volunteer_name: volunteer&.display_name,
+      case_transition_aged_status: volunteer&.casa_cases&.active&.pluck(:transition_aged_youth).to_a.join(", ")
     }
   end
 end
--- spec/services/volunteers_emails_export_csv_service_spec.rb
+++ spec/services/volunteers_emails_export_csv_service_spec.rb
@@ -5,13 +5,14 @@ RSpec.describe VolunteersEmailsExportCsvService do
   let!(:active_volunteer) { create(:volunteer, :with_casa_cases) }
   let!(:inactive_volunteer) { create(:volunteer, :inactive) }
   let(:active_volunteer_cases) { active_volunteer.casa_cases.active.pluck(:case_number).to_a.join(", ") }
+  let(:active_volunteer_case_transition_aged) { active_volunteer.casa_cases.active.pluck(:transition_aged_youth).to_a.join(", ") }
 
   describe "#perform" do
     it "Exports correct data from volunteers" do
       results = subject.split("\n")
       expect(results.count).to eq(2)
-      expect(results[0].split(",")).to eq(["Email", "Case Number"])
-      expect(results[1]).to eq("#{active_volunteer.email},\"#{active_volunteer_cases}\"")
+      expect(results[0].split(",")).to eq(["Email", "Case Number", "Volunteer Name", "Case Transition Aged Status"])
+      expect(results[1]).to eq("#{active_volunteer.email},\"#{active_volunteer_cases}\",#{active_volunteer.display_name},\"#{active_volunteer_case_transition_aged}\"")
     end
 
     it "includes active volunteers" do
