27b4b44057a7f905a48278f220ca4a771feae4ff
firelemons <shenyang64@gmail.com>
2022-10-26 21:22:06 -0500

    try find textarea

--- spec/system/all_casa_admins/patch_notes/index_spec.rb
+++ spec/system/all_casa_admins/patch_notes/index_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe "all_casa_admins/patch_notes/index", type: :system do
 
         wait_for_ajax
 
-        expect(page).to have_selector("textarea", with: patch_note_text, disabled: true)
+        expect(page).to have_selector("textarea[value='#{patch_note_text}']", disabled: true)
       end
     end
   end
