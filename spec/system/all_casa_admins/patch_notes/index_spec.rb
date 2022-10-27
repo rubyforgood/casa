185f8097d61a939ee1f7e5c329e475fde60b728c
firelemons <shenyang64@gmail.com>
2022-10-26 21:59:56 -0500

    try match textarea

--- spec/system/all_casa_admins/patch_notes/index_spec.rb
+++ spec/system/all_casa_admins/patch_notes/index_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe "all_casa_admins/patch_notes/index", type: :system do
 
         wait_for_ajax
 
-        expect(page).to have_selector("textarea[value='#{patch_note_text}']")
+        expect(page).to have_xpath("//div[contains(@class, 'patch-note-list-item') and contains(@class, 'new')]//textarea[text() = '#{patch_note_text}']")
       end
     end
   end
