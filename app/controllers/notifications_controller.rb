4ad5ef7c7011058c7e216a329d8dcfb3eaf69058
firelemons <shenyang64@gmail.com>
2022-11-03 21:20:30 -0500

    added user permitted patch notes to notifications view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications.newest_first
+    @patch_notes = PatchNote.notes_available_for_user(current_user)
   end
 end
