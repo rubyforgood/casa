2ce673f78b0b1fb24d7a56f1e24c00b3675aed34
firelemons <shenyang64@gmail.com>
2022-11-04 19:57:28 -0500

    created tests for notifications_after_and_including_deploy

--- spec/helpers/notifications_helper_spec.rb
+++ spec/helpers/notifications_helper_spec.rb
@@ -1,6 +1,44 @@
 require "rails_helper"
 
 RSpec.describe NotificationsHelper do
+  context "notifications with respect to deploy time" do
+    let(:notification_created_after_deploy_a) { create(:notification) }
+    let(:notification_created_after_deploy_b) { create(:notification, created_at: 1.day.ago) }
+    let(:notification_created_at_deploy) { create(:notification, created_at: 2.days.ago) }
+    let(:notification_created_before_deploy_a) { create(:notification, created_at: 2.days.ago - 1.hour) }
+    let(:notification_created_before_deploy_b) { create(:notification, created_at: 3.days.ago) }
+
+    before do
+      travel_to Time.new(2022, 1, 1, 0, 0, 0)
+
+      notification_created_after_deploy_a.update_attribute(:created_at, 1.hour.ago)
+      notification_created_after_deploy_b.update_attribute(:created_at, 1.day.ago)
+
+      Health.instance.update_attribute(:latest_deploy_time, 2.days.ago)
+      notification_created_at_deploy.update_attribute(:created_at, 2.days.ago)
+
+      notification_created_before_deploy_a.update_attribute(:created_at, 2.days.ago - 1.hour)
+      notification_created_before_deploy_b.update_attribute(:created_at, 3.days.ago)
+    end
+
+    describe "#notifications_after_and_including_deploy" do
+      let(:notifications_after_and_including_deploy) { helper.notifications_after_and_including_deploy(Notification.all) }
+      it "returns all notifications from the given list after and including deploy time" do
+        expect(notifications_after_and_including_deploy).to include(notification_created_after_deploy_a)
+        expect(notifications_after_and_including_deploy).to include(notification_created_after_deploy_b)
+        expect(notifications_after_and_including_deploy).to include(notification_created_at_deploy)
+      end
+
+      it "does not contain notifications before the deploy time" do
+        expect(notifications_after_and_including_deploy).to_not include(notification_created_before_deploy_a)
+        expect(notifications_after_and_including_deploy).to_not include(notification_created_before_deploy_b)
+      end
+    end
+
+    describe "notifications_before_deploy" do
+    end
+  end
+
   describe "#notification_icon" do
     context "notification has been read" do
       it "is blank" do
