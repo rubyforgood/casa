14bd342a86635bfb99b9a25679418f931b1ac9d3
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-11-08 21:53:36 -0300

    fixed test

--- spec/system/casa_cases/emancipation/show_spec.rb
+++ spec/system/casa_cases/emancipation/show_spec.rb
@@ -1,6 +1,6 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "casa_cases/show", type: :system do
+RSpec.describe 'casa_cases/show', type: :system do
   let(:organization) { build(:casa_org) }
   let(:volunteer) { build(:volunteer, casa_org: organization) }
   let(:casa_case) { build(:casa_case, casa_org: organization) }
@@ -14,21 +14,22 @@ RSpec.describe "casa_cases/show", type: :system do
     visit casa_case_emancipation_path(casa_case.id)
   end
 
-  context "volunteer user", js: true do
+  context 'volunteer user', js: true do
     let(:user) { volunteer }
 
-    it "sees title" do
-      expect(page).to have_content("Emancipation Checklist")
+    it 'sees title, opens and select option, and unselect option trough main input' do
+      expect(page).to have_content('Emancipation Checklist')
 
       expect(page).to have_content(emancipation_category.name)
       expect(page).to_not have_content(emancipation_option.name)
 
-      find(".emancipation-category").click # Open list of options for this category
-      expect(page).to have_content(emancipation_option.name) # Make sure list of options is showing
-      find(".check-item").click # Select the first option
-      find(".emancipation-category").click # Close the list of options for this category
-      # TODO fix flakiness
-      expect(page).to have_css(".async-success-indicator", text: "Unchecked #{emancipation_option.name}")
+      find('.emacipation-category-input-label-pair').click
+      expect(page).to have_content(emancipation_option.name)
+      find('.check-item').click
+      find('.emacipation-category-input-label-pair').click
+      # # find('.category-collapse-icon').click # Close the list of options for this category
+      # # TODO fix flakiness
+      expect(page).to have_css('.async-success-indicator', text: "Unchecked #{emancipation_option.name}")
 
       # TODO more asserts here - checking and unchecking items
     end
