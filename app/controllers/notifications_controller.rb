61da82bd97bec9f8e240cb31aa3488bc322dd8fa
firelemons <shenyang64@gmail.com>
2022-11-11 20:01:28 -0600

    patch notes can be displayed

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,16 @@ class NotificationsController < ApplicationController
   def index
     @deploy_time = Health.instance.latest_deploy_time
     @notifications = current_user.notifications.newest_first
-    @patch_notes = PatchNote.notes_available_for_user(current_user)
+    @patch_notes = {}
+
+    PatchNote.notes_available_for_user(current_user).each do |patch_note|
+      patch_note_type_name = patch_note.patch_note_type.name
+
+      unless @patch_notes.has_key?(patch_note_type_name)
+        @patch_notes[patch_note_type_name] = []
+      end
+
+      @patch_notes[patch_note_type_name].push(patch_note.note)
+    end
   end
 end
