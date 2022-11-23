ef234053f08f82afe12a09d1f946f48a4b78590e
firelemons <shenyang64@gmail.com>
2022-11-23 10:48:04 -0600

    created test stubs

---
+++ spec/views/notifications/index.html.erb_spec.rb
@@ -0,0 +1,74 @@
+require "rails_helper"
+
+RSpec.describe "notifications/index", type: :view do
+  before do
+    travel_to Date.new(2021, 1, 1)
+  end
+
+  context "when there is a deploy date" do
+    let(:notification_created_after_deploy_a) { create(:notification) }
+    let(:notification_created_after_deploy_b) { create(:notification) }
+    let(:notification_created_at_deploy) { create(:notification) }
+    let(:notification_created_before_deploy_a) { create(:notification) }
+    let(:notification_created_before_deploy_b) { create(:notification) }
+
+    before do
+      Health.instance.update_attribute(:latest_deploy_time, 2.days.ago)
+    end
+
+    context "when there are notifications" do
+      before do
+        # TODO invalid notifications that break the view
+        # notification_created_after_deploy_a.update_attribute(:created_at, 1.hour.ago)
+        # notification_created_after_deploy_b.update_attribute(:created_at, 1.day.ago)
+        # notification_created_at_deploy.update_attribute(:created_at, 2.days.ago)
+        # notification_created_before_deploy_a.update_attribute(:created_at, 2.days.ago - 1.hour)
+        # notification_created_before_deploy_b.update_attribute(:created_at, 3.days.ago)
+      end
+
+      xit "has all notifications created after and including the deploy date above the patch note" do
+        # TODO fill in after notification factory is given ability to create a notification with notes
+      end
+
+      xit "has all notifications created after and including the deploy date above the patch note" do
+        # TODO fill in after notification factory is filled out
+      end
+    end
+
+    context "when there are patch notes" do
+      let(:patch_note_group_all_users) { create(:patch_note_group, :all_users) }
+      let(:patch_note_group_no_volunteers) { create(:patch_note_group, :only_supervisors_and_admins) }
+      let(:patch_note_type_a) { create(:patch_note_type, name: "patch_note_type_a") }
+      let(:patch_note_type_b) { create(:patch_note_type, name: "patch_note_type_b") }
+      let(:patch_note_1) { create(:patch_note, note: "*Sy@\\<iiF>(\\\"Q7") }
+      let(:patch_note_2) { create(:patch_note, note: "(W!;Ros>cIWNKX}") }
+
+      before do
+        Health.instance.update_attribute(:latest_deploy_time, Date.today)
+        assign(:patch_notes, PatchNote.all)
+      end
+
+      context "as an admin" do
+        it "shows all the patch notes available to the admin" do
+        end
+
+        it "shows the patch notes under the correct type" do
+        end
+      end
+
+      context "as a volunteer" do
+        it "shows all the patch notes available to the volunteer" do
+        end
+
+        it "does not show the patch notes unavailable to the volunteer" do
+        end
+      end
+    end
+  end
+
+  context "without a deploy date" do
+    xit "shows the correct number of notifications" do
+      # TODO fill in after notification factory is given ability to create displayable notifications
+    end
+  end
+end
