3773c04561902ecf99b11c0c42828053bca291a0
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-12-05 11:27:34 -0300

    refactored a bit

--- app/helpers/court_dates_helper.rb
+++ app/helpers/court_dates_helper.rb
@@ -1,13 +1,17 @@
 module CourtDatesHelper
   def when_do_we_have_court_dates(casa_case)
     court_dates = casa_case.court_dates.includes(:hearing_type).ordered_ascending.load
-    past = []
-    future = []
+    past = false
+    future = false
 
-    court_dates.each do |date|
-      Date.current.to_time.after?(date.date.to_time) ?  past.push(date.date.to_time) : future.push(date.date.to_time)
+    court_dates.each do |court_date|
+      court_date.date <= Date.current ? past = true : future = true
     end
 
-    return past, future
+    if future && !past
+      'future'
+    elsif !future && past
+      'past'
+    end
   end
 end
