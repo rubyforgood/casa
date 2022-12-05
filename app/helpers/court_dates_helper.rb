14e0027faecf12bd0a19e07315f094fbba347e9d
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-12-05 12:46:00 -0300

    refactored to remove loop
    If the last court date is the past, and the query returns them
    in ascending order, then all are in the past. If the last is in
    the future, then the first could be in the past, so we check for that.
    So we only need to check the first and last court_dates, not all.

--- app/helpers/court_dates_helper.rb
+++ app/helpers/court_dates_helper.rb
@@ -1,17 +1,16 @@
+# frozen_string_literal: true
+
+# Helper methods for court_dates
 module CourtDatesHelper
   def when_do_we_have_court_dates(casa_case)
-    court_dates = casa_case.court_dates.includes(:hearing_type).ordered_ascending.load
-    past = false
-    future = false
+    date_query = casa_case.court_dates.includes(:hearing_type).ordered_ascending
+    last_court_date = date_query.last
+    first_court_date = date_query.first
 
-    court_dates.each do |court_date|
-      court_date.date <= Date.current ? past = true : future = true
-    end
-
-    if future && !past
-      'future'
-    elsif !future && past
+    if last_court_date.date < Date.current
       'past'
+    elsif last_court_date.date > Date.current && first_court_date.date > Date.current
+      'future'
     end
   end
 end
