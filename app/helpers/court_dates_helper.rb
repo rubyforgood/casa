c7660b6b740016a6ce8140dc2b16c98f1f1914ad
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-12-06 11:47:52 -0300

    updated logic
    – If the first is the future, then the last is also
    – Updated court_dates query, I don't think we need to include
    hearing_type in this one
    – stored when_do_we_have_court_dates(@casa_case) in a variable
    to avoid querying the database multiple times at different points
    in time.

--- app/helpers/court_dates_helper.rb
+++ app/helpers/court_dates_helper.rb
@@ -3,13 +3,12 @@
 # Helper methods for court_dates
 module CourtDatesHelper
   def when_do_we_have_court_dates(casa_case)
-    date_query = casa_case.court_dates.includes(:hearing_type).ordered_ascending
-    last_court_date = date_query.last
-    first_court_date = date_query.first
+    court_dates = casa_case.court_dates.ordered_ascending
+    date_now = Date.current
 
-    if last_court_date.date < Date.current
+    if court_dates.last.date < date_now
       "past"
-    elsif last_court_date.date > Date.current && first_court_date.date > Date.current
+    elsif court_dates.first.date > date_now
       "future"
     end
   end
