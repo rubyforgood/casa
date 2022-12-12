3ecacad6fd4e7c27ee3317c18fcf4754faa0da93
shamitomita <shamitomita@gmail.com>
2022-12-12 12:45:57 -0500

    pending tests

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -6,6 +6,11 @@ FactoryBot.define do
 
     trait :followup do
       type { "FollowupNotification" }
+      with_follow_up
     end
+
+    trait(:with_follow_up) { followup }
+    #following the relationship between courtdates and judges?
+    #I think transient traits would be a good place to pick up
   end
 end
