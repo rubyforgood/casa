6389581361a6bbf1e10072e32c67f4e474d601b7
shamitomita <shamitomita@gmail.com>
2022-12-13 11:20:38 -0500

    removing unrelated code from issue 4244

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -3,14 +3,5 @@ FactoryBot.define do
     association :recipient, factory: :volunteer
     recipient_type { "Volunteer" }
     type { "FollowupResolvedNotification" }
-
-    trait :followup do
-      type { "FollowupNotification" }
-      with_follow_up
-    end
-
-    trait(:with_follow_up) { followup }
-    #following the relationship between courtdates and judges?
-    #I think transient traits would be a good place to pick up
-  end
+  end 
 end
