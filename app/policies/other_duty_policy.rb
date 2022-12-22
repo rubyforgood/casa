71e3e6d4ad9eae11b76d644fd005de40d226f9a9
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2022-12-22 12:11:48 -0800

    Remove unnecessary resolve; fix flaky spec

--- app/policies/other_duty_policy.rb
+++ app/policies/other_duty_policy.rb
@@ -6,10 +6,6 @@ class OtherDutyPolicy < UserPolicy
       @user = user
       @scope = scope
     end
-
-    def resolve
-      scope.where(casa_org_id: user.casa_org_id)
-    end
   end
 
   def index?
