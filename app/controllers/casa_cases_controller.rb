2dd05f283e33192109d8ef6b1167e3f1ac08b731
André Moreira <andre.lumor1@gmail.com>
2022-12-23 14:09:59 -0300

    feat: allow empty case creation button
    Created empty court date checkbox
    Allowed empty court date when checkbox is checked

--- app/controllers/casa_cases_controller.rb
+++ app/controllers/casa_cases_controller.rb
@@ -40,8 +40,14 @@ class CasaCasesController < ApplicationController
   end
 
   def create
+    create_params = if casa_case_params[:empty_court_date] == '1'
+      casa_case_params.except(:court_dates_attributes)
+    else
+      casa_case_params
+    end
+
     @casa_case = CasaCase.new(
-      casa_case_params.merge(
+      create_params.except(:empty_court_date).merge(
         casa_org: current_organization
       )
     )
@@ -155,8 +161,9 @@ class CasaCasesController < ApplicationController
       :case_number,
       :birth_month_year_youth,
       :court_report_due_date,
-      court_dates_attributes: [:date],
-      casa_case_contact_types_attributes: [:contact_type_id]
+      :empty_court_date,
+      casa_case_contact_types_attributes: [:contact_type_id],
+      court_dates_attributes: [:date]
     )
   end
 
