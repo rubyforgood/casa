081cc3440bf90c3fd6b921f2d41f1c1e885ee98e
Italo Matos <italomatos@gmail.com>
2022-12-26 23:54:32 -0300

    fix select2 component and button size

--- app/javascript/src/select.js
+++ app/javascript/src/select.js
@@ -1,5 +1,11 @@
 /* global $ */
 
 $('document').ready(() => {
-  $('.select2').select2({ theme: 'classic' })
+  $('.select2').select2(
+    {
+      theme: "bootstrap-5",
+      width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+      placeholder: $( this ).data( 'placeholder' ),
+    } 
+  )
 })
