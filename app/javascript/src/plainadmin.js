547de5788948f43fc465c35a92680b1388d0a025
Mike Esposito <mikeesposito6852@gmail.com>
2023-01-06 14:42:32 -0800

    Add plainadmin styling for new patch notes and fix JS error from missing menu button in Admin mode

--- app/javascript/src/plainadmin.js
+++ app/javascript/src/plainadmin.js
@@ -6,26 +6,28 @@
   const menuToggleButtonIcon = document.querySelector("#menu-toggle i");
   const overlay = document.querySelector(".overlay");
 
-  menuToggleButton.addEventListener("click", () => {
-    sidebarNavWrapper.classList.toggle("active");
-    overlay.classList.add("active");
-    mainWrapper.classList.toggle("active");
+  if (menuToggleButton) {
+    menuToggleButton.addEventListener("click", () => {
+      sidebarNavWrapper.classList.toggle("active");
+      overlay.classList.add("active");
+      mainWrapper.classList.toggle("active");
 
-    if (document.body.clientWidth > 1200) {
-      if (menuToggleButtonIcon.classList.contains("lni-chevron-left")) {
-        menuToggleButtonIcon.classList.remove("lni-chevron-left");
-        menuToggleButtonIcon.classList.add("lni-menu");
+      if (document.body.clientWidth > 1200) {
+        if (menuToggleButtonIcon.classList.contains("lni-chevron-left")) {
+          menuToggleButtonIcon.classList.remove("lni-chevron-left");
+          menuToggleButtonIcon.classList.add("lni-menu");
+        } else {
+          menuToggleButtonIcon.classList.remove("lni-menu");
+          menuToggleButtonIcon.classList.add("lni-chevron-left");
+        }
       } else {
-        menuToggleButtonIcon.classList.remove("lni-menu");
-        menuToggleButtonIcon.classList.add("lni-chevron-left");
+        if (menuToggleButtonIcon.classList.contains("lni-chevron-left")) {
+          menuToggleButtonIcon.classList.remove("lni-chevron-left");
+          menuToggleButtonIcon.classList.add("lni-menu");
+        }
       }
-    } else {
-      if (menuToggleButtonIcon.classList.contains("lni-chevron-left")) {
-        menuToggleButtonIcon.classList.remove("lni-chevron-left");
-        menuToggleButtonIcon.classList.add("lni-menu");
-      }
-    }
-  });
+    });
+  }
   overlay.addEventListener("click", () => {
     sidebarNavWrapper.classList.remove("active");
     overlay.classList.remove("active");
