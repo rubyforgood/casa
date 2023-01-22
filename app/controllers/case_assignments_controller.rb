0299358f43831c952427fdb93af92a20b59a009b
Stephen Anderson <stephenandersondev@gmail.com>
2023-01-21 19:29:24 -0600

    2647: Refactored CaseAssignments#create to make code climate happy

--- app/controllers/case_assignments_controller.rb
+++ app/controllers/case_assignments_controller.rb
@@ -4,41 +4,27 @@ class CaseAssignmentsController < ApplicationController
 
   def create
     authorize CaseAssignment
-    case_assignments = case_assignment_parent.case_assignments
-    existing_case_assignment = if params[:volunteer_id]
-      case_assignments.where(casa_case_id: case_assignment_params[:casa_case_id], active: false).first
-    else
-      case_assignments.where(volunteer_id: case_assignment_params[:volunteer_id], active: false).first
-    end
 
     if existing_case_assignment.present?
       if existing_case_assignment.update(active: true)
-        message = "Volunteer reassigned to case"
-        flash.notice = message
-        status = :ok
+        handle_successful_assignment("Volunteer reassigned to case")
       else
         errors = existing_case_assignment.errors.full_messages.join(". ")
-        message = "Unable to reassigned volunteer to case: #{errors}."
-        flash.alert = message
-        status = :unprocessable_entity
+        handle_failed_assignment("Unable to reassign volunteer to case: #{errors}.")
       end
     else
       case_assignment = case_assignment_parent.case_assignments.new(case_assignment_params)
       if case_assignment.save
-        message = "Volunteer assigned to case"
-        flash.notice = message
-        status = :ok
+        handle_successful_assignment("Volunteer assigned to case")
       else
         errors = case_assignment.errors.full_messages.join(". ")
-        message = "Unable to assign volunteer to case: #{errors}."
-        flash.alert = message
-        status = :unprocessable_entity
+        handle_failed_assignment("Unable to assign volunteer to case: #{errors}.")
       end
     end
 
     respond_to do |format|
       format.html { redirect_to after_action_path(case_assignment_parent) }
-      format.json { render json: message, status: status }
+      format.json { render json: @message, status: @status }
     end
   end
 
@@ -117,5 +103,30 @@ class CaseAssignmentsController < ApplicationController
         .find(params[:id])
   rescue ActiveRecord::RecordNotFound
     head :not_found
+  end
+
+  def load_existing_case_assignment
+    case_assignments = case_assignment_parent.case_assignments
+    if params[:volunteer_id]
+      case_assignments.where(casa_case_id: case_assignment_params[:casa_case_id], active: false).first
+    else
+      case_assignments.where(volunteer_id: case_assignment_params[:volunteer_id], active: false).first
+    end
+  end
+
+  def existing_case_assignment
+    @existing_case_assignment ||= load_existing_case_assignment
+  end
+
+  def handle_successful_assignment(msg)
+    @message = msg
+    flash.notice = msg
+    @status = :ok
+  end
+
+  def handle_failed_assignment(msg)
+    @message = msg
+    flash.alert = msg
+    @status = :unprocessable_entity
   end
 end
