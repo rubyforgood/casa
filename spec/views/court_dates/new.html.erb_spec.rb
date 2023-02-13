99861259f6091c8302933ea702a1349cdfbe63ef
Shami Tomita <70528966+ShamiTomita@users.noreply.github.com>
2023-02-13 13:51:55 -0500

    Final spec fixes for new_theme??? (#4544)
    * change casacase back to volunteer
    
    * added missing emancipation-category-check-box class
    
    * changed btn-primary to primary-btn for court_dates
    
    * added type button for additiona expenses
    
    * added match: :first for checklist_items/destroy
    
    * added flash messages for all_casa_admins edit
    
    * removed duplicate flash message on all_casa dashboard
    
    * removed conditional, just added flash messages
    
    * applied anshu's fix for spec sign out
    
    * lint
    
    * addint data-bs to toggle and target for contacts

--- spec/views/court_dates/new.html.erb_spec.rb
+++ spec/views/court_dates/new.html.erb_spec.rb
@@ -16,5 +16,5 @@ RSpec.describe "court_dates/new", type: :view do
   it { is_expected.to have_selector("h2", text: "New Court Date") }
   it { is_expected.to have_selector("h6", text: casa_case.case_number) }
   it { is_expected.to have_link(casa_case.case_number, href: "/casa_cases/#{casa_case.case_number.parameterize}") }
-  it { is_expected.to have_selector(".btn-primary") }
+  it { is_expected.to have_selector(".primary-btn") }
 end
