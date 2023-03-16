a01b39d9d676ab219ed36673021be88fe056fedb
Nathan Thomas <thomas.nathanm@gmail.com>
2023-03-16 10:24:59 -0500

    Update timer to 2 hours 58 minutes

--- app/javascript/src/session_timeout_poller.js
+++ app/javascript/src/session_timeout_poller.js
@@ -1,5 +1,5 @@
 
-setTimeout(warningBoxAndReload, 5000);
+setTimeout(warningBoxAndReload, 10680);
  
 function warningBoxAndReload() {
   alert("Warning: You will be logged off in 2 minutes due to inactivity.");
