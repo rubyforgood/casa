32dd9017e10fa85cc1acf85f24693f21d3e0844e
Nathan Thomas <thomas.nathanm@gmail.com>
2023-03-16 10:20:54 -0500

    Simple method with timer and reload

--- app/javascript/src/session_timeout_poller.js
+++ app/javascript/src/session_timeout_poller.js
@@ -1,51 +1,7 @@
-let heartBeatActivated = false;
-class HeartBeat {
-  constructor() {
-    document.addEventListener('DOMContentLoaded', () => {
-      this.initHeartBeat();
-    });
-  }
 
-  initHeartBeat() {
-    this.lastActive = new Date().valueOf;
-    if (!heartBeatActivated) {
-      ['mousemove', 'scroll', 'click', 'keydown'].forEach((activity) => {
-        document.addEventListener(activity, (ev) => {
-          this.lastActive = ev.timeStamp + PerformanceEntry.startTime;
-        }, false);
-      });
-      heartBeatActivated = true;
-    }
-  }
+setTimeout(warningBoxAndReload, 5000);
+ 
+function warningBoxAndReload() {
+  alert("Warning: You will be logged off in 2 minutes due to inactivity.");
+  location.reload();
 }
-
-window.heartBeat = new HeartBeat();
-
-const sessionTimeoutPollFrequency = 5;
-function pollForSessionTimeout() {
-  if ((Date.now() - window.heartBeat.lastActive) < (sessionTimeoutPollFrequency * 1000)) {
-    // setTimeout(pollForSessionTimeout, (sessionTimeoutPollFrequency * 1000));
-    return;
-  }
-  
-  let request = new XMLHttpRequest();
-  request.onload = function (event) {
-    var status = event.target.status;
-    var response = event.target.response;
-    
-    if (status === 200 && (response <= 10790)) {
-      alert("2 minutes left");
-    }
-
-    // if the remaining valid time for the current user session is less than or equals to 0 seconds.
-    if (status === 200 && (response <= 0)) {
-      window.location.href = '/session_timeout';
-    }
-  };
-  request.open('GET', '/check_session_timeout', true);
-  request.responseType = 'json';
-  request.send();
-  setTimeout(pollForSessionTimeout, (sessionTimeoutPollFrequency * 1000));
-}
-
-setTimeout(pollForSessionTimeout, (sessionTimeoutPollFrequency * 1000));
