41e40bacc6e492457e6064b45271bdd374a6317e
crespire <hello@crespire.dev>
2023-03-19 14:14:36 -0400

    fix: filename typo

--- spec/components/notification_component_spec.rb
+++ spec/components/notification_component_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe NotifcationComponent, type: :component do
+RSpec.describe NotificationComponent, type: :component do
   pending "add some examples to (or delete) #{__FILE__}"
 
   # it "renders something useful" do
