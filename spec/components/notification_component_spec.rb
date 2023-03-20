811a5f76b80ad67006741b34c047935129724b88
crespire <hello@crespire.dev>
2023-03-20 12:56:25 -0400

    feat: add Notification component spec

--- spec/components/notification_component_spec.rb
+++ spec/components/notification_component_spec.rb
@@ -3,13 +3,42 @@
 require "rails_helper"
 
 RSpec.describe NotificationComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:followup_with_note) { create(:notification, :followup_with_note) }
+  let(:followup_no_note) { create(:notification, :followup_without_note) }
+  let(:followup_read) { create(:notification, :followup_read) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders a followup with note" do
+    component = described_class.new(notification: followup_with_note.to_notification)
+
+    render_inline(component)
+    expect(page).to have_text('New followup')
+    expect(page).to have_text('Note: ')
+    expect(page).to have_text(followup_with_note.to_notification.message)
+  end
+
+
+  it "renders a followup without a note" do
+    component = described_class.new(notification: followup_no_note.to_notification)
+
+    render_inline(component)
+    expect(page).to have_text('New followup')
+    expect(page).not_to have_text('Note: ')
+    expect(page).to have_text(followup_no_note.to_notification.message)
+  end
+
+  it "renders unread followups with the correct styles" do
+    component = described_class.new(notification: followup_with_note.to_notification)
+
+    render_inline(component)
+    expect(page).not_to have_css('a.bg-light.text-muted')
+    expect(page).to have_css('i.fas.fa-bell')
+  end
+
+  it "renders read followups with the correct styles" do
+    component = described_class.new(notification: followup_read.to_notification)
+
+    render_inline(component)
+    expect(page).to have_css('a.bg-light.text-muted')
+    expect(page).not_to have_css('i.fas.fa-bell')
+  end
 end
