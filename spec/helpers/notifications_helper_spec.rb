0575363618953428e75dbe2f773066e62c8afa9f
crespire <hello@crespire.dev>
2023-03-20 12:54:09 -0400

    fix: remove tests replicated in component spec

--- spec/helpers/notifications_helper_spec.rb
+++ spec/helpers/notifications_helper_spec.rb
@@ -52,38 +52,6 @@ RSpec.describe NotificationsHelper do
     end
   end
 
-  describe "#notification_icon" do
-    context "notification has been read" do
-      it "is blank" do
-        notification = build_stubbed(:notification, read_at: Time.current)
-        expect(helper.notification_icon(notification)).to be_blank
-      end
-    end
-
-    context "notification has not been read" do
-      it "shows the bell icon" do
-        notification = build_stubbed(:notification, read_at: nil)
-        expect(helper.notification_icon(notification)).to eq("<i class='fas fa-bell'></i>")
-      end
-    end
-  end
-
-  describe "#notification_row_class" do
-    context "notification has been read" do
-      it "returns 'bg-light text-muted'" do
-        notification = build_stubbed(:notification, read_at: Time.current)
-        expect(helper.notification_row_class(notification)).to eq(" bg-light text-muted ")
-      end
-    end
-
-    context "notification has not been read" do
-      it "is blank" do
-        notification = build_stubbed(:notification, read_at: nil)
-        expect(helper.notification_row_class(notification)).to be_blank
-      end
-    end
-  end
-
   describe "#patch_notes_as_hash_keyed_by_type_name" do
     it "returns a hash where the keys are the names of the patch note type and the values are lists of patch note strings belonging to the type" do
       patch_note_type_a = create(:patch_note_type, name: "patch_note_type_a")
