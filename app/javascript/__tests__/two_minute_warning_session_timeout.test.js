4e7fd5cefbcf79de8ee8ceeba83d0a7c6ebfe61b
Nathan Thomas <thomas.nathanm@gmail.com>
2023-03-21 22:22:23 -0500

    Clean up jest test

--- app/javascript/__tests__/two_minute_warning_session_timeout.test.js
+++ app/javascript/__tests__/two_minute_warning_session_timeout.test.js
@@ -1,14 +1,8 @@
 /* eslint-env jest */
 
 require('jest')
-// jest.useFakeTimers();
-// jest.spyOn(global, 'setInterval');
-
-// import { myTimer, warningBoxAndReload } from '../src/session_timeout_poller.js';
 
 describe('warningBoxAndReload', () => {
-
-  const sessionTimeoutPoller = require('../src/session_timeout_poller.js')
 
   test("warning box displays 2 minutes before Devise Timeout", () => {
     warningBoxAndReload = jest.fn();
