0ff68f878ef4a36899ee731f6e6abd0409d45dff
Nathan Thomas <thomas.nathanm@gmail.com>
2023-03-23 12:05:34 -0500

    Fix lint issues

--- app/javascript/src/session_timeout_poller.js
+++ app/javascript/src/session_timeout_poller.js
@@ -1,29 +1,31 @@
 
-const deviseTimeoutInMinutes = 180;
-const twoMinuteWarning = deviseTimeoutInMinutes - 2;
-const totalTimerAmount = twoMinuteWarning * 60 * 1000;
-const deviseTimeoutInMilliseconds = deviseTimeoutInMinutes * 60 * 1000;
-const startTime = new Date().getTime();
-var lastTime = new Date().getTime();
-var timeElapsed;
+const deviseTimeoutInMinutes = 180
+const twoMinuteWarning = deviseTimeoutInMinutes - 2
+const totalTimerAmount = twoMinuteWarning * 60 * 1000
+const deviseTimeoutInMilliseconds = deviseTimeoutInMinutes * 60 * 1000
+const startTime = new Date().getTime()
+let lastTime = new Date().getTime()
+let timeElapsed
+let currentTime
+let alert
+let location
 
-function warningBoxAndReload() {
-  alert("Warning: You will be logged off in 2 minutes due to inactivity.");
-  location.reload();
+function warningBoxAndReload () {
+  alert('Warning: You will be logged off in 2 minutes due to inactivity.')
+  location.reload()
 }
 
-setInterval(myTimer, 1000);
+setInterval(myTimer, 1000)
 
-function myTimer() {
-  timeElapsed = Math.abs(lastTime - startTime);
-  currentTime = new Date().getTime();
+function myTimer () {
+  timeElapsed = Math.abs(lastTime - startTime)
+  currentTime = new Date().getTime()
   // console.log("Should go up by 1 second", timeElapsed);
-  
   if (timeElapsed > deviseTimeoutInMilliseconds) {
-    location.reload();
+    location.reload()
   } else if (timeElapsed > totalTimerAmount) {
-    warningBoxAndReload();
+    warningBoxAndReload()
   } else {
-    lastTime = currentTime;  
+    lastTime = currentTime
   }
 };
