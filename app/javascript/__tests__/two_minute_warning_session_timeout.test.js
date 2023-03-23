909954d2c5f703c088eda5eae9f5b93177b2c1ac
Nathan Thomas <thomas.nathanm@gmail.com>
2023-03-23 12:06:09 -0500

    Fix lint issues

--- app/javascript/__tests__/two_minute_warning_session_timeout.test.js
+++ app/javascript/__tests__/two_minute_warning_session_timeout.test.js
@@ -3,16 +3,14 @@
 require('jest')
 
 describe('warningBoxAndReload', () => {
+  test('warning box displays 2 minutes before Devise Timeout', () => {
+    const warningBoxAndReload = jest.fn()
+    const myTimer = jest.fn()
 
-  test("warning box displays 2 minutes before Devise Timeout", () => {
-    warningBoxAndReload = jest.fn();
-    myTimer = jest.fn();
+    myTimer()
+    expect(myTimer).toHaveBeenCalled()
 
-    myTimer();
-    expect(myTimer).toHaveBeenCalled();
-
-    warningBoxAndReload();
-    expect(warningBoxAndReload).toHaveBeenCalled();
-
-  });
-});
+    warningBoxAndReload()
+    expect(warningBoxAndReload).toHaveBeenCalled()
+  })
+})
