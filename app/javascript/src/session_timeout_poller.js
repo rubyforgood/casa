09fc1f7739100dce4f9e343305697da680e10cb1
Nathan Thomas <thomas.nathanm@gmail.com>
2023-03-23 20:56:36 -0500

    Declare alert and location functions

--- app/javascript/src/session_timeout_poller.js
+++ app/javascript/src/session_timeout_poller.js
@@ -5,14 +5,12 @@ const totalTimerAmount = twoMinuteWarning * 60 * 1000
 const deviseTimeoutInMilliseconds = deviseTimeoutInMinutes * 60 * 1000
 const startTime = new Date().getTime()
 let lastTime = new Date().getTime()
-let timeElapsed
 let currentTime
-let alert
-let location
+let timeElapsed
 
 function warningBoxAndReload () {
-  alert('Warning: You will be logged off in 2 minutes due to inactivity.')
-  location.reload()
+  window.alert('Warning: You will be logged off in 2 minutes due to inactivity.')
+  window.location.reload()
 }
 
 setInterval(myTimer, 1000)
@@ -20,9 +18,9 @@ setInterval(myTimer, 1000)
 function myTimer () {
   timeElapsed = Math.abs(lastTime - startTime)
   currentTime = new Date().getTime()
-  // console.log("Should go up by 1 second", timeElapsed);
+  // console.log('Should go up by 1 second', timeElapsed)
   if (timeElapsed > deviseTimeoutInMilliseconds) {
-    location.reload()
+    window.location.reload()
   } else if (timeElapsed > totalTimerAmount) {
     warningBoxAndReload()
   } else {
