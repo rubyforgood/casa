541022317931367195c605a8f60f7dd381a38813
firelemons <shenyang64@gmail.com>
2023-03-27 09:23:59 -0500

    removed logged in tests because the route ignores authentication

--- spec/requests/error_spec.rb
+++ spec/requests/error_spec.rb
@@ -1,49 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "/error", type: :request do
-  let(:organization) { create(:casa_org) }
-  let!(:admin) { create(:casa_admin, casa_org: organization) }
-  let!(:volunteer) { create(:volunteer, casa_org: organization) }
-  let!(:supervisor) { create(:supervisor, casa_org: organization) }
-
-  describe "GET /error" do
-    context "when logged in as admin user" do
-      it "raises an error causing an internal server error" do
-        sign_in admin
-
-        expect {
-          get error_path
-        }.to raise_error(StandardError, /This is an intentional test exception/)
-      end
-    end
-
-    context "when logged in as volunteer" do
-      it "raises an error causing an internal server error" do
-        sign_in volunteer
-
-        expect {
-          get error_path
-        }.to raise_error(StandardError, /This is an intentional test exception/)
-      end
-    end
-
-    context "when logged in as supervisor" do
-      it "raises an error causing an internal server error" do
-        sign_in supervisor
-
-        expect {
-          get error_path
-        }.to raise_error(StandardError, /This is an intentional test exception/)
-      end
-    end
-
-    context "when not logged in" do
-      it "raises an error causing an internal server error" do
-
-        expect {
-          get error_path
-        }.to raise_error(StandardError, /This is an intentional test exception/)
-      end
-    end
+  it "raises an error causing an internal server error" do
+    expect {
+      get error_path
+    }.to raise_error(StandardError, /This is an intentional test exception/)
   end
 end
