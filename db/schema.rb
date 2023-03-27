491e23c17a88f22306322c7a89ab6d9c98d46c33
shamitomita <shamitomita@gmail.com>
2023-03-27 12:07:11 -0400

    adding confirmable and email atrs to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_152808) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_27_155053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -516,7 +516,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_152808) do
     t.string "phone_number", default: ""
     t.boolean "receive_sms_notifications", default: false, null: false
     t.boolean "receive_email_notifications", default: true
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "old_emails", default: [], array: true
+    t.string "email_confirmation"
     t.index ["casa_org_id"], name: "index_users_on_casa_org_id"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
