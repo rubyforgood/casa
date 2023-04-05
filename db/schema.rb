cbccbc7a0586098c9eb785d778120e8f6c366b07
shamitomita <shamitomita@gmail.com>
2023-04-05 17:11:03 -0400

    remove email confirmation from models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_155053) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_202939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -539,7 +539,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_155053) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "old_emails", default: [], array: true
-    t.string "email_confirmation"
     t.index ["casa_org_id"], name: "index_users_on_casa_org_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
