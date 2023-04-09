0dc48b96a0a7644a6eee4f6724959019082b9394
shamitomita <shamitomita@gmail.com>
2023-04-09 18:43:45 -0400

    moved Users.update_all to after_party task

--- db/migrate/20230327154626_add_confirmable_to_users.rb
+++ db/migrate/20230327154626_add_confirmable_to_users.rb
@@ -10,7 +10,7 @@ class AddConfirmableToUsers < ActiveRecord::Migration[7.0]
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
     # users as confirmed, do the following
-    User.update_all confirmed_at: DateTime.now
+    # User.update_all confirmed_at: DateTime.now
     # All existing user accounts should be able to log in after this.
   end
 
--- db/migrate/20230405202939_remove_email_confirmation_from_users.rb
+++ db/migrate/20230405202939_remove_email_confirmation_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveEmailConfirmationFromUsers < ActiveRecord::Migration[7.0]
   def change
-    safety_assured { remove_column :users, :email_confirmation }
+    safety_assured { remove_column :users, :email_confirmation, :string }
   end
 end
