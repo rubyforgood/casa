5b63f546c9d2750b13dafb5b06ae40d38edd4d21
firelemons <shenyang64@gmail.com>
2023-04-10 23:02:58 -0500

    separate json and default request formats

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -1,11 +1,25 @@
 require "rails_helper"
 
 RSpec.describe "Health", type: :request do
+  before do
+    Casa::Application.load_tasks
+    Rake::Task["after_party:store_deploy_time"].invoke
+  end
+
   describe "GET /health" do
     before do
-      Casa::Application.load_tasks
-      Rake::Task["after_party:store_deploy_time"].invoke
       get "/health"
+    end
+
+    it "renders an html file" do
+      # delete this test when there are more specific tests about the page
+      expect(response.header["Content-Type"]).to include("text/html")
+    end
+  end
+
+  describe "GET /health.json" do
+    before do
+      get "/health.json"
     end
 
     it "renders a json file" do
