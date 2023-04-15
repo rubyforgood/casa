1d8238e7d374120fbe2da2e268d9dad9af919b03
Gustavo Valenzuela <GALTdea@gmail.com>
2023-04-15 13:36:38 -0700

    restructures preference_set_table_state_service_spec code better readability

--- app/controllers/preference_sets_controller.rb
+++ app/controllers/preference_sets_controller.rb
@@ -1,13 +1,12 @@
 class PreferenceSetsController < ApplicationController
+  before_action :skip_authorization
   def table_state
-    skip_authorization
     render json: PreferenceSetTableStateService.new(user_id: current_user.id).table_state(
       table_name: "volunteers_table"
     )
   end
 
   def table_state_update
-    skip_authorization
     render json: PreferenceSetTableStateService.new(user_id: current_user.id).update!(
       table_state: params["table_state"],
       table_name: "volunteers_table"
--- spec/services/preference_set_table_state_service_spec.rb
+++ spec/services/preference_set_table_state_service_spec.rb
@@ -2,44 +2,47 @@ require "rails_helper"
 
 RSpec.describe PreferenceSetTableStateService do
   subject { described_class.new(user_id: user.id) }
+  
   let!(:user) { create(:user) }
   let(:preference_set) { user.preference_set }
   let!(:table_state) { { "volunteers_table" => { "columns" => [{"visible" => true}] }} }
   let(:table_state2) { { "columns" => [{"visible" => false}] }}
   let(:table_name) { "volunteers_table" }
 
-
   describe '#update!' do
-    it 'updates the table state' do
-      expect {
-        PreferenceSetTableStateService.new(user_id: user.id).update!(table_state: table_state2, table_name: table_name)
-      }.to change {
-        preference_set.reload.table_state
-      }.from({}).to({ table_name => table_state2 })
+    context 'when the update is successful' do
+      it 'updates the table state' do
+        expect {
+          subject.update!(table_state: table_state2, table_name: table_name)
+        }.to change {
+          preference_set.reload.table_state
+        }.from({}).to({ table_name => table_state2 })
+      end
     end
 
     context 'when the update fails' do
       before do
         allow_any_instance_of(PreferenceSet).to receive(:save).and_return(false)
       end
+      
       it 'raises an error' do
         expect {
-          PreferenceSetTableStateService.new(user_id: user.id).update!(table_state: table_state2, table_name: table_name)
+          subject.update!(table_state: table_state2, table_name: table_name)
         }.to raise_error(PreferenceSetTableStateService::UpdateFailedError, "Failed to update table state for table_name: '#{table_name}'")
       end
     end
   end
 
   describe '#table_state' do
-    before do
-      table_state = { "columns" => [{"visible" => true}]}
-      user.preference_set.table_state["volunteers_table"] = table_state
-      user.preference_set.save! 
-    end
-   
     context 'when the preference set exists' do
+      before do
+        table_state = { "columns" => [{"visible" => true}]}
+        user.preference_set.table_state["volunteers_table"] = table_state
+        user.preference_set.save! 
+      end
+
       it 'returns the table state' do
-        expect(PreferenceSetTableStateService.new(user_id: user.id).table_state(table_name: "volunteers_table")).to eq(table_state["volunteers_table"])
+        expect(subject.table_state(table_name: "volunteers_table")).to eq(table_state["volunteers_table"])
       end
     end
 
@@ -49,7 +52,7 @@ RSpec.describe PreferenceSetTableStateService do
         preference_set.save
       end
 
-      it 'returns an empty hash' do
+      it 'returns nil' do
         expect(subject.table_state(table_name: table_name)).to eq(nil)
       end
     end
