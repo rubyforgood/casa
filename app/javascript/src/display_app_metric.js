2e6284f287fe9fcf154a1e71fa98aabe97d79721
aboongm <aboongm@yahoo.com>
2023-04-26 14:25:12 +0530

    Refactor sucess : function to avoid Code Climate not more than 25 lines linting rules

--- app/javascript/src/display_app_metric.js
+++ app/javascript/src/display_app_metric.js
@@ -3,59 +3,61 @@ import 'chartjs-adapter-luxon'
 
 Chart.register(...registerables)
 
-$(document).ready(function() {
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+$(document).ready(function () {
   $.ajax({
     type: 'GET',
     url: '/case_contacts/case_contacts_creation_times_in_last_week',
-    success: handleResponse,
-    error: function(xhr, status, error) {
-      // handle any errors here
+    success: function (data) {
+      const timestamps = data.timestamps
+
+      const counts = getCountsByDayAndHour(timestamps)
+      const dataset = getDatasetFromCounts(counts)
+
+      createChart(dataset)
     }
   })
 })
 
-function handleResponse(data) {
-  const counts = getCountsByDayAndHour(data.timestamps)
-  const dataset = createDatasetFromCounts(counts)
-  createChart(dataset)
-}
-
-function getCountsByDayAndHour(timestamps) {
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+function getCountsByDayAndHour (timestamps) {
   const counts = {}
+
   for (let i = 0; i < timestamps.length; i++) {
     const timestamp = new Date(timestamps[i] * 1000)
     const day = days[timestamp.getUTCDay()]
     const hour = timestamp.getUTCHours()
     const key = day + ' ' + hour
-    if (key in counts) {
-      counts[key] += 1
-    } else {
-      counts[key] = 1
-    }
+    counts[key] = (counts[key] || 0) + 1
   }
+
   return counts
 }
 
-function createDatasetFromCounts(counts) {
+function getDatasetFromCounts (counts) {
   const dataset = []
+
   for (const key in counts) {
     const parts = key.split(' ')
     const day = parts[0]
     const hour = parseInt(parts[1])
+    const count = counts[key]
+
     dataset.push({
       x: hour,
       y: days.indexOf(day),
-      r: Math.sqrt(counts[key]) * 2,
-      count: counts[key]
+      r: Math.sqrt(count) * 2,
+      count
     })
   }
+
   return dataset
 }
 
-function createChart(dataset) {
+function createChart (dataset) {
   const ctx = document.getElementById('myChart').getContext('2d')
-  const chart = new Chart(ctx, {
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
     type: 'bubble',
     data: {
       datasets: [{
@@ -65,63 +67,54 @@ function createChart(dataset) {
         borderColor: 'rgba(255, 99, 132, 1)'
       }]
     },
-    options: {
-      scales: {
-        x: {
-          ticks: {
-            beginAtZero: true,
-            stepSize: 1
-          }
-        },
-        y: {
-          ticks: {
-            beginAtZero: true,
-            stepSize: 1,
-            callback: function(value, index, values) {
-              const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-              return days[value]
-            }
-          }
-        }
-      },
-      plugins: {
-        tooltip: {
-          callbacks: {
-            label: function(context) {
-              const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-              const datum = context.dataset.data[context.dataIndex]
-              return datum.count + ' case contacts created on ' + days[datum.y] + ' at ' + datum.x + ':00'
-            }
-          }
-        }
-      }
-    }
+    options: getChartOptions()
   })
 }
 
-function getCountsByDayAndHour(timestamps) {
-  const counts = {}
-  for (let i = 0; i < timestamps.length; i++) {
-    const timestamp = new Date(timestamps[i] * 1000)
-    const day = timestamp.getUTCDay()
-    const hour = timestamp.getUTCHours()
-    const key = `${day} ${hour}`
-    counts[key] = (counts[key] || 0) + 1
+function getChartOptions () {
+  return {
+    scales: {
+      x: getXScale(),
+      y: getYScale()
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: getTooltipLabelCallback()
+        }
+      }
+    }
   }
-  return counts
 }
 
-function createDatasetFromCounts(counts) {
-  const dataset = []
-  for (const key in counts) {
-    const [dayIndex, hour] = key.split(' ')
-    const count = counts[key]
-    dataset.push({
-      x: Number(hour),
-      y: Number(dayIndex),
-      r: Math.sqrt(count) * 2,
-      count
-    })
+function getXScale () {
+  return {
+    ticks: {
+      beginAtZero: true,
+      stepSize: 1
+    }
   }
-  return dataset
+}
+
+function getYScale () {
+  return {
+    ticks: {
+      beginAtZero: true,
+      stepSize: 1,
+      callback: getYTickCallback()
+    }
+  }
+}
+
+function getYTickCallback () {
+  return function (value, index, values) {
+    return days[value]
+  }
+}
+
+function getTooltipLabelCallback () {
+  return function (context) {
+    const datum = context.dataset.data[context.dataIndex]
+    return datum.count + ' case contacts created on ' + days[datum.y] + ' at ' + datum.x + ':00'
+  }
 }
