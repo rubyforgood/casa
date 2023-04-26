b05b2fabdd6efdf31f7878f0780b69f0baafc4da
aboongm <aboongm@yahoo.com>
2023-04-26 12:04:16 +0530

    Update display_app_metric to correct code climate error

--- app/javascript/src/display_app_metric.js
+++ app/javascript/src/display_app_metric.js
@@ -3,84 +3,90 @@ import 'chartjs-adapter-luxon'
 
 Chart.register(...registerables)
 
-$(document).ready(function () {
+$(document).ready(function() {
   $.ajax({
     type: 'GET',
     url: '/case_contacts/case_contacts_creation_times_in_last_week',
-    success: function (data) {
-      // handle the response data here
-      // extract the timestamps array from the response data
-      const timestamps = data.timestamps
-
-      const dataset = []
-      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-      const counts = {}
-      for (let i = 0; i < timestamps.length; i++) {
-        const timestamp = new Date(timestamps[i] * 1000)
-        const day = days[timestamp.getUTCDay()]
-        const hour = timestamp.getUTCHours()
-        const key = day + ' ' + hour
-        if (key in counts) {
-          counts[key] += 1
-        } else {
-          counts[key] = 1
-        }
-      }
-      for (const key in counts) {
-        const parts = key.split(' ')
-        const day = parts[0]
-        const hour = parseInt(parts[1])
-        dataset.push({
-          x: hour,
-          y: days.indexOf(day),
-          r: Math.sqrt(counts[key]) * 2,
-          count: counts[key]
-        })
-      }
-
-      // create a chart with Chart.js
-      const ctx = document.getElementById('myChart').getContext('2d')
-      // eslint-disable-next-line no-new
-      new Chart(ctx, {
-        type: 'bubble',
-        data: {
-          datasets: [{
-            label: 'Case Contacts Creation Times in Last Week',
-            data: dataset,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)'
-          }]
-        },
-        options: {
-          scales: {
-            x: {
-              ticks: {
-                beginAtZero: true,
-                stepSize: 1
-              }
-            },
-            y: {
-              ticks: {
-                beginAtZero: true,
-                stepSize: 1,
-                callback: function (value, index, values) {
-                  return days[value]
-                }
-              }
-            }
-          },
-          plugins: {
-            tooltip: {
-              callbacks: {
-                label: function (context) {
-                  const datum = context.dataset.data[context.dataIndex]
-                  return datum.count + ' case contacts created on ' + days[datum.y] + ' at ' + datum.x + ':00'
-                }
-              }
-            }
-          }
-        }
-      })
+    success: handleResponse,
+    error: function(xhr, status, error) {
+      // handle any errors here
     }
   })
 })
+
+function handleResponse(data) {
+  const timestamps = data.timestamps
+  const counts = getCountsByDayAndHour(timestamps)
+  const dataset = createDatasetFromCounts(counts)
+
+  const ctx = document.getElementById('myChart').getContext('2d')
+  const chart = new Chart(ctx, {
+    type: 'bubble',
+    data: {
+      datasets: [{
+        label: 'Case Contacts Creation Times in Last Week',
+        data: dataset,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)'
+      }]
+    },
+    options: {
+      scales: {
+        x: {
+          ticks: {
+            beginAtZero: true,
+            stepSize: 1
+          }
+        },
+        y: {
+          ticks: {
+            beginAtZero: true,
+            stepSize: 1,
+            callback: function(value, index, values) {
+              const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+              return days[value]
+            }
+          }
+        }
+      },
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label: function(context) {
+              const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+              const datum = context.dataset.data[context.dataIndex]
+              return datum.count + ' case contacts created on ' + days[datum.y] + ' at ' + datum.x + ':00'
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+function getCountsByDayAndHour(timestamps) {
+  const counts = {}
+  for (let i = 0; i < timestamps.length; i++) {
+    const timestamp = new Date(timestamps[i] * 1000)
+    const day = timestamp.getUTCDay()
+    const hour = timestamp.getUTCHours()
+    const key = `${day} ${hour}`
+    counts[key] = (counts[key] || 0) + 1
+  }
+  return counts
+}
+
+function createDatasetFromCounts(counts) {
+  const dataset = []
+  for (const key in counts) {
+    const [dayIndex, hour] = key.split(' ')
+    const count = counts[key]
+    dataset.push({
+      x: Number(hour),
+      y: Number(dayIndex),
+      r: Math.sqrt(count) * 2,
+      count
+    })
+  }
+  return dataset
+}
