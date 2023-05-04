07631019d505abe96cf7b7eea346cf88bc072009
shamitomita <shamitomita@gmail.com>
2023-05-03 21:57:56 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -108,13 +108,13 @@ class ApplicationController < ActionController::Base
     redirect_to(root_url)
   end
 
-  def admin_email_change(user)
+  def admin_email_change(user) #for casa admins
     if user.saved_changes.include?("unconfirmed_email")
       redirect_to edit_polymorphic_path(user), notice: "Confirmation Email Sent To #{user.role}."
     end
   end
 
-  def all_casa_admin_email_change(user, casa_org)
+  def all_casa_admin_email_change(user, casa_org) #for all casa admins
     if user.saved_changes.include?("unconfirmed_email")
       redirect_to edit_all_casa_admins_casa_org_casa_admin_path(casa_org), notice: "Confirmation Email Sent To Casa Admin."
     end
