fd528a7f041c639edc3af31180986a409b3edac2
Shami Tomita <shamitomita@gmail.com>
2023-05-17 14:16:55 -0400

    WIP frontend

--- app/javascript/src/casa_org.js
+++ app/javascript/src/casa_org.js
@@ -1,13 +1,21 @@
 //class="collapse" aria-labelledby="headingOne" data-bs-parent="#accordionTwilio"
 $('document').ready(() => {
-    if ($('#casa_org_twilio_enabled').is(":checked")){
+    //class='enable_twilio' class='form-check-input' data - bs - toggle="collapse" data - bs - target="#collapseTwilio" aria - expanded="true"
+    //$('.accordionTwilio').addClass("enable_twilio")
+    $('.accordionTwilio').attr('data-bs-toggle', "collapse")
+    $('.accordionTwilio').attr('data-bs-target', "#collapseTwilio")
+    $('.accordionTwilio').attr('aria-expanded', "true")
+    console.log("Accordion Twilio", $('.accordionTwilio').val())
+    if ($('.accordionTwilio').is(":checked")){
         console.log("Check please")
-        $('#accordionTwilio').removeAttr('aria_expanded')
-        $('#accordionTwilio').removeClass('collapsed')
+        //$('.accordionTwilio').val('1')
+        $('.accordionTwilio').removeAttr('aria_expanded')
+        $('.accordionTwilio').removeClass('collapsed')
         $('#collapseTwilio').addClass('show')
         //$('#accordionTwilio').removeClass('collapse')
         //$('#accordionTwilio').addClass('show')
     }else{
+        //$('#accordionTwilio').val('0')
         //$('#accordionTwilio').addClass('collapsed')
     }
 })
