81a1ee4dc16bcf5e503a5167734628977a6c4c46
Shami Tomita <shamitomita@gmail.com>
2023-05-19 12:04:57 -0400

    form works?

--- app/javascript/src/casa_org.js
+++ app/javascript/src/casa_org.js
@@ -5,19 +5,25 @@ const TWILIO_PHONE_NUMBER = "#casa_org_twilio_phone_number"
 function hello(){
     console.log("hello")
     if ($('.accordionTwilio').is(":checked")) {
-        $("#casa_org_twilio_phone_number").attr("required", "true")
-        $("#casa_org_twilio_account_sid").attr("required", "true")
-        $("#casa_org_twilio_api_key_sid").attr("required", "true")
-        $("#casa_org_twilio_api_key_secret").attr("required", "true")
+        console.log("checked")
+        $("#casa_org_twilio_phone_number").attr("required", true)
+        $("#casa_org_twilio_phone_number").removeAttr("disabled")
+        $("#casa_org_twilio_account_sid").attr("required", true)
+        $("#casa_org_twilio_account_sid").removeAttr("disabled")
+        $("#casa_org_twilio_api_key_sid").attr("required", true)
+        $("#casa_org_twilio_api_key_sid").removeAttr("disabled")
+        $("#casa_org_twilio_api_key_secret").attr("required", true)
+        $("#casa_org_twilio_api_key_secret").removeAttr("disabled")
     }else{
-        $("#casa_org_twilio_phone_number").removeAttr("required", "false")
-        $("#casa_org_twilio_phone_number").attr("value", "")
-        $("#casa_org_twilio_account_sid").removeAttr("required", "false")
-        $("#casa_org_twilio_account_sid").attr("value", "")
-        $("#casa_org_twilio_api_key_sid").removeAttr("required", "false")
-        $("#casa_org_twilio_api_key_sid").attr("value", "")
-        $("#casa_org_twilio_api_key_secret").removeAttr("required", "false")
-        $("#casa_org_twilio_api_key_secret").attr("value", "")
+        console.log("unchecked")
+        $("#casa_org_twilio_phone_number").removeAttr("required", false)
+        $("#casa_org_twilio_phone_number").attr("disabled", true)
+        $("#casa_org_twilio_account_sid").removeAttr("required", false)
+        $("#casa_org_twilio_account_sid").attr("disabled", true)
+        $("#casa_org_twilio_api_key_sid").removeAttr("required", false)
+        $("#casa_org_twilio_api_key_sid").attr("disabled", true)
+        $("#casa_org_twilio_api_key_secret").removeAttr("required", false)
+        $("#casa_org_twilio_api_key_secret").attr("disabled", true)
     }
 }
 $('document').ready(() => {
