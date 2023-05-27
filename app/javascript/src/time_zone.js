dff050409c2b0f34f1077a3085033f39451f5248
Eric Halverson <ericleifhalverson@gmail.com>
2023-05-27 15:17:21 -0700

    Create new js file to capture and store local user timezone in a cookie and require file in `application.js`

---
+++ app/javascript/src/time_zone.js
@@ -0,0 +1,20 @@
+import Cookies from 'js-cookie'
+import jstz from 'jstz'
+
+// Rails doesn't support every timezone that Intl supports
+export function findTimeZone() {
+  const oldIntl = window.Intl
+  try {
+    window.Intl = undefined
+    const tz = jstz.determine().name()
+    window.Intl = oldIntl
+    return tz
+  } catch (e) {
+    // sometimes (on android) you can't override intl
+    return jstz.determine().name()
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  Cookies.set("browser_time_zone", findTimeZone(), { expires: 365, path: "/", secure: true, sameSite: 'strict' })
+})
