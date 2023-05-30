a23910686dfb54b98a5d1e3162ea8663e31e2bc3
Gustavo Valenzuela <GALTdea@gmail.com>
2023-05-30 12:47:03 -0700

    WIP: adding preference_set requests specs

--- spec/requests/preference_sets_spec.rb
+++ spec/requests/preference_sets_spec.rb
@@ -1,7 +1,46 @@
 require "rails_helper"
 
 RSpec.describe "PreferenceSets", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /preference_sets/table_state/volunteers_table" do
+    
+    let!(:casa_org) { create(:casa_org, ) }
+    let!(:supervisor) { create(:supervisor, casa_org: casa_org) }
+
+    let!(:table_state) { { "volunteers_table" => { "columns" => [{ "visible" => true }, { "visible" => false }] } } }
+    let!(:supervisor_table_state) { { "volunteers_table" => { "columns" => [{ "visible" => true }, { "visible" => false }] } } }
+    let!(:supervisor_preference_set) do
+      preference_set = PreferenceSet.create!(user: supervisor)
+      puts "Before update: #{preference_set.table_state}"
+      preference_set.update!(table_state: supervisor_table_state)
+      puts "After update: #{preference_set.reload.table_state}"
+      preference_set
+    end
+    
+    
+
+    it "returns http success" do
+      sign_in supervisor
+      get "/preference_sets/table_state/volunteers_table"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns the correct table state" do
+      sign_in supervisor
+      get table_state_preference_sets_path(table_name: "volunteers_table")
+      
+      puts "Response body: #{response.body}"
+
+      expect(response.body).to eq(table_state.to_json)
+    end
+
+    it "testing test" do
+      sign_in supervisor
+      
+      puts "Before GET request: #{PreferenceSet.find_by(user: supervisor).reload.table_state}"
+      get table_state_preference_sets_path(table_name: "volunteers_table")
+      puts "After GET request: #{PreferenceSet.find_by(user: supervisor).table_state}"
+      
+    end
+    
   end
 end
