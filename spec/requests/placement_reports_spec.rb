ccb5eb56d839331b918982e58b3f7fec6b037665
Rae Stanton <raestanton93@gmail.com>
2023-05-31 13:07:24 -0700

    lint

--- spec/requests/placement_reports_spec.rb
+++ spec/requests/placement_reports_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "/placement_reports", type: :request do
   let(:admin) { create(:casa_admin) }
 
-  describe 'GET /index' do
-    it 'renders a successful response' do
+  describe "GET /index" do
+    it "renders a successful response" do
       sign_in admin
 
-      get placement_reports_path, headers: { "ACCEPT" => "*/*" }
+      get placement_reports_path, headers: {"ACCEPT" => "*/*"}
       expect(response).to be_successful
     end
   end
