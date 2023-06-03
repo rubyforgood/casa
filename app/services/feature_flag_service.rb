dd6f38737e001d5b2a3b364804f733a4be36ad8c
Jos O'shea <jos.oshea.ky@gmail.com>
2023-06-02 23:08:25 -0400

    add feature flag and update view

--- app/services/feature_flag_service.rb
+++ app/services/feature_flag_service.rb
@@ -1,7 +1,8 @@
 class FeatureFlagService
   SOME_FLAG = "some_flag"
   SHOW_ADDITIONAL_EXPENSES_FLAG = "show_additional_expenses"
-
+  SHOW_OTHER_DUTIES_FLAG = "show_other_duties"
+  
   def self.is_enabled?(feature_flag_name)
     FeatureFlag.find_by(name: feature_flag_name)&.enabled
   end
