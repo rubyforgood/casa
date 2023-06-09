a099c48e2b235762378a6bb803b557c5706f4c6b
Eric Halverson <ericleifhalverson@gmail.com>
2023-06-09 13:39:28 -0700

    Add helper method to 'DRY' up other method blocks

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -9,63 +9,39 @@ class UserDecorator < Draper::Decorator
     h.browser_time_zone
   end
 
-  def formatted_created_at
+  # helper method to 'DRY' up the other methods : )
+  def formatted_timestamp(attribute)
     format_key = context[:format] || :full
+    timestamp = object.public_send(attribute)
 
     if format_key == :edit_profile
-      I18n.l(object.created_at&.in_time_zone(local_time_zone), format: format_key, default: nil)
+      I18n.l(timestamp&.in_time_zone(local_time_zone), format: format_key, default: nil)
     else
-      I18n.l(object.created_at, format: format_key, default: nil)
+      I18n.l(timestamp, format: format_key, default: nil)
     end
+  end
+
+  def formatted_created_at
+    formatted_timestamp(:created_at)
   end
 
   def formatted_updated_at
-    format_key = context[:format] || :full
-
-    if format_key == :edit_profile
-      I18n.l(object.updated_at&.in_time_zone(local_time_zone), format: format_key, default: nil)
-    else
-      I18n.l(object.updated_at, format: format_key, default: nil)
-    end
+    formatted_timestamp(:updated_at)
   end
 
   def formatted_current_sign_in_at
-    format_key = context[:format] || :full
-
-    if format_key == :edit_profile
-      I18n.l(object.current_sign_in_at&.in_time_zone(local_time_zone), format: format_key, default: nil)
-    else
-      I18n.l(object.current_sign_in_at, format: format_key, default: nil)
-    end
+    formatted_timestamp(:current_sign_in_at)
   end
 
   def formatted_invitation_accepted_at
-    format_key = context[:format] || :full
-
-    if format_key == :edit_profile
-      I18n.l(object.invitation_accepted_at&.in_time_zone(local_time_zone), format: format_key, default: nil)
-    else
-      I18n.l(object.invitation_accepted_at, format: format_key, default: nil)
-    end
+    formatted_timestamp(:invitation_accepted_at)
   end
 
   def formatted_reset_password_sent_at
-    format_key = context[:format] || :full
-
-    if format_key == :edit_profile
-      I18n.l(object.reset_password_sent_at&.in_time_zone(local_time_zone), format: format_key, default: nil)
-    else
-      I18n.l(object.reset_password_sent_at, format: format_key, default: nil)
-    end
+    formatted_timestamp(:reset_password_sent_at)
   end
 
   def formatted_invitation_sent_at
-    format_key = context[:format] || :full
-
-    if format_key == :edit_profile
-      I18n.l(object.invitation_sent_at&.in_time_zone(local_time_zone), format: format_key, default: nil)
-    else
-      I18n.l(object.invitation_sent_at, format: format_key, default: nil)
-    end
+    formatted_timestamp(:invitation_sent_at)
   end
 end
