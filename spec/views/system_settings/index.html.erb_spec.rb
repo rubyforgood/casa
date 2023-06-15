36886960231fa85253ecbc45789372ec306e0131
Daniel Aguilar <danaguilar6@gmail.com>
2023-06-15 17:23:10 -0500

    Added controller and form to update FeatureFlag

--- spec/views/system_settings/index.html.erb_spec.rb
+++ spec/views/system_settings/index.html.erb_spec.rb
@@ -10,7 +10,12 @@ RSpec.describe "System Settings", type: :view do
   end
 
   it "has a Feature Toggles menu option" do
+    assign :show_additional_expenses, true
+
     render template: "system_settings/index"
+
     expect(rendered).to have_text("Feature Toggles")
+    expect(rendered).to have_text("New Case Contact Form")
+    expect(rendered).to have_checked_field("show_additional_expenses")
   end
 end
