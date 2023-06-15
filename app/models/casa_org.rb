91a84bfd276a3f074662e3667968b6891d991ba3
SHAMI_TOMITA <shamitomita@gmail.com>
2023-06-15 16:46:54 -0400

    check for twilio enabled

--- app/models/casa_org.rb
+++ app/models/casa_org.rb
@@ -8,7 +8,7 @@ class CasaOrg < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates_with CasaOrgValidator
-  validate :validate_twilio_credentials, if: -> { twilio_account_sid.present? || twilio_api_key_sid.present? || twilio_api_key_secret.present? }, on: :update
+  validate :validate_twilio_credentials, if: -> { twilio_enabled || twilio_account_sid.present? || twilio_api_key_sid.present? || twilio_api_key_secret.present? }, on: :update
 
   has_many :users, dependent: :destroy
   has_many :casa_cases, dependent: :destroy
