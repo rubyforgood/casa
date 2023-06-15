7a5e4f353a6c50fb33e22ed10ae51919ec9eaf8e
SHAMI_TOMITA <shamitomita@gmail.com>
2023-06-15 10:51:48 -0400

    services updates

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -6,23 +6,18 @@ class TwilioService
   attr_writer :api_key, :api_secret, :acc_sid, :casa_org
 
   def initialize(casa_org)
-    #if or lazily create, not exceptional! they are slower than conditonal, 
-    # eliminating 
-    raise TwilioCasaOrgError.new "Twilio is disabled for #{casa_org.name}" unless casa_org.twilio_enabled? #this makes it hard to test in isolation
-
     @api_key = casa_org.twilio_api_key_sid
     @api_secret = casa_org.twilio_api_key_secret
     @acc_sid = casa_org.twilio_account_sid
-
-    #@client = Twilio::REST::Client.new(@api_key, @api_secret, @acc_sid)
+    @enabled = casa_org.twilio_enabled
   end
 
-  def client
-    @client ||= Twilio::REST::Client.new(@api_key, @api_secret, @acc_sid)
+  def client #lazy create client only if twilio enabled
+    @client = Twilio::REST::Client.new(@api_key, @api_secret, @acc_sid)
   end 
 
   def enabled? 
-    #as long as casa_org twilio check 
+   @enabled
   end 
   # this method takes in a hash
   # required keys are: From:, To:, Body:
@@ -30,14 +25,17 @@ class TwilioService
   def send_sms(params)
     #return unless casa_org twilio enabled
     #add check here, Twilio client
+    if !self.enabled? 
+      return nil 
+    end  
     from = params[:From]
     body = params.key?(:URL) ? params[:Body] + params[:URL] : params[:Body]
     to = params[:To]
     # returns a twilio API message object
     # refer to docs: https://www.twilio.com/docs/sms/api/message-resource#message-properties
     begin
-      #clientMethod.create if you ever get here, then it would create (i.e. lazy create)
-      @client.messages.create(
+      client 
+      client.messages.create(
         from: from,
         body: body,
         to: to
