e366e4fb6829bcefad3b554c5367ce090d06ab4c
SHAMI_TOMITA <shamitomita@gmail.com>
2023-06-16 11:52:31 -0400

    Javascript files

---
+++ app/javascript/src/sms_reactivation_toggle.js
@@ -0,0 +1,16 @@
+
+$('document').ready(() => {
+  if ($('#twilio_disabled').length) {
+    $('#twilio_disabled').removeClass('main-btn danger-btn-outline btn-hover btn-sm my-1')
+    $('#twilio_disabled').addClass('main-btn deactive-btn btn-sm my-1')
+    $('#twilio_tooltip').attr('data-bs-toggle', 'tooltip')
+    $('#twilio_tooltip').attr('data-bs-placement', 'bottom')
+    $('#twilio_tooltip').attr('data-turbo', 'false')
+    $('#twilio_tooltip').attr('title', "Twilio is not enabled for this user's CASA org")
+
+    $('#twilio_disabled').on('click', function (event) {
+      event.preventDefault()
+      console.log('tooltip?')
+    })
+  }
+})
