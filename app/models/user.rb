884ef466743ba443438cae5b95f2e5985bd6fb92
Xihai Luo <xihai.luo01@gmail.com>
2023-06-21 15:59:25 -0400

    add back confirmable module to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   validates_with UserValidator
 
-  devise :database_authenticatable, :invitable, :recoverable, :validatable, :timeoutable, :trackable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  devise :database_authenticatable, :invitable, :recoverable, :validatable, :timeoutable, :trackable, :confirmable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   belongs_to :casa_org
 
