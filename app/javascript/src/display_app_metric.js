51d4bd54f8a39101e5d697c1a2bb7dad30b946ac
Nwabor <knwabor@gmail.com>
2023-06-30 14:58:24 +0100

    display error when the chart faills to load

--- app/javascript/src/display_app_metric.js
+++ app/javascript/src/display_app_metric.js
@@ -1,120 +1,140 @@
-import { Chart, registerables } from 'chart.js'
-import 'chartjs-adapter-luxon'
+import { Chart, registerables } from "chart.js";
+import "chartjs-adapter-luxon";
 
-Chart.register(...registerables)
+Chart.register(...registerables);
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 $(document).ready(function () {
   $.ajax({
-    type: 'GET',
-    url: '/health/case_contacts_creation_times_in_last_week',
+    type: "GET",
+    url: "/health/case_contacts_creation_times_in_last_week",
     success: function (data) {
-      const timestamps = data.timestamps
+      const timestamps = data.timestamps;
+      const counts = getCountsByDayAndHour(timestamps);
+      const dataset = getDatasetFromCounts(counts);
 
-      const counts = getCountsByDayAndHour(timestamps)
-      const dataset = getDatasetFromCounts(counts)
+      createChart(dataset);
+    },
+    error: function (xhr, status, error) {
+      const errorMessage = $("<p>", {
+        class: "error-message",
+        text: getErrorMessage(xhr),
+      });
+      $("#chart-container").append(errorMessage);
+      $(".text-center").hide();
+    },
+  });
+});
 
-      createChart(dataset)
-    }
-  })
-})
-
-function getCountsByDayAndHour (timestamps) {
-  const counts = {}
+function getCountsByDayAndHour(timestamps) {
+  const counts = {};
 
   for (let i = 0; i < timestamps.length; i++) {
-    const timestamp = new Date(timestamps[i] * 1000)
-    const day = days[timestamp.getUTCDay()]
-    const hour = timestamp.getUTCHours()
-    const key = day + ' ' + hour
-    counts[key] = (counts[key] || 0) + 1
+    const timestamp = new Date(timestamps[i] * 1000);
+    const day = days[timestamp.getUTCDay()];
+    const hour = timestamp.getUTCHours();
+    const key = day + " " + hour;
+    counts[key] = (counts[key] || 0) + 1;
   }
 
-  return counts
+  return counts;
 }
 
-function getDatasetFromCounts (counts) {
-  const dataset = []
+function getDatasetFromCounts(counts) {
+  const dataset = [];
 
   for (const key in counts) {
-    const parts = key.split(' ')
-    const day = parts[0]
-    const hour = parseInt(parts[1])
-    const count = counts[key]
+    const parts = key.split(" ");
+    const day = parts[0];
+    const hour = parseInt(parts[1]);
+    const count = counts[key];
 
     dataset.push({
       x: hour,
       y: days.indexOf(day),
       r: Math.sqrt(count) * 2,
-      count
-    })
+      count,
+    });
   }
 
-  return dataset
+  return dataset;
 }
 
-function createChart (dataset) {
-  const ctx = document.getElementById('myChart').getContext('2d')
+function createChart(dataset) {
+  const ctx = document.getElementById("myChart").getContext("2d");
   // eslint-disable-next-line no-unused-vars
   const myChart = new Chart(ctx, {
-    type: 'bubble',
+    type: "bubble",
     data: {
-      datasets: [{
-        label: 'Case Contacts Creation Times in Last Week',
-        data: dataset,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)'
-      }]
+      datasets: [
+        {
+          label: "Case Contacts Creation Times in Last Week",
+          data: dataset,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+        },
+      ],
     },
-    options: getChartOptions()
-  })
+    options: getChartOptions(),
+  });
 }
 
-function getChartOptions () {
+function getChartOptions() {
   return {
     scales: {
       x: getXScale(),
-      y: getYScale()
+      y: getYScale(),
     },
     plugins: {
       tooltip: {
         callbacks: {
-          label: getTooltipLabelCallback()
-        }
-      }
-    }
-  }
+          label: getTooltipLabelCallback(),
+        },
+      },
+    },
+  };
 }
 
-function getXScale () {
-  return {
-    ticks: {
-      beginAtZero: true,
-      stepSize: 1
-    }
-  }
-}
-
-function getYScale () {
+function getXScale() {
   return {
     ticks: {
       beginAtZero: true,
       stepSize: 1,
-      callback: getYTickCallback()
-    }
-  }
+    },
+  };
 }
 
-function getYTickCallback () {
+function getYScale() {
+  return {
+    ticks: {
+      beginAtZero: true,
+      stepSize: 1,
+      callback: getYTickCallback(),
+    },
+  };
+}
+
+function getYTickCallback() {
   return function (value, index, values) {
-    return days[value]
-  }
+    return days[value];
+  };
 }
 
-function getTooltipLabelCallback () {
+function getTooltipLabelCallback() {
   return function (context) {
-    const datum = context.dataset.data[context.dataIndex]
-    return datum.count + ' case contacts created on ' + days[datum.y] + ' at ' + datum.x + ':00'
+    const datum = context.dataset.data[context.dataIndex];
+    return datum.count + " case contacts created on " + days[datum.y] + " at " + datum.x + ":00";
+  };
+}
+
+function getErrorMessage(xhr) {
+  // Get the specific error message based on status codes or response from server
+  if (xhr.status === 404) {
+    return "The requested resource was not found";
+  } else if (xhr.status === 500) {
+    return "Internal server error occurred.";
+  } else {
+    return "An unknown error occurred.";
   }
 }
