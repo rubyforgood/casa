91c5c6ac78683f92bcbd5c967c3128a859223fda
compwron <compwron@users.noreply.github.com>
2023-07-01 14:33:38 -0700

    bundle exec standardrb --fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -345,5 +345,4 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-
 end
