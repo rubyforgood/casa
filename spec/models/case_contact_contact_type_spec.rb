8d17bf5d56f87f4c50b1d89ebce3346dd68f5a27
Gauri Joshi <gauri.joshi@planetargon.com>
2023-07-10 19:59:13 -0700

    Test for contact type groups listed in order

--- spec/models/case_contact_contact_type_spec.rb
+++ spec/models/case_contact_contact_type_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe CaseContactContactType, type: :model do
       case_contact.contact_types << contact_type
     }.to raise_error(ActiveRecord::RecordInvalid)
   end
+
+  it "sorts contact types alphabetically" do 
+    case_contact = create(:case_contact)
+    contact_type_groups = []
+    contact_type = create(:contact_type)
+    contact_type_B = create(:contact_type)
+    contact_type_groups << contact_type
+    contact_type_groups << contact_type_B 
+
+    expect(contact_type_groups).to eq([contact_type, contact_type_B])
+  end
 end
