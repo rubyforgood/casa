6f8b008a8e24aa09594a576c3eca89ec44bf89a0
Erin Claudio <eclaudio@gmail.com>
2023-07-19 14:18:23 -0400

    Commented out unused vcariable

--- app/javascript/src/session_timeout_poller.js
+++ app/javascript/src/session_timeout_poller.js
@@ -1,5 +1,5 @@
 const deviseTimeoutInMinutes = 180
-const timeoutDuration = 180;
+// const timeoutDuration = 180
 const twoMinuteWarning = deviseTimeoutInMinutes - 2
 const totalTimerAmount = twoMinuteWarning * 60 * 1000
 const deviseTimeoutInMilliseconds = deviseTimeoutInMinutes * 60 * 1000
@@ -7,7 +7,7 @@ const startTime = new Date().getTime()
 let lastTime = new Date().getTime()
 let currentTime
 let timeElapsed
-console.log("here", window.timeout)
+console.log('here', window.timeout)
 
 function warningBoxAndReload () {
   window.alert('Warning: You will be logged off in 2 minutes due to inactivity.')
