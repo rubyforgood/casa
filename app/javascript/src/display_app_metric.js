089dedd776fb7fd3ca911f9d323d60be9c7ac893
firelemons <shenyang64@gmail.com>
2023-07-21 21:32:17 -0500

    fix js errors

--- app/javascript/src/display_app_metric.js
+++ app/javascript/src/display_app_metric.js
@@ -6,26 +6,30 @@ Chart.register(...registerables)
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 $(document).ready(function () {
-  $.ajax({
-    type: 'GET',
-    url: '/health/case_contacts_creation_times_in_last_week',
-    success: function (data) {
-      const timestamps = data.timestamps
-      const counts = getCountsByDayAndHour(timestamps)
-      const dataset = getDatasetFromCounts(counts)
+  const chartElement = document.getElementById('myChart')
 
-      createChart(dataset)
-    },
-    error: function (xhr, status, error) {
-      console.error('Failed to fetch data for case contact entry times chart display')
-      console.error(error)
-      $('#chart-error-message').append(`
-        <div class="alert alert-danger" role="alert">
-           Failed to display metric chart. Check the console for error details.
-        </div>`)
-      $('.text-center').hide()
-    }
-  })
+  if (chartElement) {
+    $.ajax({
+      type: 'GET',
+      url: '/health/case_contacts_creation_times_in_last_week',
+      success: function (data) {
+        const timestamps = data.timestamps
+        const counts = getCountsByDayAndHour(timestamps)
+        const dataset = getDatasetFromCounts(counts)
+
+        createChart(chartElement, dataset)
+      },
+      error: function (xhr, status, error) {
+        console.error('Failed to fetch data for case contact entry times chart display')
+        console.error(error)
+        $('#chart-error-message').append(`
+          <div class="alert alert-danger" role="alert">
+            Failed to display metric chart. Check the console for error details.
+          </div>`)
+        $('.text-center').hide()
+      }
+    })
+  }
 })
 
 function getCountsByDayAndHour (timestamps) {
@@ -62,10 +66,10 @@ function getDatasetFromCounts (counts) {
   return dataset
 }
 
-function createChart (dataset) {
-  const ctx = document.getElementById('myChart').getContext('2d')
-  // eslint-disable-next-line no-unused-vars
-  const myChart = new Chart(ctx, {
+function createChart (chartElement, dataset) {
+  const ctx = chartElement.getContext('2d')
+
+  new Chart(ctx, {
     type: 'bubble',
     data: {
       datasets: [
