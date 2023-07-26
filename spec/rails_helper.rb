016790abf1ebeb13f5ff17c0ed429fd1df2442aa
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-07-26 16:40:59 -0700

    Remove webdrivers gem since selenium-webdriver is updated to 4.10
    https://github.com/titusfortner/webdrivers/issues/247

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 require "pundit/rspec"
-require "webdrivers" unless ENV["DOCKER"]
 require "view_component/test_helpers"
 require "capybara/rspec"
 
