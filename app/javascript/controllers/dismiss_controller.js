130c8d363adac3ef6e0c51b6032d6cc6fc0961ec
Mike Dalton <mdalton@triumphpay.com>
2023-07-28 15:08:29 -0400

    Fix linters

--- app/javascript/controllers/dismiss_controller.js
+++ app/javascript/controllers/dismiss_controller.js
@@ -1,13 +1,13 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['element'];
+  static targets = ['element']
 
-  dismiss(event) {
+  dismiss (event) {
     event.preventDefault();
 
-    const { id } = event.params;
-    document.cookie = `dismiss_${id}=true`;
-    this.elementTarget.classList.add('d-none');
+    const { id } = event.params
+    document.cookie = `dismiss_${id}=true`
+    this.elementTarget.classList.add('d-none')
   }
 }
