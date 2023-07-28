21bbeaa23685dbe9d2e36a1a41c78dc36f64b726
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-07-28 16:53:06 -0400

    Un-DRY the learning hours report model spec

--- spec/models/learning_hours_report_spec.rb
+++ spec/models/learning_hours_report_spec.rb
@@ -2,23 +2,21 @@ require "rails_helper"
 require "csv"
 
 RSpec.describe LearningHoursReport, type: :model do
-  let!(:casa_org) { build(:casa_org) }
-  let!(:users) { create_list(:user, 3, casa_org: casa_org) }
-
   describe "#to_csv" do
-    let(:result) { CSV.parse(described_class.new(casa_org.id).to_csv) }
-
     context "when there are learning hours" do
-      let!(:learning_hours) do
-        [
-          create(:learning_hour, user: users[0]),
-          create(:learning_hour, user: users[1], learning_type: :movie),
-          create(:learning_hour, user: users[2], learning_type: :webinar)
-        ]
-      end
-
       it "includes all learning hours" do
+        casa_org = build(:casa_org)
+        users = create_list(:user, 3, casa_org: casa_org)
+        learning_hours =
+          [
+            create(:learning_hour, user: users[0]),
+            create(:learning_hour, user: users[1], learning_type: :movie),
+            create(:learning_hour, user: users[2], learning_type: :webinar)
+          ]
+        result = CSV.parse(described_class.new(casa_org.id).to_csv)
+
         expect(result.length).to eq(learning_hours.length + 1)
+
         learning_hours.each_with_index do |learning_hour, index|
           wait_for_csv_parse(learning_hour, result[index + 1], %i[name learning_type])
           expect(result[index + 1]).to eq([
@@ -33,9 +31,10 @@ RSpec.describe LearningHoursReport, type: :model do
     end
 
     context "when there are no learning hours" do
-      let(:result) { CSV.parse(described_class.new(casa_org.id).to_csv) }
-
       it "returns only the header" do
+        casa_org = build(:casa_org)
+        result = CSV.parse(described_class.new(casa_org.id).to_csv)
+
         expect(result.length).to eq(1)
         expect(result[0]).to eq([
           "Volunteer Name",
