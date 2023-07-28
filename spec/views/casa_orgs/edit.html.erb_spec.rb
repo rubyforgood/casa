2b6958a9d48be6e1fcca5353573edc5fc6444a6c
Ian Norris <iannorris@Ians-MacBook-Air.local>
2023-07-28 08:44:09 -0700

    remove more lets per feedback

--- spec/views/casa_orgs/edit.html.erb_spec.rb
+++ spec/views/casa_orgs/edit.html.erb_spec.rb
@@ -1,34 +1,39 @@
 require "rails_helper"
 
 RSpec.describe "casa_org/edit", type: :view do
-  let(:organization) { create :casa_org }
-  let(:admin) { build_stubbed(:all_casa_admin) }
 
   before do
-    allow(view).to receive(:current_organization).and_return(organization)
     assign(:contact_type_groups, [])
     assign(:contact_types, [])
     assign(:hearing_types, [])
     assign(:judges, [])
     assign(:sent_emails, [])
 
-    sign_in admin
+    sign_in build_stubbed(:all_casa_admin)
 
-    render template: "casa_org/edit"
   end
 
   it "renders and does not show download prompt if new org" do
+    organization = create :casa_org
+    allow(view).to receive(:current_organization).and_return(organization)
+
+    render template: "casa_org/edit"
+
     expect(rendered).not_to have_text("Download Current Template")
   end
 
   context "with a template uploaded" do
-    before do
-      organization.court_report_template.attach(io: File.open("#{Rails.root}/app/documents/templates/default_report_template.docx"), filename: 'default_report_template
-.docx', content_type: "application/docx")
-    end
 
     it "renders a prompt to download current template" do
-      expect(rendered).not_to have_text("Download Current Template")
+      organization = create :casa_org
+      allow(view).to receive(:current_organization).and_return(organization)
+
+      organization.court_report_template.attach(io: File.open("#{Rails.root}/app/documents/templates/default_report_template.docx"), filename: 'default_report_template
+.docx', content_type: "application/docx")
+
+      render template: "casa_org/edit"
+
+      expect(rendered).to have_text("Download Current Template")
     end
   end
 end
