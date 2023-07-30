55578b4fdf021416b21df3578943336d41578f02
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-07-30 08:24:15 -0700

    Flaky spec fix: Explicity set judge names (#5065)

--- spec/models/judge_spec.rb
+++ spec/models/judge_spec.rb
@@ -5,29 +5,31 @@ RSpec.describe Judge, type: :model do
   it { is_expected.to validate_presence_of(:name) }
 
   it "has a valid factory" do
-    expect(build(:judge).valid?).to be true
+    judge = build(:judge)
+
+    expect(judge).to be_valid
   end
 
-  describe "for_organization" do
-    let!(:casa_org_1) { create(:casa_org) }
-    let!(:casa_org_2) { create(:casa_org) }
-    let!(:record_1) { create(:judge, casa_org: casa_org_1) }
-    let!(:record_2) { create(:judge, casa_org: casa_org_2) }
-
+  describe ".for_organization" do
     it "returns only records matching the specified organization" do
+      casa_org_1 = create(:casa_org)
+      casa_org_2 = create(:casa_org)
+      record_1 = create(:judge, casa_org: casa_org_1)
+      record_2 = create(:judge, casa_org: casa_org_2)
+
       expect(described_class.for_organization(casa_org_1)).to eq([record_1])
       expect(described_class.for_organization(casa_org_2)).to eq([record_2])
     end
   end
 
   describe "default scope" do
-    let(:casa_org) { create(:casa_org) }
-    let(:judges) do
-      5.times.map { create(:judge, casa_org: casa_org) }
-    end
-
     it "orders alphabetically by name" do
-      expect(described_class.for_organization(casa_org)).to eq(judges.sort_by(&:name))
+      casa_org = create(:casa_org)
+      judge1 = create(:judge, name: "Gamma")
+      judge2 = create(:judge, name: "Alpha")
+      judge3 = create(:judge, name: "Epsilon")
+
+      expect(described_class.for_organization(casa_org)).to eq [judge2, judge3, judge1]
     end
   end
 end
