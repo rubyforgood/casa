d059b8ab0f1ff161482a40bb735bc6b95fc8ec92
Erin Claudio <eclaudio@gmail.com>
2023-08-02 13:53:41 -0400

    Update with linter changes

--- spec/system/application/timeout_warning_spec.rb
+++ spec/system/application/timeout_warning_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "/*", type: :system do
-
   context "when user is signed in" do
     let(:user) { create(:volunteer) }
     before do
@@ -10,25 +9,10 @@ RSpec.describe "/*", type: :system do
     it "renders the seconds before logout as a javascript variable" do
       visit "/"
       parsed_page = Nokogiri::HTML(page.html)
-      expect(parsed_page.at('script').text.strip).to include(user.timeout_in.in_seconds.to_s)
+      expect(parsed_page.at("script").text.strip).to include(user.timeout_in.in_seconds.to_s)
     end
 
     xit "warns the user two mintues before logout", js: true do
-      # visit "/"
-      # travel_to(user.timeout_in.from_now - 2.minutes - 1.seconds) do
-      #   sleep 2
-      #   Capybara.using_wait_time(5) do
-      #     begin
-      #       wait = Selenium::WebDriver::Wait.new(timeout: 5) # Set a timeout limit
-
-      #       expect(page).to have_text("############################")
-      #       # expect(alert.text).to include('known_text') # Replace 'known_text' with expected text
-      #       # alert.accept
-      #     rescue Selenium::WebDriver::Error::TimeoutError
-      #       fail "Alert did not appear in time"
-          end
-        end
-      end
     end
   end
 end
