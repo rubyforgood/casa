23a232325b8da37971d4b30ceb885723443fdaec
Steve Carroll <stephenalexandercarroll@gmail.com>
2023-08-03 11:19:08 -0400

    Add checkbox toggle to checkbox controller

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -1,5 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  
+  static targets = [ "checkbox" ]
+  static values = { checked: Boolean }
+
+  connect() {
+    this.checked = this.hasCheckboxTarget && this.checkboxTarget.checked
+    console.log(`Checkbox is ${this.checked}`)
+  }
+
+  toggle() {
+    this.checked = !this.checked
+    console.log(`Checkbox is ${this.checked}`)
+  }
 }
