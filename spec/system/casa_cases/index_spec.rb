57fc8e9146774bd4f379aed3b2b152ca340b7961
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-08-04 10:45:26 -0700

    Un-DRY tests

--- spec/system/casa_cases/index_spec.rb
+++ spec/system/casa_cases/index_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "casa_cases/index", type: :system do
-  let(:organization) { build(:casa_org) }
-  let(:admin) { build(:casa_admin, casa_org: organization) }
-  let(:volunteer) { build(:volunteer, display_name: "Cool Volunteer", casa_org: organization) }
-  let(:cina) { build(:casa_case, active: true, casa_org: organization, case_number: "CINA-1") }
-  let!(:tpr) { create(:casa_case, active: true, casa_org: organization, case_number: "TPR-100") }
-  let!(:no_prefix) { create(:casa_case, active: true, casa_org: organization, case_number: "123-12-123") }
-  let!(:case_assignment) { create(:case_assignment, volunteer: volunteer, casa_case: cina) }
+  it "is filterable and linkable", :js do
+    organization = build(:casa_org)
+    admin = build(:casa_admin, casa_org: organization)
+    volunteer = build(:volunteer, display_name: "Cool Volunteer", casa_org: organization)
+    cina = build(:casa_case, active: true, casa_org: organization, case_number: "CINA-1")
+    tpr = create(:casa_case, active: true, casa_org: organization, case_number: "TPR-100")
+    no_prefix = create(:casa_case, active: true, casa_org: organization, case_number: "123-12-123")
+    case_assignment = create(:case_assignment, volunteer: volunteer, casa_case: cina)
 
-  scenario "filterable and linkable", :js do
     sign_in admin
     visit casa_cases_path
 
@@ -23,5 +23,17 @@ RSpec.describe "casa_cases/index", type: :system do
     click_on "Assigned to more than 1 Volunteer"
     click_on "Assigned to Transition Aged Youth"
     click_on "Casa Case Prefix"
+  end
+
+  context "when signed in as supervisor" do
+    it "does not see option to make a new case" do
+      casa_org = build(:casa_org)
+      supervisor = create(:supervisor, casa_org: casa_org)
+
+      sign_in supervisor
+      visit casa_cases_path
+
+      expect(page).not_to have_button("New Case")
+    end
   end
 end
