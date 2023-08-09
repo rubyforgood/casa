65e3baaedfb7e4517eb9a47a835a1aef7e100dae
Weston Sandfort <sandfortw@gmail.com>
2023-08-09 12:42:28 -0600

    Add validations for case_group, incl uniq name

--- spec/models/case_group_spec.rb
+++ spec/models/case_group_spec.rb
@@ -1,6 +1,25 @@
 require "rails_helper"
 
 RSpec.describe CaseGroup, type: :model do
+
+  describe 'validations' do
+    it { should validate_presence_of(:case_group_memberships) }
+
+    it 'validates uniqueness of name scoped to casa_org' do
+      casa_org = create(:casa_org)
+      create(:case_group, casa_org: casa_org, name: 'The Johnson Family')
+      non_uniq_case_group = build(:case_group, casa_org: casa_org, name: 'The Johnson Family')
+  
+      expect(non_uniq_case_group).to_not be_valid
+      expect(non_uniq_case_group.errors[:name]).to include('has already been taken')
+    end  
+  end
+
+  describe 'relationships' do
+    it { should have_many(:case_group_memberships) }
+    it { should have_many(:casa_cases).through(:case_group_memberships) }
+  end
+
   it "has a valid factory" do
     case_group = build(:case_group)
 
