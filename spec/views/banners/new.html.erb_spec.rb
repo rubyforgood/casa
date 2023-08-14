bae578b729bfa8a22e00327c50907c265faee0c0
Steve Carroll <stephenalexandercarroll@gmail.com>
2023-08-14 12:24:23 -0400

    One day I will remember to run the linter

--- spec/views/banners/new.html.erb_spec.rb
+++ spec/views/banners/new.html.erb_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "banners/new", type: :view do
         user = build_stubbed(:casa_admin)
         current_organization = user.casa_org
         current_organization_banner = build(:banner, active: true, casa_org: current_organization)
-        
+
         allow(view).to receive(:current_user).and_return(user)
         allow(view).to receive(:current_organization).and_return(current_organization)
         allow(current_organization).to receive(:has_alternate_active_banner?).and_return(true)
