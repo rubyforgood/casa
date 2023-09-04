7138972c6753944a0745761aef98e26e81f8f923
Sajjad Ahmad <jogimar14@gmail.com>
2023-09-04 16:47:01 +0500

    Create local_time method in component and render it in partial to show user local time

---
+++ app/components/local_time_component.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class LocalTimeComponent < ViewComponent::Base
+  include ActionController::Cookies
+  attr_reader :format, :unix_timestamp
+
+  def initialize(format:, unix_timestamp:)
+    @format = format
+    @unix_timestamp = unix_timestamp
+  end
+
+  def local_time
+    # Time format should be passed as 12 or 24 only
+    unless [12, 24].include?(format)
+      raise ArgumentError, "Invalid time format argument"
+    end
+
+    time = Time.at(unix_timestamp).in_time_zone(@time_zone)
+    time_format = format == 12 ? "%I:%M %p" : "%H:%M"
+    formatted_date = time.strftime("%B %d, %Y")
+    time_of_day = time.strftime(time_format)
+    time_zone = time.zone
+    return { date: formatted_date, time: time_of_day, zone: time_zone }
+  end
+
+  private
+
+  def before_render
+    browser_tz = ActiveSupport::TimeZone.find_tzinfo(cookies[:browser_time_zone])
+    ActiveSupport::TimeZone.all.find { |zone| zone.tzinfo == browser_tz } || Time.zone
+  rescue TZInfo::UnknownTimezone, TZInfo::InvalidTimezoneIdentifier
+    @time_zone = Time.zone
+  end
+end
