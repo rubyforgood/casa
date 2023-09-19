6aab0c46bf11de273bf26be20aeaa974d1e71be2
firelemons <shenyang64@gmail.com>
2023-09-18 22:51:59 -0500

    revert verbose QA logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,5 +104,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.active_record.verbose_query_logs = ENV["APP_ENVIRONMENT"] != "production"
 end
