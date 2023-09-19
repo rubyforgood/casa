39712009ad7f52dbe9cf45a228b83750159f766e
Pavel Goloblia <iverick091@gmail.com>
2023-09-19 15:59:34 +0300

    Removed "Assigned To" column from My Cases table for volunteers

--- spec/views/casa_cases/index.html.erb_spec.rb
+++ spec/views/casa_cases/index.html.erb_spec.rb
@@ -1,6 +1,24 @@
 require "rails_helper"
 
 RSpec.describe "casa_cases/index", type: :view do
+  context "when accessed by a volunteer" do
+    it "can not see the Assigned To column" do
+      user = create(:volunteer, display_name: "Bob Loblaw")
+      enable_pundit(view, user)
+      allow(view).to receive(:current_user).and_return(user)
+
+      casa_case = build(:casa_case, active: true, casa_org: user.casa_org, case_number: "CINA-1")
+      create(:case_assignment, volunteer: user, casa_case: casa_case)
+      assign :casa_cases, [casa_case]
+      assign :duties, OtherDuty.none
+
+      render template: "casa_cases/index"
+
+      expect(rendered).not_to have_text "Assigned To"
+      expect(rendered).not_to have_text("Bob Loblaw")
+    end
+  end
+
   context "when accessed by an admin" do
     it "can see the New Case button" do
       organization = create(:casa_org)
