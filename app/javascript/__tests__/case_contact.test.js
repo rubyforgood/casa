cafe658c5192a8e9ca7138b3fd21caf77ad26063
firelemons <shenyang64@gmail.com>
2023-09-22 10:11:10 -0500

    can't trigger the jQuery dom load callback from jest

--- app/javascript/__tests__/case_contact.test.js
+++ app/javascript/__tests__/case_contact.test.js
@@ -3,19 +3,6 @@ import { validateOccurredAt, convertDateToSystemTimeZone } from '../src/case_con
 
 require('jest')
 
-test("case_contact doesn't run on pages without case contact form", () => {
-  // Set up our document body
-  const name = 'hello'
-
-  document.body.innerHTML = `<div>${name}</div>`
-
-  require('../src/case_contact')
-
-  expect(() => {
-    window.onload()
-  }).not.toThrow()
-})
-
 test("occured date field won't allow future dates and it will be set back to the current date", () => {
   const today = new Date()
   const afterTomorrow = new Date(Date.now() + 3600 * 1000 * 24 * 2)
