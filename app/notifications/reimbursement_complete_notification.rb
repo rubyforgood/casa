2cf6c0b85bbaaa234718a3554da3584fed3fb99f
Samuel Williams <50247514+schoork@users.noreply.github.com>
2023-10-08 15:18:20 -0400

    Send in app notification to volunteer and supervisor on reimbursement complete checkbox selection (#5121)
    ---------
    
    Co-authored-by: Sam Williams <sam.williams@brazen.com>

---
+++ app/notifications/reimbursement_complete_notification.rb
@@ -0,0 +1,21 @@
+class ReimbursementCompleteNotification < BaseNotification
+  deliver_by :database
+
+  param :case_contact
+
+  def title
+    "Reimbursement Approved"
+  end
+
+  def message
+    case_contact = params[:case_contact]
+    msg = "Volunteer #{case_contact.creator.display_name}'s request for reimbursement for #{case_contact.miles_driven}mi "
+    msg += " for $#{case_contact.reimbursement_amount} " if case_contact.reimbursement_amount
+    msg += "on #{case_contact.occurred_at_display} has been processed and is en route."
+    msg
+  end
+
+  def url
+    case_contacts_path(casa_case_id: params[:case_contact].casa_case_id)
+  end
+end
