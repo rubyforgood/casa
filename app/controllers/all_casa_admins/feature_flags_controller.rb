8b94c96675637f3164384ae8a7b149b020a7be8e
aisayo <aysan@ombulabs.com>
2023-10-12 12:52:40 -0400

    add feature flag

--- app/controllers/all_casa_admins/feature_flags_controller.rb
+++ app/controllers/all_casa_admins/feature_flags_controller.rb
@@ -1,9 +1,9 @@
 class AllCasaAdmins::FeatureFlagsController < AllCasaAdminsController
 
   def index
-    @feature_flags = FeatureFlag.all
+    @feature_flags = FeatureFlag.all.order(name: 'asc')
   end
-  
+
   def update
     @feature_flag = FeatureFlag.find(params[:id])
     @feature_flag.update(enabled: !@feature_flag.enabled)
--- app/javascript/src/all_casa_admin/feature_flags.js
+++ app/javascript/src/all_casa_admin/feature_flags.js
@@ -1,32 +1,75 @@
-// (window).on('load', function() {
-//   $('.form-check-input').on('change', function() {
-//     debugger
-//     const featureFlagId = $(this).data('feature-flag-id');
-//     const isChecked = $(this).prop('checked');
+// the problems to solve:
+// 1. how to invoke a request to the controller action to update value of enabled
+// 2. how to persist the state of the switch upon a refresh
 
-//     // Send an AJAX request to update the feature flag state in the database
-//     $.ajax({
-//       type: 'POST',
-//       url: '/feature_flags/update', // Define the route in your application
-//       data: { id: featureFlagId, enabled: isChecked },
-//       success: function(data) {
-//         // Handle success, if needed
-//       },
-//       error: function(err) {
-//         // Handle errors, if needed
-//       }
-//     });
+// Get all the checkboxes with a class of 'form-check-input'
+
+// document.addEventListener('DOMContentLoaded', function() {
+// let checkboxes = document.querySelectorAll('.form-check-input.toggle-switch');
+
+// checkboxes.forEach(function(checkbox) {
+//   checkbox.addEventListener('change', function(e) {
+//     e.preventDefault();
+//     e.stopPropagation()
+//     // Get the feature_flag_id from the data attribute
+//     let featureFlagId = this.getAttribute('data-feature-flag-id');
+//     let uniqueCheckbox = document.getElementById('feature-checkbox-' + featureFlagId);
+//     const csrfToken = document.querySelector("meta[name=csrf-token]").getAttribute("content");
+//     const isChecked = this.checked;
+//     toggleSwitch(uniqueCheckbox, csrfToken, featureFlagId, isChecked)
 //   });
 // });
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  // debugger
-  console.log("test!")
-})
+// function toggleSwitch(uniqueCheckbox, csrfToken, featureFlagId, checked){
+//   // if (uniqueCheckbox) {
+//     // uniqueCheckbox.checked = !uniqueCheckbox.checked;
+//     fetch(`/all_casa_admins/feature_flags/${featureFlagId}`, {
+//       method: 'PATCH',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'X-CSRF-Token': csrfToken
+//       }
+//     })
+//     .then(() => {
+//       if (checked){
+//         uniqueCheckbox.checked = true
+//       } else {
+//         uniqueCheckbox.checked = false
+//       }
+//     })
+//   // }
+// }
+// })
 
-// debugger
 
-// the problems to solve:
-// 1. how to invoke a request to the controller action to update value of enabled
-// 2. how to persist the state of the switch upon a refresh
+$(function() {
+  let checkboxes = $('.form-check-input.toggle-switch');
+
+  checkboxes.on('change', function(e) {
+    e.preventDefault();
+    let featureFlagId = $(this).data('feature-flag-id');
+    let uniqueCheckbox = $('#feature-checkbox-' + featureFlagId);
+    const csrfToken = $('meta[name=csrf-token]').attr('content');
+    const isChecked = this.checked;
+    toggleSwitch(uniqueCheckbox, csrfToken, featureFlagId, isChecked);
+  });
+
+  function toggleSwitch(uniqueCheckbox, csrfToken, featureFlagId, checked) {
+      $.ajax({
+        url: `/all_casa_admins/feature_flags/${featureFlagId}`,
+        method: 'PATCH',
+        contentType: 'application/json',
+        headers: {
+          'X-CSRF-Token': csrfToken
+        },
+        success: function() {
+          if (checked) {
+            uniqueCheckbox.prop('checked', true);
+          } else {
+            uniqueCheckbox.prop('checked', false);
+          }
+        }
+      });
+    }
+});
