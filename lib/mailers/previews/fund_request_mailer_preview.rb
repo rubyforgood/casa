8f64a8867e43e29811657f6acff02aeedefe946f
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-11-10 15:28:33 -0500

    Fixed lint issues

--- lib/mailers/previews/fund_request_mailer_preview.rb
+++ lib/mailers/previews/fund_request_mailer_preview.rb
@@ -6,20 +6,20 @@ class FundRequestMailerPreview < ActionMailer::Preview
   def send_request
     # Set the FUND_REQUEST_RECIPIENT_EMAIL environment variable for testing
     ENV["FUND_REQUEST_RECIPIENT_EMAIL"] = "recipient@example.com"
-    
+
     fund_request = FundRequest.new(
-      submitter_email: 'casa@example.cmo',
-      youth_name: 'The youth Name',
+      submitter_email: "casa@example.cmo",
+      youth_name: "The youth Name",
       payment_amount: "$123.45",
       deadline: Date.today + 7.days,
-      request_purpose: 'shoes',
-      payee_name: 'payee_name',
-      requested_by_and_relationship: 'Sample Requester',
-      other_funding_source_sought: 'Sample Funding Source',
-      impact: 'Sample Impact',
-      extra_information: 'Sample Extra Information'
+      request_purpose: "shoes",
+      payee_name: "payee_name",
+      requested_by_and_relationship: "Sample Requester",
+      other_funding_source_sought: "Sample Funding Source",
+      impact: "Sample Impact",
+      extra_information: "Sample Extra Information"
     )
 
-   FundRequestMailer.send_request(nil, fund_request)
+    FundRequestMailer.send_request(nil, fund_request)
   end
 end
