d6e83ed39c8b0aecc4256cf223767889c990e3c5
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-11-21 10:06:05 -0500

    Fixed existing failing specs of learning hours

--- spec/requests/learning_hours_spec.rb
+++ spec/requests/learning_hours_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe "LearningHours", type: :request do
 
     describe "GET /index" do
       it "succeeds" do
-        get volunteer_learning_hours_path(volunteer_id: volunteer.id)
+        get learning_hours_path
         expect(response).to have_http_status(:success)
       end
 
       it "displays the Learning Topic column if learning_topic_active is true" do
         volunteer.casa_org.update(learning_topic_active: true)
-        get volunteer_learning_hours_path(volunteer_id: volunteer.id)
+        get learning_hours_path
         expect(response.body).to include("Learning Topic")
       end
 
       it "does not display the Learning Topic column if learning_topic_active is false" do
         volunteer.casa_org.update(learning_topic_active: false)
-        get volunteer_learning_hours_path(volunteer_id: volunteer.id)
+        get learning_hours_path
         expect(response.body).not_to include("Learning Topic")
       end
     end
