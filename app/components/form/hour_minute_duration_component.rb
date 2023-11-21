95c2ffd66edbbddec8a99ae6b0dea2bd96bd0805
firelemons <shenyang64@gmail.com>
2023-11-21 17:17:20 -0600

    component allows initial durations to be nil

--- app/components/form/hour_minute_duration_component.rb
+++ app/components/form/hour_minute_duration_component.rb
@@ -7,33 +7,33 @@ class Form::HourMinuteDurationComponent < ViewComponent::Base
     if hour_value.is_a?(String)
       begin
         @hour_value = Integer(hour_value)
+
+        if @hour_value < 0
+          raise RangeError.new("Parameter hour_value must be positive")
+        end
       rescue ArgumentError
         raise ArgumentError.new("Could not convert parameter hour_value to an integer")
       end
-    elsif !hour_value.is_a?(Integer)
-      raise TypeError.new("Parameter hour_value must be an integer")
-    else
+    elsif hour_value.nil? || hour_value.is_a?(Integer)
       @hour_value = hour_value
+    else
+      raise TypeError.new("Parameter hour_value must be an integer")
     end
 
     if minute_value.is_a?(String)
       begin
         @minute_value = Integer(minute_value)
+
+        if @minute_value < 0
+          raise RangeError.new("Parameter minute_value must be positive")
+        end
       rescue ArgumentError
         raise ArgumentError.new("Could not convert parameter minute_value to an integer")
       end
-    elsif !minute_value.is_a?(Integer)
-      raise TypeError.new("Parameter minute_value must be an integer")
-    else
+    elsif minute_value.nil? || minute_value.is_a?(Integer)
       @minute_value = minute_value
-    end
-
-    if @hour_value < 0
-      raise RangeError.new("Parameter hour_value must be positive")
-    end
-
-    if @minute_value < 0
-      raise RangeError.new("Parameter minute_value must be positive")
+    else
+      raise TypeError.new("Parameter minute_value must be an integer")
     end
   end
 end
