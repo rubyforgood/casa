73e803eb88653abc2e2308ab1c8e73b83a348b6a
firelemons <shenyang64@gmail.com>
2023-11-21 17:45:08 -0600

    fix negative numbers not throwing an error

--- app/components/form/hour_minute_duration_component.rb
+++ app/components/form/hour_minute_duration_component.rb
@@ -6,15 +6,17 @@ class Form::HourMinuteDurationComponent < ViewComponent::Base
 
     if hour_value.is_a?(String)
       begin
-        @hour_value = Integer(hour_value)
-
-        if @hour_value < 0
-          raise RangeError.new("Parameter hour_value must be positive")
-        end
+        hour_value = Integer(hour_value)
       rescue ArgumentError
         raise ArgumentError.new("Could not convert parameter hour_value to an integer")
       end
-    elsif hour_value.nil? || hour_value.is_a?(Integer)
+    end
+
+    if hour_value.is_a?(Integer) && hour_value < 0
+      raise RangeError.new("Parameter hour_value must be positive")
+    end
+
+    if hour_value.nil? || hour_value.is_a?(Integer)
       @hour_value = hour_value
     else
       raise TypeError.new("Parameter hour_value must be an integer")
@@ -22,15 +24,17 @@ class Form::HourMinuteDurationComponent < ViewComponent::Base
 
     if minute_value.is_a?(String)
       begin
-        @minute_value = Integer(minute_value)
-
-        if @minute_value < 0
-          raise RangeError.new("Parameter minute_value must be positive")
-        end
+        minute_value = Integer(minute_value)
       rescue ArgumentError
         raise ArgumentError.new("Could not convert parameter minute_value to an integer")
       end
-    elsif minute_value.nil? || minute_value.is_a?(Integer)
+    end
+
+    if minute_value.is_a?(Integer) && minute_value < 0
+      raise RangeError.new("Parameter minute_value must be positive")
+    end
+
+    if minute_value.nil? || minute_value.is_a?(Integer)
       @minute_value = minute_value
     else
       raise TypeError.new("Parameter minute_value must be an integer")
