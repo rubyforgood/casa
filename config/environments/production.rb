c6d07d8a20d0655cadabb5bb277836f4f244de5a
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-01-08 18:28:54 -0500

    Ruby Standard fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.force_ssl = true
 
   # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
+  config.logger = ActiveSupport::Logger.new($stdout)
     .tap { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
