6f728d52cfdf7d35ff9cd619f8f551796d34d217
Sam Williams <sam.williams@brazen.com>
2024-01-31 22:26:33 -0500

    Lint fix

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,40 +2,40 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from './application'
 
-import AlertController from "./alert_controller"
-application.register("alert", AlertController)
+import AlertController from './alert_controller'
 
-import AutosaveController from "./autosave_controller"
-application.register("autosave", AutosaveController)
+import AutosaveController from './autosave_controller'
 
-import DisableFormController from "./disable_form_controller"
-application.register("disable-form", DisableFormController)
+import DisableFormController from './disable_form_controller'
 
-import DismissController from "./dismiss_controller"
-application.register("dismiss", DismissController)
+import DismissController from './dismiss_controller'
 
-import ExtendedNestedFormController from "./extended_nested_form_controller"
-application.register("extended-nested-form", ExtendedNestedFormController)
+import ExtendedNestedFormController from './extended_nested_form_controller'
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from './hello_controller'
 
-import IconToggleController from "./icon_toggle_controller"
-application.register("icon-toggle", IconToggleController)
+import IconToggleController from './icon_toggle_controller'
 
-import MultipleSelectController from "./multiple_select_controller"
-application.register("multiple-select", MultipleSelectController)
+import MultipleSelectController from './multiple_select_controller'
 
-import NavbarController from "./navbar_controller"
-application.register("navbar", NavbarController)
+import NavbarController from './navbar_controller'
 
-import SelectAllController from "./select_all_controller"
-application.register("select-all", SelectAllController)
+import SelectAllController from './select_all_controller'
 
-import SidebarController from "./sidebar_controller"
-application.register("sidebar", SidebarController)
+import SidebarController from './sidebar_controller'
 
-import SidebarGroupController from "./sidebar_group_controller"
-application.register("sidebar-group", SidebarGroupController)
+import SidebarGroupController from './sidebar_group_controller'
+application.register('alert', AlertController)
+application.register('autosave', AutosaveController)
+application.register('disable-form', DisableFormController)
+application.register('dismiss', DismissController)
+application.register('extended-nested-form', ExtendedNestedFormController)
+application.register('hello', HelloController)
+application.register('icon-toggle', IconToggleController)
+application.register('multiple-select', MultipleSelectController)
+application.register('navbar', NavbarController)
+application.register('select-all', SelectAllController)
+application.register('sidebar', SidebarController)
+application.register('sidebar-group', SidebarGroupController)
