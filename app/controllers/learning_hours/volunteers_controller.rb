9acf80c0a91a4fa0a4a1d1dbad96853c8c8a773f
Jade <85654561+jp524@users.noreply.github.com>
2024-02-27 14:56:42 -0500

    Add learning_hours_table partial to LearningHours::Volunteers#show view

--- app/controllers/learning_hours/volunteers_controller.rb
+++ app/controllers/learning_hours/volunteers_controller.rb
@@ -1,7 +1,7 @@
 class LearningHours::VolunteersController < ApplicationController
   def show
-    volunteer = User.includes(:learning_hours).find(params[:id])
-    authorize volunteer
-    @learning_hours = LearningHour.where(user: volunteer)
+    @volunteer = User.includes(:learning_hours).find(params[:id])
+    authorize @volunteer
+    @learning_hours = LearningHour.where(user: @volunteer)
   end
 end
