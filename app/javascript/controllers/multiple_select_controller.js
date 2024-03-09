7194d224dbbd9057c73543672205d29bee2bd6da
Ariel Fogel <fogeltine@gmail.com>
2024-03-09 12:16:40 -0500

    enable multiselect for contact types

--- app/javascript/controllers/multiple_select_controller.js
+++ app/javascript/controllers/multiple_select_controller.js
@@ -3,15 +3,68 @@ import TomSelect from 'tom-select'
 
 export default class extends Controller {
   static targets = ['select']
+  static values = {
+    options: Array,
+    optionGroups: Array,
+    optgroupField: String,
+    labelField: String,
+    defaultSelected: Array
+   }
 
   connect () {
-    /* eslint-disable no-new */
-    new TomSelect(this.selectTarget, {
-      plugins: {
-        remove_button: {
-          title: 'Remove this item'
+    const isGrouped = this.hasOptionsValue &&
+                        this.hasOptionGroupsValue &&
+                        this.hasOptgroupFieldValue &&
+                        this.hasLabelFieldValue
+    if (isGrouped) {
+      /* eslint-disable no-new */
+      return new TomSelect("#case_contact_casa_case_ids", {
+        plugins: {
+          remove_button: {
+            title: 'Remove this item',
+            className: 'text-primary p-1 mx-2 deactive-bg rounded-circle',
+            label: `<i class="lni lni-close p-1"></i>`
+          }
+        },
+        options: this.optionsValue,
+        optgroups: this.optionGroupsValue,
+        optgroupField: this.optgroupFieldValue,
+        labelField: this.labelFieldValue,
+        items: this.defaultSelectedValue,
+        searchField: [this.labelFieldValue, this.optgroupFieldValue],
+        render: {
+          option: function(data, escape) {
+            return `
+                <div class='d-flex gap-1'>
+                  <span class='d-flex gap-1'>${escape(data.label)}</span>
+                  <span class="fst-italic text-muted">${escape(data.inline_option)}</span>
+                </div>
+              `.replace(/(\r\n|\n|\r)/gm,"");
+          },
+          optgroup_header: function(data, escape) {
+            return `
+              <span class='d-flex optgroup-header'>
+                ${escape(data.label)}
+              </span>
+            `.replace(/(\r\n|\n|\r)/gm,"");
+          },
+          item: function(data, escape) {
+            return `
+              <div class="badge rounded-pill primary-bg py-2 px-3 active form-check-label">
+                ${escape(data.label)}
+              </div>
+            `.replace(/(\r\n|\n|\r)/gm,"");
+          }
         }
-      }
-    })
+      })
+    }
+      /* eslint-disable no-new */
+      new TomSelect(this.selectTarget, {
+        plugins: {
+          remove_button: {
+            title: 'Remove this item'
+          }
+        }
+      })
   }
 }
