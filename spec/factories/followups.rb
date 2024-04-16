46959689605ffa5f2fa71865e1b57c4c429e013b
Andrew Casarsa <acasarsa@gmail.com>
2024-04-16 19:27:24 -0400

    * create backfill after_party rake task * add test for rake task

--- spec/factories/followups.rb
+++ spec/factories/followups.rb
@@ -4,10 +4,19 @@ FactoryBot.define do
     status { :requested }
     case_contact
 
-    # TODO polymorph Simulating the dual-writing or transitional setup during polymorphic migration
+    # TODO polymorph Simulating the dual-writing setup during polymorphic migration
     # remove after migration completed
-    after(:build) do |followup|
-      followup.followupable = followup.case_contact
+    after(:build) do |followup, evaluator|
+      unless evaluator.instance_variable_defined?(:@without_dual_writing)
+        followup.followupable = followup.case_contact
+      end
+    end
+
+    trait :without_dual_writing do
+      after(:build) do |followup|
+        followup.followupable_id = nil
+        followup.followupable_type = nil
+      end
     end
 
     trait :with_note do
