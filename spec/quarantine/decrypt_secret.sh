f9370477ef4464fe770e895b148ca685e6d93f8b
Sam Williams <sam.williams@brazen.com>
2024-04-23 12:41:32 -0400

    file location

--- spec/quarantine/decrypt_secret.sh
+++ spec/quarantine/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$QUARANTINE_SERVICE_ACCOUNT_PASSPHRASE" \
---output $HOME/secrets/service-account.json service-account.json.gpg
+--output $HOME/secrets/service-account.json ./spec/quarantine/service-account.json.gpg
