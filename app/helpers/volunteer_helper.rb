0b7f1b04202da8bce4cc4fce85b709d592d7cea6
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-04-26 18:10:00 -0400

    Lint fix

--- app/helpers/volunteer_helper.rb
+++ app/helpers/volunteer_helper.rb
@@ -1,13 +1,13 @@
 module VolunteerHelper
   def volunteer_badge(casa_case, current_user)
-    return '' if current_user.volunteer?
+    return "" if current_user.volunteer?
 
     badge_content = if casa_case.assigned_volunteers.present?
-                      casa_case.assigned_volunteers.map(&:display_name).join(", ")
-                    else
-                      'Unassigned'
-                    end
+      casa_case.assigned_volunteers.map(&:display_name).join(", ")
+    else
+      "Unassigned"
+    end
 
-    content_tag(:span, badge_content, class: 'badge badge-pill light-bg text-black fs-6 fw-medium')
+    content_tag(:span, badge_content, class: "badge badge-pill light-bg text-black fs-6 fw-medium")
   end
 end
