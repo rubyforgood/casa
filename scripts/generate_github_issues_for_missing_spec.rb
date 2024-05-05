a37f38aa10ca57065985e27b3da41e6fb496afb8
compwron <compiledwrong+github+config@gmail.com>
2024-05-05 18:34:33 -0400

    standardrb --fix

--- scripts/generate_github_issues_for_missing_spec.rb
+++ scripts/generate_github_issues_for_missing_spec.rb
@@ -2,7 +2,7 @@
 Dir.glob("spec/**/*spec.rb").each do |filename|
   File.open(filename, "r").readlines.select { |line| line.include?("xit \"") }.each do |xit_line|
     line_number = $.
-    clean_test_name = xit_line.gsub("xit ", "").gsub(" do\n", "").gsub('"', "").gsub("\n", "").strip
+    clean_test_name = xit_line.gsub("xit ", "").gsub(" do\n", "").delete('"').delete("\n").strip
     # clean_test_name = xit_line.gsub('xit', '').gsub('\"do.*', '').gsub('"', '').gsub("\n", '').strip
     title = "Fix or remove xit-ignored test in #{filename}:#{line_number} '#{clean_test_name}'"
     `gh issue create --title "#{title}" --body "#{title}"`
