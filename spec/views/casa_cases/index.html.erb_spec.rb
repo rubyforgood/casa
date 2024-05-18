50547de1522227e1a12ca142b3e615701cc70a82
Jos O'shea <jos.oshea.ky@gmail.com>
2024-05-18 15:41:49 -0400

    add tests for feature flag

--- spec/views/casa_cases/index.html.erb_spec.rb
+++ spec/views/casa_cases/index.html.erb_spec.rb
@@ -50,4 +50,38 @@ RSpec.describe "casa_cases/index", type: :view do
       expect(rendered).to_not have_link "New Case"
     end
   end
+
+  context "When SHOW_OTHER_DUTIES_FLAG disabled" do
+    it "does not see the New Duty button" do
+      organization = create(:casa_org)
+      user = build_stubbed(:supervisor, casa_org: organization)
+      enable_pundit(view, user)
+      allow(view).to receive(:current_user).and_return(user)
+      FeatureFlagService.disable!("show_other_duties")
+
+      assign :casa_cases, []
+      assign :duties, OtherDuty.none
+
+      render template: "casa_cases/index"
+
+      expect(rendered).to_not have_link "New Duty"
+    end
+  end
+
+  context "When SHOW_OTHER_DUTIES_FLAG enabled" do
+    it "shows the New Duty button" do
+      organization = create(:casa_org)
+      user = build_stubbed(:supervisor, casa_org: organization)
+      enable_pundit(view, user)
+      allow(view).to receive(:current_user).and_return(user)
+      FeatureFlagService.enable!("show_other_duties")
+
+      assign :casa_cases, []
+      assign :duties, OtherDuty.none
+
+      render template: "casa_cases/index"
+
+      expect(rendered).to have_link "New Duty"
+    end
+  end
 end
