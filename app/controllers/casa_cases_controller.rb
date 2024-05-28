67ed9326b883a94e8b232dbd648c261676823cb7
Ronak Gupta <iamronakgupta@gmail.com>
2024-05-28 23:28:18 +0530

    added helper for empty_cout_case variable

--- app/controllers/casa_cases_controller.rb
+++ app/controllers/casa_cases_controller.rb
@@ -55,7 +55,7 @@ class CasaCasesController < ApplicationController
       end
     else
       set_contact_types
-      @empty_court_date = params[:casa_case][:empty_court_date] == "1"
+      @empty_court_date = court_date_unknown?
       respond_to do |format|
         format.html { render :new }
         format.json { render json: @casa_case.errors.full_messages, status: :unprocessable_entity }
