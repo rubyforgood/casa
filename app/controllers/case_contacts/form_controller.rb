5b91a6f2dc7882712f6fef20e13826b1812bf379
Ronak Gupta <iamronakgupta@gmail.com>
2024-05-30 22:46:44 +0530

    form steps are dynamically changed according to case_contact

--- app/controllers/case_contacts/form_controller.rb
+++ app/controllers/case_contacts/form_controller.rb
@@ -4,9 +4,8 @@ class CaseContacts::FormController < ApplicationController
   before_action :set_progress
   before_action :require_organization!
   before_action :set_case_contact, only: [:show, :update]
+  prepend_before_action :set_steps, only: [:show, :update]
   after_action :verify_authorized
-
-  steps(*CaseContact::FORM_STEPS)
 
   # wizard_path
   def show
@@ -147,5 +146,9 @@ class CaseContacts::FormController < ApplicationController
     else
       0
     end
+  end
+
+  def set_steps
+    self.steps = CaseContact.find(params[:case_contact_id]).form_steps
   end
 end
