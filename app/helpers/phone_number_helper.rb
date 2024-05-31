54204c9f9ac178c1c6f5a0822213dbc2522463e4
Peter Cho <pcho231@gmail.com>
2024-05-31 11:53:05 -0400

    Lint

--- app/helpers/phone_number_helper.rb
+++ app/helpers/phone_number_helper.rb
@@ -1,10 +1,9 @@
 module PhoneNumberHelper
   VALID_PHONE_NUMBER_LENGTHS = [10, 11]
   VALID_COUNTRY_CODE = "+1"
+  ERROR_MESSAGE = "must be 10 digits or 12 digits including country code (+1)"
 
   def valid_phone_number(number)
-    message = "must be 10 digits or 12 digits including country code (+1)"
-
     if number.nil? || number.empty?
       return true, nil
     end
@@ -12,30 +11,23 @@ module PhoneNumberHelper
     number = strip_unnecessary_characters(number)
 
     if !VALID_PHONE_NUMBER_LENGTHS.include?(number.length)
-      return false, message
+      return false, ERROR_MESSAGE
     end
 
-    if number.length == 12
-      country_code = number[0..1]
-      phone_number = number[2..number.length]
-
-      if country_code != VALID_COUNTRY_CODE
-        return false, message
-      end
-    elsif number.length == 11
+    if number.length == 11
       country_code = number[0..0]
       phone_number = number[1..number.length]
       valid_country_code = VALID_COUNTRY_CODE[1..1]
 
       if country_code != valid_country_code
-        return false, message
+        return false, ERROR_MESSAGE
       end
     else
       phone_number = number
     end
 
     if !phone_number.scan(/\D/).empty?
-      return false, message
+      return false, ERROR_MESSAGE
     end
 
     [true, nil]
--- app/values/volunteer_parameters.rb
+++ app/values/volunteer_parameters.rb
@@ -4,8 +4,6 @@ class VolunteerParameters < UserParameters
   def initialize(params)
     params[:volunteer][:phone_number] = strip_unnecessary_characters(params[:volunteer][:phone_number])
 
-    params
-
     super(params, :volunteer)
   end
 end
