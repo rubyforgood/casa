1ba4dbe489fe7f9689474c681e227f4eb363e543
Peter Cho <pcho231@gmail.com>
2024-05-31 11:11:04 -0400

    Sanitize phone numbers prior to saving

--- app/values/volunteer_parameters.rb
+++ app/values/volunteer_parameters.rb
@@ -1,5 +1,11 @@
 class VolunteerParameters < UserParameters
+  include PhoneNumberHelper
+
   def initialize(params)
+    params[:volunteer][:phone_number] = strip_unnecessary_characters(params[:volunteer][:phone_number])
+
+    params
+
     super(params, :volunteer)
   end
 end
