64d5755d90a17a78175aaf38e406c07404f646ec
guswhitten <whittenoswald@gmail.com>
2024-06-03 07:08:08 -0400

    git push mistake

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @deploy_time = Health.instance.latest_deploy_time
-    @notifications = Noticed::Notification.where(recipient: user).newest_first.includes(:event)
+    @notifications = Noticed::Notification.where(recipient: current_user).newest_first.includes(:event)
     @patch_notes = PatchNote.notes_available_for_user(current_user)
   end
 end
