42249da491a3185b8f6bed760f862fa0f3c197b7
Ronak Gupta <iamronakgupta@gmail.com>
2024-06-10 13:20:28 +0530

    added: migration to add other_duties_enabled to casa_org table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_31_172823) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_071054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -161,6 +161,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_31_172823) do
     t.boolean "twilio_enabled", default: false
     t.boolean "additional_expenses_enabled", default: false
     t.boolean "learning_topic_active", default: false
+    t.boolean "other_duties_enabled", default: true
     t.index ["slug"], name: "index_casa_orgs_on_slug", unique: true
   end
 
