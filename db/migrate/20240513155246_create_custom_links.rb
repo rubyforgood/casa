c25ba65d7f18ef7eca374186540c920f65117804
Aakash Nepal <aakashnepal77@gmail.com>
2024-06-23 22:43:04 +0545

    feat: add soft delete column to custom links table

--- db/migrate/20240513155246_create_custom_links.rb
+++ db/migrate/20240513155246_create_custom_links.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
+# Migration file to create custom_links table
 class CreateCustomLinks < ActiveRecord::Migration[7.1]
   def change
     create_table :custom_links do |t|
       t.string :text
       t.text :url
       t.references :casa_org, null: false, foreign_key: true
+      t.boolean :soft_delete, null: false, default: false
       t.timestamps
     end
   end
