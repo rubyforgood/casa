c3dcacb5de76a7d744c7ce51f5c4034b5df302dc
Aakash Nepal <aakashnepal77@gmail.com>
2024-06-23 23:33:51 +0545

    feat: add validation for text and url

--- app/models/custom_link.rb
+++ app/models/custom_link.rb
@@ -1,6 +1,12 @@
 class CustomLink < ApplicationRecord
   belongs_to :casa_org
   validates :soft_delete, inclusion: [true, false]
+  # Validate that the URL is present, has a valid format, and is unique
+  validates :url, presence: true,
+                  format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, message: 'must be a valid URL' }
+  # Validate that the title is present and has a maximum length of 255 characters
+  validates :text, presence: true, length: { maximum: 255 }
+
   scope :active, -> { where(active: true, soft_delete: false) }
 end
 
