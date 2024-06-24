26b6c192aa1201438ce1c70dbab6e1b0279afadd
elasticspoon <mail@yuribocharov.dev>
2024-06-24 16:02:40 -0400

    feat: add functionality for volunteer to delete drafts
    Volunteers are having issues with creating drafts and those polluting
    their case contact screen.
    
    This PR enables volunteers to delete the case contacts that are still in
    draft status for them.

--- app/policies/case_contact_policy.rb
+++ app/policies/case_contact_policy.rb
@@ -7,6 +7,10 @@ class CaseContactPolicy < ApplicationPolicy
     is_creator? || admin_or_supervisor?
   end
 
+  def destroy?
+    admin_or_supervisor? || (is_creator? && is_draft?)
+  end
+
   def additional_expenses_allowed?
     FeatureFlagService.is_enabled?(FeatureFlagService::SHOW_ADDITIONAL_EXPENSES_FLAG) &&
       current_organization.additional_expenses_enabled
@@ -16,8 +20,6 @@ class CaseContactPolicy < ApplicationPolicy
   alias_method :new?, :admin_or_supervisor_or_volunteer?
   alias_method :show?, :is_creator_or_casa_admin?
   alias_method :drafts?, :admin_or_supervisor_or_volunteer?
-  alias_method :create?, :is_creator_or_casa_admin?
-  alias_method :destroy?, :admin_or_supervisor?
   alias_method :update?, :is_creator_or_supervisor_or_casa_admin?
   alias_method :edit?, :is_creator_or_supervisor_or_casa_admin?
   alias_method :restore?, :is_admin?
@@ -43,6 +45,10 @@ class CaseContactPolicy < ApplicationPolicy
   end
 
   private
+
+  def is_draft?
+    !record.active?
+  end
 
   def is_creator?
     record.creator == user
