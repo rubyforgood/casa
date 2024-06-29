b8f25bff2afc1233cd822da6bd9bc08cc94d2b8e
Aakash Nepal <aakashnepal77@gmail.com>
2024-06-29 09:23:16 +0545

    feat: remove sof delete validation and condition

--- app/models/custom_link.rb
+++ app/models/custom_link.rb
@@ -1,13 +1,12 @@
 class CustomLink < ApplicationRecord
   belongs_to :casa_org
-  validates :soft_delete, inclusion: [true, false]
   # Validate that the URL is present, has a valid format, and is unique
   validates :url, presence: true,
-    format: {with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, message: "must be a valid URL"}
+                  format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, message: 'must be a valid URL' }
   # Validate that the title is present and has a maximum length of 255 characters
-  validates :text, presence: true, length: {maximum: 255}
+  validates :text, presence: true, length: { maximum: 255 }
 
-  scope :active, -> { where(active: true, soft_delete: false) }
+  scope :active, -> { where(active: true) }
 end
 
 # == Schema Information
@@ -16,7 +15,6 @@ end
 #
 #  id          :bigint           not null, primary key
 #  active      :boolean          default(TRUE), not null
-#  soft_delete :boolean          default(FALSE), not null
 #  text        :string
 #  url         :text
 #  created_at  :datetime         not null
