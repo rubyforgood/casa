68aff5a2c689f78272d0129d0a996735e16b7ba2
Ronak Gupta <iamronakgupta@gmail.com>
2024-07-05 12:42:33 +0530

    margin right added to span

--- app/components/dropdown_menu_component.rb
+++ app/components/dropdown_menu_component.rb
@@ -27,6 +27,6 @@ class DropdownMenuComponent < ViewComponent::Base
   end
 
   def button_label
-    content_tag(:span, @menu_title, class: @hide_label ? "sr-only" : nil)
+    content_tag(:span, @menu_title, class: @hide_label ? "sr-only" : "mr-5")
   end
 end
