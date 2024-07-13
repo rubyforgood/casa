ffb7615b6d2dab6628746ab569a22afdc8fe113c
MclPio <113801014+MclPio@users.noreply.github.com>
2024-07-13 18:39:09 -0400

    Temporary solution

--- app/javascript/controllers/sidebar_group_controller.js
+++ app/javascript/controllers/sidebar_group_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
 
   connect () {
     this.toggleShow()
+    this.toggleShowAnchorList()
   }
 
   // Expands list if a link is active
@@ -15,5 +16,19 @@ export default class extends Controller {
         this.listTarget.classList.add('show')
       }
     })
+  }
+
+  // For group list with anchor links where method above does not work because 
+  // active does not get triggered in sidebar_helper.rb
+  // This will be temporary until ScrollSpy is implemented so active state toggles
+  // with scrolling.
+  // Goal: When link is assigned active with other JS implementation.
+  //       trigger the bellow toggle to Happen.
+  toggleShowAnchorList () {
+    let currentPath = window.location.pathname
+    if (currentPath == "/casa_org/1/edit" && this.titleTarget.innerText == "Edit Organization") {
+      this.titleTarget.classList.remove('collapsed')
+      this.listTarget.classList.add('show')
+    }
   }
 }
