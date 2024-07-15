cf5681588eca3836a2ad91d4ca449c86f476cbbc
Dalma Boros <dalma.boros@gmail.com>
2024-07-14 20:29:12 -0500

    Create create_default_standard_court_orders_service.rb

---
+++ spec/services/deployment/create_default_standard_court_orders_service_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe Deployment::CreateDefaultStandardCourtOrdersService do
+  describe "#create_defaults" do
+    let!(:casa_org_1) { create(:casa_org) }
+    let!(:casa_org_2) { create(:casa_org) }
+
+    it 'creates StandardCourtOrders from DEFAULT_STANDARD_COURT_ORDERS for each org' do
+      stub_const("Deployment::CreateDefaultStandardCourtOrdersService::DEFAULT_STANDARD_COURT_ORDERS", 
+        ["Default 1", "Default 2"])
+
+      described_class.new.create_defaults
+
+      expect(StandardCourtOrder.count).to eq(4)
+      expect(casa_org_1.standard_court_orders.map(&:value)).to eq(["Default 1", "Default 2"])
+      expect(casa_org_2.standard_court_orders.map(&:value)).to eq(["Default 1", "Default 2"])
+    end
+  end
+end
