13d3a11ef852d6c31a1f5d806b61180dd4fc9326
MclPio <113801014+MclPio@users.noreply.github.com>
2024-07-15 15:47:41 -0400

    Init observer implementation

--- app/javascript/controllers/sidebar_group_controller.js
+++ app/javascript/controllers/sidebar_group_controller.js
@@ -5,7 +5,10 @@ export default class extends Controller {
 
   connect () {
     this.toggleShow()
-    this.toggleShowAnchorList()
+    if (this.isEditOrganization()) {
+      this.toggleShowAnchorList()
+      this.obsFunc()
+    }
   }
 
   // Expands list if a link is active
@@ -20,15 +23,43 @@ export default class extends Controller {
 
   // For group list with anchor links where method above does not work because 
   // active does not get triggered in sidebar_helper.rb
-  // This will be temporary until ScrollSpy is implemented so active state toggles
-  // with scrolling.
-  // Goal: When link is assigned active with other JS implementation.
-  //       trigger the bellow toggle to Happen.
   toggleShowAnchorList () {
+    this.titleTarget.classList.remove('collapsed')
+    this.listTarget.classList.add('show')
+  }
+
+  obsFunc () {
+    console.log('obsFunc is run!')
+    let anchorLinks = [];
+    this.linkTargets.forEach((link) => {
+      if (link.children[0].href.includes('/casa_org/1/edit#')) {
+        anchorLinks.push(link);
+      }
+    });
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          let headerID = entry.target.id
+          this.linkTargets.forEach((link) => {
+            if (link.children[0].href.includes(headerID)) {
+              link.classList.add('active');
+            } else {
+              link.classList.remove('active');
+            }
+          })
+        }
+      });
+    });
+  
+    document.querySelectorAll('h1[id], h2[id]').forEach((header) => {
+      observer.observe(header);
+    });
+  }
+
+
+  isEditOrganization () {
     let currentPath = window.location.pathname
-    if (currentPath == "/casa_org/1/edit" && this.titleTarget.innerText == "Edit Organization") {
-      this.titleTarget.classList.remove('collapsed')
-      this.listTarget.classList.add('show')
-    }
+    return (currentPath == "/casa_org/1/edit" && this.titleTarget.innerText == "Edit Organization");
   }
 }
