3301d8f92a96be29df474680c8542a88cbfde9cb
MclPio <113801014+MclPio@users.noreply.github.com>
2024-07-16 10:56:47 -0400

    Fix JS linter not recognizing Intersection Observer

--- app/javascript/controllers/sidebar_group_controller.js
+++ app/javascript/controllers/sidebar_group_controller.js
@@ -1,3 +1,5 @@
+/* global IntersectionObserver */
+
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
@@ -32,33 +34,32 @@ export default class extends Controller {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
-          let headerID = entry.target.id;
+          const headerID = entry.target.id
 
-          this.linkTargets.forEach(link => link.classList.remove('active'));
+          this.linkTargets.forEach(link => link.classList.remove('active'))
 
-          const activeLink = this.linkTargetsMap[headerID];
-          if(activeLink) {
-            activeLink.classList.add('active');
+          const activeLink = this.linkTargetsMap[headerID]
+          if (activeLink) {
+            activeLink.classList.add('active')
           }
         }
-      });
-    });
+      })
+    })
 
-    this.linkTargetsMap = {};
+    this.linkTargetsMap = {}
     this.linkTargets.forEach(link => {
-      const href = link.children[0].href;
-      const headerID = href.substring(href.indexOf('#') + 1);
-      this.linkTargetsMap[headerID] = link;
-    });
+      const href = link.children[0].href
+      const headerID = href.substring(href.indexOf('#') + 1)
+      this.linkTargetsMap[headerID] = link
+    })
 
     document.querySelectorAll('h1[id], h2[id]').forEach((header) => {
-      observer.observe(header);
-    });
+      observer.observe(header)
+    })
   }
 
-
   isEditOrganization () {
-    let currentPath = window.location.pathname
-    return (currentPath == "/casa_org/1/edit" && this.titleTarget.innerText == "Edit Organization");
+    const currentPath = window.location.pathname
+    return (currentPath === '/casa_org/1/edit' && this.titleTarget.innerText === 'Edit Organization')
   }
 }
