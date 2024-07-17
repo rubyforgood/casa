11fba7935f4a1909d6e7c780094cfb3f92bb2a51
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-07-17 15:41:54 -0500

    [#5777] Add spec for step navigation component

---
+++ spec/components/form/step_navigation_component_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Form::StepNavigationComponent, type: :component do
+  context "can handle enabled button" do
+    it "enables button if value exists" do
+      render_inline(described_class.new(nav_back: '/details'))
+      expect(page).to have_selector(:link_or_button, 'Back step')
+    end
+  end
+  
+  context "can handle disabled button" do
+    it "disables buttons if value is nil" do
+      render_inline(described_class.new())
+      expect(page).not_to have_selector(:link_or_button, 'Next step')
+    end
+  end
+end
