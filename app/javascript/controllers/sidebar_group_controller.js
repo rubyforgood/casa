7769ada9b70ca2477dcbc38575645edacf9f84e4
MclPio <113801014+MclPio@users.noreply.github.com>
2024-07-17 14:07:51 -0400

    Refactor observer function

--- app/javascript/controllers/sidebar_group_controller.js
+++ app/javascript/controllers/sidebar_group_controller.js
@@ -1,6 +1,7 @@
 /* global IntersectionObserver */
 
 import { Controller } from '@hotwired/stimulus'
+import { has } from 'lodash'
 
 export default class extends Controller {
   static targets = ['title', 'list', 'link']
@@ -9,7 +10,7 @@ export default class extends Controller {
     this.toggleShow()
     if (this.isEditOrganization()) {
       this.toggleShowAnchorList()
-      this.obsFunc()
+      this.menuHighlight()
     }
   }
 
@@ -30,15 +31,35 @@ export default class extends Controller {
     this.listTarget.classList.add('show')
   }
 
-  obsFunc () {
+  // Highlights menu items as user scrolls on page. Implemented for casa_org/1/edit#organization-details.
+  menuHighlight() {
+    /**
+     * Constructs a map where the keys are header IDs extracted from the href attributes of the links,
+     * and the values are the corresponding link elements.
+     * 
+     * @returns {Object} A hash map with header IDs as keys and link elements as values.
+     */
+    const linkTargetsMap = () => {
+      let hash = {};
+      this.linkTargets.forEach(link => {
+        const href = link.children[0].href;
+        const headerID = href.substring(href.indexOf('#') + 1);
+        hash[headerID] = link;
+      })
+      return hash;
+    };
+
+    // Call linkTargetsMap() and assign it to linkHash
+    const linkHash = linkTargetsMap();
+
+    // add class active to link if it on page
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
-          const headerID = entry.target.id
-
           this.linkTargets.forEach(link => link.classList.remove('active'))
 
-          const activeLink = this.linkTargetsMap[headerID]
+          const headerID = entry.target.id
+          const activeLink = linkHash[headerID]
           if (activeLink) {
             activeLink.classList.add('active')
           }
@@ -46,18 +67,17 @@ export default class extends Controller {
       })
     })
 
-    this.linkTargetsMap = {}
-    this.linkTargets.forEach(link => {
-      const href = link.children[0].href
-      const headerID = href.substring(href.indexOf('#') + 1)
-      this.linkTargetsMap[headerID] = link
-    })
-
+    // Observe edit org page header, if they match linkTargetMap, observer gets triggered
     document.querySelectorAll('h1[id], h2[id]').forEach((header) => {
       observer.observe(header)
     })
   }
 
+  /**
+   * Checks to see if user is in /casa_org/1/edit path.
+   * 
+   * @returns {Boolean}
+   */
   isEditOrganization () {
     const currentPath = window.location.pathname
     return (currentPath === '/casa_org/1/edit' && this.titleTarget.innerText === 'Edit Organization')
