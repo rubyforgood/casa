8b76177b83862345c4272d1c50f94ddaad6e92c0
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-07-17 14:54:51 -0500

    [#5777] Remove excess behavior

--- app/components/form/step_navigation_component.rb
+++ app/components/form/step_navigation_component.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class Form::StepNavigationComponent < ViewComponent::Base
-  def initialize(nav_back: nil, nav_next: nil, submit_back: false, submit_next: false)
+  def initialize(nav_back: nil, nav_next: nil)
     @nav_back = nav_back
     @nav_next = nav_next
-    @submit_back = submit_back
-    @submit_next = submit_next
   end
 end
