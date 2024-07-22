3db49b31d39f8910539327d7e558c75d20bbc8a0
kerman07 <kerman07@live.com>
2024-07-22 20:00:46 +0200

    Fix N+1 issues in casa_cases#show #5893

--- app/models/case_contact.rb
+++ app/models/case_contact.rb
@@ -233,7 +233,7 @@ class CaseContact < ApplicationRecord
 
   def contact_groups_with_types
     hash = Hash.new { |h, k| h[k] = [] }
-    contact_types.each do |contact_type|
+    contact_types.includes(:contact_type_group).each do |contact_type|
       hash[contact_type.contact_type_group.name] << contact_type.name
     end
     hash
