744d82f8342fea886926a0afcd7071a3f354c638
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-07-22 14:58:22 -0500

    [#5777] Refactor with slot

--- app/components/form/title_component.rb
+++ app/components/form/title_component.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class Form::TitleComponent < ViewComponent::Base
-  def initialize(title:, subtitle:, step: nil, total_steps: nil, notes: nil, autosave: false, navigable: nil)
+  # `Form::StepNavigationComponent` is defined in another file, so we can refer to it by class name.
+  renders_one :navigable, Form::StepNavigationComponent
+  
+  def initialize(title:, subtitle:, step: nil, total_steps: nil, notes: nil, autosave: false)
     @title = title
     @subtitle = subtitle
     @notes = notes
     @autosave = autosave
-    @navigable = navigable
 
     if step && total_steps
       @steps_in_text = "Step #{step} of #{total_steps}"
