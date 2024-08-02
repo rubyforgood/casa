fc030c3466f5b02bcafdb0204000b73f3dfc2ba6
Jon Roberts <jon.roberts.8@gmail.com>
2024-08-02 16:24:08 -0400

    BulkCourtDateController spec implemented
    Addresses #5563

--- spec/factories/case_groups.rb
+++ spec/factories/case_groups.rb
@@ -1,11 +1,16 @@
 FactoryBot.define do
   factory :case_group do
+    transient do
+      case_count { 1 }
+    end
     casa_org { CasaOrg.first || create(:casa_org) }
     sequence(:name) { |n| "Family #{n}" }
 
-    after(:build) do |case_group, _|
+    after(:build) do |case_group, evaluator|
       if case_group.case_group_memberships.empty?
-        case_group.case_group_memberships.build(casa_case: create(:casa_case))
+        evaluator.case_count.times do
+          case_group.case_group_memberships.build(casa_case: create(:casa_case))
+        end
       end
     end
   end
