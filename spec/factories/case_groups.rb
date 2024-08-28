b919a5556f695f517892958ce25f1fa66bd5362a
Jon Roberts <jon.roberts.8@gmail.com>
2024-08-27 23:45:59 -0400

    case_group slight improvement

--- spec/factories/case_groups.rb
+++ spec/factories/case_groups.rb
@@ -2,20 +2,21 @@ FactoryBot.define do
   factory :case_group do
     transient do
       case_count { 1 }
-      casa_cases { [] }
+      casa_cases { nil }
     end
     casa_org { CasaOrg.first || create(:casa_org) }
     sequence(:name) { |n| "Family #{n}" }
 
     after(:build) do |case_group, evaluator|
-      if evaluator.casa_cases.size > 0
-        evaluator.casa_cases.each do |casa_case|
-          case_group.case_group_memberships.build(casa_case: casa_case)
-        end
+      casa_cases = if evaluator.casa_cases.present?
+        evaluator.casa_cases
       elsif case_group.case_group_memberships.empty?
-        evaluator.case_count.times do
-          case_group.case_group_memberships.build(casa_case: create(:casa_case))
-        end
+        build_list(:casa_case, evaluator.case_count)
+      else
+        []
+      end
+      casa_cases.each do |casa_case|
+        case_group.case_group_memberships.build(casa_case: casa_case)
       end
     end
   end
