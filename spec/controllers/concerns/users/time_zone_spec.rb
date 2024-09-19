ba67f346317e03b8b663f57b70d9aa67471b76d6
Darius Pirvulescu <organicdarius@gmail.com>
2024-09-18 20:38:45 -0500

    add partial specs for to_user_timezone helper

--- spec/controllers/concerns/users/time_zone_spec.rb
+++ spec/controllers/concerns/users/time_zone_spec.rb
@@ -6,6 +6,8 @@ end
 
 RSpec.describe MockController, type: :controller do
   let(:browser_time_zone) { "America/Los_Angeles" }
+  let(:default_time_zone) { 'Eastern Time (US & Canada)' }
+  let(:time_date) { '2024-09-18 15:00:00 UTC' }
   before do
     allow(controller).to receive(:cookies).and_return(browser_time_zone: browser_time_zone)
   end
@@ -34,6 +36,24 @@ RSpec.describe MockController, type: :controller do
 
       it "returns the default time zone" do
         expect(controller.browser_time_zone).to eq(Time.zone)
+      end
+    end
+  end
+
+  describe '#to_user_timezone' do
+    it 'returns the default time even if browser time zone has an invalid value' do
+      expected_time = time_date.in_time_zone(default_time_zone)
+      expect(controller.send(:to_user_timezone, time_date.in_time_zone(Time.zone))).to eq(expected_time)
+    end
+
+    context 'when browser time zone is not set' do
+      before do
+        allow(controller).to receive(:cookies).and_return({})
+      end
+
+      it 'returns the default timezone' do
+        p controller.browser_time_zone == Time.zone
+        expect(controller.send(:to_user_timezone, time_date)).to eq(time_date.in_time_zone(default_time_zone))
       end
     end
   end
