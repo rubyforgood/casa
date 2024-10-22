53bef092ddad0e52f88f4186985a8a2f4c0ab6aa
Jon Roberts <jon.roberts.8@gmail.com>
2024-10-22 13:31:06 -0400

    actually override todos as stated in comment

--- lib/tasks/data_post_processors/contact_type_populator.rb
+++ lib/tasks/data_post_processors/contact_type_populator.rb
@@ -1,6 +1,6 @@
 module ContactTypePopulator
   def self.populate
-    CasaOrg.all.each do |casa_org|
+    CasaOrg.find_each do |casa_org|
       ContactTypeGroup.generate_for_org!(casa_org)
     end
   end
