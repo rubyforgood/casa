9ed1ae8c5aa677aa714299d723d35f1304d4c1bd
Jon Roberts <jon.roberts.8@gmail.com>
2024-11-01 15:51:00 -0400

    move WebMock config to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -153,3 +153,8 @@ Shoulda::Matchers.configure do |shoulda_config|
     with.library :rails
   end
 end
+
+WebMock.disable_net_connect!(
+  allow_localhost: true,
+  allow: "selenium_chrome:4444"
+)
