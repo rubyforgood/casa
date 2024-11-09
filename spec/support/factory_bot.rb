5bad3ba6b4cf22aa2ab04f4b7904a584084b363c
Jon Roberts <jon.roberts.8@gmail.com>
2024-11-08 20:49:29 -0500

    don't track factories unless we want to

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -10,7 +10,7 @@ RSpec.configure do |config|
       execution_time_in_seconds = finish - start
 
       if execution_time_in_seconds >= 0.5
-        warn "Slow factory: #{payload[:name]} takes #{execution_time_in_seconds} seconds using strategy #{payload[:strategy]}"
+        Rails.logger.warn { "Slow factory: #{payload[:name]} takes #{execution_time_in_seconds} seconds using strategy #{payload[:strategy]}" }
       end
     end
   end
@@ -23,24 +23,22 @@ RSpec.configure do |config|
   #   end
   # end
 
-  # This will output total database records being created. Commented out to
-  # keep the spec output clean.
-
-  factory_bot_results = {}
-  config.before(:suite) do
-    ActiveSupport::Notifications.subscribe("factory_bot.run_factory") do |name, start, finish, id, payload|
-      factory_name = payload[:name]
-      strategy_name = payload[:strategy]
-      factory_bot_results[factory_name] ||= {}
-      factory_bot_results[factory_name][:total] ||= 0
-      factory_bot_results[factory_name][:total] += 1
-      factory_bot_results[factory_name][strategy_name] ||= 0
-      factory_bot_results[factory_name][strategy_name] += 1
+  # This will output total database records being created.
+  if ENV.fetch("SPEC_OUTPUT_FACTORY_BOT_OBJECT_CREATION_STATS", false)
+    factory_bot_results = {}
+    config.before(:suite) do
+      ActiveSupport::Notifications.subscribe("factory_bot.run_factory") do |name, start, finish, id, payload|
+        factory_name = payload[:name]
+        strategy_name = payload[:strategy]
+        factory_bot_results[factory_name] ||= {}
+        factory_bot_results[factory_name][:total] ||= 0
+        factory_bot_results[factory_name][:total] += 1
+        factory_bot_results[factory_name][strategy_name] ||= 0
+        factory_bot_results[factory_name][strategy_name] += 1
+      end
     end
-  end
 
-  config.after(:suite) do
-    if ENV.fetch("SPEC_OUTPUT_FACTORY_BOT_OBJECT_CREATION_STATS", "false") == "true"
+    config.after(:suite) do
       puts "How many objects did factory_bot create? (probably too many- let's tune some factories...)"
       pp factory_bot_results
     end
