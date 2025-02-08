87016824e833f18751bdd3807b8c6939b3a0300e
7riumph <triumpharamunde@gmail.com>
2025-02-08 00:02:55 -0700

    Utilized SHA-256 Hash to store tokens securely

--- app/models/api_credential.rb
+++ app/models/api_credential.rb
@@ -1,3 +1,12 @@
 class ApiCredential < ApplicationRecord
   belongs_to :user
+
+  before_save :hash_tokens
+
+  private
+
+  def hash_tokens
+    self.api_token_digest = Digest::SHA256.hexdigest(api_token) if api_token_changed?
+    self.refresh_token_digest = Digest::SHA256.hexdigest(refresh_token) if refresh_token_changed?
+  end
 end
