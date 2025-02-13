159092dfb56a9d3aab68d125fc389129810b862a
7riumph <triumpharamunde@gmail.com>
2025-02-13 15:44:23 -0700

    Reduced redundancy in create method

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -34,11 +34,13 @@ class VolunteersController < ApplicationController
     if @volunteer.save && @volunteer.email.match?(URI::MailTo::EMAIL_REGEXP)
       @volunteer.invite!(current_user)
       # call short io api here
-      raw_token = @volunteer.raw_invitation_token
-      invitation_url = Rails.application.routes.url_helpers.accept_user_invitation_url(invitation_token: raw_token, host: request.base_url)
+      invitation_url = Rails.application.routes.url_helpers.accept_user_invitation_url(
+        invitation_token: @volunteer.raw_invitation_token, host: request.base_url
+      )
+
       hash_of_short_urls = @volunteer.phone_number.blank? ? {0 => nil, 1 => nil} : handle_short_url([invitation_url, request.base_url + "/users/edit"])
-      body_msg = account_activation_msg("volunteer", hash_of_short_urls)
-      sms_status = deliver_sms_to @volunteer, body_msg
+
+      sms_status = deliver_sms_to @volunteer, account_activation_msg("volunteer", hash_of_short_urls)
       redirect_to edit_volunteer_path(@volunteer), notice: sms_acct_creation_notice("volunteer", sms_status)
     else
       render :new, status: :unprocessable_entity
