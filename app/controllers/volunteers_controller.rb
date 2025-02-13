1bee519a40e4df32b6514ffe6c21a11bb8856022
7riumph <triumpharamunde@gmail.com>
2025-02-13 16:29:27 -0700

    Renamed and refactored for clarity 'hash_of_short_url' --> 'short_url_key_value_pairs'

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -34,14 +34,12 @@ class VolunteersController < ApplicationController
     if @volunteer.save && @volunteer.email.match?(URI::MailTo::EMAIL_REGEXP)
       @volunteer.invite!(current_user)
       # call short io api here
-      invitation_url = Rails.application.routes.url_helpers.accept_user_invitation_url(
+
+      short_url_key_value_pairs = @volunteer.phone_number.blank? ? {0 => nil, 1 => nil} : handle_short_url([Rails.application.routes.url_helpers.accept_user_invitation_url(
         invitation_token: @volunteer.raw_invitation_token, host: request.base_url
-      )
+      ), request.base_url + "/users/edit"])
 
-      hash_of_short_urls = {}
-      hash_of_short_urls = handle_short_url([invitation_url, "#{request.base_url}/users/edit"]) if @volunteer.phone_number.present?
-
-      sms_status = deliver_sms_to @volunteer, account_activation_msg("volunteer", hash_of_short_urls)
+      sms_status = deliver_sms_to @volunteer, account_activation_msg("volunteer", short_url_key_value_pairs)
       redirect_to edit_volunteer_path(@volunteer), notice: sms_acct_creation_notice("volunteer", sms_status)
     else
       render :new, status: :unprocessable_entity
