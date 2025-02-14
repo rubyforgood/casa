260803b9d678a843f35a98f62e1018615b907b4b
7riumph <triumpharamunde@gmail.com>
2025-02-13 21:29:25 -0700

    change to if statement

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -35,7 +35,12 @@ class VolunteersController < ApplicationController
       @volunteer.invite!(current_user)
       # call short io api here
       invitation_url = Rails.application.routes.url_helpers.accept_user_invitation_url(invitation_token: @volunteer.raw_invitation_token, host: request.base_url)
-      hash_of_short_urls = @volunteer.phone_number.blank? ? {0 => nil, 1 => nil} : handle_short_url([invitation_url, request.base_url + "/users/edit"])
+
+      hash_of_short_urls = {0 => nil, 1 => nil}
+      if @volunteer.phone_number.present?
+        handle_short_url([invitation_url, request.base_url + "/users/edit"])
+      end
+
       sms_status = deliver_sms_to @volunteer, account_activation_msg("volunteer", hash_of_short_urls)
       redirect_to edit_volunteer_path(@volunteer), notice: sms_acct_creation_notice("volunteer", sms_status)
     else
