bf2d43d8be0a1b8f4955f2bf567380c4ad99fbe7
xihai01 <xihai.luo01@gmail.com>
2025-02-26 11:43:00 -0500

    add sign out route to controller

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -8,6 +8,22 @@ class Api::V1::Users::SessionsController < Api::V1::BaseController
     end
   end
 
+  def destroy
+    # fetch refresh token from request header
+    refresh_token = request.headers["Authorization"]&.split(" ")&.last
+    # find user's api credentials by refresh token
+    api_credential = ApiCredential.find_by(refresh_token_digest: Digest::SHA256.hexdigest(refresh_token))
+    # set api and refresh tokens to nil; otherwise render 401
+    if api_credential
+      api_credential.revoke_token("api_token")
+      api_credential.revoke_token("refresh_token")
+      render json: {message: "Signed out successfully."}, status: 200
+    else
+      render json: {message: "An error occured when signing out."}, status: 401
+      return
+    end
+  end
+
   private
 
   def user_params
