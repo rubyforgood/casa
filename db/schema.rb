7bb6657e06abf6c36e5cf9e597e542dd204a0ff7
Nizamuddin Khan <108728893+nizam12khan@users.noreply.github.com>
2025-05-28 09:59:44 +0000

    Update schema after adding migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_07_011754) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_28_092341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
