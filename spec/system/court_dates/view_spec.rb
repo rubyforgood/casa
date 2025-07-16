65758bbc24035fbbcf0b9006bf6d926daf01e57d
Shen Yang <shenyang128@proton.me>
2025-07-15 19:41:07 -0500

    stubs for new tests

--- spec/system/court_dates/view_spec.rb
+++ spec/system/court_dates/view_spec.rb
@@ -1,18 +1,55 @@
 require "rails_helper"
 
 RSpec.describe "court_dates/edit", type: :system do
-  context "with date"
   let(:now) { Date.new(2021, 1, 1) }
-  let(:organization) { create(:casa_org) }
-  let(:admin) { create(:casa_admin, casa_org: organization) }
-  let(:volunteer) { create(:volunteer) }
-  let(:supervisor) { create(:casa_admin, casa_org: organization) }
-  let!(:casa_case) { create(:casa_case, casa_org: organization) }
-  let!(:court_date) { create(:court_date, :with_court_details, casa_case: casa_case, date: now - 1.week) }
-  let!(:future_court_date) { create(:court_date, :with_court_details, casa_case: casa_case, date: now + 1.week) }
+  let(:organization_containing_court_date) { create(:casa_org) }
+  let(:organization_not_containing_court_date) { create(:casa_org) }
+  let(:admin) { create(:casa_admin, casa_org: organization_containing_court_date) }
+  let(:volunteer) { create(:volunteer, casa_org: organization_containing_court_date) }
+  let!(:casa_case) { create(:casa_case, casa_org: organization_containing_court_date) }
+  let!(:court_date) { create(:court_date, :with_court_details, casa_case: casa_case, date: now + 1.week) }
 
   before do
     travel_to now
+  end
+
+  shared_examples "user can view court date" do |user_type, organization|
+    let(:user) { create(user_type, casa_org: organization) }
+
+    before(:all) do
+      sign_in user
+      visit casa_case_court_date_path(casa_case, court_date)
+    end
+
+    it "can visit the court order page" do
+      expect(page).not_to have_content "Sorry, you are not authorized to perform this action."
+    end
+
+    it "can see the court date" do
+    end
+
+    it "can see the associated case number" do
+    end
+
+    it "can see the associated court report due date" do
+    end
+
+    it "can see the associated judge" do
+    end
+
+    it "can see the associated hearing type" do
+    end
+
+    it "can see associated court orders" do
+    end
+  end
+
+  shared_examples "user cannot view court date" do |user_type, organization|
+    let(:user) { create(user_type, casa_org: organization) }
+
+    it "is not allowed to visit the court order page" do
+      expect(page).to have_content "Sorry, you are not authorized to perform this action."
+    end
   end
 
   context "as a volunteer" do
