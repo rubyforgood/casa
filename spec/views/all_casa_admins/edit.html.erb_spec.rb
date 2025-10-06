dab8daea5080c10e405806a359b057427cc6ab50
Audrea Cook <audrea@codewitch.dev>
2025-10-06 10:23:27 -0600

    Test: add view specs for edit profile form and change password section
    Signed-off-by: Audrea Cook <audrea@codewitch.dev>

---
+++ spec/views/all_casa_admins/edit.html.erb_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'all_casa_admins/edit', type: :view do
+  let(:user) { build_stubbed(:all_casa_admin) }
+
+  before do
+    assign(:user, user)
+    render
+  end
+
+  it 'renders the edit profile form', :aggregate_failures do
+    expect(rendered).to have_selector("form[action='#{all_casa_admins_path}'][method='post']")
+    expect(rendered).to have_field('all_casa_admin[email]')
+    expect(rendered).to have_button('Update Profile')
+  end
+
+  it 'renders the change password collapse section, hidden by default', :aggregate_failures do
+    expect(rendered).to have_selector('#collapseOne.collapse')
+    expect(rendered).not_to include('class="collapse show"')
+    expect(rendered).to have_field('all_casa_admin[password]')
+    expect(rendered).to have_field('all_casa_admin[password_confirmation]')
+    expect(rendered).to have_button('Update Password')
+  end
+
+  context 'when there are error and flash messages' do
+    before do
+      user.errors.add(:email, "can't be blank")
+      flash[:notice] = 'Profile updated'
+      render
+    end
+
+    it 'renders error and flash messages partials', :aggregate_failures do
+      expect(rendered).to have_selector('#error_explanation.alert')
+      expect(rendered).to have_text("can't be blank")
+      expect(rendered).to have_selector('.header-flash')
+      expect(rendered).to have_text('Profile updated')
+    end
+  end
+end
