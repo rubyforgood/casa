1d957625a32e774673089765d27edec308634905
Linda Goldstein <linda.goldstein@parachutehealth.com>
2025-10-07 10:05:57 -0700

    fix spec errors

--- spec/channels/application_cable/channel_spec.rb
+++ spec/channels/application_cable/channel_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Channel, type: :channel do
+RSpec.describe "Channel", type: :channel do
   # TODO: Add tests for Channel
 
   pending "add some tests for Channel"
--- spec/channels/application_cable/connection_spec.rb
+++ spec/channels/application_cable/connection_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Connection, type: :channel do
+RSpec.describe "Connection", type: :channel do
   # TODO: Add tests for Connection
 
   pending "add some tests for Connection"
