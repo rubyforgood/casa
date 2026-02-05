b02467275b505923589d53557f67bb2a8055251e
Stefanni Brasil <stefannibrasil@proton.me>
2026-02-05 13:30:43 -0700

    whaat

---
+++ app/javascript/src/select.js
@@ -0,0 +1,12 @@
+$(() => {
+  // JQuery's callback for the DOM loading
+  $(".select2").select2({
+    theme: "bootstrap-5",
+    width: $(this).data("width")
+      ? $(this).data("width")
+      : $(this).hasClass("w-100")
+      ? "100%"
+      : "style",
+    placeholder: $(this).data("placeholder"),
+  });
+});
