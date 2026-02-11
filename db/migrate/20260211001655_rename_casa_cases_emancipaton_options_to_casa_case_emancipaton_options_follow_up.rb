288f68cd71a21cbef73c1fc1e195e47c76486839
Shen Yang <shenyang128@proton.me>
2026-02-10 18:41:34 -0600

    specify migration not reversible

--- db/migrate/20260211001655_rename_casa_cases_emancipaton_options_to_casa_case_emancipaton_options_follow_up.rb
+++ db/migrate/20260211001655_rename_casa_cases_emancipaton_options_to_casa_case_emancipaton_options_follow_up.rb
@@ -1,8 +1,12 @@
 class RenameCasaCasesEmancipatonOptionsToCasaCaseEmancipatonOptionsFollowUp < ActiveRecord::Migration[7.2]
-  def change
+  def up
     validate_foreign_key "casa_case_emancipation_options", "casa_cases", validate: false
     validate_foreign_key "casa_case_emancipation_options", "emancipation_options", validate: false
 
     drop_table :casa_cases_emancipation_options
+  end
+
+  def down
+    fail ActiveRecord::IrreversibleMigration
   end
 end
