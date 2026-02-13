1316b26e8ec0204bc7c1a6cdc12a05010591239b
Shen Yang <shenyang128@proton.me>
2026-02-13 00:10:18 -0600

    fix major copy paste error

--- db/migrate/20260211001655_rename_casa_cases_emancipaton_options_to_casa_case_emancipaton_options_follow_up.rb
+++ db/migrate/20260211001655_rename_casa_cases_emancipaton_options_to_casa_case_emancipaton_options_follow_up.rb
@@ -1,7 +1,7 @@
 class RenameCasaCasesEmancipatonOptionsToCasaCaseEmancipatonOptionsFollowUp < ActiveRecord::Migration[7.2]
   def up
-    validate_foreign_key "casa_case_emancipation_options", "casa_cases", validate: false
-    validate_foreign_key "casa_case_emancipation_options", "emancipation_options", validate: false
+    validate_foreign_key "casa_case_emancipation_options", "casa_cases"
+    validate_foreign_key "casa_case_emancipation_options", "emancipation_options"
 
     drop_table :casa_cases_emancipation_options
   end
