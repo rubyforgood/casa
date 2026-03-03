ac2c9e3a1f776cc8b360403efe88c61360d110b5
Linda Goldstein <linda.goldstein@parachutehealth.com>
2026-03-02 19:04:32 -0800

    add prosopite

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,6 +75,10 @@ Rails.application.configure do
     Bullet.bullet_logger = true
   end
 
+  # Prosopite N+1 query detection
+  config.prosopite_enabled = true
+  config.prosopite_min_n_queries = 5  # More lenient for development
+
   # Annotate rendered view with file names.
   config.action_view.annotate_rendered_view_with_filenames = true
 
