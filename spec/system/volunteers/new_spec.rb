c42192ec56eca3a87b6e3facae283c41a6aa32ec
Stefanni Brasil <stefannibrasil@proton.me>
2026-03-04 17:07:51 -0700

    Refactor volunteer creation email invitation system test
    The system test was not checking for anything on the page.
    This test is better placed at the model level, since
    it's related to business logic.

--- spec/system/volunteers/new_spec.rb
+++ spec/system/volunteers/new_spec.rb
@@ -1,27 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "volunteers/new", type: :system do
-  context "when admin" do
-    let(:admin) { create(:casa_admin) }
-
-    it "creates a new volunteer and sends invitation" do
-      sign_in admin
-      visit new_volunteer_path
-
-      fill_in "Email", with: "new_volunteer@example.com"
-      fill_in "Display name", with: "New Volunteer Display Name"
-      fill_in "Date of birth", with: Date.new(2001, 8, 8)
-
-      click_on "Create Volunteer"
-
-      last_email = ActionMailer::Base.deliveries.last
-      expect(last_email.to).to eq ["new_volunteer@example.com"]
-      expect(last_email.subject).to have_text "CASA Console invitation instructions"
-      expect(last_email.html_part.body.encoded).to have_text "your new Volunteer account."
-      expect(Volunteer.find_by(email: "new_volunteer@example.com").invitation_created_at).not_to be_nil
-    end
-  end
-
   context "when supervisor" do
     let(:supervisor) { create(:supervisor) }
 
