dac6d009f2c43093d014a3af21605263008d5063
John Costa <costajohnt@gmail.com>
2026-03-08 20:51:48 -0700

    Remove database-level assertions from session system tests
    Replace LoginActivity database checks with page-level assertions
    in login_spec.rb to eliminate race conditions when CI servers are
    under load. The page already shows the user's email on success and
    an error message on failure, making DB checks redundant for system
    tests.
    
    Closes #6699

--- spec/system/sessions/login_spec.rb
+++ spec/system/sessions/login_spec.rb
@@ -3,7 +3,8 @@ require "rails_helper"
 RSpec.describe "User Login", type: :system do
   %w[volunteer supervisor casa_admin].each do |user_type|
     let!(:user) { create(user_type.to_sym) }
-    it "creates a login activity record on successful login" do
+
+    it "shows the user's email after successful login" do
       visit new_user_session_path
       fill_in "Email", with: user.email
       fill_in "Password", with: "12345678"
@@ -12,13 +13,9 @@ RSpec.describe "User Login", type: :system do
       end
 
       expect(page).to have_text user.email
-
-      login_activity = LoginActivity.last
-      expect(login_activity.user).to eq(user)
-      expect(login_activity.success).to be true
     end
 
-    it "creates a login activity record on failed login" do
+    it "shows an error message after failed login" do
       visit new_user_session_path
       fill_in "Email", with: user.email
       fill_in "Password", with: "wrong_password"
@@ -27,10 +24,6 @@ RSpec.describe "User Login", type: :system do
       end
 
       expect(page).to have_content("Invalid Email or password.")
-
-      login_activity = LoginActivity.last
-      expect(login_activity.success).to be false
-      expect(login_activity.failure_reason).to eq("invalid")
     end
   end
 end
