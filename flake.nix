37e716e785704ce0d8cf8cd36ca8933a8d497e95
Yago Santos <yagosansz@gmail.com>
2026-03-25 23:40:52 -0400

    upgrade: ruby from 3.3.10 to 4.0.1

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
         gemConfig = { };
         # See available versions here: https://github.com/bobvanderlinden/nixpkgs-ruby/blob/master/ruby/versions.json
-        ruby = pkgs."ruby-3.3.10";
+        ruby = pkgs."ruby-4.0.1";
 
         bundixcli = bundix.packages.${system}.default;
       in rec {
