6eae19c6cddab70b477e6b70c2fbb071977228aa
Stefanni Brasil <stefannibrasil@proton.me>
2026-04-01 16:29:52 -0600

    Add request spec for PlacementsController
    The PlacementsController is a full CRUD for Youth
    placement records but only has a pending
    controller placeholder spec.
    This commit adds coverage for it.

--- spec/controllers/placements_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe PlacementsController, type: :controller do
-  # TODO: Add tests for PlacementsController
-
-  pending "add some tests for PlacementsController"
-end
