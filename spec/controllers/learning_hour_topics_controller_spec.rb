22826ce97df8e00855b525d4dc218374c6a074e4
Stefanni Brasil <stefannibrasil@proton.me>
2026-04-09 16:17:12 -0600

    Add coverage for Learning hour topics and types
    Closes https://github.com/rubyforgood/casa/issues/6840
    
    `learning_hour_topics_controller.rb` and `learning_hour_types_controller.rb`
    do not have any coverage. Let's add coverage to
    them so we catch any errors in the future.

--- spec/controllers/learning_hour_topics_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe LearningHourTopicsController, type: :controller do
-  # TODO: Add tests for LearningHourTopicsController
-
-  pending "add some tests for LearningHourTopicsController"
-end
