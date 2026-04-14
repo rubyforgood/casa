3c43a0d110fe8c607167d2adb004eb05ecc4f44c
Stefanni Brasil <stefannibrasil@proton.me>
2026-04-14 17:47:53 -0600

    Add coverage for Followups and Mileages reports
    Followup report handle CSV exports with
    authorization and the Mileage report handles
    Reimbursement CSV exports. Both have placeholder
    specs only.
    
    Let's add coverage to ensure they work as
    expected and no bugs are introduced.
    
    The services that generate the report data have
    extensive coverage already, so keep this unit test
    focused on the request, I only tested for the format
    and the headers.

--- app/controllers/followup_reports_controller.rb
+++ app/controllers/followup_reports_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class FollowupReportsController < ApplicationController
   after_action :verify_authorized
 
--- spec/controllers/mileage_reports_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe MileageReportsController, type: :controller do
-  # TODO: Add tests for MileageReportsController
-
-  pending "add some tests for MileageReportsController"
-end
