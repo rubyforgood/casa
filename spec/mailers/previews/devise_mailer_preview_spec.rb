7030c049b16953ff08fa3ccb4ef790ce1935ac1e
Alan Parmenter <alanparmenter@gmail.com>
2026-04-23 19:14:22 +0100

    Update devise_mailer_preview_spec.rb
    DRY up admin, supervisor and volunteer specs using shared_examples

--- spec/mailers/previews/devise_mailer_preview_spec.rb
+++ spec/mailers/previews/devise_mailer_preview_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 require Rails.root.join("lib/mailers/previews/devise_mailer_preview").to_s
 
+RSpec.shared_examples "invitation instructions" do |factory|
+  let!(:record) { create(factory) }
+  let(:message) { subject.send(:"invitation_instructions_as_#{factory}") }
+
+  it { expect(message.to).to eq [record.email] }
+end
+
 RSpec.describe DeviseMailerPreview do
   let(:subject) { described_class.new }
   let!(:user) { create(:user) }
@@ -29,30 +36,18 @@ RSpec.describe DeviseMailerPreview do
   end
 
   describe "#invitation_instructions_as_all_casa_admin" do
-    let!(:all_casa_admin) { create(:all_casa_admin) }
-    let(:message) { subject.invitation_instructions_as_all_casa_admin }
-
-    it { expect(message.to).to eq [all_casa_admin.email] }
+    it_behaves_like "invitation instructions", :all_casa_admin
   end
 
   describe "#invitation_instructions_as_casa_admin" do
-    let!(:casa_admin) { create(:casa_admin) }
-    let(:message) { subject.invitation_instructions_as_casa_admin }
-
-    it { expect(message.to).to eq [casa_admin.email] }
+    it_behaves_like "invitation instructions", :casa_admin
   end
 
   describe "#invitation_instructions_as_supervisor" do
-    let!(:supervisor) { create(:supervisor) }
-    let(:message) { subject.invitation_instructions_as_supervisor }
-
-    it { expect(message.to).to eq [supervisor.email] }
+    it_behaves_like "invitation instructions", :supervisor
   end
 
   describe "#invitation_instructions_as_volunteer" do
-    let!(:volunteer) { create(:volunteer) }
-    let(:message) { subject.invitation_instructions_as_volunteer }
-
-    it { expect(message.to).to eq [volunteer.email] }
+    it_behaves_like "invitation instructions", :volunteer
   end
 end
