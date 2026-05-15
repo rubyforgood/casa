ac8147ce0e25c6e8e2696ef40f7d16016c3a1bc8
Stefanni Brasil <stefannibrasil@proton.me>
2026-05-15 12:59:56 -0600

    remove this extra config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,6 @@ Rails.application.configure do
   # Enable static file serving from the `/public` folder (turn off if using NGINX/Apache for it).
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Cache assets for far-future expiry since they are all digest stamped.
-  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
