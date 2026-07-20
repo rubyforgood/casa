04c8921c11777d2dbb0616f0a9b7e22f75c3faa6
Salvador Fuentes Jr <9240+fuentesjr@users.noreply.github.com>
2026-07-20 07:23:02 -0700

    5563 - Replace notification spec stubs with real tests (#7072)
    * test: replace BaseNotifier spec stub with real tests (#5563)
    
    * test: replace FollowupNotifier spec stub with real tests (#5563)
    
    * test: replace FollowupResolvedNotifier spec stub with real tests (#5563)
    
    * test: replace YouthBirthdayNotifier spec stub with real tests (#5563)

--- spec/notifications/youth_birthday_notifier_spec.rb
+++ spec/notifications/youth_birthday_notifier_spec.rb
@@ -1,7 +1,24 @@
 require "rails_helper"
 
-RSpec.describe YouthBirthdayNotifier do
-  # TODO: Add tests for YouthBirthdayNotifier
+RSpec.describe YouthBirthdayNotifier, type: :model do
+  let(:casa_case) { create(:casa_case) }
+  let(:notifier) { YouthBirthdayNotifier.with(casa_case: casa_case) }
 
-  pending "add some tests for YouthBirthdayNotifier"
+  describe "title" do
+    it "returns 'Youth Birthday Notification'" do
+      expect(notifier.title).to eq "Youth Birthday Notification"
+    end
+  end
+
+  describe "message" do
+    it "contains the casa case number" do
+      expect(notifier.message).to include casa_case.case_number
+    end
+  end
+
+  describe "url" do
+    it "returns the casa case path" do
+      expect(notifier.url).to eq "/casa_cases/#{casa_case.id}"
+    end
+  end
 end
