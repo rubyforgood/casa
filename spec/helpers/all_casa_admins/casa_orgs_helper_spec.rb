c7e6a7dbf14808872c7beb381add595939259f96
Salvador Fuentes Jr. <9240+fuentesjr@users.noreply.github.com>
2026-07-22 17:31:07 -0700

    test: replace AllCasaAdmins::CasaOrgsHelper spec stub with real tests (#5563)

--- spec/helpers/all_casa_admins/casa_orgs_helper_spec.rb
+++ spec/helpers/all_casa_admins/casa_orgs_helper_spec.rb
@@ -1,7 +1,16 @@
 require "rails_helper"
 
 RSpec.describe AllCasaAdmins::CasaOrgsHelper, type: :helper do
-  # TODO: Add tests for AllCasaAdmins::CasaOrgsHelper
+  describe "#selected_organization" do
+    it "returns the assigned @casa_org" do
+      casa_org = build(:casa_org)
+      assign(:casa_org, casa_org)
 
-  pending "add some tests for AllCasaAdmins::CasaOrgsHelper"
+      expect(helper.selected_organization).to eq(casa_org)
+    end
+
+    it "returns nil when no @casa_org is assigned" do
+      expect(helper.selected_organization).to be_nil
+    end
+  end
 end
