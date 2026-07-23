2d7eacee460b3d4a613cdbdeae65a106e2dcce3c
Salvador Fuentes Jr. <9240+fuentesjr@users.noreply.github.com>
2026-07-22 17:31:06 -0700

    test: replace RequestHeaderHelper spec stub with real tests (#5563)

--- spec/helpers/request_header_helper_spec.rb
+++ spec/helpers/request_header_helper_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 RSpec.describe RequestHeaderHelper, type: :helper do
-  # TODO: Add tests for RequestHeaderHelper
+  describe "ACCEPT_JSON" do
+    it "is the JSON accept header" do
+      expect(RequestHeaderHelper::ACCEPT_JSON).to eq({"Accept" => "application/json"})
+    end
+  end
 
-  pending "add some tests for RequestHeaderHelper"
+  describe "CONTENT_TYPE_JSON" do
+    it "is the JSON content type header" do
+      expect(RequestHeaderHelper::CONTENT_TYPE_JSON).to eq({"Content-Type" => "application/json"})
+    end
+  end
 end
